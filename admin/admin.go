e81d25baf13dafcc5bc9aa71500cd677a80d59d7
zeekay <zk@monoid.io>
2014-10-24 02:09:08 -0500

    Add admin.

---
+++ admin/admin.go
@@ -0,0 +1,19 @@
+package admin
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	router := gin.Default()
+
+    router.Use(gin.Logger())
+    router.Use(gin.Recovery())
+
+	router.GET("/admin/", func(ctx *gin.Context) {
+		ctx.String(200, "api")
+	})
+
+	http.Handle("/admin/", router)
+}
