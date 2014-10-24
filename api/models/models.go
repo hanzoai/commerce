32fa719c5953efce0793015ddbd6c32093a2aea1
Zach Kelling <z@zeekay.io>
2014-10-24 02:15:45 -0500

    Gofmt.

--- api/models/models.go
+++ api/models/models.go
@@ -8,21 +8,21 @@ type Currency struct {
 	value int64
 }
 
-func (c Currency) Add() {}
-func (c Currency) Sub() {}
-func (c Currency) Mul() {}
+func (c Currency) Add()    {}
+func (c Currency) Sub()    {}
+func (c Currency) Mul()    {}
 func (c Currency) String() {}
 
 type LineItem struct {
 	product     Product
-	variant		ProductVariant
+	variant     ProductVariant
 	description string
-	quantity	int
+	quantity    int
 }
 
 type Cart struct {
-	id		  string
-	items	  []LineItem
+	id        string
+	items     []LineItem
 	createdAt time.Time
 }
 
@@ -32,39 +32,39 @@ type ShippingOption struct {
 }
 
 type Order struct {
-	id			    string
-	items	        []LineItem
+	id              string
+	items           []LineItem
 	createdAt       time.Time
-	user	        User
+	user            User
 	shippingAddress Address
 	billingAddress  Address
 	subtotal        int
-	tax				int
+	tax             int
 	shippingOption  ShippingOption
-	shipping		int
-	total			int
+	shipping        int
+	total           int
 }
 
 type Product struct {
-	id		    string
+	id          string
 	title       string
 	variants    []ProductVariant
-	images	    []Image
+	images      []Image
 	description string
 	stocked     int
 	available   bool
-	released	time.Time
-	addLabel    string  // Pre-order now or Add to cart
+	released    time.Time
+	addLabel    string // Pre-order now or Add to cart
 }
 
 type ProductVariant struct {
-	sku		   string
-	price	   Currency
-	stock	   int
+	sku        string
+	price      Currency
+	stock      int
 	weight     int
 	dimensions string
-	color	   string
-	size	   string
+	color      string
+	size       string
 }
 
 type Image struct {
@@ -73,7 +73,7 @@ type Image struct {
 }
 
 type User struct {
-	id	   string
+	id     string
 	name   string
 	email  string
 	phone  string
@@ -82,10 +82,10 @@ type User struct {
 }
 
 type Address struct {
-	street	   string
+	street     string
 	unit       string
 	city       string
 	state      string
 	postalCode string
-	country	   string
+	country    string
 }
