9ba8d24d5e084179b1a9a7974af19c73b652e558
zeekay <zk@monoid.io>
2014-10-25 18:11:50 -0500

    No need to use new scope.

--- api/api.go
+++ api/api.go
@@ -15,16 +15,15 @@ func init() {
 	router.Use(middleware.Host())
 
 	api := router.Group("/v1")
-	{
-		api.GET("/", func(c *gin.Context) {
-			c.Redirect(301, "http://crowdstart.io")
-		})
 
-		api.GET("/cart/:id", cart.Get)
-		api.POST("/cart",	 cart.Add)
-		api.PUT("/cart",	 cart.Update)
-		api.DELETE("/cart",  cart.Delete)
-	}
+	api.GET("/", func(c *gin.Context) {
+		c.Redirect(301, "http://crowdstart.io")
+	})
+
+	api.GET("/cart/:id", cart.Get)
+	api.POST("/cart",	 cart.Add)
+	api.PUT("/cart",	 cart.Update)
+	api.DELETE("/cart",  cart.Delete)
 
 	http.Handle("/v1/", router)
 }
