6433cbac1332b5b1d037d8f350aecfb4178bfddf
zeekay <zk@monoid.io>
2014-10-27 05:51:31 -0500

    Update checkout process.

--- checkout/handlers.go
+++ checkout/handlers.go
@@ -1,24 +1,36 @@
 package checkout
 
 import (
+	"crowdstart.io/cardconnect"
 	"crowdstart.io/util/template"
 	"github.com/gin-gonic/gin"
 	"github.com/mholt/binding"
 )
 
-func showCheckout(c *gin.Context) {
+func checkout(c *gin.Context) {
 	template.Render(c, "checkout.html", nil)
 }
 
-func processCheckout(c *gin.Context) {
+func checkoutComplete(c *gin.Context) {
+	template.Render(c, "checkout-complete.html", nil)
+}
+
+func submitOrder(c *gin.Context) {
 	checkoutForm := new(CheckoutForm)
-	errs := binding.Bind(c.Request, checkoutForm)
-	if len(errs) > 0 {
-		// Failed, show errors
-		template.Render(c, "checkout.html", nil)
-	} else {
-		// Success! show that
-		template.Render(c, "checkout.html", nil)
+	if binding.Bind(c.Request, checkoutForm).Handle(c.Writer) {
+		// Failed, binding will return errors
+		return
 	}
 
+	// Authorize order
+	ares, err := cardconnect.Authorize(checkoutForm.Order); switch {
+	case err != nil:
+		c.JSON(500, gin.H{"status": "Unable to authorize payment."})
+	case ares.Status == "A":
+		c.JSON(200, gin.H{"status": "ok"})
+	case ares.Status == "B":
+		c.JSON(200, gin.H{"status": "retry"})
+	case ares.Status == "C":
+		c.JSON(200, gin.H{"status": "declined"})
+	}
 }
