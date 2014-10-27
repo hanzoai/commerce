68197823aac9b595e706df737852a613d69c0696
zeekay <zk@monoid.io>
2014-10-27 15:11:52 -0500

    Reorganize templates / improve 500 error handling.

--- util/template/template.go
+++ util/template/template.go
@@ -6,20 +6,16 @@ import (
 	"os"
 )
 
-var templateCache map[string]*pongo2.Template
 var cwd, _ = os.Getwd()
 
 func Render(c *gin.Context, path string, ctx pongo2.Context) (err error) {
 	// All templates are expected to be in templates dir
-	path = cwd + "/templates/" + path
+	path = cwd + "/../templates/" + path
 
-	template, ok := templateCache[path]
-	if !ok {
-		template, err = pongo2.FromFile(path)
-		if err != nil {
-			c.Fail(500, err)
-			return err
-		}
+	template, err := pongo2.FromCache(path)
+	if err != nil {
+		c.Fail(500, err)
+		return err
 	}
 
 	if err := template.ExecuteWriter(ctx, c.Writer); err != nil {
