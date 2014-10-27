34f2abe4eb71aaaf54877cf00c5dc00fbe7a4661
Zach Kelling <z@zeekay.io>
2014-10-27 04:48:53 -0500

    Update cardconnect API and models.

--- models/cart.go
+++ models/cart.go
@@ -1,75 +1,109 @@
 package models
 
 import (
-	"time"
-	"net/http"
 	"github.com/mholt/binding"
+	"net/http"
+	"time"
 )
 
 type LineItem struct {
-	SKU			string
-	Description string
-	Quantity    int
+	FieldMapMixin
+	SKU          string
+	Cost         int64
+	Description  string
+	DiscountAmnt int64
+	LineNo       int
+	Quantity     int
+	UOM          string
+	// Material     string
+	// NetAmnt      string
+	// TaxAmnt      string
+	// UPC          string
 }
 
 func (li LineItem) Validate(req *http.Request, errs binding.Errors) binding.Errors {
 	if li.SKU == "" {
 		errs = append(errs, binding.Error{
-			FieldNames:		[]string{"SKU"},
-			Classification:	"InputError",
-			Message:		"SKU cannot be empty.",
+			FieldNames:     []string{"SKU"},
+			Classification: "InputError",
+			Message:        "SKU cannot be empty.",
+		})
+	}
+
+	if li.Quantity < 1 {
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Quantity"},
+			Classification: "InputError",
+			Message:        "Quantity cannot be less than 1.",
+		})
+	}
+
+	// Validate against database?
+	if li.Cost < 1 {
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Cost"},
+			Classification: "InputError",
+			Message:        "Cost is too low.",
 		})
 	}
 	return errs
 }
 
 type Cart struct {
+	FieldMapMixin
 	Id        string
 	Items     []LineItem
 	CreatedAt time.Time
-	FieldMapMixin
 }
 
 func (c Cart) Validate(req *http.Request, errs binding.Errors) binding.Errors {
 	// Cart cannot be empty.
 	if len(c.Items) == 0 {
-        errs = append(errs, binding.Error{
-            FieldNames:     []string{"Items"},
-            Classification: "InputError",
-            Message:        "Cart is empty.",
-        })
-    } else {
-		for _,v := range c.Items {
-    		errs = v.Validate(req,errs)
-    	}
-    }
-    return errs
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Items"},
+			Classification: "InputError",
+			Message:        "Cart is empty.",
+		})
+	} else {
+		for _, v := range c.Items {
+			errs = v.Validate(req, errs)
+		}
+	}
+	return errs
+}
+
+type PaymentAccount struct {
+	CVV2    string
+	Expiry  string
+	Number  string
+	Type    string
 }
 
 type Order struct {
+	FieldMapMixin
 	Id              string
 	Items           []LineItem
 	CreatedAt       time.Time
 	User            User
 	ShippingAddress Address
 	BillingAddress  Address
-	Subtotal        int
-	Tax             int
-	ShippingOption  ShippingOption
-	Shipping        int
-	Total           int
-	FieldMapMixin
+	Subtotal        int64
+	Tax             int64
+	Shipping        int64
+	Total           int64
+	Account         PaymentAccount
+	// ShippingOption  ShippingOption
 }
 
 func (o Order) Validate(req *http.Request, errs binding.Errors) binding.Errors {
 	if len(o.Items) == 0 {
-		errs = append(errs, binding.Error {
-			FieldNames:		[]string{"Items"},
-			Classification:	"InputError",
-			Message:		"Order has no items.",
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Items"},
+			Classification: "InputError",
+			Message:        "Order has no items.",
 		})
 	} else {
-		for _,v := range o.Items {
+		for _, v := range o.Items {
 			errs = v.Validate(req, errs)
 		}
 	}
@@ -84,10 +118,10 @@ type ShippingOption struct {
 
 func (so ShippingOption) Validate(req *http.Request, errs binding.Errors) binding.Errors {
 	if so.Name == "" {
-		errs = append(errs, binding.Error {
-			FieldNames:		[]string{"Name"},
-			Classification:	"InputError",
-			Message:		"Shipping option has no name.",
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Name"},
+			Classification: "InputError",
+			Message:        "Shipping option has no name.",
 		})
 	}
 	return errs
