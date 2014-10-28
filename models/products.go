e46a1d9ce1c24e8a0d81b545dd38e5f3f071d5c2
Zach Kelling <z@zeekay.io>
2014-10-27 19:10:27 -0500

    Display price is fun to get.

--- models/products.go
+++ models/products.go
@@ -1,21 +1,23 @@
 package models
 
 import (
-	"time"
-	"net/http"
+	"github.com/dustin/go-humanize"
 	"github.com/mholt/binding"
 	"math"
-	"strconv"
+	"net/http"
 	"reflect"
+	"strconv"
+	"strings"
+	"time"
 )
 
-func FormatPrice(price int64) float64   {
+func FloatPrice(price int64) float64 {
 	return math.Floor(float64(price)*100+0.5) / 1000000
 }
 
 type Product struct {
 	Id          string
-	Slug		string
+	Slug        string
 	Title       string
 	Variants    []ProductVariant
 	Images      []Image
@@ -40,7 +42,11 @@ func (p Product) MinPrice() int64 {
 }
 
 func (p Product) DisplayPrice() string {
-	return strconv.FormatFloat(FormatPrice(p.MinPrice()), 'f', 2, 64)
+	f := strconv.FormatFloat(FloatPrice(p.MinPrice()), 'f', 2, 64)
+	bits := strings.Split(f, ".")
+	decimal := bits[1]
+	integer, _ := strconv.ParseInt(bits[0], 10, 64)
+	return humanize.Comma(integer) + "." + decimal
 }
 
 // TODO: Don't do this.
@@ -53,44 +59,44 @@ func (p Product) VariantOptions(name string) (options []string) {
 		set[f.String()] = true
 	}
 
-    for key := range set {
-    	options = append(options, key)
-    }
+	for key := range set {
+		options = append(options, key)
+	}
 
 	return options
 }
 
 func (p Product) Validate(req *http.Request, errs binding.Errors) binding.Errors {
 	if p.Title == "" {
-		errs = append(errs, binding.Error {
-			FieldNames:		[]string{"Title"},
-			Classification:	"InputError",
-			Message:		"Product does not have a title.",
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Title"},
+			Classification: "InputError",
+			Message:        "Product does not have a title.",
 		})
 	}
 
 	if len(p.Images) > 0 {
-		for _,image := range p.Images {
+		for _, image := range p.Images {
 			errs = image.Validate(req, errs)
 		}
 	}
 
 	if len(p.Variants) == 0 {
-		errs = append(errs, binding.Error {
-			FieldNames:		[]string{"Variants"},
-			Classification:	"InputError",
-			Message:		"No Variants on Product",
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Variants"},
+			Classification: "InputError",
+			Message:        "No Variants on Product",
 		})
 	} else {
-		for _,v := range p.Variants {
-			errs = v.Validate(req,errs)
+		for _, v := range p.Variants {
+			errs = v.Validate(req, errs)
 		}
 	}
 	return errs
 }
 
 type ProductVariant struct {
-	Id		   string
+	Id         string
 	SKU        string
 	Price      int64
 	Stock      int
@@ -103,18 +109,18 @@ type ProductVariant struct {
 
 func (pv ProductVariant) Validate(req *http.Request, errs binding.Errors) binding.Errors {
 	if pv.SKU == "" {
-		errs = append(errs, binding.Error {
-			FieldNames:		[]string{"SKU"},
-			Classification:	"InputError",
-			Message:		"Variant does not have a SKU",
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"SKU"},
+			Classification: "InputError",
+			Message:        "Variant does not have a SKU",
 		})
 	}
 
 	if pv.Dimensions == "" {
-		errs = append(errs, binding.Error {
-			FieldNames:		[]string{"Dimensions"},
-			Classification:	"InputError",
-			Message:		"Variant has no given dimensions",
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Dimensions"},
+			Classification: "InputError",
+			Message:        "Variant has no given dimensions",
 		})
 	}
 	return errs
@@ -126,13 +132,12 @@ type Image struct {
 	FieldMapMixin
 }
 
-
 func (i Image) Validate(req *http.Request, errs binding.Errors) binding.Errors {
 	if i.Url == "" {
-		errs = append(errs, binding.Error {
-			FieldNames:		[]string{"Url"},
-			Classification:	"InputError",
-			Message:		"Image does not have a URL",
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Url"},
+			Classification: "InputError",
+			Message:        "Image does not have a URL",
 		})
 	}
 	return errs
