dbde2fae5cbd2243aa5b226f0961af7fd446e42c
Zach Kelling <z@zeekay.io>
2014-10-28 23:15:00 -0500

    Improve cart behavior.

--- static/js/crowdstart-cart.js
+++ static/js/crowdstart-cart.js
@@ -3,35 +3,68 @@ var templateEl = $('.template');
 templateEl.parent().remove();
 
 csio.renderLineItem = function(lineItem, index) {
-    var cartEl = templateEl.clone(false);
+    var el = templateEl.clone(false);
+    var $quantity = el.find('.quantity input');
 
-    cartEl.find('img.thumbnail').attr('src', lineItem.img);
-    cartEl.find('input.slug').val(lineItem.slug).attr('name', 'Order.Items.' + index + '.Product.Slug');
-    cartEl.find('input.sku').val(lineItem.sku).attr('name', 'Order.Items.' + index + '.Variant.SKU');
-    cartEl.find('a.title').text(lineItem.name);
-    cartEl.find('div.variant-info').text([lineItem.color, lineItem.size].join(' / '));
-    cartEl.find('.quantity input').val(lineItem.quantity).attr('name', 'Order.Items.' + index + '.Quantity');
-    cartEl.find('.price span').text(formatCurrency(lineItem.price));
+    // get list of variants
+    var variantInfo = []
+    if (lineItem.color !== "")
+      variantInfo.push(lineItem.color)
 
-    cartEl.find('.remove-item').click(function() {
-      csio.removeLineItem(lineItem.sku);
+    if (lineItem.size !== "")
+      variantInfo.push(lineItem.size)
+
+    el.find('img.thumbnail').attr('src', lineItem.img);
+    el.find('input.slug').val(lineItem.slug).attr('name', 'Order.Items.' + index + '.Product.Slug');
+    el.find('input.sku').val(lineItem.sku).attr('name', 'Order.Items.' + index + '.Variant.SKU');
+    el.find('a.title').text(lineItem.name);
+    el.find('div.variant-info').text(variantInfo.join(' / '));
+    el.find('.price span').text(formatCurrency(lineItem.price));
+    $quantity.val(lineItem.quantity).attr('name', 'Order.Items.' + index + '.Quantity');
+
+    // Handle quantity changes
+    $quantity.change(function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      // Get quantity
+      var quantity = $(this).val()
+
+      // Prevent less than one quantity
+      if (quantity < 1) {
+        quantity = 1
+        $(this).val(1)
+      }
+
+      // Update quantity
+      lineItem.quantity = quantity
+
+      // Update line item
+      csio.updateLineItem(lineItem, el);
+    })
+
+    // Handle lineItem removals
+    el.find('.remove-item').click(function() {
+      csio.removeLineItem(lineItem.sku, el);
     });
 
-    cartEl.removeClass('template');
+    el.removeClass('template');
 
-    $('.cart-container tbody').append(cartEl);
+    $('.cart-container tbody').append(el);
 };
 
 csio.renderCart = function(modifiedCart) {
   var cart = modifiedCart || csio.getCart();
   var numItems = 0;
-  var subTotal = 0;
+  var subtotal = 0;
   var i = 0;
+
+  $('.cart-container tbody').html('');
 
   for (var k in cart) {
     var lineItem = cart[k];
     numItems += lineItem.quantity;
-    subTotal += lineItem.price * lineItem.quantity;
+    subtotal += lineItem.price * lineItem.quantity;
     csio.renderLineItem(lineItem, i);
     i += 1;
   }
@@ -40,17 +73,41 @@ csio.renderCart = function(modifiedCart) {
     $('.cart-container').hide();
     $('.empty-message').show();
   } else {
-    $('.subtotal .price span').text(formatCurrency(price));
+    updateSubtotal(subtotal)
   }
 };
 
-csio.removeLineItem = function(sku) {
+csio.getSubtotal = function() {
+    var subtotal = 0
+    var cart = csio.getCart();
+    for (var k in cart) {
+      subtotal += cart[k].quantity * cart[k].price
+    }
+    return subtotal
+}
+
+csio.updateSubtotal = function(subtotal) {
+  var subtotal = subtotal || csio.getSubtotal();
+
+  $('.subtotal .price span').text(formatCurrency(subtotal));
+}
+
+csio.removeLineItem = function(sku, el) {
   var cart = csio.getCart();
 
   delete cart[sku];
 
   csio.setCart(cart);
-  csio.renderCart(cart);
+  csio.updateSubtotal();
+  $(el).remove()
+};
+
+csio.updateLineItem = function(lineItem, el) {
+  var cart = csio.getCart();
+
+  cart[lineItem.sku] = lineItem;
+  csio.setCart(cart);
+  csio.updateSubtotal();
 };
 
 csio.renderCart();
