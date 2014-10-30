e9c03344fefa67b99edd8d79fcf05792e311c8a0
ssb <boomsparkysparky@gmail.com>
2014-10-29 21:38:32 -0500

    Email validation

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,3 +1,14 @@
+var validation = {
+    isEmpty: function (str) {
+        return str.trim().length == 0;
+    },
+    isEmail: function(email) {
+        var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+        return pattern.test(email);
+    }
+}
+    
+
 $('div.field').on('click', function() {
     $(this).removeClass('error');
 });
@@ -5,7 +16,14 @@ $('div.field').on('click', function() {
 $visibleRequired = $('div:visible.required > input');
 
 $('#form').submit(function(e) {
-    var empty = $visibleRequired.filter(function() {return $(this).val() == "";});
+    var empty = $visibleRequired.filter(function() {return validation.isEmpty($(this).val())});
+
+    var email = $('input[name="User.Email"]')
+    if (!validation.isEmail(email)) {
+        email.parent().addClass('error')
+        e.preventDefault()
+    }
+    
     if (empty.length > 0) {
         e.preventDefault();
         empty.parent().addClass('error');
@@ -13,7 +31,7 @@ $('#form').submit(function(e) {
 });
 
 $visibleRequired.change(function(){
-    var empty = $visibleRequired.filter(function() {return $(this).val() == "";});
+    var empty = $visibleRequired.filter(function() {return $(this).val().trim() == "";});
 
     if (empty.length == 0) {
         var fieldset = $('div.sqs-checkout-form-payment-content > fieldset');
@@ -28,7 +46,7 @@ $('#form').card({
     numberInput: 'input[name="Order.Account.Number"]',
     expiryInput: 'input[name="RawExpiry"]',
     cvcInput: 'input[name="Order.Account.CVV2"]',
-    nameInput: 'input[name="Order.User.FirstName"], input[name="Order.User.LastName"]'
+    nameInput: 'input[name="User.FirstName"], input[name="User.LastName"]'
 });
 
 var firstTime = true
