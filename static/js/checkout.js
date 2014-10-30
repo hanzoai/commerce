954481fea40998e2acfa15ec3c17ddff9a5ebd10
Zach Kelling <z@zeekay.io>
2014-10-29 19:12:58 -0500

    Efficient change detection for form.

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,33 +1,49 @@
+/* global csio */
+
+// Globals
+window.csio = window.csio || {};
+
 $('div.field').on('click', function() {
     $(this).removeClass('error');
 });
 
 $('#form').submit(function(e) {
-    var empty = $('div.required > input').filter(function() {return $(this).val() == "";});
+    var empty = $('div.required > input').filter(function() {return $(this).val() === '';});
     if (empty.length > 0) {
         e.preventDefault();
         empty.parent().addClass('error');
     }
 });
 
-var t = setInterval(function(){
-    var empty = $('div:visible.required > input').filter(function() {return $(this).val() == "";});
 
-    if (empty.length == 0) {
-        var fieldset = $('div.sqs-checkout-form-payment-content > fieldset');
-        fieldset.css('display', 'block');
-        fieldset.css('opacity', '0');
-        fieldset.fadeTo(1000, 1);
-        clearInterval(t);
+// Show payment options when first half is competed.
+$(document).ready(function() {
+  var $required = $('div:visible.required > input')
+
+  var showPaymentOptions = $.debounce(250, function() {
+    console.log('checking for completion...')
+
+    for (var i=0; i< $required.length; i++) {
+      if ($required[i].value === '') return
     }
-}, 500);
+
+    var fieldset = $('div.sqs-checkout-form-payment-content > fieldset');
+    fieldset.css('display', 'block');
+    fieldset.css('opacity', '0');
+    fieldset.fadeTo(1000, 1);
+
+    $required.unbind('keyup', showPaymentOptions)
+  })
+
+  $required.bind('keyup', showPaymentOptions)
+})
 
 $('#form').card({
     container: '#card-wrapper',
     numberInput: 'input[name="Order.Account.Number"]',
     expiryInput: 'input[name="RawExpiry"]',
     cvcInput: 'input[name="Order.Account.CVV2"]',
-    nameInput: 'input[name="Order.User.FirstName"], input[name="Order.User.LastName"]'
+    nameInput: 'input[name="User.FirstName"], input[name="User.LastName"]'
 });
 
 $('input[name="ShipToBilling"]').change(function(){
