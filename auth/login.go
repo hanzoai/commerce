2dbbec50811c4be296bfd5319eb589d5b08504b6
ssb <boomsparkysparky@gmail.com>
2014-11-07 19:43:22 -0600

    Organized auth

--- auth/login.go
+++ auth/login.go
@@ -5,48 +5,15 @@ import (
 	"crowdstart.io/datastore"
 	"crowdstart.io/models"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/sessions"
 	"log"
 )
 
 // const sessionName = "crowdstartLogin"
-const secret = "askjaakjl12"
-
-var store = sessions.NewCookieStore([]byte(secret))
-
 const kind = "user"
-const sessionName = "logged-in-" + kind
-const loginKey = "login-key"
 
 func IsLoggedIn(c *gin.Context) bool {
 	value, err := GetSession(c, loginKey)
 	return err == nil && value != ""
-}
-
-func ClearSession(c *gin.Context) error {
-	session, err := store.Get(c.Request, sessionName)
-	if err != nil {
-		return err
-	}
-	session.Values[loginKey] = ""
-	return session.Save(c.Request, c.Writer)
-}
-
-func SetSession(c *gin.Context, key, value string) error {
-	session, err := store.Get(c.Request, sessionName)
-	if err != nil {
-		return err
-	}
-	session.Values[key] = value
-	return session.Save(c.Request, c.Writer)
-}
-
-func GetSession(c *gin.Context, key string) (string, error) {
-	session, err := store.Get(c.Request, sessionName)
-	if err != nil {
-		return "", err
-	}
-	return session.Values[key].(string), nil
 }
 
 func VerifyUser(c *gin.Context) error {
---
+++ auth/sessions.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"github.com/gorilla/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+const secret = "askjaakjl12"
+const sessionName = "logged-in-" + kind
+const loginKey = "login-key"
+
+var store = sessions.NewCookieStore([]byte(secret))
+
+func ClearSession(c *gin.Context) error {
+	session, err := store.Get(c.Request, sessionName)
+	if err != nil {
+		return err
+	}
+	session.Values[loginKey] = ""
+	return session.Save(c.Request, c.Writer)
+}
+
+func SetSession(c *gin.Context, key, value string) error {
+	session, err := store.Get(c.Request, sessionName)
+	if err != nil {
+		return err
+	}
+	session.Values[key] = value
+	return session.Save(c.Request, c.Writer)
+}
+
+func GetSession(c *gin.Context, key string) (string, error) {
+	session, err := store.Get(c.Request, sessionName)
+	if err != nil {
+		return "", err
+	}
+	return session.Values[key].(string), nil
+}
