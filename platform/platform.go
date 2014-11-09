25aebdf3e87973e871b9e352bdce7a4a6901d41b
ssb <boomsparkysparky@gmail.com>
2014-11-09 13:55:53 -0600

    Admin -> Platform. Organized routes

---
+++ platform/platform.go
@@ -0,0 +1,55 @@
+package platform
+
+import (
+	"appengine/urlfetch"
+	"crowdstart.io/auth"
+	"crowdstart.io/config"
+	"crowdstart.io/datastore"
+	"crowdstart.io/middleware"
+	"crowdstart.io/models"
+	"crowdstart.io/util/router"
+	"crowdstart.io/util/template"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type TokenData struct {
+	Access_token           string
+	Error                  string
+	Error_description      string
+	Livemode               bool
+	Refresh_token          string
+	Scope                  string
+	Stripe_publishable_key string
+	Stripe_user_id         string
+	Token_type             string
+}
+
+// Defines the routes for the platform
+func init() {
+	admin := router.New("/admin/")
+	router.New("/user/") // for future usage
+	
+	admin.GET("/", adminIndex)
+
+	admin.GET("/register", adminRegister)
+	admin.POST("/register", adminSubmitRegister)
+
+	admin.GET("/login", adminLogin)
+	admin.POST("/login", adminSubmitLogin)
+
+	admin.GET("logout", adminLogout)
+
+	admin.GET("/profile", middleware.LoginRequired(), adminProfile)
+	admin.POST("/profile", adminSubmitProfile)
+
+	admin.GET("/dashboard", middleware.LoginRequired(), adminDashboard)
+	admin.GET("/connect", middleware.LoginRequired(), adminConnect)
+
+	admin.GET("/stripe/callback", middleware.LoginRequired(), stripeCallback)
+}
