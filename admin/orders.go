926e3177c59810ddcd5392562842112cccf8fc86
ssb <boomsparkysparky@gmail.com>
2014-11-09 13:21:38 -0600

    Skeleton for removeOrder

--- admin/orders.go
+++ admin/orders.go
@@ -1,20 +1,22 @@
 package admin
 
 import (
-	"github.com/gin-gonic/gin"
+//	"appengine"
+	"crowdstart.io/auth"
 	"crowdstart.io/datastore"
 	"crowdstart.io/models"
-	"crowdstart.io/auth"
+	"github.com/gin-gonic/gin"
 )
 
 func listOrders(c *gin.Context) {
 	db := datastore.New(c)
+	
 	id, err := auth.GetUsername(c)
 
 	if err != nil {
 		return
 	}
-	
+
 	var orders []models.Order
 	q := db.Query("order").
 		Filter("User.Id =", id)
@@ -25,9 +27,12 @@ func listOrders(c *gin.Context) {
 
 // Extracts the Order.Id from the url and removes it from the datastore.
 func removeOrder(c *gin.Context) {
-	id := c.Request.URL.Query().Get("orderId")
-	db := datastore.New(c)
-	db.Delete(id)
+	
+	/*id := c.Request.URL.Query().Get("orderId")
+	
+	db.RunInTransaction(func(c appengine.Context) (err error) {
+		db := datastore.New(c)
+	})*/
 
 	// Return json success
 }
