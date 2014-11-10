a2e6bb31a369cb3967459f6516a5a3b97754daf6
ssb <boomsparkysparky@gmail.com>
2014-11-09 22:44:55 -0600

    Test creation of new user

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -2,16 +2,57 @@ package auth
 
 import (
 	"appengine/aetest"
-	"testing"
+	"crowdstart.io/models"
+	"errors"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestNewUser(t *testing.T) {
-	c, err := aetest.NewContext(nil)
+	ctx, err := aetest.NewContext(nil)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
-	
-	NewUser(, f)
+
+	c := &gin.Context{}
+	c.Set("appengine", ctx)
+
+	userId := "AzureDiamond"
+	password := "hunter2"
+
+	f := models.RegistrationForm{
+		Email:    userId,
+		Password: password,
+	}
+
+	err = NewUser(c, f)
+	if err != nil {
+		t.Error(err)
+	}
+
+	db := datastore.New(ctx)
+	var user models.User
+	err = db.GetKey("user", userId, user)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if user == nil {
+		t.Error(errors.New("User is nil"))
+		t.FailNow()
+	}
+
+	if user.Id != userId {
+		t.Logf("User id is not valid \n\tExpected: %s \n\tActual: %s", userId, user.Id)
+		t.Fail()
+	}
+
+	if user.PasswordHash != f.PasswordHash() {
+		t.Logf("User id is not valid \n\tExpected: %s \n\tActual: %s", user.PasswordHash, f.PasswordHash())
+		t.Fail()
+	}
 }
