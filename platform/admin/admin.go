acd81e670d52918409659b728459d1dc2b6bc538
ssb <boomsparkysparky@gmail.com>
2014-11-18 20:58:11 -0600

    Pulled out stripe code

--- platform/admin/admin.go
+++ platform/admin/admin.go
@@ -1,20 +1,12 @@
 package admin
 
 import (
-	"appengine/urlfetch"
+	"log"
+
 	"crowdstart.io/auth"
 	"crowdstart.io/config"
-	"crowdstart.io/datastore"
-	"crowdstart.io/middleware"
-	"crowdstart.io/models"
 	"crowdstart.io/util/template"
-	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/url"
-	"strings"
 )
 
 type TokenData struct {
@@ -29,23 +21,27 @@ type TokenData struct {
 	Token_type             string
 }
 
+// Index
 func Index(c *gin.Context) {
 	template.Render(c, "index.html")
 }
 
-// Admin Register
+// Register
 func Register(c *gin.Context) {
 	template.Render(c, "adminlte/register.html")
 }
 
+// Post registration form
 func SubmitRegister(c *gin.Context) {
 	c.Redirect(301, "dashboard")
 }
 
+// Render login form
 func Login(c *gin.Context) {
 	template.Render(c, "adminlte/login.html")
 }
 
+// Post login form
 func SubmitLogin(c *gin.Context) {
 	if err := auth.VerifyUser(c); err == nil {
 		log.Println("Success")
@@ -57,15 +53,18 @@ func SubmitLogin(c *gin.Context) {
 	}
 }
 
+//
 func Logout(c *gin.Context) {
-	auth.ClearSession(c)
+	auth.Logout(c) // Deletes the loginKey from session.Values
 	c.Redirect(301, "/")
 }
 
-// Admin User Profile
+// Renders the admin user page
 func Profile(c *gin.Context) {
+
 }
 
+// Handles submission on profile page
 func SubmitProfile(c *gin.Context) {
 	c.Redirect(301, "profile")
 }
@@ -78,76 +77,4 @@ func Dashboard(c *gin.Context) {
 // Admin Payment Connectors
 func Connect(c *gin.Context) {
 	template.Render(c, "adminlte/connect.html", "clientid", config.Get().Stripe.ClientId)
-}
-
-// Stripe End Points
-func StripeCallback(c *gin.Context) {
-	req := c.Request
-	code := req.URL.Query().Get("code")
-	errStr := req.URL.Query().Get("error")
-
-	// Failed to get back authorization code from Stripe
-	if errStr != "" {
-		template.Render(c, "stripe/failure.html", "error", errStr)
-		return
-	}
-
-	ctx := middleware.GetAppEngine(c)
-	client := urlfetch.Client(ctx)
-
-	data := url.Values{}
-	data.Set("client_secret", config.Get().Stripe.APISecret)
-	data.Add("code", code)
-	data.Add("grant_type", "authorization_code")
-
-	tokenReq, err := http.NewRequest("POST", "https://connect.stripe.com/oauth/token", strings.NewReader(data.Encode()))
-	if err != nil {
-		c.Fail(500, err)
-		return
-	}
-
-	// try to post to OAuth API
-	res, err := client.Do(tokenReq)
-	defer res.Body.Close()
-	if err != nil {
-		c.Fail(500, err)
-		return
-	}
-
-	// decode the json
-	jsonBlob, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		c.Fail(500, err)
-		return
-	}
-
-	token := new(TokenData)
-
-	// try and extract the json struct
-	if err := json.Unmarshal(jsonBlob, token); err != nil {
-		c.Fail(500, err)
-	}
-
-	// Stripe returned an error
-	if token.Error != "" {
-		template.Render(c, "adminlte/connect.html", "error", token.Error, "clientid", config.Get().Stripe.ClientId)
-		return
-	}
-
-	// Success
-	template.Render(c, "stripe/success.html", "token", token.Access_token)
-
-	// Update the user
-	campaign := new(models.Campaign)
-
-	db := datastore.New(ctx)
-
-	// Get user instance
-	db.GetKey("campaign", "skully", campaign)
-
-	// Update  stripe token
-	campaign.StripeToken = token.Access_token
-
-	// Update in datastore
-	db.PutKey("campaign", "skully", campaign)
 }
