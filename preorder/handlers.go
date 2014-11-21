b416ff9593d5545d392dde255118e09b4f92695f
zeekay <zk@monoid.io>
2014-11-21 03:21:25 -0800

    Fix config, production logging.

--- preorder/handlers.go
+++ preorder/handlers.go
@@ -2,6 +2,7 @@ package preorder
 
 import (
 	"crowdstart.io/auth"
+	"crowdstart.io/config"
 	"crowdstart.io/datastore"
 	"crowdstart.io/models"
 	"crowdstart.io/util/json"
@@ -73,6 +74,7 @@ func Thanks(c *gin.Context) {
 }
 
 func Index(c *gin.Context) {
+	log.Info("Prefix: "+config.Get().PrefixFor("preorder"), c)
 	template.Render(c, "login.html")
 }
 
