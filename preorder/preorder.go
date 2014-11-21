56b94e20664be719ad45df17cc4dc0d35d605422
ssb <boomsparkysparky@gmail.com>
2014-11-20 18:09:46 -0600

    Login is done

--- preorder/preorder.go
+++ preorder/preorder.go
@@ -2,12 +2,13 @@ package preorder
 
 import (
 	"crowdstart.io/config"
+	"crowdstart.io/middleware"
 	"crowdstart.io/util/router"
 )
 
 func init() {
 	router := router.New(config.Get().PrefixFor("preorder"))
 
-	router.GET("/", Login)
-	router.GET("/:token", Get)
+	router.GET("/", middleware.LoginRequired(), Preorder)
+	router.GET("/:token", WithToken)
 }
