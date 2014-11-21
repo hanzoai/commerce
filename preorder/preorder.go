8ed08abad004ac11bb6accd29f77fd67bd45fabe
Zach Kelling <z@zeekay.io>
2014-11-20 17:21:34 -0800

    Save orders.

--- preorder/preorder.go
+++ preorder/preorder.go
@@ -2,13 +2,15 @@ package preorder
 
 import (
 	"crowdstart.io/config"
-	"crowdstart.io/middleware"
 	"crowdstart.io/util/router"
 )
 
 func init() {
 	router := router.New(config.Get().PrefixFor("preorder"))
 
-	router.GET("/", middleware.LoginRequired(), Preorder)
-	router.GET("/:token", WithToken)
+	router.GET("/", Index)
+	router.POST("/", Login)
+
+	router.GET("/:token", GetPreorder)
+	router.POST("/preorder", SavePreorder)
 }
