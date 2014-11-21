10e81fa7ae5ddc2f0dd7b55540ed30c49167fc0d
zeekay <zk@monoid.io>
2014-11-21 06:14:58 -0800

    Improve logging.

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -41,11 +41,11 @@ func (d *Datastore) Get(key string, value interface{}) error {
 
 func (d *Datastore) GetKey(kind, key string, value interface{}) error {
 	k := NewKey(d.Context, kind, key, 0, nil)
-	err := nds.Get(d.Context, k, value)
-	if err != nil {
+	if err := nds.Get(d.Context, k, value); err != nil {
 		log.Error("%v, %v, %v", kind, key, err, d.Context)
+		return err
 	}
-	return err
+	return nil
 }
 
 func (d *Datastore) GetMulti(keys []string, values []interface{}) error {
@@ -53,7 +53,7 @@ func (d *Datastore) GetMulti(keys []string, values []interface{}) error {
 
 	for _, v := range keys {
 		if key, err := DecodeKey(v); err != nil {
-			d.Context.Errorf("%#v", err)
+			log.Error("%v", err, d.Context)
 			return err
 		} else {
 			_keys = append(_keys, key)
@@ -77,7 +77,7 @@ func (d *Datastore) Put(kind string, src interface{}) (string, error) {
 	k := NewIncompleteKey(d.Context, kind, nil)
 	k, err := nds.Put(d.Context, k, src)
 	if err != nil {
-		d.Context.Errorf("%#v", err)
+		log.Error("%v", err, d.Context)
 		return "", err
 	}
 	return k.Encode(), nil
@@ -87,7 +87,7 @@ func (d *Datastore) PutKey(kind string, key string, src interface{}) (string, er
 	k := NewKey(d.Context, kind, key, 0, nil)
 	k, err := nds.Put(d.Context, k, src)
 	if err != nil {
-		d.Context.Errorf("%#v", err)
+		log.Error("%v", err, d.Context)
 		return "", err
 	}
 	return k.Encode(), nil
@@ -103,7 +103,7 @@ func (d *Datastore) PutMulti(kind string, srcs []interface{}) (keys []string, er
 
 	_keys, err = nds.PutMulti(d.Context, _keys, srcs)
 	if err != nil {
-		d.Context.Errorf("%#v", err)
+		log.Error("%v", err, d.Context)
 		return keys, err
 	}
 
@@ -125,7 +125,7 @@ func (d *Datastore) PutKeyMulti(kind string, keys []string, srcs []interface{}) 
 
 	_keys, err := nds.PutMulti(d.Context, _keys, srcs)
 	if err != nil {
-		d.Context.Errorf("%#v", err)
+		log.Error("%v", err, d.Context)
 		return keys, err
 	}
 
@@ -137,17 +137,17 @@ func (d *Datastore) PutKeyMulti(kind string, keys []string, srcs []interface{}) 
 }
 
 func (d *Datastore) Update(key string, src interface{}) (string, error) {
-	log.Warn("DEPRECATED. DOES NOTHING PUT DOES NOT.")
+	log.Warn("DEPRECATED. DOES NOTHING PUT DOES NOT.", d.Context)
 
 	k, err := DecodeKey(key)
 	if err != nil {
-		d.Context.Errorf("%#v", err)
+		log.Error("%v", err, d.Context)
 		return "", err
 	}
 
 	k, err = nds.Put(d.Context, k, src)
 	if err != nil {
-		d.Context.Errorf("%#v", err)
+		log.Error("%v", err, d.Context)
 		return "", err
 	}
 	return k.Encode(), nil
@@ -156,7 +156,7 @@ func (d *Datastore) Update(key string, src interface{}) (string, error) {
 func (d *Datastore) Delete(key string) error {
 	k, err := DecodeKey(key)
 	if err != nil {
-		d.Context.Errorf("%#v", err)
+		log.Error("%v", err, d.Context)
 		return err
 	}
 	return nds.Delete(d.Context, k)
