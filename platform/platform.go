0e59a8544de6c43c6ec8e1ec91e503c53e7ae436
zeekay <zk@monoid.io>
2014-11-22 12:58:21 -0800

    make routing more predictable and sensible.

--- platform/platform.go
+++ platform/platform.go
@@ -3,32 +3,27 @@ package platform
 import (
 	"crowdstart.io/middleware"
 	"crowdstart.io/platform/admin"
-	"crowdstart.io/platform/user"
 	"crowdstart.io/util/router"
 )
 
 // Defines the routes for the platform
 func init() {
-	adminR := router.New("/admin/")
-	userR := router.New("/user/")
+	router := router.New("platform")
 
-	userR.GET("/login", user.Login)
+	router.GET("/", admin.Index)
 
-	adminR.GET("/", admin.Index)
+	router.GET("/login", admin.Login)
+	router.POST("/login", admin.SubmitLogin)
+	router.GET("/logout", admin.Logout)
 
-	adminR.GET("/register", admin.Register)
-	adminR.POST("/register", admin.SubmitRegister)
+	router.GET("/register", admin.Register)
+	router.POST("/register", admin.SubmitRegister)
 
-	adminR.GET("/login", admin.Login)
-	adminR.POST("/login", admin.SubmitLogin)
+	router.GET("/profile", middleware.LoginRequired(), admin.Profile)
+	router.POST("/profile", admin.SubmitProfile)
 
-	adminR.GET("logout", admin.Logout)
+	router.GET("/dashboard", middleware.LoginRequired(), admin.Dashboard)
+	router.GET("/connect", middleware.LoginRequired(), admin.Connect)
 
-	adminR.GET("/profile", middleware.LoginRequired(), admin.Profile)
-	adminR.POST("/profile", admin.SubmitProfile)
-
-	adminR.GET("/dashboard", middleware.LoginRequired(), admin.Dashboard)
-	adminR.GET("/connect", middleware.LoginRequired(), admin.Connect)
-
-	adminR.GET("/stripe/callback", middleware.LoginRequired(), admin.StripeCallback)
+	router.GET("/stripe/callback", middleware.LoginRequired(), admin.StripeCallback)
 }
