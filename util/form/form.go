824c8964bce73579ff73494405fa935814206aac
zeekay <zk@monoid.io>
2014-11-22 18:51:39 -0800

    Ignore unknown keys when processing forms.

--- util/form/form.go
+++ util/form/form.go
@@ -10,6 +10,7 @@ import (
 var decoder = schema.NewDecoder()
 
 func Parse(c *gin.Context, form interface{}) error {
+	decoder.IgnoreUnknownKeys(true)
 	c.Request.ParseForm()
 	err := decoder.Decode(form, c.Request.PostForm)
 	log.Debug("%#v", form)
