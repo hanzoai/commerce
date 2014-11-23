c700086fbb78e9a27f5c0dd54821bf995538d50d
zeekay <zk@monoid.io>
2014-11-23 14:18:41 -0800

    Reorganize things.
    - Move deploy script some place sensible
    - Add landing page with links for dev
    - Redirect to us when hitting static endpoint in production

--- _default/default.go
+++ _default/default.go
@@ -13,6 +13,45 @@ import (
 func Init() {
 	router := router.New("default")
 
+	router.GET("/", func(c *gin.Context) {
+		if config.IsDevelopment {
+			c.Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+			c.Writer.WriteHeader(200)
+			c.Writer.Write([]byte(`
+			<html>
+				<head>
+					<title>crowdstart</title>
+					<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+					<style>
+						body {
+							font-family:monospace;
+							margin:20px
+						}
+
+						ul {
+
+						}
+					</style>
+				</head>
+				<body>
+					<h4>200 ok (crowdstart/1.0.0)</h4>
+
+					<ul>
+						<li><a href="/api">api</a></li>
+						<li><a href="/checkout">checkout</a></li>
+						<li><a href="/platform">platform</a></li>
+						<li><a href="/preorder">preorder</a></li>
+						<li><a href="/store">store</a></li>
+					</ul>
+
+					<a href="http://localhost:8000">admin</a>
+				</body>
+			</html>`))
+		} else {
+			c.Redirect(301, "http://www.crowdstart.io")
+		}
+	})
+
 	// Warmup: install fixtures, etc.
 	router.GET("/_ah/warmup", func(c *gin.Context) {
 		ctx := appengine.NewContext(c.Request)
