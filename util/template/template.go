d66af527028f7cb224f7280d642cf9e08e45210f
Zach Kelling <z@zeekay.io>
2014-11-24 03:21:04 -0800

    More idiomatic pongo2 usage, template set, globals, etc.

--- util/template/template.go
+++ util/template/template.go
@@ -10,14 +10,27 @@ import (
 
 var cwd, _ = os.Getwd()
 
-type Context map[string]interface{}
+func TemplateSet() *pongo2.TemplateSet {
+	set := pongo2.NewSet("default")
+	set.Debug = config.IsDevelopment
+
+	set.Globals["staticUrl"] = config.StaticUrl
+	set.Globals["siteTitle"] = config.SiteTitle
+	set.Globals["moduleUrl"] = func(moduleName string) string {
+		return config.ModuleUrl(moduleName)
+	}
+
+	return set
+}
+
+var templateSet = TemplateSet()
 
 func Render(c *gin.Context, path string, pairs ...interface{}) (err error) {
 	// All templates are expected to be in templates dir
 	path = cwd + "/templates/" + path
 
 	// Get template from cache
-	template, err := pongo2.FromCache(path)
+	template, err := templateSet.FromCache(path)
 	if err != nil {
 		c.Fail(500, err)
 		return err
@@ -25,13 +38,6 @@ func Render(c *gin.Context, path string, pairs ...interface{}) (err error) {
 
 	// Create context from pairs
 	ctx := pongo2.Context{}
-
-	// Default context
-	ctx["staticUrl"] = config.StaticUrl
-	ctx["siteTitle"] = config.SiteTitle
-	ctx["moduleUrl"] = func(moduleName string) string {
-		return config.ModuleUrl(moduleName)
-	}
 
 	for i := 0; i < len(pairs); i = i + 2 {
 		ctx[pairs[i].(string)] = pairs[i+1]
