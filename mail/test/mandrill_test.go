a84db2b12e42505e9b0212cd5920db10603d9c4d
ssb <boomsparkysparky@gmail.com>
2014-11-25 06:29:10 -0600

    Error is in the json in SendMail fix tomorrow

---
+++ mail/test/mandrill_test.go
@@ -0,0 +1,51 @@
+package test
+
+import (
+	"testing"
+
+	"appengine"
+	"appengine/aetest"
+
+	"crowdstart.io/mail"
+)
+
+func TestPingMandrill(t *testing.T) {
+	instance, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer instance.Close()
+
+	req, err := instance.NewRequest("", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := appengine.NewContext(req)
+
+	if !mail.PingMandrill(ctx) {
+		t.Error("Ping failed")
+	}
+}
+
+func TestSendMail(t *testing.T) {
+	// t.Skip("for now")
+	instance, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer instance.Close()
+
+	req, err := instance.NewRequest("", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := appengine.NewContext(req)
+
+	err = mail.SendMail(ctx, "from_name", "dev@hanzo.ai", "to_name",
+		"marvel.j.mathew@gmail.com",
+		"test")
+
+	if err != nil {
+		t.Error(err)
+	}
+}
