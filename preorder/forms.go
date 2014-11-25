42f20a25c3413f424e08c21884323c2d4904960c
zeekay <zk@monoid.io>
2014-11-24 16:47:45 -0800

    Ensure order is persisted.

--- preorder/forms.go
+++ preorder/forms.go
@@ -21,6 +21,14 @@ func (f *PreorderForm) Parse(c *gin.Context) error {
 		return err
 	}
 
+	// For some reason gorilla/schema deserializes an extra nil lineItem,
+	// we need to remove this.
+	if f.Order.Items[0].SKU() == "" {
+		slice := make([]models.LineItem, 0)
+		f.Order.Items = append(slice, f.Order.Items[1:]...)
+	}
+
+	// Set password hash
 	f.User.PasswordHash = auth.HashPassword(f.Password)
 
 	return nil
