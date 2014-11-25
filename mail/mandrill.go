60d0f28d225cb186b0207a2ad61609ef8c5bf8e2
ssb <boomsparkysparky@gmail.com>
2014-11-25 06:29:10 -0600

    Error is in the json in SendMail fix tomorrow

--- mail/mandrill.go
+++ mail/mandrill.go
@@ -6,9 +6,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 
-	"github.com/gin-gonic/gin"
-
+	"appengine"
 	"appengine/urlfetch"
 
 	"crowdstart.io/util/log"
@@ -18,22 +19,27 @@ const apiKey = "wJ3LGLp5ZOUZlSH8wwqmTg"
 const root = "http://mandrillapp.com/api/1.0"
 
 var html = func() string {
-	b, err := ioutil.ReadFile("resources/confirmation_email.html")
+	wd, _ := os.Getwd()
+	log.Info(wd)
+	b, err := ioutil.ReadFile("../../resources/confirmation_email.html")
 	if err != nil {
 		log.Panic(err.Error())
 		return ""
 	}
 
-	return string(b)
+	s := strings.Replace(string(b), `"`, `\"`, -1)
+	return s
 }()
 
 // PingMandrill checks if our credentials/url are okay
 // Returns true if Mandrill replies with  a 200 OK
-func PingMandrill(c *gin.Context) bool {
+func PingMandrill(ctx appengine.Context) bool {
 	url := root + "/users/ping.json"
-	ctx := appengineCtx(c)
+	log.Debug(url)
 
-	body := []byte(fmt.Sprintf(`{"key": "%s"}`, apiKey))
+	str := fmt.Sprintf(`{"key": "%s"}`, apiKey)
+	log.Debug(str)
+	body := []byte(str)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	if err != nil {
 		log.Panic(err.Error())
@@ -48,16 +54,18 @@ func PingMandrill(c *gin.Context) bool {
 		return false
 	}
 
+	log.Debug(res.Status)
 	return res.StatusCode == 200
 }
 
-func SendMail(c *gin.Context, from_name, from_email, to_name, to_email, subject string) error {
+func SendMail(ctx appengine.Context, from_name, from_email, to_name, to_email, subject string) error {
 	url := root + "/messages/send.json"
+	log.Debug(url)
 
 	j := fmt.Sprintf(`{
     "key": "%s",
     "message": {
-        "html": "%s",,
+        "html": "%s",
         "subject": "%s",
         "from_email": "%s",
         "from_name": "%s",
@@ -97,6 +105,7 @@ func SendMail(c *gin.Context, from_name, from_email, to_name, to_email, subject 
 		to_name,
 		"noreply@skullysystems.com",
 	)
+	log.Debug(j)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(j)))
 	if err != nil {
@@ -104,7 +113,6 @@ func SendMail(c *gin.Context, from_name, from_email, to_name, to_email, subject 
 		return err
 	}
 
-	ctx := appengineCtx(c)
 	client := urlfetch.Client(ctx)
 	res, err := client.Do(req)
 	defer res.Body.Close()
@@ -116,6 +124,9 @@ func SendMail(c *gin.Context, from_name, from_email, to_name, to_email, subject 
 	if res.StatusCode == 200 {
 		return nil
 	} else {
+		b, _ := ioutil.ReadAll(res.Body)
+		log.Debug(string(b))
+		log.Debug(apiKey)
 		return errors.New("Email not sent")
 	}
 }
