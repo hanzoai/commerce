5fe30c4bc9f97d8fc149635f363ae50b6e88bb60
zeekay <zk@monoid.io>
2014-11-25 08:15:22 -0800

    Update fixtures to repopulate perk information.

--- models/fixtures/fixtures.go
+++ models/fixtures/fixtures.go
@@ -38,7 +38,7 @@ var perks = map[string]Perk{
 	"2123732": Perk{
 		Id:                "2123732",
 		Title:             "SKULLY AR-1",
-		Description:       "Get 1 SKULLY AR-1 Motorcycle helmet at a limited introductory launch price. Free shipping to the United States. Sizes S-XXL in Matte Black or Gloss White. Team SKULLY will email you after the campaign for size and color choices.",
+		Description:       "Get 1 SKULLY AR-1 Motorcycle helmet at a limited introductory launch price. Free shipping to the United States. Sizes S-XXL in Matte Black or Gloss White.",
 		Price:             "$1,399 USD",
 		EstimatedDelivery: "May 2015",
 		HelmetQuantity:    1,
@@ -48,7 +48,7 @@ var perks = map[string]Perk{
 	"2291929": Perk{
 		Id:                "2291929",
 		Title:             "SKULLY NATION GEAR",
-		Description:       "Join Team SKULLY with a package of official gear: limited edition hat, shirt, and decals. Free shipping to the United States. Shirt sizes S-XXL (available in men’s and women’s cut), Flexfit hat sizes S/M, L/XL. Team SKULLY will email you after the campaign for cut and size choices. *Free w/ AR-1 preorder.",
+		Description:       "Join Team SKULLY with a package of official gear: limited edition hat, shirt, and decals. Free shipping to the United States. Shirt sizes S-XXL (available in men’s and women’s cut), Flexfit hat sizes S/M, L/XL. *Free w/ AR-1 preorder.",
 		Price:             "$49 USD",
 		EstimatedDelivery: "December 2014",
 		HelmetQuantity:    0,
@@ -58,9 +58,19 @@ var perks = map[string]Perk{
 	"2267279": Perk{
 		Id:                "2267279",
 		Title:             "$499 Now and $949 Due at Ship",
-		Description:       "Reserve 1 SKULLY AR-1 Motorcycle helmet with $499 deposit, and $949* due when the AR-1 ships. Sizes S-XXL in Matte Black or Gloss White. Reserved helmets will ship after all regular pre-orders are shipped. No credit check necessary. Free shipping to the United States. *FOR INTERNATIONAL: additional shipping fee of $99.99 USD will apply to the second payment. SKULLY will email after the campaign for size and color choice, and will email you closer to your ship date to collect the second payment.",
+		Description:       "Reserve 1 SKULLY AR-1 Motorcycle helmet with $499 deposit, and $949* due when the AR-1 ships. Sizes S-XXL in Matte Black or Gloss White. Reserved helmets will ship after all regular pre-orders are shipped. Free shipping to the United States. *FOR INTERNATIONAL: additional shipping fee of $99.99 USD will apply to the second payment. We will email you closer to your ship date to collect the second payment.",
 		Price:             "$499 USD",
 		EstimatedDelivery: "July 2015",
+		HelmetQuantity:    1,
+		GearQuantity:      1,
+	},
+
+	"2210221": Perk{
+		Id:                "2210221",
+		Title:             "International SKULLY AR-1",
+		Description:       "Includes international shipping. Get 1 SKULLY AR-1 Motorcycle helmet at our international launch price. Sizes S-XXL in Matte Black or Gloss White.",
+		Price:             "$1,599 USD",
+		EstimatedDelivery: "May 2015",
 		HelmetQuantity:    1,
 		GearQuantity:      1,
 	},
@@ -68,7 +78,7 @@ var perks = map[string]Perk{
 	"2244337": Perk{
 		Id:                "2244337",
 		Title:             "International SKULLY AR-1",
-		Description:       "Includes international shipping. Get 1 SKULLY AR-1 Motorcycle helmet at our international launch price. If you live outside of the United States, reserve your SKULLY AR-1 here. Sizes S-XXL in Matte Black or Gloss White. Team SKULLY will email you after the campaign for size and color choices.",
+		Description:       "Includes international shipping. Get 1 SKULLY AR-1 Motorcycle helmet at our international launch price. Sizes S-XXL in Matte Black or Gloss White.",
 		Price:             "$1,499 USD",
 		EstimatedDelivery: "May 2015",
 		HelmetQuantity:    1,
@@ -78,7 +88,7 @@ var perks = map[string]Perk{
 	"2123523": Perk{
 		Id:                "2123523",
 		Title:             "Signature Edition SKULLY AR-1",
-		Description:       "Get 1 Signature Edition SKULLY AR-1 Motorcycle helmet, hand-numbered and signed by CEO Marcus Weller. Sizes S-XXL in Matte Black or Gloss White. Team SKULLY will email you after the campaign for size and color choices.",
+		Description:       "Get 1 Signature Edition SKULLY AR-1 Motorcycle helmet, hand-numbered and signed by CEO Marcus Weller. Sizes S-XXL in Matte Black or Gloss White.",
 		Price:             "$1,999 USD",
 		EstimatedDelivery: "May 2015",
 		HelmetQuantity:    1,
@@ -88,7 +98,7 @@ var perks = map[string]Perk{
 	"2210249": Perk{
 		Id:                "2210249",
 		Title:             "Passenger 2-Pack Deal",
-		Description:       "Get 2 SKULLY AR-1 Motorcycle helmets and save! Sizes S-XXL in Matte Black or Gloss White. Team SKULLY will email you after the campaign for size and color choices.",
+		Description:       "Get 2 SKULLY AR-1 Motorcycle helmets and save! Sizes S-XXL in Matte Black or Gloss White.",
 		Price:             "$2,649 USD",
 		EstimatedDelivery: "May 2015",
 		HelmetQuantity:    2,
@@ -98,7 +108,7 @@ var perks = map[string]Perk{
 	"2291934": Perk{
 		Id:                "2291934",
 		Title:             "CLUB & DISTRIBUTOR 5 PACK",
-		Description:       "Outfit your crew with 5 SKULLY AR-1s and save even more! Free shipping to the United States. Sizes S-XXL in Matte Black or Gloss White. Team SKULLY will email you after the campaign for size and color choices. *FOR INTERNATIONAL, add $375 USD for shipping.",
+		Description:       "Outfit your crew with 5 SKULLY AR-1s and save even more! Free shipping to the United States. Sizes S-XXL in Matte Black or Gloss White. *FOR INTERNATIONAL, add $375 USD for shipping.",
 		Price:             "$6,495 USD",
 		EstimatedDelivery: "May 2015",
 		HelmetQuantity:    5,
@@ -108,7 +118,7 @@ var perks = map[string]Perk{
 	"2353031": Perk{
 		Id:                "2353031",
 		Title:             "20 AR-1 DISTRIBUTOR PACK",
-		Description:       "Line your store shelves with 20 SKULLY AR-1s for massive savings! Free shipping to the United States. Sizes S-XXL in Matte Black or Gloss White. Team SKULLY will email you after the campaign for size and color choices. *FOR INTERNATIONAL, add $1499 USD for shipping.",
+		Description:       "Line your store shelves with 20 SKULLY AR-1s for massive savings! Free shipping to the United States. Sizes S-XXL in Matte Black or Gloss White. *FOR INTERNATIONAL, add $1499 USD for shipping.",
 		Price:             "$24,979 USD",
 		EstimatedDelivery: "May 2015",
 		HelmetQuantity:    20,
@@ -436,11 +446,6 @@ func Install(db *datastore.Datastore) {
 		},
 	})
 
-	// No longer updating Production this way as it clobbers existing settings.
-	if config.IsProduction {
-		return
-	}
-
 	csvfile, err := os.Open("resources/contributions.csv")
 	defer csvfile.Close()
 	if err != nil {
@@ -462,7 +467,7 @@ func Install(db *datastore.Datastore) {
 	// Payment Method     5                 Shipping Zip/Postal Code 16
 	//	                                    Shipping Country         17
 	for i := 0; true; i++ {
-		// Only save first 100 in production
+		// Only save first 25 in development
 		if config.IsDevelopment && i > 25 {
 			break
 		}
@@ -535,7 +540,12 @@ func Install(db *datastore.Datastore) {
 		user.ShippingAddress = address
 		user.BillingAddress = address
 
-		db.PutKey("user", user.Email, user)
+		// No longer updating user information in production, as it would clobber any customized information.
+		if config.IsProduction {
+			return
+		} else {
+			db.PutKey("user", user.Email, user)
+		}
 
 		log.Debug("User %#v", user)
 		log.Debug("InviteToken: %#v", token)
