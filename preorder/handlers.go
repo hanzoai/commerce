abeba67c50567a06bde1a4ca57794712cfd2fea9
zeekay <zk@monoid.io>
2014-11-24 16:47:45 -0800

    Ensure order is persisted.

--- preorder/handlers.go
+++ preorder/handlers.go
@@ -96,10 +96,6 @@ func SavePreorder(c *gin.Context) {
 	order := form.Order
 
 	for i, lineItem := range order.Items {
-		if i == 0 {
-			continue
-		}
-
 		// Fetch Variant for LineItem from datastore
 		if err := db.GetKey("variant", lineItem.SKU(), &lineItem.Variant); err != nil {
 			c.Fail(500, err)
