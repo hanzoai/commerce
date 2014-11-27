86fa6f7cc76db9a6ae9dee1f253452d997f954ef
ssb <boomsparkysparky@gmail.com>
2014-11-27 17:33:20 -0600

    Fixed form params to query params bug. Pulled out order processing.

--- checkout/handlers.go
+++ checkout/handlers.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.io/config"
-	"crowdstart.io/datastore"
 	"crowdstart.io/middleware"
 	"crowdstart.io/stripe"
 	"crowdstart.io/util/log"
@@ -26,37 +25,20 @@ func checkout(c *gin.Context) {
 		return
 	}
 
-	db := datastore.New(c)
-
 	order := form.Order
 
 	log.Info("Processing order. %#v", order)
-
-	for i, lineItem := range order.Items {
-		// Fetch Variant for LineItem from datastore
-		if err := db.GetKey("variant", lineItem.SKU(), &lineItem.Variant); err != nil {
-			log.Error(err.Error())
-			formError(c)
-			return
-		}
-
-		// Fetch Product for LineItem from datastore
-		if err := db.GetKey("product", lineItem.Slug(), &lineItem.Product); err != nil {
-			log.Error(err.Error())
-			formError(c)
-			return
-		}
-
-		order.Items[i] = lineItem
-		order.Subtotal += lineItem.Price()
+	err := order.Process(c)
+	if err != nil {
+		log.Error(err.Error())
+		return
 	}
-
-	order.Total = order.Subtotal + order.Tax
 
 	template.Render(c, "checkout.html", "order", order, "config", config.Get())
 }
 
 func authorize(c *gin.Context) {
+	log.Debug("Request")
 	form := new(AuthorizeForm)
 	if err := form.Parse(c); err != nil {
 		log.Error(err.Error())
@@ -65,33 +47,16 @@ func authorize(c *gin.Context) {
 		return
 	}
 
-	db := datastore.New(c)
 	order := form.Order
-
-	log.Info("Authorizing order. Items: %#v", order.Items)
-
-	for i, lineItem := range order.Items {
-		// TODO: Figure out why this happens
-		if lineItem.SKU() == "" {
-			continue
-		}
-
-		log.Debug("SKU: %#v", lineItem.SKU())
-
-		// Fetch Variant for LineItem from datastore
-		if err := db.GetKey("variant", lineItem.SKU(), &lineItem.Variant); err != nil {
-			log.Error(err.Error())
-			formError(c)
-			return
-		}
-
-		log.Debug("Line item: %#v", lineItem)
-		order.Items[i] = lineItem
-		order.Subtotal += lineItem.Price()
+	err := order.Process(c)
+	if err != nil {
+		log.Error(err.Error())
+		return
 	}
 
+	log.Info("Authorizing order. Items: %#v", order.Items)
 	ctx := middleware.GetAppEngine(c)
-	_, err := stripe.Charge(ctx, form.StripeToken, &order)
+	_, err = stripe.Charge(ctx, form.StripeToken, &order)
 
 	if err != nil {
 		log.Error(err.Error())
