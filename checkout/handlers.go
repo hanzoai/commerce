2a5f3b35860cc212c1ebba12750b69a3eb510745
ssb <boomsparkysparky@gmail.com>
2014-11-27 15:01:13 -0600

    Form validation for token

--- checkout/handlers.go
+++ checkout/handlers.go
@@ -91,7 +91,7 @@ func authorize(c *gin.Context) {
 	}
 
 	ctx := middleware.GetAppEngine(c)
-	charge, err := stripe.Charge(ctx, token, &order)
+	_, err := stripe.Charge(ctx, form.StripeToken, &order)
 
 	if err != nil {
 		log.Error(err.Error())
