465ba86e42524fb3a872005035f2ea6b526c68c9
ssb <boomsparkysparky@gmail.com>
2014-11-27 13:37:42 -0600

    Stripe client

--- checkout/checkout.go
+++ checkout/checkout.go
@@ -1,10 +1,14 @@
 package checkout
 
 import (
+	"crowdstart.io/config"
 	"crowdstart.io/util/router"
 )
 
 func init() {
+	// Initialising stripe client
+	Sc.Init(config.Get().Stripe.APISecret, nil)
+
 	router := router.New("checkout")
 
 	router.POST("/", checkout)
