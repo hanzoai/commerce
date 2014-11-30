532980ff2bb98a23614b764db0efad96e78505e3
zeekay <zk@monoid.io>
2014-11-30 14:57:37 -0800

    Make checkout actually work.

---
+++ models/lineitem.go
@@ -0,0 +1,73 @@
+package models
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/mholt/binding"
+)
+
+type LineItem struct {
+	FieldMapMixin
+	SKU_         string         `json:"SKU"`
+	Slug_        string         `json:"Slug"`
+	Product      Product        `datastore:"-"`
+	Variant      ProductVariant `datastore:"-"`
+	Description  string
+	DiscountAmnt int64
+	LineNo       int
+	Quantity     int
+
+	// UOM string `schema:"-"`
+	// UPC          string
+	// Material     string
+	// NetAmnt      string
+	// TaxAmnt      string
+}
+
+func (li LineItem) Price() int64 {
+	return li.Variant.Price * int64(li.Quantity)
+}
+
+func (li LineItem) DisplayPrice() string {
+	return DisplayPrice(li.Price())
+}
+
+func (li LineItem) SKU() string {
+	return li.Variant.SKU
+}
+
+func (li LineItem) Slug() string {
+	return li.Product.Slug
+}
+
+func (li LineItem) Validate(req *http.Request, errs binding.Errors) binding.Errors {
+	if li.SKU() == "" {
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Variant.SKU"},
+			Classification: "InputError",
+			Message:        "SKU cannot be empty.",
+		})
+	}
+
+	if li.Quantity < 1 {
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Quantity"},
+			Classification: "InputError",
+			Message:        "Quantity cannot be less than 1.",
+		})
+	}
+
+	return errs
+}
+
+// Displays nice "/" delimited variant information.
+func (li LineItem) DisplayShortDescription() string {
+	opts := []string{}
+	for _, opt := range []string{li.Variant.Color, li.Variant.Style, li.Variant.Size} {
+		if opt != "" {
+			opts = append(opts, opt)
+		}
+	}
+	return strings.Join(opts, " / ")
+}
