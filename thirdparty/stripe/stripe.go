36641d6c971b53c518aaa66ff88c587ba86fabbb
zeekay <zk@monoid.io>
2014-12-09 02:43:55 -0800

    Update stripe API, show card error messages.

--- thirdparty/stripe/stripe.go
+++ thirdparty/stripe/stripe.go
@@ -9,10 +9,11 @@ import (
 	"github.com/stripe/stripe-go/currency"
 
 	"crowdstart.io/models"
+	"crowdstart.io/util/json"
 	"crowdstart.io/util/log"
 )
 
-func Charge(ctx appengine.Context, accessToken string, authorizationToken string, order *models.Order) (string, error) {
+func Charge(ctx appengine.Context, accessToken string, authorizationToken string, order *models.Order) (*models.Charge, error) {
 	backend := stripe.NewInternalBackend(urlfetch.Client(ctx), "")
 	// Stripe advises using client-level methods
 	// in a concurrent context
@@ -33,28 +34,34 @@ func Charge(ctx appengine.Context, accessToken string, authorizationToken string
 
 	stripeCharge, err := sc.Charges.New(params)
 
+	charge := new(models.Charge)
+
 	// Charges and tokens are recorded regardless of success/failure.
 	// It doesn't record whether each charge/token is success or failure.
-	// It should be possible to query the stripe api for this though.
-	charge := models.Charge{
-		stripeCharge.ID,
-		stripeCharge.Captured,
-		stripeCharge.Created,
-		stripeCharge.Desc,
-		stripeCharge.Email,
-		stripeCharge.FailCode,
-		stripeCharge.FailMsg,
-		stripeCharge.Live,
-		stripeCharge.Paid,
-		stripeCharge.Refunded,
-		stripeCharge.Statement,
-		// TODO: Figure out if this is dangerous
-		int64(stripeCharge.Amount),
-		int64(stripeCharge.AmountRefunded),
+	if err != nil {
+		// &stripe.Error{Type:"card_error", Msg:"Your card was declined.", Code:"card_declined", Param:"", HTTPStatusCode:402}
+		stripeErr, ok := err.(*stripe.Error)
+		if ok {
+			charge.FailCode = json.Encode(stripeErr.Code)
+			charge.FailMsg = stripeErr.Msg
+			charge.FailType = json.Encode(stripeErr.Type)
+		}
+	} else {
+		charge.ID = stripeCharge.ID
+		charge.Captured = stripeCharge.Captured
+		charge.Created = stripeCharge.Created
+		charge.Desc = stripeCharge.Desc
+		charge.Email = stripeCharge.Email
+		charge.Live = stripeCharge.Live
+		charge.Paid = stripeCharge.Paid
+		charge.Refunded = stripeCharge.Refunded
+		charge.Statement = stripeCharge.Statement
+		charge.Amount = int64(stripeCharge.Amount)
+		charge.AmountRefunded = int64(stripeCharge.AmountRefunded)
 	}
 
-	order.Charges = append(order.Charges, charge)
+	order.Charges = append(order.Charges, *charge)
 	order.StripeTokens = append(order.StripeTokens, authorizationToken)
 
-	return charge.ID, err
+	return charge, err
 }
