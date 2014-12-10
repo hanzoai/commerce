c9171306f4270d1aa2758008f6c0e440f7928ef8
ssb <boomsparkysparky@gmail.com>
2014-12-10 16:23:55 -0600

    Shows SKU and quantity

--- preorder/preorder.go
+++ preorder/preorder.go
@@ -10,8 +10,8 @@ func init() {
 	router.GET("/", Index)
 	router.POST("/", Login)
 
-	router.GET("/order/:token", GetMultiPreorder)
-	router.POST("/order/save", SaveMultiPreorder)
+	router.GET("/order/:token", GetPreorder)
+	router.POST("/order/save", SavePreorder)
 
 	router.GET("/thanks", Thanks)
 }
