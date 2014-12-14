116f1c8989eed81b28b6cc7e93d470afc7289f91
Zach Kelling <z@zeekay.io>
2014-12-13 20:12:49 -0800

    Implement user profile backend.

--- store/user/forms.go
+++ store/user/forms.go
@@ -3,13 +3,44 @@ package user
 import (
 	"github.com/gin-gonic/gin"
 
+	"crowdstart.io/models"
 	"crowdstart.io/util/form"
 )
 
+// Forgotten Password form
 type ForgotPasswordForm struct {
 	Email string
 }
 
 func (f *ForgotPasswordForm) Parse(c *gin.Context) error {
+	return form.Parse(c, f)
+}
+
+// User profile form (contact)
+type ContactForm struct {
+	User models.User
+}
+
+func (f *ContactForm) Parse(c *gin.Context) error {
+	return form.Parse(c, f)
+}
+
+// User profile form (billing)
+type BillingForm struct {
+	BillingAddress models.Address
+}
+
+func (f *BillingForm) Parse(c *gin.Context) error {
+	return form.Parse(c, f)
+}
+
+// User profile form (change password)
+type ChangePasswordForm struct {
+	Password        string
+	NewPassword     string
+	ConfirmPassword string
+}
+
+func (f *ChangePasswordForm) Parse(c *gin.Context) error {
 	return form.Parse(c, f)
 }
