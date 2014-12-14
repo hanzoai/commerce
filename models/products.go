4d86c320324338266b04ed262d576b9201147623
zeekay <zk@monoid.io>
2014-12-13 20:17:28 -0800

    Split out listing.

--- models/products.go
+++ models/products.go
@@ -4,55 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"reflect"
-	"strings"
 	"time"
 
 	"github.com/mholt/binding"
 
 	"crowdstart.io/util/json"
 )
-
-type Listing struct {
-	FieldMapMixin
-	Id          string
-	SKU         string
-	Title       string
-	Description string `datastore:",noindex"`
-	Disabled    bool
-
-	Images []Image
-
-	Configs []Config
-}
-
-func (l Listing) GetProductSlugs() []string {
-	productConfigs := l.Configs
-	slugs := make([]string, len(productConfigs), len(productConfigs))
-	for i, productConfig := range productConfigs {
-		slugs[i] = productConfig.Product
-	}
-	return slugs
-}
-
-func (l Listing) GetProductSlugsString() string {
-	return strings.Join(l.GetProductSlugs(), " ")
-}
-
-func (l Listing) GetDescriptionParagraphs() []string {
-	return SplitParagraph(l.Description)
-}
-
-func (l Listing) DisplayTitle() string {
-	return DisplayTitle(l.Title)
-}
-
-type Config struct {
-	FieldMapMixin
-	Product         string //product id
-	Variant         string //optional variant sku
-	Quantity        int    //number of products of optional variant type
-	PriceAdjustment int    //cost adjustment for individual product in package (subtracted from actual price)
-}
 
 // Prune down since Product Listing has a lot of this info now
 type Product struct {
