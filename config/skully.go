9a9e35753d778e5d353e4f687819ce407246382a
Zach Kelling <z@zeekay.io>
2014-12-17 11:44:47 -0800

    Add Google API keys and comment out broken cardconnect stufff

--- config/skully.go
+++ config/skully.go
@@ -31,5 +31,8 @@ func Skully() *Config {
 		config.Stripe.RedirectURL = "https:" + config.UrlFor("platform", "/stripe/callback")
 		config.Stripe.WebhookURL = "https:" + config.UrlFor("platform", "/stripe/hook")
 	}
+
+	config.Google.APIKey = "AIza_REDACTED"
+
 	return config
 }
