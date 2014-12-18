e743812defab8d837475e8127e06ecc59c6706ac
Zach Kelling <z@zeekay.io>
2014-12-17 21:12:11 -0800

    Um fun times.

--- store/card/card.go
+++ store/card/card.go
@@ -4,12 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.io/auth"
+	"crowdstart.io/config"
 	"crowdstart.io/util/template"
 )
 
 func GetCard(c *gin.Context) {
 	user, _ := auth.GetUser(c)
-	template.Render(c, "skullycard.html", "user", user)
+	template.Render(c, "skullycard.html",
+		"user", user,
+		"GCSBucket", config.Google.Bucket.ImageUploads,
+		"GCSAPIKey", config.Google.APIKey)
 }
 
 func GetGiftCard(c *gin.Context) {
