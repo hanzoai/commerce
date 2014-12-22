7c3f3952a3a0ff07e589db7c9083b456299ac20f
David Tai <antisocialmunky@gmail.com>
2014-12-22 16:58:34 -0600

    pegged stripe-go due to go 1.3 depedency

--- thirdparty/salesforce/salesforce.go
+++ thirdparty/salesforce/salesforce.go
@@ -1,1 +1,10 @@
 package salesforce
+
+import (
+	"github.com/davidtai/go-force/force"
+)
+
+func Init(accessToken, instanceUrl, id, issuedAt, signature string) (*force.ForceApi, error) {
+	api, err := force.Set(accessToken, instanceUrl, id, issuedAt, signature)
+	return api, err
+}
