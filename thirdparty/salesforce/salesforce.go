baac96ac7e18c7a2454884182634bad7f70a3958
David Tai <antisocialmunky@gmail.com>
2015-01-06 14:32:56 -0600

    refactored salesforce, added salesforce hook to checkout

--- thirdparty/salesforce/salesforce.go
+++ thirdparty/salesforce/salesforce.go
@@ -15,177 +15,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 
+	"appengine/delay"
 	"appengine/urlfetch"
 )
-
-//Paths
-var LoginUrl = "https://login.salesforce.com/services/oauth2/token"
-var DescribePath = "/services/data/v29.0/"
-var SObjectDescribePath = DescribePath + "sobjects/"
-var ContactQueryPath = DescribePath + "query/?q=SELECT+Id+from+Contact+where+Contact.Email+=+"
-var ContactUpsertUsingEmailPath = SObjectDescribePath + "Contact/CrowdstartId__c/%v"
-
-// Salesforce Structs
-type SalesforceError struct {
-	Message   string `json:"message"`
-	ErrorCode string `json:"errorCode"`
-}
-
-type DescribeResponse struct {
-	SObjects     string `json:"sobjects"`
-	Connect      string `json:"connect"`
-	Query        string `json:"query"`
-	Theme        string `json:"theme"`
-	QueryAll     string `json:"queryAll"`
-	Tooling      string `json:"tooling"`
-	Chatter      string `json:"chatter"`
-	Analytics    string `json:"analytics"`
-	Recent       string `json:"recent"`
-	Commerce     string `json:"commerce"`
-	Licensing    string `json:"licensing"`
-	Identity     string `json:"identity"`
-	FlexiPage    string `json:"flexiPage"`
-	Search       string `json:"search"`
-	QuickActions string `json:"quickActions"`
-	AppMenu      string `json:"appMenu"`
-
-	Message   string `json:"message"`
-	ErrorCode string `json:"errorCode"`
-}
-
-type SObjectUrls struct {
-	SObject     string `json:"sobject"`
-	Describe    string `json:"describe"`
-	RowTemplate string `json:"rowTemplate"`
-}
-
-type SObject struct {
-	Name        string `json:"name"`
-	Label       string `json:"label"`
-	KeyPrefix   string `json:"keyPrefix"`
-	LabelPlural string `json:"labelPlural"`
-
-	Urls SObjectUrls `json:"urls"`
-
-	// grammatically annoying bools
-	Custom              bool `json:"custom"`
-	Layoutable          bool `json:"layoutable"`
-	Activateable        bool `json:"activateable"`
-	Searchable          bool `json:"searchable"`
-	Updateable          bool `json:"updateable"`
-	Createable          bool `json:"createable"`
-	DeprecatedAndHidden bool `json:"deprecatedAndHidden"`
-	CustomSetting       bool `json:"customSetting"`
-	Deletable           bool `json:"deletable"`
-	FeedEnable          bool `json:"feedEnabled"`
-	Mergeable           bool `json:"mergeable"`
-	Queryable           bool `json:"queryable"`
-	Replicateable       bool `json:"replicateable"`
-	Retrieveable        bool `json:"retrieveable"`
-	Undeleteable        bool `json:"undeleteable"`
-	Triggerable         bool `json:"triggerable"`
-}
-
-type SObjectDescribeResponse struct {
-	Encoding     string    `json:"encoding"`
-	MaxBatchSize string    `json:"maxBatchSize"`
-	SObjects     []SObject `json:"sobjects"`
-}
-
-type Attribute struct {
-	Type string `json:"type"`
-	Url  string `json:"url"`
-}
-
-type ContactQueryAttributes struct {
-	Id         string    `json:"Id"`
-	Attributes Attribute `json:"attributes"`
-}
-
-type ContactQueryResponse struct {
-	TotalSize int                      `json:"totalSize"`
-	Done      bool                     `json:"done"`
-	Records   []ContactQueryAttributes `json:"attributes"`
-}
-
-type Contact struct {
-	// Don't manually specify these
-
-	// Response Only Fields
-	Attributes     Attribute `json:"attributes,omitempty"`
-	Id             string    `json:"Id,omitempty"`
-	IsDeleted      bool      `json:"IsDeleted,omitempty"`
-	MasterRecordId string    `json:"MasterRecordId,omitempty"`
-	AccountId      string    `json:"AccountId,omitempty"`
-
-	// Unique External Id, currently using email (max length 255)
-	CrowdstartIdC string `json:"CrowdstartId__C,omitempty"`
-
-	// Read Only
-	Name             string `json:"Name,omitempty"`
-	CreatedById      string `json:"CreatedById,omitempty"`
-	LastModifiedById string `json:"LastModifiedById,omitempty"`
-
-	// You can manually specify these
-
-	// Data Fields
-	LastName                     string `json:"LastName,omitempty"`
-	FirstName                    string `json:"FirstName,omitempty"`
-	Salutation                   string `json:"Salutation,omitempty"`
-	MailingStreet                string `json:"MailingStreet,omitempty"`
-	MailingCity                  string `json:"MailingCity,omitempty"`
-	MailingState                 string `json:"MailingState,omitempty"`
-	MailingPostalCode            string `json:"MailingPostalCode,omitempty"`
-	MailingCountry               string `json:"MailingCountry,omitempty"`
-	MailingStateCode             string `json:"MailingStateCode,omitempty"`
-	MailingCountryCode           string `json:"MailingCountryCode,omitempty"`
-	MailingLatitude              string `json:"MailingLatitude,omitempty"`
-	MailingLongitude             string `json:"MailingLongitude,omitempty"`
-	Phone                        string `json:"Phone,omitempty"`
-	Fax                          string `json:"Fax,omitempty"`
-	MobilePhone                  string `json:"MobilePhone,omitempty"`
-	ReportsToId                  string `json:"ReportsToId,omitempty"`
-	Email                        string `json:"Email,omitempty"`
-	Title                        string `json:"Title,omitempty"`
-	Department                   string `json:"Department,omitempty"`
-	OwnerId                      string `json:"OwnerId,omitempty"`
-	CreatedDate                  string `json:"CreatedDate,omitempty"`
-	LastModifiedDate             string `json:"LastModifiedDate,omitempty"`
-	SystemModstamp               string `json:"SystemModstamp,omitempty"`
-	LastActivityDate             string `json:"LastActivityDate,omitempty"`
-	LastCURequestDate            string `json:"LastCURequestDate,omitempty"`
-	LastCUUpdateDate             string `json:"LastCUUpdateDate,omitempty"`
-	LastViewedDate               string `json:"LastViewedDate,omitempty"`
-	LastReferencedDate           string `json:"LastReferencedDate,omitempty"`
-	EmailBouncedReason           string `json:"EmailBouncedReason,omitempty"`
-	EmailBouncedDate             string `json:"EmailBouncedDate,omitempty"`
-	IsEmailBounced               bool   `json:"IsEmailBounced,omitempty"`
-	JigsawContactId              string `json:"JigsawContactId,omitempty"`
-	ZendeskLastSyncDateC         string `json:"Zendesk__Last_Sync_Date__c,omitempty"`
-	ZendeskLastSyncStatusC       string `json:"Zendesk__Last_Sync_Status__c,omitempty"`
-	ZendeskResultC               string `json:"Zendesk__Result__c,omitempty"`
-	ZendeskTagsC                 string `json:"Zendesk__Tags__c,omitempty"`
-	ZendeskZendeskOutofSyncC     string `json:"Zendesk__Zendesk_OutofSync__c,omitempty"`
-	ZendeskZendeskOldTagsC       string `json:"Zendesk__Zendesk_oldTags__c,omitempty"`
-	ZendeskIsCreatedUpdatedFlagC string `json:"Zendesk__isCreatedUpdatedFlag__c,omitempty"`
-	ZendeskNotesC                string `json:"Zendesk__notes__c,omitempty"`
-	ZendeskZendeskIdC            string `json:"Zendesk__zendesk_id__c,omitempty"`
-	UniquePreorderLinC           string `json:"Unique_Preorder_Link__c,omitempty"`
-	FullfillmentStatusC          string `json:"Fulfillment_Status__c,omitempty"`
-	PreorderC                    string `json:"Preorder__c,omitempty"`
-	ShippingAddressC             string `json:"Shipping_Address__c,omitempty"`
-	ShippingCityC                string `json:"Shipping_City__c,omitempty"`
-	ShippingStateC               string `json:"Shipping_State__c,omitempty"`
-	ShippingPostalZipC           string `json:"Shipping_Postal_Zip__c,omitempty"`
-	ShippingCountryC             string `json:"Shipping_Country__c,omitempty"`
-	MC4SFMCSubscriberC           string `json:"MC4SF__MC_Subscriber__c,omitempty"`
-}
-
-type UpsertResponse struct {
-	Id      string            `json:"id"`
-	Success bool              `json:"success"`
-	Errors  []SalesforceError `json:"errors"`
-}
 
 // Api Data Container
 type SalesforceTokens struct {
@@ -204,6 +36,7 @@ type Api struct {
 	Tokens       SalesforceTokens
 	LastQuery    *http.Request
 	LastJsonBlob string
+	Client       *http.Client
 }
 
 func (a *Api) request(method, url string, data string) (*http.Request, error) {
@@ -236,12 +69,13 @@ func Init(c *gin.Context, accessToken, refreshToken, instanceUrl, id, issuedAt, 
 			InstanceUrl:  instanceUrl,
 			Id:           id,
 			IssuedAt:     issuedAt,
-			Signature:    signature}})
+			Signature:    signature},
+		Client: getClient(c)})
 
 	// Hit the topmost RESTful endpoint to test if credentials work
 	response := make([]DescribeResponse, 1, 1)
 
-	if err := Describe(api, c, response); err != nil {
+	if err := Describe(api, response); err != nil {
 		return nil, err
 	}
 
@@ -253,7 +87,7 @@ func Init(c *gin.Context, accessToken, refreshToken, instanceUrl, id, issuedAt, 
 		}
 
 		// Try to hit the endpoint again
-		err := Describe(api, c, response)
+		err := Describe(api, response)
 		if err != nil {
 			return nil, err
 		}
@@ -266,8 +100,8 @@ func Init(c *gin.Context, accessToken, refreshToken, instanceUrl, id, issuedAt, 
 	return api, nil
 }
 
-func request(api *Api, c *gin.Context, method, path string, headers map[string]string, data string) ([]byte, error) {
-	client := getClient(c)
+func request(api *Api, method, path string, headers map[string]string, data string) ([]byte, error) {
+	client := api.Client
 
 	req, err := api.request(method, api.Tokens.InstanceUrl+path, data)
 
@@ -296,7 +130,13 @@ func request(api *Api, c *gin.Context, method, path string, headers map[string]s
 	return jsonBlob, nil
 }
 
-func UpsertContactByEmail(api *Api, c *gin.Context, contact *Contact) error {
+var UpsertContactTask = delay.Func("SaveContact", func(api *Api, contact *Contact) {
+	if err := UpsertContact(api, contact); err != nil {
+		log.Panic("UpsertContactTask failed: %v", err)
+	}
+})
+
+func UpsertContact(api *Api, contact *Contact) error {
 	if contact.Email == "" {
 		errors.New("Email is required")
 	}
@@ -311,7 +151,7 @@ func UpsertContactByEmail(api *Api, c *gin.Context, contact *Contact) error {
 	// strings.Replace required to bypass broken Salesforce period parsing
 	path := fmt.Sprintf(ContactUpsertUsingEmailPath, strings.Replace(contact.Email, ".", "_", -1))
 
-	jsonBlob, err := request(api, c, "PATCH", path, map[string]string{"Content-Type": "application/json"}, contactJSON)
+	jsonBlob, err := request(api, "PATCH", path, map[string]string{"Content-Type": "application/json"}, contactJSON)
 	if err != nil {
 		return err
 	}
@@ -331,26 +171,26 @@ func UpsertContactByEmail(api *Api, c *gin.Context, contact *Contact) error {
 	return nil
 }
 
-func GetContactByEmail(api *Api, c *gin.Context, email string) ([]Contact, error) {
+func GetContactByEmail(api *Api, email string) ([]Contact, error) {
 	// Not the safest thing in the world
 	path := ContactQueryPath + "%27" + email + "%27"
 
-	jsonBlob, err := request(api, c, "GET", path, map[string]string{}, "")
+	jsonBlob, err := request(api, "GET", path, map[string]string{}, "")
 	if err != nil {
 		return nil, err
 	}
 
-	contactQueryResponse := new(ContactQueryResponse)
+	queryResponse := new(QueryResponse)
 
-	if err := json.Unmarshal(jsonBlob, contactQueryResponse); err != nil {
+	if err := json.Unmarshal(jsonBlob, queryResponse); err != nil {
 		return nil, err
 	}
 
-	length := len(contactQueryResponse.Records)
+	length := len(queryResponse.Records)
 
 	response := make([]Contact, length, length)
-	for i, record := range contactQueryResponse.Records {
-		jsonBlob, err = request(api, c, "GET", record.Attributes.Url, map[string]string{}, "")
+	for i, record := range queryResponse.Records {
+		jsonBlob, err = request(api, "GET", record.Attributes.Url, map[string]string{}, "")
 		if err != nil {
 			return nil, err
 		}
@@ -367,8 +207,8 @@ func GetContactByEmail(api *Api, c *gin.Context, email string) ([]Contact, error
 	return response, err
 }
 
-func SObjectDescribe(api *Api, c *gin.Context, response *SObjectDescribeResponse) error {
-	jsonBlob, err := request(api, c, "GET", SObjectDescribePath, map[string]string{}, "")
+func SObjectDescribe(api *Api, response *SObjectDescribeResponse) error {
+	jsonBlob, err := request(api, "GET", SObjectDescribePath, map[string]string{}, "")
 	if err != nil {
 		return err
 	}
@@ -380,8 +220,8 @@ func SObjectDescribe(api *Api, c *gin.Context, response *SObjectDescribeResponse
 	return nil
 }
 
-func Describe(api *Api, c *gin.Context, response []DescribeResponse) error {
-	jsonBlob, err := request(api, c, "GET", DescribePath, map[string]string{}, "")
+func Describe(api *Api, response []DescribeResponse) error {
+	jsonBlob, err := request(api, "GET", DescribePath, map[string]string{}, "")
 	if err != nil {
 		return err
 	}
