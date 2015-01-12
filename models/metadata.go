ab2d30df56faa3071fd4b770c300e7d718e848b2
David Tai <antisocialmunky@gmail.com>
2015-01-12 17:19:07 -0600

    fixed metadata.go typo, started styling stuff

--- models/metadata.go
+++ models/metadata.go
@@ -1,30 +1,32 @@
 package models
 
 import (
+	"net/http"
+
 	"github.com/mholt/binding"
 )
 
 type Metadata struct {
-	string Type
-	string Value
+	Type  string
+	Value string
 }
 
 func (m Metadata) Validate(req *http.Request, errs binding.Errors) binding.Errors {
 	// Type and Value cannot be the empty string.
 
 	if m.Type == "" {
-		errs = append(errs, binding.Error {
-			FieldNames:		[]string{"Type"},
-			Classification:	"InputError",
-			Message:		"Type cannot be empty.",
+		errs = append(errs, binding.Error{
+			FieldNames:     []string{"Type"},
+			Classification: "InputError",
+			Message:        "Type cannot be empty.",
 		})
 	}
 
 	if m.Value == "" {
 		errs = append(errs, binding.Error{
-			FieldNames:		[]string{"Value"},
-			Classification:	"InputError",
-			Message:		"Value cannot be empty.",
+			FieldNames:     []string{"Value"},
+			Classification: "InputError",
+			Message:        "Value cannot be empty.",
 		})
 	}
 	return errs
