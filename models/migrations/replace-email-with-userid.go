2440794bd3c2e23f371b31b94b54718d597f234c
David Tai <david@verus.io>
2015-01-15 17:05:59 -0600

    reverted to using the original datastore

--- models/migrations/replace-email-with-userid.go
+++ models/migrations/replace-email-with-userid.go
@@ -4,13 +4,10 @@ import (
 	"time"
 
 	"appengine"
+	"appengine/datastore"
 	"appengine/delay"
 
-	"crowdstart.io/datastore"
 	"crowdstart.io/util/log"
-	"crowdstart.io/util/queries"
-
-	. "appengine/datastore"
 
 	. "crowdstart.io/models"
 )
@@ -65,225 +62,245 @@ type OldOrder struct {
 }
 
 var replaceEmailWithUserIdUserOnly = delay.Func("migrate-replace-email-with-userid-user-only", func(c appengine.Context) {
-	db := datastore.New(c)
+	log.Info("Migrating Users", c)
 
-	log.Debug("Migrating users", c)
+	t := datastore.NewQuery("user").Run(c)
 
-	t := db.Query("user").Run(c)
-
-	var ks []string
+	// Keys to batch delete
+	var u User
+	var id int64
+	var k, newK *datastore.Key
+	var err error
+	var ok bool
 
 	for {
-		var u User
-		k, err := t.Next(&u)
+		// Report memory stats
+		k, err = t.Next(&u)
 
 		if err != nil {
 			// Done
-			if err == Done {
+			if err == datastore.Done {
 				break
 			}
 
 			// Ignore field mismatch, otherwise skip record
-			if _, ok := err.(*ErrFieldMismatch); !ok {
+			if _, ok = err.(*datastore.ErrFieldMismatch); !ok {
 				log.Error("Error fetching user: %v\n%v", k, err, c)
 				continue
 			}
 		}
 
-		// Empty the ID so Upsert auto generates it
-
-		id := db.AllocateId("user")
-
-		u.Id = db.EncodeId("user", id)
-		newK, err := db.DecodeKey(u.Id)
-		if err != nil {
-			log.Error("Could not decode key: %v", newK, c)
-		}
-
-		db.PutKey("user", newK, &u)
-		log.Info("Inserting Encoded Key %v", u.Id, c)
-
-		ks = append(ks, k.Encode())
-	}
-
-	// Delete old User record
-	log.Info("Deleting %d Keys", len(ks), c)
-	db.DeleteMulti(ks)
-})
-
-var replaceEmailWithUserId = delay.Func("migrate-replace-email-with-userid", func(c appengine.Context) {
-	db := datastore.New(c)
-	q := queries.New(c)
-
-	log.Debug("Migrating users", c)
-
-	t := db.Query("user").Run(c)
-
-	for {
-		var u User
-		k, err := t.Next(&u)
-
-		if err != nil {
-			// Done
-			if err == Done {
-				break
-			}
-
-			// Ignore field mismatch, otherwise skip record
-			if _, ok := err.(*ErrFieldMismatch); !ok {
-				log.Error("Error fetching user: %v\n%v", k, err, c)
-				continue
-			}
-		}
-
-		// Delete old User record
-		log.Info("Deleting Key %v", k, c)
-		db.Delete(k.Encode())
-
-		// Empty the ID so Upsert auto generates it
-
-		id := db.AllocateId("user")
-
-		u.Id = db.EncodeId("user", id)
-		newK, err := db.DecodeKey(u.Id)
-		if err != nil {
-			log.Error("Could not decode key: %v", newK, c)
-		}
-
-		db.PutKey("user", newK, &u)
-		log.Info("Inserting Encoded Key %v", u.Id, c)
-	}
-
-	log.Debug("Migrating contributions", c)
-
-	t = db.Query("contribution").Run(c)
-
-	for {
-		var oCon OldContribution
-		k, err := t.Next(&oCon)
-
-		if err != nil {
-			//Done
-			if err == Done {
-				break
-			}
-
-			// Error, ignore field mismatch
-			if _, ok := err.(*ErrFieldMismatch); ok {
-				log.Error("Contribution appears to be Updated: %v", err, c)
-				continue
-			}
-		}
-
-		// Get the corresponding user
-		var u User
-		if err = q.GetUserByEmail(oCon.Email, &u); err != nil {
-			log.Error("Could not look up user: %v\n%v", oCon.Email, err, c)
+		// Skip if we've already performed this
+		if u.Id != u.Email {
+			log.Info("Skipping Migrated User %v", u.Id, c)
 			continue
 		}
 
-		// Update to new record and replace old one
-		con := Contribution{
-			Id:            oCon.Id,
-			UserId:        u.Id,
-			FundingDate:   oCon.FundingDate,
-			PaymentMethod: oCon.PaymentMethod,
-			Perk:          oCon.Perk,
-			Status:        oCon.Status,
-		}
+		log.Info("Migrating Key %v", u.Id, c)
+		datastore.RunInTransaction(c, func(tc appengine.Context) error {
+			// Empty the ID so Upsert auto generates it
+			id, _, err = datastore.AllocateIDs(tc, "user", nil, 1)
+			if err != nil {
+				log.Error("Could not get Key %v", err, tc)
+				return err
+			}
 
-		db.PutKey("contribution", k, &con)
+			newK = datastore.NewKey(tc, "user", "", id, nil)
+			u.Id = newK.Encode()
+
+			log.Info("Inserting Key", id, tc)
+
+			if _, err = datastore.Put(tc, newK, &u); err != nil {
+				log.Error("Could not Put User %v", newK, tc)
+				return err
+			}
+
+			log.Info("Deleting Key %v", k.StringID(), tc)
+
+			// Delete old User record
+			log.Info("Deleting Key %v", k, tc)
+			if err != datastore.Delete(tc, k) {
+				log.Error("Could not Delete User %v", newK, tc)
+			}
+
+			return err
+		}, nil)
 	}
+})
 
-	log.Debug("Migrating tokens", c)
+var replaceEmailWithUserId = delay.Func("migrate-replace-email-with-userid", func(c appengine.Context) {
+	// db := datastore.New(c)
+	// q := queries.New(c)
 
-	t = db.Query("token").Run(c)
+	// log.Info("Migrating users", c)
 
-	for {
-		var oTo OldToken
-		k, err := t.Next(&oTo)
+	// t := db.Query("user").Run(c)
 
-		if err != nil {
-			//Done
-			if err == Done {
-				break
-			}
+	// for {
+	// 	var u User
+	// 	k, err := t.Next(&u)
 
-			// Error, ignore field mismatch
-			if _, ok := err.(*ErrFieldMismatch); ok {
-				log.Error("Token appears to be Updated: %v", err, c)
-				continue
-			}
-		}
+	// 	if err != nil {
+	// 		// Done
+	// 		if err == Done {
+	// 			break
+	// 		}
 
-		// Get the corresponding user
-		var u User
-		if err = q.GetUserByEmail(oTo.Email, &u); err != nil {
-			log.Error("Could not look up user: %v\n%v", oTo.Email, err, c)
-			break
-		}
+	// 		// Ignore field mismatch, otherwise skip record
+	// 		if _, ok := err.(*ErrFieldMismatch); !ok {
+	// 			log.Error("Error fetching user: %v\n%v", k, err, c)
+	// 			continue
+	// 		}
+	// 	}
 
-		// Update to new record and replace old one
-		to := Token{
-			Id:      oTo.Id,
-			UserId:  u.Id,
-			Used:    oTo.Used,
-			Expired: oTo.Expired,
-		}
+	// 	// Delete old User record
+	// 	log.Info("Deleting Key %v", k, c)
+	// 	db.Delete(k.Encode())
 
-		db.PutKey("token", k, &to)
-	}
+	// 	// Empty the ID so Upsert auto generates it
 
-	log.Debug("Migrating orders", c)
+	// 	id := db.AllocateId("user")
 
-	t = db.Query("order").Run(c)
+	// 	u.Id = db.EncodeId("user", id)
+	// 	newK, err := db.DecodeKey(u.Id)
+	// 	if err != nil {
+	// 		log.Error("Could not decode key: %v", newK, c)
+	// 	}
 
-	for {
-		var oO OldOrder
-		k, err := t.Next(&oO)
+	// 	db.PutKey("user", newK, &u)
+	// 	log.Info("Inserting Encoded Key %v", u.Id, c)
+	// }
 
-		if err != nil {
-			//Done
-			if err == Done {
-				break
-			}
+	// log.Info("Migrating contributions", c)
 
-			// Error, ignore field mismatch
-			if _, ok := err.(*ErrFieldMismatch); ok {
-				log.Error("Order appears to be Updated: %v", err, c)
-				continue
-			}
-		}
+	// t = db.Query("contribution").Run(c)
 
-		// Get the corresponding user
-		var u User
-		if err = q.GetUserByEmail(oO.Email, &u); err != nil {
-			log.Error("Could not look up user: %v\n%v", oO.Email, err, c)
-			break
-		}
+	// for {
+	// 	var oCon OldContribution
+	// 	k, err := t.Next(&oCon)
 
-		// Update to new record and replace old one
-		o := Order{
-			BillingAddress:  oO.BillingAddress,
-			ShippingAddress: oO.ShippingAddress,
-			CreatedAt:       oO.CreatedAt,
-			UpdatedAt:       oO.UpdatedAt,
-			Id:              oO.Id,
-			UserId:          u.Id,
-			Shipping:        oO.Shipping,
-			Tax:             oO.Tax,
-			Subtotal:        oO.Subtotal,
-			Total:           oO.Total,
-			Items:           oO.Items,
-			StripeTokens:    oO.StripeTokens,
-			Charges:         oO.Charges,
-			CampaignId:      oO.CampaignId,
-			Preorder:        oO.Preorder,
-			Cancelled:       oO.Cancelled,
-			Shipped:         oO.Shipped,
-			Test:            oO.Test,
-		}
+	// 	if err != nil {
+	// 		//Done
+	// 		if err == Done {
+	// 			break
+	// 		}
 
-		db.PutKey("order", k, &o)
-	}
+	// 		// Error, ignore field mismatch
+	// 		if _, ok := err.(*ErrFieldMismatch); ok {
+	// 			log.Error("Contribution appears to be Updated: %v", err, c)
+	// 			continue
+	// 		}
+	// 	}
+
+	// 	// Get the corresponding user
+	// 	var u User
+	// 	if err = q.GetUserByEmail(oCon.Email, &u); err != nil {
+	// 		log.Error("Could not look up user: %v\n%v", oCon.Email, err, c)
+	// 		continue
+	// 	}
+
+	// 	// Update to new record and replace old one
+	// 	con := Contribution{
+	// 		Id:            oCon.Id,
+	// 		UserId:        u.Id,
+	// 		FundingDate:   oCon.FundingDate,
+	// 		PaymentMethod: oCon.PaymentMethod,
+	// 		Perk:          oCon.Perk,
+	// 		Status:        oCon.Status,
+	// 	}
+
+	// 	db.PutKey("contribution", k, &con)
+	// }
+
+	// log.Info("Migrating tokens", c)
+
+	// t = db.Query("token").Run(c)
+
+	// for {
+	// 	var oTo OldToken
+	// 	k, err := t.Next(&oTo)
+
+	// 	if err != nil {
+	// 		//Done
+	// 		if err == Done {
+	// 			break
+	// 		}
+
+	// 		// Error, ignore field mismatch
+	// 		if _, ok := err.(*ErrFieldMismatch); ok {
+	// 			log.Error("Token appears to be Updated: %v", err, c)
+	// 			continue
+	// 		}
+	// 	}
+
+	// 	// Get the corresponding user
+	// 	var u User
+	// 	if err = q.GetUserByEmail(oTo.Email, &u); err != nil {
+	// 		log.Error("Could not look up user: %v\n%v", oTo.Email, err, c)
+	// 		break
+	// 	}
+
+	// 	// Update to new record and replace old one
+	// 	to := Token{
+	// 		Id:      oTo.Id,
+	// 		UserId:  u.Id,
+	// 		Used:    oTo.Used,
+	// 		Expired: oTo.Expired,
+	// 	}
+
+	// 	db.PutKey("token", k, &to)
+	// }
+
+	// log.Info("Migrating orders", c)
+
+	// t = db.Query("order").Run(c)
+
+	// for {
+	// 	var oO OldOrder
+	// 	k, err := t.Next(&oO)
+
+	// 	if err != nil {
+	// 		//Done
+	// 		if err == Done {
+	// 			break
+	// 		}
+
+	// 		// Error, ignore field mismatch
+	// 		if _, ok := err.(*ErrFieldMismatch); ok {
+	// 			log.Error("Order appears to be Updated: %v", err, c)
+	// 			continue
+	// 		}
+	// 	}
+
+	// 	// Get the corresponding user
+	// 	var u User
+	// 	if err = q.GetUserByEmail(oO.Email, &u); err != nil {
+	// 		log.Error("Could not look up user: %v\n%v", oO.Email, err, c)
+	// 		break
+	// 	}
+
+	// 	// Update to new record and replace old one
+	// 	o := Order{
+	// 		BillingAddress:  oO.BillingAddress,
+	// 		ShippingAddress: oO.ShippingAddress,
+	// 		CreatedAt:       oO.CreatedAt,
+	// 		UpdatedAt:       oO.UpdatedAt,
+	// 		Id:              oO.Id,
+	// 		UserId:          u.Id,
+	// 		Shipping:        oO.Shipping,
+	// 		Tax:             oO.Tax,
+	// 		Subtotal:        oO.Subtotal,
+	// 		Total:           oO.Total,
+	// 		Items:           oO.Items,
+	// 		StripeTokens:    oO.StripeTokens,
+	// 		Charges:         oO.Charges,
+	// 		CampaignId:      oO.CampaignId,
+	// 		Preorder:        oO.Preorder,
+	// 		Cancelled:       oO.Cancelled,
+	// 		Shipped:         oO.Shipped,
+	// 		Test:            oO.Test,
+	// 	}
+
+	// 	db.PutKey("order", k, &o)
+	// }
 })
