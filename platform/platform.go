0592f0901aea3dacb8885b98d1a43b7b677ba373
David Tai <antisocialmunky@gmail.com>
2015-01-17 17:15:27 -0600

    2 way salesforce working with test end points

--- platform/platform.go
+++ platform/platform.go
@@ -40,4 +40,5 @@ func init() {
 	router.GET("/salesforce/callback", loginRequired, admin.SalesforceCallback)
 
 	router.GET("/salesforce/test", loginRequired, admin.TestSalesforceConnection)
+	router.GET("/salesforce/sync", loginRequired, admin.TestSalesforcePullLatest)
 }
