fad7d73b5678adcf2ec5714ad82d9fbbb22a3c1d
Timothy Messer <tmesser@verus.io>
2015-01-18 16:02:05 -0600

    Adding a metadata map type with appengine load/save

--- models/metadata.go
+++ models/metadata.go
@@ -2,16 +2,40 @@ package models
 
 import (
 	"net/http"
-
+	"appengine/datastore"
 	"github.com/mholt/binding"
 )
 
-type Metadata struct {
+// Flexible collection of datum
+type Metadata map[string]Datum
+
+func (m Metadata) Load(c <-chan datastore.Property) error {
+	// Loop over properties stored and rebuild map
+	for p := range c {
+		m[p.Name] = p.Value.(Datum)
+	}
+	return nil
+}
+
+func (m Metadata) Save(c chan<- datastore.Property) error {
+	defer close(c)
+	// Loop over key, value pairs in instance and feed into datastore
+	for k, v := range m {
+		c <- datastore.Property {
+			Name: k,
+			Value: v,
+		}
+	}
+	return nil
+}
+
+// A single piece of metadata
+type Datum struct {
 	Type  string
 	Value string
 }
 
-func (m Metadata) Validate(req *http.Request, errs binding.Errors) binding.Errors {
+func (m Datum) Validate(req *http.Request, errs binding.Errors) binding.Errors {
 	// Type and Value cannot be the empty string.
 
 	if m.Type == "" {
