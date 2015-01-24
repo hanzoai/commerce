25c3dbef7a2c3222a79d06acec4c08a20f27ac10
Marvel Mathew <marvel@verus.io>
2015-01-24 13:04:11 -0600

    Use ginkgo for TestNewUser

--- test/auth/auth_test.go
+++ test/auth/auth_test.go
@@ -1,55 +1,77 @@
 package test
 
 import (
-	"errors"
-	"reflect"
 	"testing"
+
+	gaed "appengine/datastore"
+
+	"github.com/gin-gonic/gin"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
 	"crowdstart.io/auth"
 	"crowdstart.io/datastore"
 	"crowdstart.io/models"
-	"github.com/gin-gonic/gin"
-
-	"appengine/aetest"
+	"github.com/zeekay/aetest"
 )
 
 const kind = "user"
 
-func TestNewUser(t *testing.T) {
-	t.Skip()
-	ctx, err := aetest.NewContext(nil)
-	defer ctx.Close()
-	if err != nil {
-		t.Error(err)
-	}
+var (
+	ctx aetest.Context
+	db  *datastore.Datastore
+	c   *gin.Context
+)
 
-	c := &gin.Context{}
-	c.Set("appengine", ctx)
-
-	regForm := auth.RegistrationForm{
-		User:     models.User{Email: "e@example.com"},
-		Password: "hunter2",
-	}
-	regForm.User.Id = regForm.User.Email
-
-	err = auth.NewUser(c, &regForm)
-	if err != nil {
-		t.Error(err)
-	}
-
-	db := datastore.New(ctx)
-	user := new(models.User)
-	err = db.GetKey(kind, regForm.User.Email, user)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if reflect.DeepEqual(*user, models.User{}) {
-		t.Error(errors.New("User is empty"))
-	}
-
-	if user.Id != regForm.User.Id {
-		t.Logf("User id is not valid \n\tExpected: %s \n\tActual: %s", regForm.User.Id, user.Id)
-		t.Fail()
-	}
+func TestDatastore(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Datastore test suite")
 }
+
+var _ = BeforeSuite(func() {
+	var err error
+	ctx, err = aetest.NewContext(&aetest.Options{StronglyConsistentDatastore: true})
+	Expect(err).ToNot(HaveOccurred())
+	c = &gin.Context{}
+	c.Set("appengine", ctx)
+})
+
+var _ = AfterSuite(func() {
+	err := ctx.Close()
+	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = Describe("NewUser", func() {
+	var regForm auth.RegistrationForm
+	Context("Registering with unique email", func() {
+		It("should not error", func() {
+			regForm = auth.RegistrationForm{
+				User:     models.User{Email: "e@example.com"},
+				Password: "hunter2",
+			}
+			err := auth.NewUser(c, &regForm)
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+
+	Context("Query api get", func() {
+		var keys []*gaed.Key
+		It("should not error", func() {
+			var err error
+			keys, err = db.Query(kind).
+				Filter("Email =", regForm.User.Email).
+				KeysOnly().Limit(1).GetAll(ctx, nil)
+			Expect(err).ToNot(HaveOccurred())
+		})
+		It("should return one key", func() {
+			Expect(keys).To(HaveLen(1))
+		})
+	})
+
+	Context("Re-registering", func() {
+		It("should error", func() {
+			err := auth.NewUser(c, &regForm)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+})
