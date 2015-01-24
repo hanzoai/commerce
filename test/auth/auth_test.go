1a8b9784d55ac831ee3e5a7109ff291f21193af7
Marvel Mathew <marvel@verus.io>
2015-01-24 13:13:22 -0600

    Make tests isolated to facilitate parallel execution.

--- test/auth/auth_test.go
+++ test/auth/auth_test.go
@@ -3,8 +3,6 @@ package test
 import (
 	"testing"
 
-	gaed "appengine/datastore"
-
 	"github.com/gin-gonic/gin"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -42,11 +40,10 @@ var _ = AfterSuite(func() {
 })
 
 var _ = Describe("NewUser", func() {
-	var regForm auth.RegistrationForm
 	Context("Registering with unique email", func() {
 		It("should not error", func() {
-			regForm = auth.RegistrationForm{
-				User:     models.User{Email: "e@example.com"},
+			regForm := auth.RegistrationForm{
+				User:     models.User{Email: "a@example.com"},
 				Password: "hunter2",
 			}
 			err := auth.NewUser(c, &regForm)
@@ -55,21 +52,29 @@ var _ = Describe("NewUser", func() {
 	})
 
 	Context("Query api get", func() {
-		var keys []*gaed.Key
 		It("should not error", func() {
-			var err error
-			keys, err = db.Query(kind).
+			regForm := auth.RegistrationForm{
+				User:     models.User{Email: "b@example.com"},
+				Password: "hunter2",
+			}
+			auth.NewUser(c, &regForm)
+
+			keys, err := db.Query(kind).
 				Filter("Email =", regForm.User.Email).
 				KeysOnly().Limit(1).GetAll(ctx, nil)
 			Expect(err).ToNot(HaveOccurred())
-		})
-		It("should return one key", func() {
 			Expect(keys).To(HaveLen(1))
 		})
 	})
 
 	Context("Re-registering", func() {
 		It("should error", func() {
+			regForm := auth.RegistrationForm{
+				User:     models.User{Email: "b@example.com"},
+				Password: "hunter2",
+			}
+			auth.NewUser(c, &regForm)
+
 			err := auth.NewUser(c, &regForm)
 			Expect(err).To(HaveOccurred())
 		})
