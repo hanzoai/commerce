52b3a6b87358083984d7b01b030632f5e9db2739
zeekay <zk@monoid.io>
2015-02-05 15:28:12 -0600

    Add Email to contribution.

---
+++ models/migrations/tasks/add-email-to-contribution.go
@@ -0,0 +1,14 @@
+package tasks
+
+import (
+	"crowdstart.io/datastore"
+	"crowdstart.io/datastore/parallel"
+	"crowdstart.io/models"
+)
+
+var AddEmailToContribution = parallel.Task("add-email-to-contribution", func(db *datastore.Datastore, key datastore.Key, contribution models.Contribution) {
+	user := new(models.User)
+	db.Get(contribution.UserId, user)
+	contribution.Email = user.Email
+	db.PutKey(key, contribution)
+})
