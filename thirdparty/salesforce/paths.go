485ca30edf3fefc3dcf68c3a97d27b62fb71b2e9
David Tai <david@verus.io>
2015-02-13 14:44:57 -0600

    initial work on product, made order satisfy the interfaces we need

--- thirdparty/salesforce/paths.go
+++ thirdparty/salesforce/paths.go
@@ -18,3 +18,8 @@ var OrderBasePath = SObjectDescribePath + "Order/"
 var OrderPath = OrderBasePath + "%v/"
 var OrderExternalIdPath = OrderBasePath + "CrowdstartId__c/%v"
 var OrdersUpdatedPath = OrderBasePath + "updated/?start=%v&end=%v"
+
+var ProductBasePath = SObjectDescribePath + "Product/"
+var ProductPath = ProductBasePath + "%v/"
+var ProductExternalIdPath = ProductBasePath + "CrowdstartId__c/%v"
+var ProductsUpdatedPath = ProductBasePath + "updated/?start=%v&end=%v"
