01dd34956596b3ed68384b032eda13718f222197
Zach Kelling <z@zeekay.io>
2015-02-20 13:33:34 -0600

    Update configurations for Salesforce.

--- config/production.go
+++ config/production.go
@@ -26,19 +26,20 @@ func Production() *Config {
 
 	config.Mandrill.APIKey = "wJ3LGLp5ZOUZlSH8wwqmTg"
 
+	config.Salesforce.CallbackURL = "https:" + config.UrlFor("platform", "/salesforce/callback")
+	config.Stripe.RedirectURL = "https:" + config.UrlFor("platform", "/stripe/callback")
+	config.Stripe.WebhookURL = "https:" + config.UrlFor("platform", "/stripe/hook")
+
 	config.DemoMode = true
 
 	// Only use production credentials if demo mode is off.
 	if !config.DemoMode {
 		config.Salesforce.ConsumerKey = "3MVG9xOCXq4ID1uElRYWhpUWjXSbiTVg4WO6q9DvWdvBjQ_DFlwSc7jZ9AbY3z9Jv_V29W7xq1nPjTYQhYJqF"
 		config.Salesforce.ConsumerSecret = "3811316853831925498"
-		config.Salesforce.CallbackURL = "https://admin.crowdstart.io/salesforce/callback"
 
 		config.Stripe.ClientId = "ca_53yyRUNpMtTRUgMlVlLAM3vllY1AVybU"
 		config.Stripe.APIKey = "pk_live_APr2mdiUblcOO4c2qTeyQ3hq"
 		config.Stripe.APISecret = ""
-		config.Stripe.RedirectURL = "https:" + config.UrlFor("platform", "/stripe/callback")
-		config.Stripe.WebhookURL = "https:" + config.UrlFor("platform", "/stripe/hook")
 	}
 
 	return config
