be8392b5c6e8dc2ebc0157e4ed296d416f51fae5
zeekay <zk@monoid.io>
2015-02-21 05:33:44 -0600

    Add integration test, fix appenginetesting.

---
+++ test/util/task/integration/integration_test.go
@@ -0,0 +1,83 @@
+package integration
+
+import (
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"appengine"
+	"appengine/memcache"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/davidtai/appenginetesting"
+
+	"crowdstart.io/util/log"
+)
+
+func Test(t *testing.T) {
+	log.SetVerbose(testing.Verbose())
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "util/task/integration")
+}
+
+var (
+	ctx *appenginetesting.Context
+)
+
+// Setup appengine context and datastore before tests
+var _ = BeforeSuite(func() {
+	var err error
+
+	//Spin up an appengine dev server with the default module
+	ctx, err = appenginetesting.NewContext(&appenginetesting.Options{
+		AppId:      "crowdstart-io",
+		Debug:      appenginetesting.LogWarning,
+		Testing:    GinkgoT(),
+		TaskQueues: []string{"default"},
+		Modules: []appenginetesting.ModuleConfig{
+			{
+				Name: "default",
+				Path: filepath.Join("../../../../config/test/app.yaml"),
+			},
+		},
+	})
+
+	Expect(err).NotTo(HaveOccurred())
+})
+
+// Tear-down appengine context
+var _ = AfterSuite(func() {
+	ctx.Close()
+})
+
+var _ = Describe("Run", func() {
+	It("Should call task successfully", func() {
+		// Get default module host
+		host, err := appengine.ModuleHostname(ctx, "default", "", "")
+		Expect(err).NotTo(HaveOccurred())
+
+		// Start task
+		url := "http://" + host + "/task/foo"
+		res, err := http.Get(url)
+
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+		log.Debug("%v", string(body))
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.StatusCode).To(Equal(200))
+
+		// Wait for task to run
+		time.Sleep(1 * time.Second)
+
+		// Check if memcache is set
+		foo, err := memcache.Get(ctx, "foo")
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(string(foo.Value)).To(Equal("bar"))
+	})
+})
--- test/util/task/integration/tasks/tasks.go
+++ test/util/task/integration/tasks/tasks.go
@@ -1,1 +1,23 @@
 package tasks
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"appengine"
+	"appengine/memcache"
+
+	"crowdstart.io/util/log"
+	"crowdstart.io/util/task"
+)
+
+var Foo = task.Func("foo", func(c *gin.Context) {
+	foo := &memcache.Item{
+		Key:   "foo",
+		Value: []byte("bar"),
+	}
+
+	ctx := c.MustGet("appengine").(appengine.Context)
+	if err := memcache.Set(ctx, foo); err != nil {
+		log.Error(err, c)
+	}
+})
