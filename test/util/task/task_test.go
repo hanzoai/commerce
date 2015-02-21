b58f00544a5c4a2804c36b60fd4ab094545b052f
Zach Kelling <z@zeekay.io>
2015-02-21 03:18:45 -0600

    Update util/task tests.

--- test/util/task/task_test.go
+++ test/util/task/task_test.go
@@ -31,22 +31,37 @@ func TestDatastore(t *testing.T) {
 // 	Expect(err).ToNot(HaveOccurred())
 // })
 
+type X struct {
+	x string
+}
+
 var _ = Describe("Register", func() {
 	It("Should add task to registry", func() {
 		Expect(len(task.Registry["foo"])).To(Equal(0))
-
+		task.Register("foo", X{"foo"})
+		Expect(len(task.Registry["foo"])).To(Equal(1))
 	})
 
 	It("Should append additional tasks when name is re-used", func() {
-		task.Register("foo", struct{ x string }{"foo"})
-		Expect(len(task.Registry["foo"])).To(Equal(1))
+		task.Register("bar", X{"bar"})
+		Expect(len(task.Registry["bar"])).To(Equal(1))
 
-		task.Register("foo", struct{ x string }{"foo"})
-		Expect(len(task.Registry["foo"])).To(Equal(2))
+		task.Register("bar", X{"bar"})
+		Expect(len(task.Registry["bar"])).To(Equal(2))
 
-		for _, v := range task.Registry["foo"] {
-			foo, _ := v.(struct{ x string })
-			Expect(foo).To(Equal(struct{ x string }{"foo"}))
+		for _, v := range task.Registry["bar"] {
+			bar, _ := v.(X)
+			Expect(bar).To(Equal(X{"bar"}))
+		}
+	})
+
+	It("Should append multiple tasks at once", func() {
+		task.Register("baz", X{"baz"}, X{"baz"}, X{"baz"})
+		Expect(len(task.Registry["baz"])).To(Equal(3))
+
+		for _, v := range task.Registry["baz"] {
+			baz, _ := v.(X)
+			Expect(baz).To(Equal(X{"baz"}))
 		}
 	})
 })
