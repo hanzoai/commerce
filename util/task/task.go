3f2c57620b3a8b63e700a13905593fd6a53a3b9e
Zach Kelling <z@zeekay.io>
2015-02-21 01:53:05 -0600

    Fake context, improved tasks.

--- util/task/task.go
+++ util/task/task.go
@@ -9,49 +9,25 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.io/middleware"
+	"crowdstart.io/util/fakecontext"
 	"crowdstart.io/util/log"
 )
 
 var (
-	tasks       = make(map[string][]interface{})
+	registry    = make(map[string][]interface{})
 	contextType = reflect.TypeOf((**gin.Context)(nil)).Elem()
 )
 
-// Register a new task
-func Register(name string, fns ...interface{}) {
-	// Create slice for task set
-	if _, ok := tasks[name]; !ok {
-		tasks[name] = make([]interface{}, 0)
-	}
-
-	// Append task fns
-	for _, fn := range fns {
-		tasks[name] = append(tasks[name], fn)
-	}
+type Task struct {
+	ExpectsQuery bool
+	Fn           interface{}
+	Value        reflect.Value
+	DelayFn      *delay.Function
 }
 
-// Run task
-func Run(ctx *gin.Context, name string, args ...interface{}) {
-	fns := tasks[name]
-	for _, fn := range fns {
-		switch v := fn.(type) {
-		case *delay.Function:
-			v.Call(middleware.GetAppEngine(ctx), args...)
-		case func(*gin.Context, ...interface{}):
-			v(ctx, args...)
-		case func(*gin.Context):
-			v(ctx)
-		case func(appengine.Context):
-			v(middleware.GetAppEngine(ctx)) // TODO: Remove after updating older tasks.
-		default:
-			log.Error("Don't know how to call %v", reflect.ValueOf(v).Type(), ctx)
-		}
-	}
-}
+func NewTask(fn interface{}) *Task {
+	task := new(Task)
 
-// Creates a new parallel datastore worker task, which will operate on a single
-// entity of a given kind at a time (but all of them eventually, in parallel).
-func Func(name string, fn interface{}) *delay.Function {
 	// Check type of worker func to ensure it matches required signature.
 	t := reflect.TypeOf(fn)
 
@@ -72,12 +48,71 @@ func Func(name string, fn interface{}) *delay.Function {
 	}
 
 	// Get reflect.Value of fn so we can call from delay.Func
-	fnValue := reflect.ValueOf(fn)
+	task.Value = reflect.ValueOf(fn)
+	return task
+}
+
+// Register a new task
+func Register(name string, tasks ...interface{}) {
+	// Create slice for task set
+	_tasks, ok := registry[name]
+
+	if !ok {
+		_tasks = make([]interface{}, 0)
+	}
+
+	// Append tasks
+	_tasks = append(_tasks, tasks...)
+
+	registry[name] = _tasks
+}
+
+// Run task
+func Run(ctx *gin.Context, name string, args ...interface{}) {
+	tasks, ok := registry[name]
+
+	if !ok {
+		log.Panic("Unknown task: %v", name, ctx)
+	}
+
+	for _, task := range tasks {
+		switch v := task.(type) {
+		case *delay.Function:
+			v.Call(middleware.GetAppEngine(ctx), args...)
+		case func(*gin.Context, ...interface{}):
+			v(ctx, args...)
+		case func(*gin.Context):
+			v(ctx)
+		case func(appengine.Context):
+			v(middleware.GetAppEngine(ctx)) // TODO: Remove after updating older tasks.
+		case *Task:
+			v.DelayFn.Call(middleware.GetAppEngine(ctx), fakecontext.NewContext(ctx))
+		default:
+			log.Panic("Don't know how to call %v", reflect.ValueOf(v).Type(), ctx)
+		}
+	}
+}
+
+// Creates a new parallel datastore worker task, which will operate on a single
+// entity of a given kind at a time (but all of them eventually, in parallel).
+func Func(name string, fn interface{}) *delay.Function {
+	task := NewTask(fn)
 
 	// Create actual delay func
 	delayFn := delay.Func(name, func(c appengine.Context, args ...interface{}) {
-		// Setup gin context
 		ctx := new(gin.Context)
+
+		// If passed a context, use that
+		if len(args) > 0 {
+			if _ctx, ok := args[0].(*fakecontext.Context); ok {
+				ctx, _ = _ctx.Context()
+			}
+
+			// Remove context from args
+			args = args[:len(args)-1]
+		}
+
+		// Ensure App Engine context is set on gin Context
 		ctx.Set("appengine", c)
 
 		// Build arguments for fn
@@ -89,11 +124,14 @@ func Func(name string, fn interface{}) *delay.Function {
 		}
 
 		// Call fn
-		fnValue.Call(in)
+		task.Value.Call(in)
 	})
 
-	// Autoregister with HTTP handler
-	Register(name, delayFn)
+	// Save reference to delay function for easy access from Task
+	task.DelayFn = delayFn
+
+	// Auto-register with HTTP handler
+	Register(name, task)
 
 	return delayFn
 }
