8f0f685c5105bb46662b006d0948e458ed9f8269
zeekay <zk@monoid.io>
2015-02-21 21:26:11 -0600

    Stylistic changes to task integration test.

--- test/util/task/integration/task_integration_test.go
+++ test/util/task/integration/task_integration_test.go
@@ -18,45 +18,45 @@ func Test(t *testing.T) {
 	ginkgo.SetupTest("util/task/integration", t)
 }
 
-var (
-	ctx ae.Context
-)
+var ctx ae.Context
 
-// Setup appengine context
-var _ = BeforeSuite(func() {
-	_ctx, err := ae.NewContext(ae.Options{
-		Modules:    []string{"default"},
-		TaskQueues: []string{"default"},
-	})
-	Expect(err).NotTo(HaveOccurred())
-
-	// Save reference to appengine.Context
-	ctx = _ctx
-
-	// Wait for task to run
-	time.Sleep(5 * time.Second)
-})
-
-// Tear-down appengine context
-var _ = AfterSuite(func() {
-	ctx.Close()
-})
-
-var _ = Describe("Run", func() {
-	It("Should call task successfully", func() {
-		// Start task
-		client := httpclient.New(ctx, "default")
-
-		res, err := client.Get("/task/foo")
+func init() {
+	// Setup appengine context
+	BeforeSuite(func() {
+		_ctx, err := ae.NewContext(ae.Options{
+			Modules:    []string{"default"},
+			TaskQueues: []string{"default"},
+		})
 		Expect(err).NotTo(HaveOccurred())
-		Expect(res.StatusCode).To(Equal(200))
+
+		// Save reference to appengine.Context
+		ctx = _ctx
 
 		// Wait for task to run
-		time.Sleep(1 * time.Second)
-
-		// Check if memcache is set
-		foo, err := memcache.Get(ctx, "foo")
-		Expect(err).NotTo(HaveOccurred())
-		Expect(string(foo.Value)).To(Equal("bar"))
+		time.Sleep(5 * time.Second)
 	})
-})
+
+	// Tear-down appengine context
+	AfterSuite(func() {
+		ctx.Close()
+	})
+
+	Describe("Run", func() {
+		It("Should call task successfully", func() {
+			// Start task
+			client := httpclient.New(ctx, "default")
+
+			res, err := client.Get("/task/foo")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(res.StatusCode).To(Equal(200))
+
+			// Wait for task to run
+			time.Sleep(1 * time.Second)
+
+			// Check if memcache is set
+			foo, err := memcache.Get(ctx, "foo")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(foo.Value)).To(Equal("bar"))
+		})
+	})
+}
