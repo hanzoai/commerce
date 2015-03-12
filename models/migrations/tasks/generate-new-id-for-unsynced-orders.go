90dfd46ce87383ed166e034e9931548a81860438
David Tai <antisocialmunky@gmail.com>
2015-03-12 13:37:24 -0500

    simplified migration script for regenerating ids

--- models/migrations/tasks/generate-new-id-for-unsynced-orders.go
+++ models/migrations/tasks/generate-new-id-for-unsynced-orders.go
@@ -12,29 +12,11 @@ import (
 //If we don't get a sync, then there is some problem with the order id key and it needs to be regenerated
 var GenerateNewIdForUnsyncedOrders = parallel.Task("generate-new-id-for-unsynced-orders", func(db *datastore.Datastore, key datastore.Key, order models.Order) {
 	// skip orders that were synced
-	if order.SalesforceId() != "" {
+	if order.PrimarySalesforceId_ != "" {
 		return
 	}
 
-	orders := make([]models.Order, 0)
-	var err error
-
-	// Get Contributions
-	if _, err = db.Query("order").Filter("Id =", order.Id).GetAll(db.Context, &orders); err != nil {
-		log.Error("Task has encountered error: %v", err, db.Context)
-		return
-	}
-
-	if len(orders) <= 0 {
-		log.Error("This should never happen! %v", order, db.Context)
-		return
-	}
-
-	if len(orders) <= 1 {
-		return
-	}
-
-	log.Debug("Regenerating Order Id for %v", order, db.Context)
+	log.Debug("Regenerating Id for order %v", order, db.Context)
 
 	order.Id = key.Encode()
 	order.UpdatedAt = time.Now()
