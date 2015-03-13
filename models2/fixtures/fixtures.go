58ea1d7764b4630b059ace9097bceb05fb1567e6
Zach Kelling <z@zeekay.io>
2015-03-13 02:10:50 -0500

    Start work on new fixtures.

--- models2/fixtures/fixtures.go
+++ models2/fixtures/fixtures.go
@@ -3,22 +3,22 @@ package fixtures
 import (
 	"strings"
 
-	"crowdstart.io/config"
+	// "crowdstart.io/config"
 	"crowdstart.io/util/task"
 )
 
 func init() {
-	// Remove a few dangerous fixtures from production
-	if config.IsProduction {
-		task.Unregister("contributors")
-		task.Unregister("skully-campaign")
-		task.Unregister("users")
-	}
+	// // Remove a few dangerous fixtures from production
+	// if config.IsProduction {
+	// 	task.Unregister("contributors")
+	// 	task.Unregister("skully-campaign")
+	// 	task.Unregister("users")
+	// }
 
 	// Register all fixtures under a fixtures-all task name
 	for name, tasks := range task.Registry {
-		if strings.HasPrefix(name, "fixtures-") {
-			task.Register("fixtures-all", tasks...)
+		if strings.HasPrefix(name, "models2-fixtures-") {
+			task.Register("models2-fixtures-all", tasks...)
 		}
 	}
 }
