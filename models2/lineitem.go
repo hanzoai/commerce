e9d420f6733426d791de2bf7e27e0d8370705308
zeekay <zk@monoid.io>
2015-03-12 21:53:04 -0500

    Update orders, line items.

--- models2/lineitem.go
+++ models2/lineitem.go
@@ -1,79 +1,70 @@
 package models
 
-import (
-	"net/http"
-	"strings"
-
-	"github.com/mholt/binding"
-)
-
 type LineItem struct {
 	SalesforceSObject
 
-	SKU_         string `json:"SKU"`
-	Slug_        string `json:"Slug"`
-	VariantId    string
-	Product      Product        `datastore:"-"`
-	Variant      ProductVariant `datastore:"-"`
-	Description  string
-	DiscountAmnt Cents
-	LineNo       int
-	Quantity     int
+	ProductId   string
+	ProductName string
+	ProductSlug string
 
-	// UOM string `schema:"-"`
-	// UPC          string
-	// Material     string
-	// NetAmnt      string
-	// TaxAmnt      string
+	CollectionId string
+
+	VariantId   string
+	VariantName string
+	VariantSKU  string
+
+	// Unit price
+	Price Cents
+
+	// Number of units
+	Quantity int
+
+	// Unit weight
+	Weight int
+
+	// Whether taxes apply to this line item
+	Taxable bool
 }
 
-func (li LineItem) Price() Cents {
-	return Cents(int(li.Variant.Price) * li.Quantity)
+func (li LineItem) TotalPrice() Cents {
+	return li.Price * Cents(li.Quantity)
 }
 
 func (li LineItem) DisplayPrice() string {
-	return DisplayPrice(li.Price())
+	return DisplayPrice(li.TotalPrice())
 }
 
-func (li LineItem) SKU() string {
-	return li.Variant.SKU
-}
+// func (li LineItem) Validate(req *http.Request, errs binding.Errors) binding.Errors {
+// 	if li.SKU() == "" {
+// 		errs = append(errs, binding.Error{
+// 			FieldNames:     []string{"Variant.SKU"},
+// 			Classification: "InputError",
+// 			Message:        "SKU cannot be empty.",
+// 		})
+// 	}
 
-func (li LineItem) Slug() string {
-	return li.Product.Slug
-}
+// 	if li.Quantity < 1 {
+// 		errs = append(errs, binding.Error{
+// 			FieldNames:     []string{"Quantity"},
+// 			Classification: "InputError",
+// 			Message:        "Quantity cannot be less than 1.",
+// 		})
+// 	}
 
-func (li LineItem) Validate(req *http.Request, errs binding.Errors) binding.Errors {
-	if li.SKU() == "" {
-		errs = append(errs, binding.Error{
-			FieldNames:     []string{"Variant.SKU"},
-			Classification: "InputError",
-			Message:        "SKU cannot be empty.",
-		})
-	}
-
-	if li.Quantity < 1 {
-		errs = append(errs, binding.Error{
-			FieldNames:     []string{"Quantity"},
-			Classification: "InputError",
-			Message:        "Quantity cannot be less than 1.",
-		})
-	}
-
-	return errs
-}
+// 	return errs
+// }
 
 // Displays nice "/" delimited variant information.
-func (li LineItem) DisplayShortDescription() string {
-	opts := []string{}
-	for _, opt := range []string{li.Product.Title, li.Variant.Color, li.Variant.Style, li.Variant.Size} {
-		if opt != "" {
-			opts = append(opts, opt)
-		}
-	}
-	if len(opts) > 0 {
-		return strings.Join(opts, " / ")
-	} else {
-		return li.SKU()
-	}
-}
+// func (li LineItem) DisplayShortDescription() string {
+// 	opts := []string{}
+// 	for _, opt := range []string{li.Product.Title, li.Variant.Color, li.Variant.Style, li.Variant.Size} {
+// 		if opt != "" {
+// 			opts = append(opts, opt)
+// 		}
+// 	}
+// 	if len(opts) > 0 {
+// 		return strings.Join(opts, " / ")
+// 	} else {
+// 		return li.SKU()
+// 	}
+// }
