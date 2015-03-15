0450b3ea6e5c5daf3cfd6014289e0ffd667ad091
David Tai <david@verus.io>
2015-03-15 17:34:36 -0500

    comments for access token middleware

--- middleware/accesstoken.go
+++ middleware/accesstoken.go
@@ -12,20 +12,24 @@ import (
 // Require login to view route
 func RequiresOrgToken() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// Get the access token from the Request
 		accessToken := c.Request.Header.Get("Authorization")
 
 		db := datastore.New(c)
 		o := organization.New(db)
 
+		// Try to validate the org's access token
 		if err := o.GetWithAccessToken(accessToken); err != nil {
 			c.Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 			c.Abort(500)
 		}
 
+		// Try to set the namespace to the org's key
 		if c2, err := appengine.Namespace(GetAppEngine(c), o.Id()); err != nil {
 			c.Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 			c.Abort(500)
 		} else {
+			// Overwrite hte old appengine Context on gin Context
 			c.Set("appengine", c2)
 		}
 	}
