5139ccd514567a3dc9cd6506755690579dad3d24
zeekay <zk@monoid.io>
2015-03-15 22:37:26 -0500

    Secure API routes.

--- middleware/accesstoken.go
+++ middleware/accesstoken.go
@@ -7,36 +7,66 @@ import (
 
 	"crowdstart.io/datastore"
 	"crowdstart.io/models2/organization"
+	"crowdstart.io/util/json"
+	"crowdstart.io/util/log"
 )
 
 // Require login to view route
-func RequiresOrgToken() gin.HandlerFunc {
+func TokenRequired() gin.HandlerFunc {
+	fail := func(c *gin.Context, code int, message string, err error) {
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Writer.Write(json.EncodeBytes(gin.H{"code": code, "message": message}))
+
+		if err != nil {
+			log.Error(message+": %v", err, c)
+		}
+
+		c.Abort(code)
+	}
+
 	return func(c *gin.Context) {
 		// Get the access token from the Request
 		accessToken := c.Request.Header.Get("Authorization")
 
-		db := datastore.New(c)
+		// If not set using Authorization header, check for token query param.
+		if accessToken == "" {
+			query := c.Request.URL.Query()
+			accessToken = query.Get("token")
+		}
+
+		// Bail if we still don't have an access token
+		if accessToken == "" {
+			fail(c, 401, "Access token is invalid.", nil)
+			return
+		}
+
+		ctx := GetAppEngine(c)
+		db := datastore.New(ctx)
 		o := organization.New(db)
 
 		// Try to validate the org's access token
 		if err := o.GetWithAccessToken(accessToken); err != nil {
-			c.Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
-			c.Abort(500)
+			fail(c, 401, "Access token is invalid.", nil)
+			return
 		}
 
-		// Try to set the namespace to the org's key
-		if c2, err := appengine.Namespace(GetAppEngine(c), o.Id()); err != nil {
-			c.Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
-			c.Abort(500)
+		// Try to get the namespace to the org's key
+		if ctx2, err := appengine.Namespace(ctx, o.Id()); err != nil {
+			fail(c, 500, "Failed to get namespace for organization.", err)
 		} else {
-			// set crowdstart org on gin Context
-			c.Set("crowdstart-org", o)
+			// Save organization in session
+			c.Set("org", o)
+			// Save namespace in session
 			// Overwrite hte old appengine Context on gin Context
-			c.Set("appengine", c2)
+			c.Set("namespace", ctx2)
 		}
 	}
 }
 
+func GetNamespace(c *gin.Context) appengine.Context {
+	return c.MustGet("namespace").(appengine.Context)
+}
+
 func GetOrg(c *gin.Context) *organization.Organization {
-	return c.MustGet("crowdstart-org").(*organization.Organization)
+	return c.MustGet("org").(*organization.Organization)
 }
