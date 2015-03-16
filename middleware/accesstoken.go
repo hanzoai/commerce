50e5ec38490153867cbfe422510af600edea6be5
zeekay <zk@monoid.io>
2015-03-16 05:54:18 -0500

    Make tests pass.

--- middleware/accesstoken.go
+++ middleware/accesstoken.go
@@ -8,22 +8,10 @@ import (
 	"crowdstart.io/datastore"
 	"crowdstart.io/models2/organization"
 	"crowdstart.io/util/json"
-	"crowdstart.io/util/log"
 )
 
 // Require login to view route
 func TokenRequired() gin.HandlerFunc {
-	fail := func(c *gin.Context, code int, message string, err error) {
-		c.Writer.Header().Set("Content-Type", "application/json")
-		c.Writer.Write(json.EncodeBytes(gin.H{"code": code, "message": message}))
-
-		if err != nil {
-			log.Error(message+": %v", err, c)
-		}
-
-		c.Abort(code)
-	}
-
 	return func(c *gin.Context) {
 		// Get the access token from the Request
 		accessToken := c.Request.Header.Get("Authorization")
@@ -36,7 +24,7 @@ func TokenRequired() gin.HandlerFunc {
 
 		// Bail if we still don't have an access token
 		if accessToken == "" {
-			fail(c, 401, "Access token is invalid.", nil)
+			json.Fail(c, 401, "Access token is invalid.", nil)
 			return
 		}
 
@@ -46,13 +34,13 @@ func TokenRequired() gin.HandlerFunc {
 
 		// Try to validate the org's access token
 		if err := o.GetWithAccessToken(accessToken); err != nil {
-			fail(c, 401, "Access token is invalid.", nil)
+			json.Fail(c, 401, "Access token is invalid.", nil)
 			return
 		}
 
 		// Try to get the namespace to the org's key
 		if ctx2, err := appengine.Namespace(ctx, o.Id()); err != nil {
-			fail(c, 500, "Failed to get namespace for organization.", err)
+			json.Fail(c, 500, "Failed to get namespace for organization.", err)
 		} else {
 			// Save organization in session
 			c.Set("org", o)
