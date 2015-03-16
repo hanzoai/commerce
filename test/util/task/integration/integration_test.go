44cf62b0e5b11be1318044bb4b04e70c7288bd70
zeekay <zk@monoid.io>
2015-03-15 19:05:23 -0500

    Add tests.

--- test/util/task/integration/integration_test.go
+++ test/util/task/integration/integration_test.go
@@ -7,70 +7,66 @@ import (
 
 	"appengine/memcache"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
 	"crowdstart.io/util/test/ae"
-	"crowdstart.io/util/test/ginkgo"
 	"crowdstart.io/util/test/httpclient"
+
+	. "crowdstart.io/util/test/ginkgo"
 )
 
 func Test(t *testing.T) {
-	ginkgo.Setup("util/task/integration", t)
+	Setup("util/task/integration", t)
 }
 
 var ctx ae.Context
 
-func init() {
-	// Setup appengine context
-	BeforeSuite(func() {
-		ctx = ae.NewContext(ae.Options{
-			Modules:    []string{"default"},
-			TaskQueues: []string{"default"},
-		})
+// Setup appengine context
+var _ = BeforeSuite(func() {
+	ctx = ae.NewContext(ae.Options{
+		Modules:    []string{"default"},
+		TaskQueues: []string{"default"},
+	})
+
+	// Wait for task to run
+	time.Sleep(3 * time.Second)
+})
+
+// Tear-down appengine context
+var _ = AfterSuite(func() {
+	ctx.Close()
+})
+
+var _ = Describe("Run", func() {
+	It("Should call task successfully", func() {
+		// Start task
+		client := httpclient.New(ctx, "default")
+
+		res, err := client.PostForm("/task/foo", url.Values{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.StatusCode).To(Equal(200))
 
 		// Wait for task to run
-		time.Sleep(3 * time.Second)
+		time.Sleep(2 * time.Second)
+
+		// Check if memcache is set
+		foo, err := memcache.Get(ctx, "foo")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(string(foo.Value)).To(Equal("bar"))
 	})
 
-	// Tear-down appengine context
-	AfterSuite(func() {
-		ctx.Close()
+	It("Should call nested tasks successfully", func() {
+		// Start task
+		client := httpclient.New(ctx, "default")
+
+		res, err := client.PostForm("/task/nested-baz", url.Values{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.StatusCode).To(Equal(200))
+
+		// Wait for task to run
+		time.Sleep(8 * time.Second)
+
+		// Check if memcache is set
+		baz, err := memcache.Get(ctx, "baz")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(string(baz.Value)).To(Equal("qux"))
 	})
-
-	Describe("Run", func() {
-		It("Should call task successfully", func() {
-			// Start task
-			client := httpclient.New(ctx, "default")
-
-			res, err := client.PostForm("/task/foo", url.Values{})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(res.StatusCode).To(Equal(200))
-
-			// Wait for task to run
-			time.Sleep(2 * time.Second)
-
-			// Check if memcache is set
-			foo, err := memcache.Get(ctx, "foo")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(string(foo.Value)).To(Equal("bar"))
-		})
-
-		It("Should call nested tasks successfully", func() {
-			// Start task
-			client := httpclient.New(ctx, "default")
-
-			res, err := client.PostForm("/task/nested-baz", url.Values{})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(res.StatusCode).To(Equal(200))
-
-			// Wait for task to run
-			time.Sleep(8 * time.Second)
-
-			// Check if memcache is set
-			baz, err := memcache.Get(ctx, "baz")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(string(baz.Value)).To(Equal("qux"))
-		})
-	})
-}
+})
