6c90c3442e4213d7e9c640a04161a226ce35f87e
zeekay <zk@monoid.io>
2015-03-17 02:09:18 -0500

    Uncapture my heart.

--- api/payment/capture.go
+++ api/payment/capture.go
@@ -4,8 +4,23 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.io/models2/order"
+	"crowdstart.io/models2/organization"
+	"crowdstart.io/thirdparty/stripe2"
 )
 
-func capture(c *gin.Context) (o *order.Order, err error) {
-	return o, err
+func capture(c *gin.Context, org *organization.Organization, ord *order.Order) (*order.Order, error) {
+	// Get namespaced context off order
+	ctx := ord.Db.Context
+
+	// Get client we can use for API calls
+	client := stripe.New(ctx, org.Stripe.AccessToken)
+
+	// Capture any uncaptured payments
+	for _, payment := range ord.Payments {
+		if !payment.Captured {
+			client.Capture(payment.ChargeId)
+		}
+	}
+
+	return ord, nil
 }
