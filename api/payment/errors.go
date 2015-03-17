a2430e57a8a364be33e85a549cbf2dbc075925a1
Zach Kelling <z@zeekay.io>
2015-03-17 02:19:18 -0500

    Update payment API.

--- api/payment/errors.go
+++ api/payment/errors.go
@@ -6,4 +6,5 @@ var (
 	OrderDoesNotExist         = errors.New("Order does not exist.")
 	FailedToDecodeRequestBody = errors.New("Failed to decode request body.")
 	FailedToCreateCustomer    = errors.New("Failed to create customer.")
+	FailedToCaptureCharge     = errors.New("Failed to capture charge.")
 )
