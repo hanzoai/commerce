9463d3581e522892a2e6abdadc565c8e410dbd55
zeekay <zk@monoid.io>
2015-03-17 01:04:22 -0500

    Better stripe errors.

--- thirdparty/stripe2/errors.go
+++ thirdparty/stripe2/errors.go
@@ -6,3 +6,13 @@ var (
 	FailedToCreateCustomer = errors.New("Failed to create Stripe customer.")
 	FailedtoUpdateCustomer = errors.New("Failed to update Stripe customer.")
 )
+
+type Error struct {
+	Type    string
+	Message string
+	Code    string
+}
+
+func (e Error) Error() string {
+	return e.Message
+}
