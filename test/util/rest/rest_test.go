386b1f80e61aba097b0841343275b711d1a8dd66
Zach Kelling <z@zeekay.io>
2015-03-21 15:05:45 -0500

    Update tests.

--- test/util/rest/rest_test.go
+++ test/util/rest/rest_test.go
@@ -2,13 +2,14 @@ package test
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
-	"github.com/gin-gonic/gin"
-
+	"crowdstart.io/datastore"
+	"crowdstart.io/models2/organization"
+	"crowdstart.io/util/permission"
 	"crowdstart.io/util/rest"
 	"crowdstart.io/util/test/ae"
+	"crowdstart.io/util/test/ginclient"
 
 	. "crowdstart.io/util/test/ginkgo"
 )
@@ -17,11 +18,21 @@ func Test(t *testing.T) {
 	Setup("util/rest", t)
 }
 
-var ctx ae.Context
+var (
+	ctx         ae.Context
+	accessToken string
+)
 
 // Setup appengine context
 var _ = BeforeSuite(func() {
 	ctx = ae.NewContext()
+
+	// Setup organization so Authorization middleware works
+	db := datastore.New(ctx)
+	org := organization.New(db)
+	accessToken = org.AddToken("admin", permission.Admin)
+	err := org.Put()
+	Expect(err).NotTo(HaveOccurred())
 })
 
 // Tear-down appengine context
@@ -37,30 +48,29 @@ func (m Model) Kind() string {
 	return "test-model"
 }
 
-func newRouter() *gin.Engine {
-	router := gin.New()
-	router.Use(func(c *gin.Context) {
-		c.Set("appengine", ctx)
-	})
-	return router
-}
-
-func request(router *gin.Engine, method, url string) *httptest.ResponseRecorder {
-	req, _ := http.NewRequest(method, url, nil)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	return w
-}
-
 var _ = Describe("New", func() {
 	It("Should create a new Rest object with CRUD routes", func() {
-		router := newRouter()
+		client := ginclient.New(ctx)
 
 		// Create routes for Model
 		rest := rest.New(Model{})
-		rest.Route(router)
+		rest.Route(client.Router)
 
-		w := request(router, "GET", "/test-model")
+		// Should not be authorized
+		w := client.Get("/test-model")
+		Expect(w.Code).To(Equal(401))
+
+		// Set authorization header for subsequent requests
+		client.Setup(func(r *http.Request) {
+			r.Header.Set("Authorization", accessToken)
+		})
+
+		// Get should work ok
+		w = client.Get("/test-model")
 		Expect(w.Code).To(Equal(200))
+
+		// Should 404
+		w = client.Get("/test-model2")
+		Expect(w.Code).To(Equal(404))
 	})
 })
