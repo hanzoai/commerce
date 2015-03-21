2715dc68e6c93994b774891a746f686a78f68f8e
zeekay <zk@monoid.io>
2015-03-21 17:28:22 -0500

    Add permissions tests, Any, None, permission support.

---
+++ test/util/permission/permission_test.go
@@ -0,0 +1,49 @@
+package test
+
+import (
+	"testing"
+
+	"crowdstart.io/util/bit"
+	"crowdstart.io/util/permission"
+
+	. "crowdstart.io/util/test/ginkgo"
+)
+
+func Test(t *testing.T) {
+	Setup("util/bit", t)
+}
+
+var _ = Describe("Permission", func() {
+	It("Any should pass for any permission", func() {
+		field := new(bit.Field)
+		field.Set(permission.Any)
+
+		Expect(field.Has(permission.Admin)).To(Equal(true))
+		Expect(field.Has(permission.Charge)).To(Equal(true))
+		Expect(field.Has(permission.Capture)).To(Equal(true))
+	})
+
+	It("None should never pass", func() {
+		field := new(bit.Field)
+		field.Set(permission.None)
+
+		Expect(field.Has(permission.Admin)).To(Equal(false))
+		Expect(field.Has(permission.Charge)).To(Equal(false))
+		Expect(field.Has(permission.Capture)).To(Equal(false))
+	})
+
+	It("Should be able to build set of permissions from None", func() {
+		permissions := permission.None
+		masks := []bit.Mask{permission.Charge, permission.Capture}
+		for _, mask := range masks {
+			permissions |= mask
+		}
+
+		field := new(bit.Field)
+		field.Set(permissions)
+
+		Expect(field.Has(permission.Admin)).To(Equal(false))
+		Expect(field.Has(permission.Charge)).To(Equal(true))
+		Expect(field.Has(permission.Capture)).To(Equal(true))
+	})
+})
