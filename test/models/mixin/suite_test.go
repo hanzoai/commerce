a275041f115cb806be40e5c8b6d186402160f7bc
zeekay <zk@monoid.io>
2015-03-21 14:32:06 -0500

    Update access token tests.

---
+++ test/models/mixin/suite_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"testing"
+
+	"crowdstart.io/datastore"
+	"crowdstart.io/util/test/ae"
+	. "crowdstart.io/util/test/ginkgo"
+)
+
+func Test(t *testing.T) {
+	Setup("models/mixin", t)
+}
+
+var (
+	ctx ae.Context
+	db  *datastore.Datastore
+)
+
+// Setup appengine context and datastore before tests
+var _ = BeforeSuite(func() {
+	ctx = ae.NewContext()
+	db = datastore.New(ctx)
+})
+
+// Tear-down appengine context
+var _ = AfterSuite(func() {
+	ctx.Close()
+})
