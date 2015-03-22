43f25599e371890532fef744df2cdf181917415e
Timothy Messer <tmesser@verus.io>
2015-03-22 17:28:02 -0500

    Fix variant fixtures having same sku.

--- models2/fixtures/variant.go
+++ models2/fixtures/variant.go
@@ -24,7 +24,7 @@ func Variant(c *gin.Context) []*variant.Variant {
 	v.MustPut()
 
 	v2 := variant.New(db)
-	v2.SKU = "T-SHIRT-M"
+	v2.SKU = "T-SHIRT-W"
 	v2.GetOrCreate("SKU=", v2.SKU)
 	v2.ProductId = prod.Id()
 	v2.Options = []variant.Option{variant.Option{Name: "Size", Value: "Wow"}}
