0f4b8bccb9208d3574377e4bf74f5a252300c712
zeekay <zk@monoid.io>
2015-03-22 17:50:45 -0500

    Kill all ripples!

--- static/frontend/js/app.js
+++ static/frontend/js/app.js
@@ -18,9 +18,6 @@ var App = function() {
         var yearCopy = $('#year-copy'), d = new Date();
         yearCopy.html(d.getFullYear().toString())
 
-        // Intialize ripple effect on buttons
-        rippleEffect($('.btn-effect-ripple'), 'btn-ripple');
-
         // Initialize tabs
         $('[data-toggle="tabs"] a, .enable-tabs a').click(function(e){ e.preventDefault(); $(this).tab('show'); });
 
@@ -89,58 +86,6 @@ var App = function() {
                 navigation: '.scroller-nav'
             });
         }
-    };
-
-    /* Ripple effect on click functionality */
-    var rippleEffect = function(element, cl){
-        // Add required classes to the element
-        element.css({
-            'overflow': 'hidden',
-            'position': 'relative'
-        });
-
-        // On element click
-        element.on('click', function(e){
-            var elem = $(this), ripple, d, x, y;
-
-            // If the ripple element doesn't exist in this element, add it..
-            if(elem.children('.' + cl).length == 0) {
-                elem.prepend('<span class="' + cl + '"></span>');
-            }
-            else { // ..else remove .animate class from ripple element
-                elem.children('.' + cl).removeClass('animate');
-            }
-
-            // Get the ripple element
-            var ripple = elem.children('.' + cl);
-
-            // If the ripple element doesn't have dimensions set them accordingly
-            if(!ripple.height() && !ripple.width()) {
-                d = Math.max(elem.outerWidth(), elem.outerHeight());
-                ripple.css({height: d, width: d});
-            }
-
-            // Get coordinates for our ripple element
-            x = e.pageX - elem.offset().left - ripple.width()/2;
-            y = e.pageY - elem.offset().top - ripple.height()/2;
-
-            // Position the ripple element and add the class .animate to it
-            ripple.css({top: y + 'px', left: x + 'px'}).addClass('animate');
-        });
-    };
-
-    /* Handles Header */
-    var handleHeader = function(){
-        var header = $('header');
-
-        $(window).scroll(function() {
-            // If the user scrolled a bit (150 pixels) alter the header class to change it
-            if ($(this).scrollTop() > header.outerHeight()) {
-                header.addClass('header-scroll');
-            } else {
-                header.removeClass('header-scroll');
-            }
-        });
     };
 
     /* Handles Main Menu */
