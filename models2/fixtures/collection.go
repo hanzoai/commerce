2a5af292ae86540e33e575be57368e118ed2e7f3
zeekay <zk@monoid.io>
2015-03-22 18:40:01 -0500

    Add basic fixtures.

---
+++ models2/fixtures/collection.go
@@ -0,0 +1,19 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.io/models2/collection"
+)
+
+func Collection(c *gin.Context) *collection.Collection {
+	db := getDb(c)
+
+	collection := collection.New(db)
+	collection.Slug = "such-tees-pack"
+	collection.Name = "Such tees pack"
+	collection.Description = "Much tees in one pack!"
+	collection.Published = true
+
+	return collection
+}
