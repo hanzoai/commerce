3723054d69c9930943cfe945b0f58e4e60b11a51
zeekay <zk@monoid.io>
2015-03-21 19:44:26 -0500

    Update fixtures.

--- models2/fixtures/fixtures.go
+++ models2/fixtures/fixtures.go
@@ -1,24 +1,49 @@
 package fixtures
 
 import (
+	"reflect"
 	"strings"
 
-	// "crowdstart.io/config"
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.io/datastore"
+	"crowdstart.io/models2/organization"
 	"crowdstart.io/util/task"
 )
 
+// Add a fixture as a registered task
+func fixture(name string, fn interface{}) {
+	fnv := reflect.ValueOf(fn)
+	task.Func(name, func(c *gin.Context) {
+		fnv.Call([]reflect.Value{reflect.ValueOf(c)})
+	})
+}
+
+// Get db namespaced for our fixtures org
+func getDb(c *gin.Context) *datastore.Datastore {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.Name = "suchtees"
+	org.GetOrCreate("Name=", org.Name)
+
+	// Use org's namespace
+	ctx := org.Namespace(c)
+	db = datastore.New(ctx)
+	return db
+}
+
 func init() {
-	// // Remove a few dangerous fixtures from production
-	// if config.IsProduction {
-	// 	task.Unregister("contributors")
-	// 	task.Unregister("skully-campaign")
-	// 	task.Unregister("users")
-	// }
+	fixture("fixtures2-organization", Organization)
+	fixture("fixtures2-product", Product)
+	fixture("fixtures2-token", Token)
+	fixture("fixtures2-user", User)
+	fixture("fixtures2-variant", Variant)
 
 	// Register all fixtures under a fixtures-all task name
 	for name, tasks := range task.Registry {
-		if strings.HasPrefix(name, "models2-fixtures-") {
-			task.Register("models2-fixtures-all", tasks...)
+		if strings.HasPrefix(name, "fixtures2-") {
+			task.Register("fixtures2-all", tasks...)
 		}
 	}
 }
