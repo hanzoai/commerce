c34022b97e5fdfc47c06fff617b7cd0476ab2f14
Zach Kelling <z@zeekay.io>
2015-03-21 22:14:01 -0500

    Slightly less buggy fixtures.

--- models2/fixtures/variant.go
+++ models2/fixtures/variant.go
@@ -12,24 +12,27 @@ func Variant(c *gin.Context) []*variant.Variant {
 
 	// Get a product
 	prod := Product(c)
+	prod.Slug = "t-shirt"
+	prod.GetOrCreate("Slug=", prod.Slug)
 
 	v := variant.New(db)
 	v.SKU = "T-SHIRT-M"
 	v.GetOrCreate("SKU=", v.SKU)
+	v.ProductId = prod.Id()
 	v.Options = []variant.Option{variant.Option{Name: "Size", Value: "Much"}}
 	v.ProductId = prod.Id()
-	if err := v.Put(); err != nil {
-		panic(err)
-	}
+	v.Put()
 
 	v2 := variant.New(db)
 	v2.SKU = "T-SHIRT-M"
 	v2.GetOrCreate("SKU=", v2.SKU)
+	v2.ProductId = prod.Id()
 	v2.Options = []variant.Option{variant.Option{Name: "Size", Value: "Wow"}}
 	v2.ProductId = prod.Id()
-	if err := v.Put(); err != nil {
-		panic(err)
-	}
+	v2.Put()
+
+	prod.Variants = []*variant.Variant{v, v2}
+	prod.Put()
 
 	return []*variant.Variant{v, v2}
 }
