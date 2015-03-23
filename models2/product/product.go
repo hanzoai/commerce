298ad1600e036b19006a4311d24c7a335c3d4aa8
zeekay <zk@monoid.io>
2015-03-22 21:34:51 -0500

    Fix typo.

--- models2/product/product.go
+++ models2/product/product.go
@@ -3,6 +3,7 @@ package product
 import (
 	"net/http"
 	"reflect"
+	"time"
 
 	aeds "appengine/datastore"
 
@@ -67,8 +68,8 @@ type Product struct {
 
 	// Range in which product is available. If active, it takes precedent over
 	// Available bool.
-	Availability type struct {
-		Active bool
+	Availability struct {
+		Active    bool
 		StartDate time.Time `json:"startDate"`
 		EndDate   time.Time `json:"endDate"`
 	} `json:"availability"`
