6faf31d815729e4e327956ab580cd62b1781b804
Zach Kelling <z@zeekay.io>
2015-03-23 09:53:49 -0500

    Update default fixtures.

--- models2/fixtures/organization.go
+++ models2/fixtures/organization.go
@@ -27,6 +27,7 @@ func Organization(c *gin.Context) *organization.Organization {
 	org.SecretKey = []byte("prettyprettyteesplease")
 	org.Stripe.AccessToken = "sk_test_REDACTED"
 	org.Stripe.PublishableKey = "pk_test_REDACTED"
+	org.AddDefaultTokens()
 	org.Put()
 
 	// Save org into default namespace
