2bc3eb0c8cf73a39ef1c3e5331b6d78a628b5194
zeekay <zk@monoid.io>
2015-03-24 04:00:30 -0500

    Update profile edit pages.

--- platform/user/user.go
+++ platform/user/user.go
@@ -7,6 +7,8 @@ import (
 
 	"crowdstart.io/auth2"
 	"crowdstart.io/auth2/password"
+	"crowdstart.io/config"
+	"crowdstart.io/middleware"
 	"crowdstart.io/util/log"
 	"crowdstart.io/util/template"
 	"crowdstart.io/util/val"
@@ -18,77 +20,67 @@ var ErrorPasswordTooShort = errors.New("Password must be atleast 6 characters lo
 
 // Renders the profile page
 func Profile(c *gin.Context) {
-	if u, err := auth.GetCurrentUser(c); err != nil {
-		auth.Logout(c)
-		return
-	} else {
-		template.Render(c, "admin/profile.html", "user", u)
-	}
+	template.Render(c, "admin/profile.html")
 }
 
 // Handles submission on profile page
-func SubmitProfile(c *gin.Context) {
-	if u, err := auth.GetCurrentUser(c); err != nil {
-		c.Fail(500, err)
-		return
-	} else {
-		form := new(ContactForm)
-		if err := form.Parse(c); err != nil {
-			log.Panic("Failed to save user profile: %v", err)
-		}
+func ContactSubmit(c *gin.Context) {
+	form := new(ContactForm)
+	if err := form.Parse(c); err != nil {
+		log.Panic("Failed to save user profile: %v", err)
+	}
 
-		val.SanitizeUser2(&form.User)
+	val.SanitizeUser2(&form.User)
+	if errs := form.Validate(); len(errs) > 0 {
+		c.Fail(500, ErrorInvalidProfile)
+		return
+	}
+
+	u := middleware.GetCurrentUser(c)
+
+	// Update information from form.
+	u.FirstName = form.User.FirstName
+	u.LastName = form.User.LastName
+	u.Email = form.User.Email
+	u.Phone = form.User.Phone
+
+	u.Put()
+
+	c.Redirect(301, config.UrlFor("platform/", "profile"))
+}
+
+func PasswordSubmit(c *gin.Context) {
+	form := new(ChangePasswordForm)
+	if err := form.Parse(c); err != nil {
+		log.Panic("Failed to update user password: %v", err)
+	}
+
+	u := middleware.GetCurrentUser(c)
+
+	if !password.HashAndCompare(u.PasswordHash, form.OldPassword) {
+		log.Debug("Old password is incorrect.")
+		c.Fail(500, ErrorPasswordIncorrect)
+		return
+	}
+
+	if form.Password == form.ConfirmPassword {
 		if errs := form.Validate(); len(errs) > 0 {
-			c.Fail(500, ErrorInvalidProfile)
+			c.Fail(500, ErrorPasswordTooShort)
 			return
 		}
 
-		// Update information from form.
-		u.FirstName = form.User.FirstName
-		u.LastName = form.User.LastName
-		u.Email = form.User.Email
-		u.Phone = form.User.Phone
+		var err error
+		if u.PasswordHash, err = password.Hash(form.Password); err != nil {
+			c.Fail(500, err)
+			return
+		}
 
 		u.Put()
 
-		c.Redirect(301, "profile")
-	}
-}
-
-func ChangePassword(c *gin.Context) {
-	if u, err := auth.GetCurrentUser(c); err != nil {
-		c.Fail(500, err)
-		return
+		c.Redirect(301, config.UrlFor("platform/", "profile"))
 	} else {
-		form := new(ChangePasswordForm)
-		if err := form.Parse(c); err != nil {
-			log.Panic("Failed to update user password: %v", err)
-		}
-
-		if !password.HashAndCompare(u.PasswordHash, form.OldPassword) {
-			log.Debug("Old password is incorrect.")
-			c.Fail(500, ErrorPasswordIncorrect)
-			return
-		}
-
-		if form.Password == form.ConfirmPassword {
-			if errs := form.Validate(); len(errs) > 0 {
-				c.Fail(500, ErrorPasswordTooShort)
-				return
-			}
-
-			if u.PasswordHash, err = password.Hash(form.Password); err != nil {
-				c.Fail(500, err)
-				return
-			}
-
-			u.Put()
-
-			c.Redirect(301, "profile")
-		} else {
-			log.Debug("Passwords do not match.")
-			c.Fail(500, auth.ErrorPasswordMismatch)
-			return
-		}
+		log.Debug("Passwords do not match.")
+		c.Fail(500, auth.ErrorPasswordMismatch)
+		return
 	}
 }
