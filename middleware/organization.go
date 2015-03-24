d81d325694c2cb32171cde7b77e64c9585c26b50
Zach Kelling <z@zeekay.io>
2015-03-23 20:05:53 -0500

    Update middleware, auth.

---
+++ middleware/organization.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.io/config"
+	"crowdstart.io/datastore"
+	"crowdstart.io/models2/organization"
+	"crowdstart.io/util/log"
+	"crowdstart.io/util/session"
+)
+
+func AcquireOrganization(moduleName string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		u := GetCurrentUser(c)
+
+		// How did you get this far without an organization, bra?
+		if len(u.Organizations) < 1 {
+			panic("THE WORLD MAKES NO SENSE.")
+		}
+
+		log.Debug("Found user.")
+
+		// Try and re-use last organization
+		orgId := session.GetString(c, "active-organization")
+
+		// Or default to their first organization
+		if orgId == "" {
+			orgId = u.Organizations[0]
+		}
+
+		// Fetch organization
+		db := datastore.New(c)
+		org := organization.New(db)
+		err := org.Get(orgId)
+		if err != nil {
+			log.Warn("Unable to acquire organization.")
+			session.Clear(c)
+			c.Redirect(302, config.UrlFor(moduleName, "/login"))
+			c.Abort(302)
+		} else {
+			log.Debug("Organization acquired")
+			c.Set("user", u)
+			c.Set("organization", org)
+			c.Set("active-organization", org.Id())
+		}
+	}
+}
+
+func GetOrganization(c *gin.Context) *organization.Organization {
+	return c.MustGet("organization").(*organization.Organization)
+}
