2ae225f4aa4d19568d2e0579a657a6cefdd62986
David Tai <david@verus.io>
2015-03-25 15:34:55 -0500

    tested all validation

--- test/util/val/val_test.go
+++ test/util/val/val_test.go
@@ -36,6 +36,7 @@ type ValStruct struct {
 	FloatField         float64
 }
 
+// These tests also check the reflect logic for supporting types derived from base types
 var _ = Describe("Exists", func() {
 	It("Should Fail for Empty String", func() {
 		vs := ValStruct{}
@@ -67,6 +68,78 @@ var _ = Describe("Exists", func() {
 		v := val.New(&vs)
 
 		errs := v.Check("KindOfAStringField").Exists().Execute()
+		Expect(len(errs)).To(Equal(0))
+	})
+})
+
+var _ = Describe("IsEmail", func() {
+	It("Should Fail for Non-Emails", func() {
+		vs := ValStruct{StringField: "Not An Email"}
+		v := val.New(&vs)
+
+		errs := v.Check("StringField").IsEmail().Execute()
+		Expect(len(errs)).To(Equal(1))
+	})
+
+	It("Should Work for Email String", func() {
+		vs := ValStruct{StringField: "is@a.email"}
+		v := val.New(&vs)
+
+		errs := v.Check("StringField").IsEmail().Execute()
+		Expect(len(errs)).To(Equal(0))
+	})
+})
+
+var _ = Describe("IsPassword", func() {
+	It("Should Fail for Short Passwords", func() {
+		vs := ValStruct{StringField: "Short"}
+		v := val.New(&vs)
+
+		errs := v.Check("StringField").IsPassword().Execute()
+		Expect(len(errs)).To(Equal(1))
+	})
+
+	It("Should Work for Long Passwords", func() {
+		vs := ValStruct{StringField: "LessShort"}
+		v := val.New(&vs)
+
+		errs := v.Check("StringField").IsPassword().Execute()
+		Expect(len(errs)).To(Equal(0))
+	})
+})
+
+var _ = Describe("MinLength", func() {
+	It("Should Fail for strings of lengths shorter than required", func() {
+		vs := ValStruct{StringField: "Short"}
+		v := val.New(&vs)
+
+		errs := v.Check("StringField").MinLength(6).Execute()
+		Expect(len(errs)).To(Equal(1))
+	})
+
+	It("Should Work for strings of lengths longer or equal to required", func() {
+		vs := ValStruct{StringField: "Short"}
+		v := val.New(&vs)
+
+		errs := v.Check("StringField").MinLength(5).Execute()
+		Expect(len(errs)).To(Equal(0))
+	})
+})
+
+var _ = Describe("Matches", func() {
+	It("Should Fail for strings that don't match required", func() {
+		vs := ValStruct{StringField: "Short"}
+		v := val.New(&vs)
+
+		errs := v.Check("StringField").Matches("Long", "Longer").Execute()
+		Expect(len(errs)).To(Equal(1))
+	})
+
+	It("Should Work for strings that match required", func() {
+		vs := ValStruct{StringField: "Short"}
+		v := val.New(&vs)
+
+		errs := v.Check("StringField").Matches("Short", "Shorter").Execute()
 		Expect(len(errs)).To(Equal(0))
 	})
 })
