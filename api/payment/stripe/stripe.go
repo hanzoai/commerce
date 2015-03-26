88762f8d334ce628ac11d33f1b440eb8c36a0964
zeekay <zk@monoid.io>
2015-03-26 01:04:20 -0500

    Update payment logic.

--- api/payment/stripe/stripe.go
+++ api/payment/stripe/stripe.go
@@ -9,7 +9,7 @@ import (
 	"crowdstart.io/util/log"
 )
 
-func Authorize(org *organization.Organization, ord *order.Order, u *user.User, pay *payment.Payment) error {
+func Authorize(org *organization.Organization, ord *order.Order, usr *user.User, pay *payment.Payment) error {
 	// Create stripe client
 	client := stripe.New(ord.Db.Context, org.Stripe.AccessToken)
 
@@ -19,24 +19,54 @@ func Authorize(org *organization.Organization, ord *order.Order, u *user.User, p
 		return err
 	}
 
-	// Handle new, returning customer flows
-	if u.Accounts.Stripe.CustomerId == "" {
-		return newCustomer(client, tok, u, ord, pay)
-	} else {
-		return returningCustomer(client, tok, u, ord, pay)
+	// New customer
+	if usr.Accounts.Stripe.CustomerId == "" {
+		return firstTime(client, tok, usr, ord, pay)
 	}
+
+	// Existing customer, already have card on record
+	if usr.Accounts.Stripe.CardMatches(pay.Account) {
+		return returning(client, tok, usr, ord, pay)
+	}
+
+	// Existing customer, new card
+	return returningNewCard(client, tok, usr, ord, pay)
 }
 
-func newCustomer(client *stripe.Client, tok *stripe.Token, u *user.User, ord *order.Order, pay *payment.Payment) error {
+func updatePaymentFromCard(pay *payment.Payment, card *stripe.Card) {
+	pay.Account.CardId = card.ID
+	pay.Account.Brand = string(card.Brand)
+	pay.Account.LastFour = card.LastFour
+	pay.Account.Expiration.Month = string(card.Month)
+	pay.Account.Expiration.Year = string(card.Year)
+	pay.Account.Country = card.Country
+	pay.Account.Fingerprint = card.Fingerprint
+	pay.Account.Funding = string(card.Funding)
+	pay.Account.CVCCheck = string(card.CVCCheck)
+}
+
+func updatePaymentFromUser(pay *payment.Payment, usr *user.User) {
+	acct := usr.Accounts.Stripe
+	pay.Account.CardId = acct.CardId
+	pay.Account.Brand = acct.Brand
+	pay.Account.LastFour = acct.LastFour
+	pay.Account.Expiration.Month = acct.Expiration.Month
+	pay.Account.Expiration.Year = acct.Expiration.Year
+	pay.Account.Country = acct.Country
+	pay.Account.Fingerprint = acct.Fingerprint
+	pay.Account.Funding = acct.Funding
+	pay.Account.CVCCheck = acct.CVCCheck
+}
+
+func firstTime(client *stripe.Client, tok *stripe.Token, u *user.User, ord *order.Order, pay *payment.Payment) error {
 	// Create Stripe customer, which we will attach to our payment account.
-	customer, err := client.NewCustomer(tok.ID, pay.Buyer)
+	customer, err := client.NewCustomer(tok.ID, u)
 	if err != nil {
 		return err
 	}
 	pay.Account.CustomerId = customer.ID
 
 	log.Debug("Stripe customer: %#v", customer)
-	log.Debug("Stripe source: %#v", customer.DefaultSource)
 
 	// Get default source
 	cardId := customer.DefaultSource.ID
@@ -46,21 +76,10 @@ func newCustomer(client *stripe.Client, tok *stripe.Token, u *user.User, ord *or
 	}
 
 	// Update account info
-	pay.Account.CardId = cardId
-	pay.Account.Brand = string(card.Brand)
-	pay.Account.LastFour = card.LastFour
-	pay.Account.Expiration.Month = string(card.Month)
-	pay.Account.Expiration.Year = string(card.Year)
-	pay.Account.Country = card.Country
-	pay.Account.Fingerprint = card.Fingerprint
-	pay.Account.Funding = string(card.Funding)
-	pay.Account.CVCCheck = string(card.CVCCheck)
+	updatePaymentFromCard(pay, card)
 
 	// Save account on user
 	u.Accounts.Stripe = pay.Account
-
-	// Fill with debug information about user's browser
-	// payment.Client =
 
 	// Create charge and associate with payment.
 	charge, err := client.NewCharge(customer, pay)
@@ -72,11 +91,30 @@ func newCustomer(client *stripe.Client, tok *stripe.Token, u *user.User, ord *or
 	return nil
 }
 
-func returningCustomer(client *stripe.Client, tok *stripe.Token, u *user.User, ord *order.Order, pay *payment.Payment) error {
-	// Do authorization
-	_, err := client.Authorize(pay)
+func returning(client *stripe.Client, tok *stripe.Token, usr *user.User, ord *order.Order, pay *payment.Payment) error {
+	// Old card, set as source on customer and let's use that to charge
+	cust, err := client.UpdateCustomer(tok.ID, usr)
 	if err != nil {
 		return err
 	}
-	return nil
+
+	updatePaymentFromUser(pay, usr)
+
+	// Charge using customer
+	_, err = client.NewCharge(cust, pay)
+	return err
+}
+
+func returningNewCard(client *stripe.Client, tok *stripe.Token, usr *user.User, ord *order.Order, pay *payment.Payment) error {
+	// Add new card to customer
+	card, err := client.AddCard(tok.ID, usr)
+	if err != nil {
+		return err
+	}
+
+	updatePaymentFromCard(pay, card)
+
+	// Charge using customerId on user
+	_, err = client.NewCharge(usr, pay)
+	return err
 }
--- thirdparty/stripe2/errors.go
+++ thirdparty/stripe2/errors.go
@@ -1,6 +1,11 @@
 package stripe
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/stripe/stripe-go"
+)
 
 var (
 	FailedToCreateCustomer = errors.New("Failed to create Stripe customer.")
@@ -15,4 +20,17 @@ type Error struct {
 
 func (e Error) Error() string {
 	return e.Message
+}
+
+func NewError(err error) error {
+	stripeErr, ok := err.(*stripe.Error)
+	if ok {
+		return &Error{
+			Code:    string(stripeErr.Code),
+			Message: stripeErr.Msg,
+			Type:    string(stripeErr.Type),
+		}
+	}
+
+	return &Error{Type: "unknown", Message: fmt.Sprintf("Stripe error: %v", err)}
 }
