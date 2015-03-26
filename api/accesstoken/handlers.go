48fbb86f50528f52373dc0621c79cfdfd252256b
zeekay <zk@monoid.io>
2015-03-25 19:42:48 -0500

    METHODS OVERRIKJFASDKFJas.

--- api/accesstoken/handlers.go
+++ api/accesstoken/handlers.go
@@ -5,21 +5,36 @@ import "github.com/gin-gonic/gin"
 // Access token routes
 func Get(c *gin.Context) {
 	id := c.Params.ByName("id")
+	mode := c.Params.ByName("mode")
+	test := false
+	if mode == "test" {
+		test = true
+	}
 
 	query := c.Request.URL.Query()
 	email := query.Get("email")
 	password := query.Get("password")
 
-	getAccessToken(c, id, email, password)
+	getAccessToken(c, id, email, password, test)
 }
 
 func Post(c *gin.Context) {
+	// If method override is used
+	if c.Request.Method == "DELETE" {
+		Delete(c)
+		return
+	}
+
 	id := c.Params.ByName("id")
+	mode := c.Params.ByName("mode")
+	test := false
+	if mode == "test" {
+		test = true
+	}
 
 	email := c.Request.Form.Get("email")
 	password := c.Request.Form.Get("password")
-
-	getAccessToken(c, id, email, password)
+	getAccessToken(c, id, email, password, test)
 }
 
 func Delete(c *gin.Context) {
