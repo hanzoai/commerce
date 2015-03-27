adddcc504fcef9099204c7b7908a4706efca75a0
Zach Kelling <z@zeekay.io>
2015-03-26 19:02:06 -0500

    Force login for order editing.

--- preorder/preorder.go
+++ preorder/preorder.go
@@ -1,6 +1,7 @@
 package preorder
 
 import (
+	"crowdstart.io/middleware"
 	"crowdstart.io/util/router"
 	"crowdstart.io/util/template"
 	"github.com/gin-gonic/gin"
@@ -9,11 +10,13 @@ import (
 func init() {
 	router := router.New("preorder")
 
+	loginRequired := middleware.LoginRequired("preorder")
+
 	router.GET("/", Index)
 	router.POST("/", Login)
 
-	router.GET("/order/:id", GetPreorder)
-	router.POST("/order/save", SavePreorder)
+	router.GET("/order/:id", loginRequired, GetPreorder)
+	router.POST("/order/save", loginRequired, SavePreorder)
 
 	router.GET("/thanks", func(c *gin.Context) {
 		template.Render(c, "thanks.html")
