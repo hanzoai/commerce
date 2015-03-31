268e3c5567783f31f8305d9636a16b81a2f1c2fc
zeekay <zk@monoid.io>
2015-03-31 14:51:12 -0500

    Update access token.

--- models2/fixtures/organization.go
+++ models2/fixtures/organization.go
@@ -29,7 +29,7 @@ func Organization(c *gin.Context) *organization.Organization {
 	org.Stripe.Test.UserId = "acct_14lSsRCSRlllXCwP"
 	org.Stripe.Test.AccessToken = "sk_test_REDACTED"
 	org.Stripe.Test.PublishableKey = "pk_test_REDACTED"
-	org.Stripe.Test.RefreshToken = "rt_5yS8YoaRQc7qP0hu8dtgOdfKXZ7fA5xu25q6oeTYNPiXobWH"
+	org.Stripe.Test.RefreshToken = "rt_5ySFSL7IYMd6jL8p7gijCZf8jURx0z3Cg02sGGAo3PaQAytq"
 
 	// You can only have one set of test credentials, so live/test are the same.
 	org.Stripe.Live.UserId = org.Stripe.Test.UserId
