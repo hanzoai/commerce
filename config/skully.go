17c736d36dc858942ccfbbf2fbe004926dd5368f
Zach Kelling <z@zeekay.io>
2015-03-31 06:00:31 -0500

    Refactor Stripe connect bits, save live & test tokens.

--- config/skully.go
+++ config/skully.go
@@ -15,27 +15,24 @@ func Skully() *Config {
 	config.CookieDomain = "skully.com"
 
 	config.StaticUrl = "//static.skully.com"
-	config.Mandrill.FromName = "SKULLY"
-	config.Mandrill.FromEmail = "dev@hanzo.ai"
-
-	config.Salesforce.CallbackURL = "https:" + config.UrlFor("platform", "/salesforce/callback")
-	config.Stripe.RedirectURL = "https:" + config.UrlFor("platform", "/stripe/callback")
-	config.Stripe.WebhookURL = "https:" + config.UrlFor("platform", "/stripe/hook")
 
 	config.DemoMode = false
 
-	// Only use production credentials if demo mode is off.
-	if !config.DemoMode {
-		config.Salesforce.ConsumerKey = "3MVG9xOCXq4ID1uElRYWhpUWjXSbiTVg4WO6q9DvWdvBjQ_DFlwSc7jZ9AbY3z9Jv_V29W7xq1nPjTYQhYJqF"
-		config.Salesforce.ConsumerSecret = "3811316853831925498"
+	config.Stripe.ClientId = config.Stripe.ProductionClientId
+	config.Stripe.SecretKey = config.Stripe.LiveSecretKey
+	config.Stripe.PublishableKey = config.Stripe.LivePublishablKey
+	config.Stripe.RedirectURL = "https:" + config.UrlFor("platform", "/stripe/callback")
+	config.Stripe.WebhookURL = "https:" + config.UrlFor("platform", "/stripe/hook")
 
-		config.Stripe.ClientId = "ca_REDACTED"
-		config.Stripe.APIKey = "pk_live_REDACTED"
-		config.Stripe.APISecret = ""
+	config.Google.APIKey = "AIza_REDACTED"
+	config.Google.Bucket.ImageUploads = "skully-images"
 
-		config.Google.APIKey = "AIza_REDACTED"
-		config.Google.Bucket.ImageUploads = "skully-images"
-	}
+	config.Mandrill.FromName = "SKULLY"
+	config.Mandrill.FromEmail = "dev@hanzo.ai"
+
+	config.Salesforce.ConsumerKey = "3MVG9xOCXq4ID1uElRYWhpUWjXSbiTVg4WO6q9DvWdvBjQ_DFlwSc7jZ9AbY3z9Jv_V29W7xq1nPjTYQhYJqF"
+	config.Salesforce.ConsumerSecret = "3811316853831925498"
+	config.Salesforce.CallbackURL = "https:" + config.UrlFor("platform", "/salesforce/callback")
 
 	return config
 }
