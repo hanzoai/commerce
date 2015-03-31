0c105eaa61f7793272dea4afca35cb719437691a
David Tai <antisocialmunky@gmail.com>
2015-03-31 16:18:59 -0500

    charge works now I think

--- api/payment/capture.go
+++ api/payment/capture.go
@@ -4,21 +4,25 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.io/api/payment/stripe"
+	"crowdstart.io/datastore"
 	"crowdstart.io/models2/order"
 	"crowdstart.io/models2/organization"
 )
 
 func capture(c *gin.Context, org *organization.Organization, ord *order.Order) (*order.Order, error) {
 	// We could actually capture different types of things here...
-	ord, payments, err := stripe.Capture(org, ord)
+	ord, keys, payments, err := stripe.Capture(org, ord)
 	if err != nil {
 		return nil, err
 	}
 
 	// Save order and payments
 	ord.Put()
-	for _, pay := range payments {
-		pay.Put()
+
+	db := datastore.New(org.Namespace(c))
+
+	for i, payment := range payments {
+		db.PutKind(payment.Kind(), keys[i], payment)
 	}
 
 	return ord, nil
