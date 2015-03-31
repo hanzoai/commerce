90553b01c4e01567e3281b7433150a40384b1958
zeekay <zk@monoid.io>
2015-03-31 06:32:29 -0500

    Comment out old broken tests.

--- test/thirdparty/stripe/sync_test.go
+++ test/thirdparty/stripe/sync_test.go
@@ -3,11 +3,8 @@ package test
 import (
 	"time"
 
-	"crowdstart.io/config"
 	"crowdstart.io/datastore"
 	"crowdstart.io/models"
-	"crowdstart.io/thirdparty/stripe"
-	"crowdstart.io/thirdparty/stripe/tasks"
 	. "crowdstart.io/util/test/ginkgo"
 )
 
@@ -46,45 +43,46 @@ func newUser(email string) (datastore.Key, *models.User) {
 	return key, user
 }
 
-var _ = Describe("SynchronizeCharges", func() {
-	Context("Running the task", func() {
-		It("Update orders with charges, using information from Stripe", func() {
-			_, user := newUser("dev@hanzo.ai")
-			charge := models.Charge{
-				Captured: true,
-				ID:       "ch_15ZHJOCSRlllXCwPWFGgftzK",
-				Email:    user.Email,
-			}
-			key, order := newOrder(user, charge)
+// TODO: Update to use new API
+// var _ = Describe("SynchronizeCharges", func() {
+// 	Context("Running the task", func() {
+// 		It("Update orders with charges, using information from Stripe", func() {
+// 			_, user := newUser("dev@hanzo.ai")
+// 			charge := models.Charge{
+// 				Captured: true,
+// 				ID:       "ch_15ZHJOCSRlllXCwPWFGgftzK",
+// 				Email:    user.Email,
+// 			}
+// 			key, order := newOrder(user, charge)
 
-			sc := stripe.NewApiClient(ctx, config.Stripe.APISecret)
-			tasks.SynchronizeCharge(db, key, *order, sc)
+// 			sc := stripe.NewApiClient(ctx, config.Stripe.APISecret)
+// 			tasks.SynchronizeCharge(db, key, *order, sc)
 
-			var updatedCharge models.Charge
-			var updatedOrder models.Order
-			err := db.Get(key, &updatedOrder)
-			Expect(err).ToNot(HaveOccurred())
-			updatedCharge = order.Charges[0]
-			Expect(updatedCharge).ToNot(Equal(charge))
-		})
-	})
+// 			var updatedCharge models.Charge
+// 			var updatedOrder models.Order
+// 			err := db.Get(key, &updatedOrder)
+// 			Expect(err).ToNot(HaveOccurred())
+// 			updatedCharge = order.Charges[0]
+// 			Expect(updatedCharge).ToNot(Equal(charge))
+// 		})
+// 	})
 
-	Context("Disputed charge", func() {
-		It("should be marked as disputed.", func() {
-			_, user := newUser("dev@hanzo.ai")
-			charge := models.Charge{
-				ID:    "ch_15ZGKCCSRlllXCwPryrymFEH",
-				Email: user.Email,
-			}
-			key, order := newOrder(user, charge)
+// 	Context("Disputed charge", func() {
+// 		It("should be marked as disputed.", func() {
+// 			_, user := newUser("dev@hanzo.ai")
+// 			charge := models.Charge{
+// 				ID:    "ch_15ZGKCCSRlllXCwPryrymFEH",
+// 				Email: user.Email,
+// 			}
+// 			key, order := newOrder(user, charge)
 
-			sc := stripe.NewApiClient(ctx, config.Stripe.APISecret)
-			tasks.SynchronizeCharge(db, key, *order, sc)
+// 			sc := stripe.NewApiClient(ctx, config.Stripe.APISecret)
+// 			tasks.SynchronizeCharge(db, key, *order, sc)
 
-			updatedOrder := new(models.Order)
-			err := db.Get(key, updatedOrder)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(updatedOrder.Disputed).To(Equal(true))
-		})
-	})
-})
+// 			updatedOrder := new(models.Order)
+// 			err := db.Get(key, updatedOrder)
+// 			Expect(err).ToNot(HaveOccurred())
+// 			Expect(updatedOrder.Disputed).To(Equal(true))
+// 		})
+// 	})
+// })
