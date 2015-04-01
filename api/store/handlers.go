57ac2cf355c89fc045a5c71a925548d35b1ee529
zeekay <zk@monoid.io>
2015-04-01 16:17:44 -0500

    Add store and store fixtures.

--- api/store/handlers.go
+++ api/store/handlers.go
@@ -1,33 +1,49 @@
 package store
 
 import (
-	"crowdstart.io/util/json"
 	"github.com/gin-gonic/gin"
 
-	"crowdstart.io/models2/product"
-	"crowdstart.io/models2/variant"
+	"crowdstart.io/models2/store"
+	"crowdstart.io/util/rest"
+	"crowdstart.io/util/router"
 )
 
-func GetStorePrice(c *gin.Context) {
-	storeId := c.Params.ByName("storeid")
-	id := c.Params.ByName("id")
-	entity := c.Params.ByName("entity")
+// func GetStorePrice(c *gin.Context) {
+// 	storeId := c.Params.ByName("storeid")
+// 	id := c.Params.ByName("id")
+// 	entity := c.Params.ByName("entity")
 
-	switch entity {
-	case product.Product{}.Kind():
-		product := productPriceOverride(c, storeId, id)
-		c.JSON(200, product)
-		return
-	case variant.Variant{}.Kind():
-		variant := variantPriceOverride(c, storeId, id)
-		c.JSON(200, variant)
-		return
-	}
-	json.Fail(c, 500, "Invalid store lookup", nil)
-}
+// 	switch entity {
+// 	case product.Product{}.Kind():
+// 		product := productPriceOverride(c, storeId, id)
+// 		c.JSON(200, product)
+// 		return
+// 	case variant.Variant{}.Kind():
+// 		variant := variantPriceOverride(c, storeId, id)
+// 		c.JSON(200, variant)
+// 		return
+// 	}
+// 	json.Fail(c, 500, "Invalid store lookup", nil)
+// }
 
-func PostStorePrice(c *gin.Context) {
-}
+// func PostStorePrice(c *gin.Context) {
+// }
 
-func DeleteStorePrice(c *gin.Context) {
+// func DeleteStorePrice(c *gin.Context) {
+// }
+
+func Route(router router.Router, args ...gin.HandlerFunc) {
+	api := rest.New(store.Store{})
+
+	// api.GET("/:id/product/:productid", adminRequired, store.GetStorePrice)
+	// api.GET("/:id/bundle/:bundleid", adminRequired, store.GetStorePrice)
+	// api.GET("/:id/variant/:variantid", adminRequired, store.GetStorePrice)
+
+	// api.GET("/:id/listings", adminRequired, store.GetStorePrice)
+	// api.POST("/:id/listings", adminRequired, store.GetStorePrice)
+	// api.PUT("/:id/listings", adminRequired, store.GetStorePrice)
+	// api.PATCH("/:id/listings", adminRequired, store.GetStorePrice)
+	// api.DELETE("/:id/listings", adminRequired, store.GetStorePrice)
+
+	api.Route(router, args...)
 }
