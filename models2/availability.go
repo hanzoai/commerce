d539dad18740e4f1ce773992e7a882edbd05f8a8
Zach Kelling <z@zeekay.io>
2015-04-01 18:40:24 -0500

    Update store API.

---
+++ models2/availability.go
@@ -0,0 +1,9 @@
+package models
+
+import "time"
+
+type Availability struct {
+	Active    bool      `json:"active"`
+	StartDate time.Time `json:"startDate"`
+	EndDate   time.Time `json:"endDate"`
+}
