2757fd02d34e9801128d3f3da9a4e961d4b6a595
zeekay <zk@monoid.io>
2015-04-02 02:25:39 -0500

    Add store payment APIs

---
+++ api/store/payment.go
@@ -0,0 +1,47 @@
+package store
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.io/api/payment"
+	"crowdstart.io/datastore"
+	"crowdstart.io/middleware"
+	"crowdstart.io/models2/store"
+	"crowdstart.io/util/json"
+)
+
+func authorize(c *gin.Context) {
+	ctx := middleware.GetAppEngine(c)
+	db := datastore.New(ctx)
+	id := c.Params.ByName("id")
+
+	// Get store
+	stor := store.New(db)
+	if err := stor.Get(id); err != nil {
+		json.Fail(c, 500, fmt.Sprintf("Failed to retrieve store '%v': %v", id, err), err)
+		return
+	}
+
+	// Set store and do authorize
+	c.Set("store", stor)
+	payment.Authorize(c)
+}
+
+func charge(c *gin.Context) {
+	ctx := middleware.GetAppEngine(c)
+	db := datastore.New(ctx)
+	id := c.Params.ByName("id")
+
+	// Get store
+	stor := store.New(db)
+	if err := stor.Get(id); err != nil {
+		json.Fail(c, 500, fmt.Sprintf("Failed to retrieve store '%v': %v", id, err), err)
+		return
+	}
+
+	// Set store and do charge
+	c.Set("store", stor)
+	payment.Charge(c)
+}
