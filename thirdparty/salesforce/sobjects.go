23a08d66989fb4c42027e586dadd1e5b73bf5a91
zeekay <zk@monoid.io>
2015-04-06 17:54:47 -0500

    Log string instead of []byte.

--- thirdparty/salesforce/sobjects.go
+++ thirdparty/salesforce/sobjects.go
@@ -1188,7 +1188,7 @@ func push(api SalesforceClient, p string, s SObjectSyncable) error {
 	response := new(UpsertResponse)
 
 	if err := json.Unmarshal(body, response); err != nil {
-		log.Error("Unable to unmarshall UpsertResponse: %v", body, api.GetContext())
+		log.Error("Unable to unmarshall UpsertResponse: %v", string(body), api.GetContext())
 		return err
 	}
 
