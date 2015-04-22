075760a4e67b193536f8244271e7f34c1c421e76
Zach Kelling <z@zeekay.io>
2015-04-22 14:35:43 -0500

    Add mailing list apis.

---
+++ api/mailinglist/mailinglist.go
@@ -0,0 +1,43 @@
+package store
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.io/config"
+	"crowdstart.io/datastore"
+	"crowdstart.io/models2/mailinglist"
+	"crowdstart.io/models2/subscriber"
+	"crowdstart.io/util/json"
+)
+
+var subscriberEndpoint = config.UrlFor("api", "/subscriber/")
+
+// Add subscriber to mailing list
+func addSubscriber(c *gin.Context) {
+	id := c.Params.ByName("mailinglistid")
+	db := datastore.New(c)
+
+	ml := mailinglist.New(db)
+	if err := ml.GetById(id); err != nil {
+		json.Fail(c, 404, fmt.Sprintf("Failed to retrieve mailing list '%v': %v", id, err), err)
+		return
+	}
+
+	s := subscriber.New(db)
+
+	// Decode response body for subscriber
+	if err := json.Decode(c.Request.Body, s); err != nil {
+		json.Fail(c, 400, "Failed decode request body", err)
+		return
+	}
+
+	// Save subscriber to mailing list
+	if err := ml.AddSubscriber(s); err != nil {
+		json.Fail(c, 500, "Failed to save subscriber to mailing list", err)
+	} else {
+		c.Writer.Header().Add("Location", subscriberEndpoint+s.Id())
+		c.JSON(201, s)
+	}
+}
