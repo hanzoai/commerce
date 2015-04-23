37932182fe95755367418d0497e6a3350a7a0608
Zach Kelling <z@zeekay.io>
2015-04-22 19:29:57 -0500

    Update mailing list subscription API.

--- models2/mailinglist/mailinglist.go
+++ models2/mailinglist/mailinglist.go
@@ -9,6 +9,7 @@ import (
 	"crowdstart.io/models/mixin"
 	"crowdstart.io/models2/subscriber"
 	"crowdstart.io/util/fs"
+	"crowdstart.io/util/log"
 	"crowdstart.io/util/val"
 )
 
@@ -62,9 +63,24 @@ func (m *MailingList) Validator() *val.Validator {
 }
 
 func (m *MailingList) AddSubscriber(s *subscriber.Subscriber) error {
-	s.MailingListId = s.Id()
-	s.Parent = m.Key()
-	return s.Put()
+	mkey := m.Key()
+	s.MailingListId = m.Id()
+	s.Parent = mkey
+
+	return m.RunInTransaction(func() error {
+		keys, err := subscriber.Query(m.Db).Ancestor(mkey).Filter("Email=", s.Email).KeysOnly().GetAll(nil)
+		log.Debug("keys: %v, err: %v", keys, err)
+
+		if len(keys) != 0 {
+			return SubscriberAlreadyExists
+		}
+
+		if err != nil {
+			return err
+		}
+
+		return s.Put()
+	})
 }
 
 func (m *MailingList) Js() string {
