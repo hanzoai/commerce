c61603323dfc36e2db3446e345a68391bdfd9848
Zach Kelling <z@zeekay.io>
2015-04-23 21:06:19 -0500

    Make it all compile.

--- checkout/handlers.go
+++ checkout/handlers.go
@@ -1,55 +1,48 @@
 package checkout
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 
-	"crowdstart.io/auth"
-	"crowdstart.io/config"
 	"crowdstart.io/datastore"
 	"crowdstart.io/middleware"
 	// "crowdstart.io/models"
+	"crowdstart.io/models2/campaign"
 	"crowdstart.io/util/cache"
 	"crowdstart.io/util/log"
 	// "crowdstart.io/util/queries"
 	"crowdstart.io/util/template"
-
-	mandrill "crowdstart.io/thirdparty/mandrill/tasks"
-	salesforce "crowdstart.io/thirdparty/salesforce/tasks"
 )
 
 // Helper to get campaign
-func getCampaign(args ...interface{}) models.Campaign {
-	c := args[0].(*gin.Context)
+func getCampaign(args ...interface{}) *campaign.Campaign {
 	db := args[1].(*datastore.Datastore)
-	var campaign models.Campaign
-	if err := db.GetKind("campaign", "dev@hanzo.ai", &campaign); err != nil {
-		log.Error(err, c)
-	}
-	return campaign
+	cam := campaign.New(db)
+	cam.Query().Filter("Email=", "dev@hanzo.ai").First()
+	return cam
 }
 
 // Cache stripe publishable key, access token for a minute each
 var getStripePublishableKey = cache.Memoize(func(args ...interface{}) interface{} {
-	return getCampaign(args...).Stripe.PublishableKey
+	// return getCampaign(args...).Stripe.PublishableKey
+	return getCampaign(args)
 }, 60)
 
 var getStripeAccessToken = cache.Memoize(func(args ...interface{}) interface{} {
-	return getCampaign(args...).Stripe.AccessToken
+	// return getCampaign(args...).Stripe.AccessToken
+	return getCampaign(args)
 }, 60)
 
 var getSalesforceTokens = cache.Memoize(func(args ...interface{}) interface{} {
-	return getCampaign(args...).Salesforce
+	// return getCampaign(args...).Salesforce
+	return getCampaign(args)
 }, 60)
 
 // GET /
 func index(c *gin.Context) {
 	// Redirect to store on GET
-	c.Redirect(301, config.UrlFor("store", "/cart"))
+	// c.Redirect(301, config.UrlFor("store", "/cart"))
 }
 
 // POST /
@@ -81,13 +74,13 @@ func checkout(c *gin.Context) {
 	stripePublishableKey := getStripePublishableKey(c, db).(string)
 
 	// Try to get user from datastore based on email in session.
-	user, _ := auth.GetUser(c)
+	// user, _ := auth.GetUser(c)
 
 	// Render order for checkout page
 	template.Render(c, "checkout.html",
 		"order", form.Order,
 		"stripePublishableKey", stripePublishableKey,
-		"user", user,
+		// "user", user,
 	)
 }
 
@@ -109,11 +102,11 @@ func charge(c *gin.Context) {
 	// q := queries.New(ctx)
 
 	// Populate
-	if err := form.Order.Populate(db); err != nil {
-		log.Error("Failed to repopulate order information from datastore: %v", err)
-		c.Fail(500, err)
-		return
-	}
+	// if err := form.Order.Populate(db); err != nil {
+	// 	log.Error("Failed to repopulate order information from datastore: %v", err)
+	// 	c.Fail(500, err)
+	// 	return
+	// }
 
 	// Validation
 	form.Sanitize()
@@ -122,36 +115,36 @@ func charge(c *gin.Context) {
 		return
 	}
 
-	// Update user information
-	log.Debug("Trying to get user from session...", c)
-	user, err := auth.GetUser(c)
-	if err != nil {
-		// see if this is a returning user
-		log.Debug("User is not logged in")
-		if err = user.GetByEmail(form.User.Email); err != nil {
-			log.Debug("Using form.User", c)
-			user.Id = db.EncodeId("user", db.AllocateId("user"))
-			user = &form.User
-		} else {
-			log.Debug("Returning User")
-		}
-	}
-	log.Debug("User: %#v", user)
+	// // Update user information
+	// log.Debug("Trying to get user from session...", c)
+	// // user, err := auth.GetUser(c)
+	// if err != nil {
+	// 	// see if this is a returning user
+	// 	log.Debug("User is not logged in")
+	// 	if err = user.GetByEmail(form.User.Email); err != nil {
+	// 		log.Debug("Using form.User", c)
+	// 		user.Id = db.EncodeId("user", db.AllocateId("user"))
+	// 		user = &form.User
+	// 	} else {
+	// 		log.Debug("Returning User")
+	// 	}
+	// }
+	// log.Debug("User: %#v", user)
 
 	// Set email for order
-	form.Order.UserId = user.Id
-	form.Order.Email = user.Email
-	form.Order.CampaignId = "dev@hanzo.ai"
-	form.Order.Preorder = true
+	// form.Order.UserId = user.Id
+	// form.Order.Email = user.Email
+	// form.Order.CampaignId = "dev@hanzo.ai"
+	// form.Order.Preorder = true
 
 	// Set test mode, minimum stripe transaction
-	if strings.Contains(user.Email, "@verus.io") {
-		form.Order.Test = true
-		form.Order.Shipping = 0
-		form.Order.Tax = 0
-		form.Order.Subtotal = 50 * 100 // 50 cents is Stripe's
-		form.Order.Total = 50 * 100    // minimum transaction amount.
-	}
+	// if strings.Contains(user.Email, "@verus.io") {
+	// 	form.Order.Test = true
+	// 	form.Order.Shipping = 0
+	// 	form.Order.Tax = 0
+	// 	form.Order.Subtotal = 50 * 100 // 50 cents is Stripe's
+	// 	form.Order.Total = 50 * 100    // minimum transaction amount.
+	// }
 
 	// Get access token
 	stripeAccessToken := getStripeAccessToken(c, db).(string)
@@ -179,11 +172,11 @@ func charge(c *gin.Context) {
 	// data and stripe customer consistency.
 	log.Debug("Updating and saving user...", c)
 
-	user.BillingAddress = form.Order.BillingAddress
-	user.ShippingAddress = form.Order.ShippingAddress
-	user.Phone = form.User.Phone
-	user.FirstName = form.User.FirstName
-	user.LastName = form.User.LastName
+	// 	user.BillingAddress = form.Order.BillingAddress
+	// 	user.ShippingAddress = form.Order.ShippingAddress
+	// 	user.Phone = form.User.Phone
+	// 	user.FirstName = form.User.FirstName
+	// 	user.LastName = form.User.LastName
 
 	// TODO: Use new API
 	// if err := q.UpsertUser(user); err != nil {
@@ -209,54 +202,54 @@ func charge(c *gin.Context) {
 		return
 	}
 	key, _ = db.DecodeKey(encodedKey)
-	orderId := key.IntID()
+	// orderId := key.IntID()
 
 	// Set the id as we use it to update salesforce
-	form.Order.Id = key.Encode()
+	// form.Order.Id = key.Encode()
 
-	// Synchronize Salesforce
-	salesforceTokens := getSalesforceTokens(c, db).(models.SalesforceTokens)
+	// // Synchronize Salesforce
+	// salesforceTokens := getSalesforceTokens(c, db).(models.SalesforceTokens)
 
-	if salesforceTokens.AccessToken != "" {
-		// Launch a synchronization task
-		campaign := getCampaign(c, db)
-		salesforce.CallUpsertUserTask(ctx, &campaign, user)
-		salesforce.CallUpsertOrderTask(ctx, &campaign, &form.Order)
-	}
+	// if salesforceTokens.AccessToken != "" {
+	// 	// Launch a synchronization task
+	// 	campaign := getCampaign(c, db)
+	// 	salesforce.CallUpsertUserTask(ctx, &campaign, user)
+	// 	salesforce.CallUpsertOrderTask(ctx, &campaign, &form.Order)
+	// }
 
-	// Generate invite for preorder site.
-	log.Debug("Saving invite token...", c)
-	invite := new(models.Token)
-	invite.GenerateId()
-	invite.UserId = user.Id
-	invite.Email = user.Email
-	if _, err := db.PutKind("invite-token", invite.Id, invite); err != nil {
-		log.Error("Failed to save invite-token: %v", err, c)
-		c.Fail(500, err)
-		return
-	}
+	// // Generate invite for preorder site.
+	// log.Debug("Saving invite token...", c)
+	// invite := new(models.Token)
+	// invite.GenerateId()
+	// invite.UserId = user.Id
+	// invite.Email = user.Email
+	// if _, err := db.PutKind("invite-token", invite.Id, invite); err != nil {
+	// 	log.Error("Failed to save invite-token: %v", err, c)
+	// 	c.Fail(500, err)
+	// 	return
+	// }
 
 	// Save contribution for preorder site.
-	log.Debug("Saving contribution...", c)
-	contribution := new(models.Contribution)
-	contribution.Id = strconv.Itoa(int(orderId))
-	contribution.UserId = user.Id
-	contribution.Email = user.Email
-	contribution.Perk = models.Perks["WINTER2014PROMO"]
-	if _, err := db.PutKind("contribution", contribution.Id, contribution); err != nil {
-		log.Error("Failed to save contribution: %v", err, c)
-		c.Fail(500, err)
-		return
-	}
+	// log.Debug("Saving contribution...", c)
+	// contribution := new(models.Contribution)
+	// contribution.Id = strconv.Itoa(int(orderId))
+	// contribution.UserId = user.Id
+	// contribution.Email = user.Email
+	// contribution.Perk = models.Perks["WINTER2014PROMO"]
+	// if _, err := db.PutKind("contribution", contribution.Id, contribution); err != nil {
+	// 	log.Error("Failed to save contribution: %v", err, c)
+	// 	c.Fail(500, err)
+	// 	return
+	// }
 
-	// Send order confirmation email
-	mandrill.SendTransactional.Call(ctx, "email/order-confirmation.html",
-		user.Email,
-		user.Name(),
-		fmt.Sprintf("SKULLY Order confirmation #%v", orderId))
+	// // Send order confirmation email
+	// mandrill.SendTransactional.Call(ctx, "email/order-confirmation.html",
+	// 	user.Email,
+	// 	user.Name(),
+	// 	fmt.Sprintf("SKULLY Order confirmation #%v", orderId))
 
-	log.Debug("Checkout complete!", c)
-	c.JSON(200, gin.H{"inviteId": invite.Id, "orderId": orderId})
+	// log.Debug("Checkout complete!", c)
+	// c.JSON(200, gin.H{"inviteId": invite.Id, "orderId": orderId})
 }
 
 // Success
