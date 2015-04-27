5f0e0d70a8b02fc01d9103a048acd812a15b242b
Timothy Messer <tmesser@verus.io>
2015-04-26 20:08:57 -0500

    Adding default values for the conversion trackers.

--- models/fixtures/mailinglist.go
+++ models/fixtures/mailinglist.go
@@ -17,6 +17,12 @@ var Mailinglist = New("mailinglist", func(c *gin.Context) *mailinglist.MailingLi
 	mailingList.Mailchimp = mailinglist.Mailchimp{Id: "aowieij", APIKey: "23098fIOWJEOIJFW", DoubleOptin: false, UpdateExisting: true, ReplaceInterests: true, SendWelcome: false}
 	mailingList.ThankYou.Type = thankyou.Redirect
 	mailingList.ThankYou.Url = "http://suchtees.com/thanks/"
+	mailingList.Facebook.Id = "6024480985959"
+	mailingList.Facebook.Value = "0.00"
+	mailingList.Facebook.Currency = "USD"
+
+	mailingList.Google.Category = "Subscription"
+	mailingList.Google.Name = "T-Shirt List Sign-up"
 
 	mailingList.MustPut()
 
