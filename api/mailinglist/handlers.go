4c33bded4c381ddf8a00e3a1180a40cf33abd1f8
zeekay <zk@monoid.io>
2015-04-29 12:54:48 -0500

    FUCKING CORS.

--- api/mailinglist/handlers.go
+++ api/mailinglist/handlers.go
@@ -3,6 +3,7 @@ package store
 import (
 	"github.com/gin-gonic/gin"
 
+	"crowdstart.io/middleware"
 	"crowdstart.io/models/mailinglist"
 	"crowdstart.io/util/rest"
 	"crowdstart.io/util/router"
@@ -12,6 +13,8 @@ func Route(router router.Router, args ...gin.HandlerFunc) {
 	api := rest.New(mailinglist.MailingList{})
 
 	group := router.Group("mailinglist")
+	group.Use(middleware.AccessControl("*"))
+
 	group.POST("/:mailinglistid/subscribe", addSubscriber)
 	group.GET("/:mailinglistid/js", js)
 
