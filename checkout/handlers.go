12655ded5bdf48b97a2acfdad37aa4194a9094b4
David Tai <david@verus.io>
2015-05-12 15:57:53 -0700

    skully email templating needs som work still

--- checkout/handlers.go
+++ checkout/handlers.go
@@ -251,10 +251,13 @@ func charge(c *gin.Context) {
 	}
 
 	// Send order confirmation email
+	form.Order.LoadVariantsProducts()
 	mandrill.SendTransactional.Call(ctx, "email/order-confirmation.html",
 		user.Email,
 		user.Name(),
-		fmt.Sprintf("SKULLY Order confirmation #%v", orderId))
+		fmt.Sprintf("SKULLY Order confirmation #%v", orderId),
+		"user", user,
+		"order", &form.Order)
 
 	log.Debug("Checkout complete!", c)
 	c.JSON(200, gin.H{"inviteId": invite.Id, "orderId": orderId})
