dc70a98f0a000253dfb1304817fc6fb07ca4512e
David Tai <antisocialmunky@gmail.com>
2015-05-14 12:53:53 -0700

    zero decimal support

--- static/js/util.js
+++ static/js/util.js
@@ -121,6 +121,13 @@ var Util = (function() {
   var currentCurrencySign = currencySigns[currentCurrencyCode];
   var digitsOnlyRe = new RegExp('[^\\d.-]', 'g')
 
+  var isZeroDecimal = function(code) {
+    if (code === 'bif' || code === 'clp' || code === 'djf' || code === 'gnf' || code === 'jpy' || code === 'kmf' || code === 'krw' || code === 'mga' || code === 'pyg' || code === 'rwf' || code === 'vnd' || code === 'vuv' || code === 'xaf' || code === 'xof' || code === 'xpf') {
+      return true
+    }
+    return false
+  };
+
   return {
     setCurrency: function(code){
       currentCurrencyCode = code;
@@ -130,19 +137,21 @@ var Util = (function() {
       return Util.renderUICurrencyFromJSON(Util.renderJSONCurrencyFromUI(uiCurrency));
     },
     renderUICurrencyFromJSON: function(jsonCurrency) {
-      // jsonCurrency is cents
       jsonCurrency = '' + jsonCurrency;
+      // jsonCurrency is not cents
+      if (isZeroDecimal(currentCurrencyCode)) {
+        return currentCurrencySign + jsonCurrency
+      }
+
+      // jsonCurrency is cents
       while (jsonCurrency.length < 3) {
         jsonCurrency = '0' + jsonCurrency;
-      }
-      if (currentCurrencyCode === 'jpy') {
-        return currentCurrencySign + jsonCurrency
       }
 
       return currentCurrencySign + jsonCurrency.substr(0, jsonCurrency.length - 2) + '.' + jsonCurrency.substr(-2);
     },
     renderJSONCurrencyFromUI: function(uiCurrency) {
-      if (currentCurrencyCode === 'jpy') {
+      if (isZeroDecimal(currentCurrencyCode)) {
         return parseInt(('' + uiCurrency).replace(digitsOnlyRe, '').replace(currencySeparator, ''), 10)
       }
       // uiCurrency is a whole unit of currency
