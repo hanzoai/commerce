2c18a0a826b292145ee9eabe702361a254bc1168
zeekay <zk@monoid.io>
2015-05-17 16:11:59 -0700

    Move discourse config stuff, add Redirect helper.

--- store/store.go
+++ store/store.go
@@ -27,7 +27,7 @@ func init() {
 
 	// Login
 	router.GET("/login", logoutRequired, user.Login)
-	router.GET("/login/sso", user.LoginSSO)
+	router.GET("/login/sso", logoutRequired, user.LoginSSO)
 	router.POST("/login", logoutRequired, user.SubmitLogin)
 	router.GET("/logout", user.Logout)
 
