7bc27f96dbcaf11e7da469c23b192cb212bf7256
David Tai <antisocialmunky@gmail.com>
2015-05-22 19:23:21 -0700

    bb fixture

---
+++ models/fixtures/bellabeat.go
@@ -0,0 +1,50 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/auth/password"
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/namespace"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/user"
+
+	"crowdstart.com/util/log"
+)
+
+var Bellabeat = New("bellabeat", func(c *gin.Context) *organization.Organization {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.Name = "bellabeat"
+	org.GetOrCreate("Name=", org.Name)
+
+	u := user.New(db)
+	u.Email = "bellabeat@verus.io"
+	u.GetOrCreate("Email=", u.Email)
+	u.FirstName = "Michael"
+	u.LastName = "Walker"
+	u.Organizations = []string{org.Id()}
+	u.PasswordHash, _ = password.Hash("bellabeatpassword!")
+	u.Put()
+
+	org.FullName = "bellabeat"
+	org.Owners = []string{u.Id()}
+	org.Website = "http://www.bellabeat.com"
+	org.SecretKey = []byte("yW83JZGLjkGJE2gMfB4i0bwEoP03yJa5")
+	org.AddDefaultTokens()
+
+	// Save org into default namespace
+	org.Put()
+
+	// Save namespace so we can decode keys for this organization later
+	ns := namespace.New(db)
+	ns.Name = org.Name
+	ns.IntId = org.Key().IntID()
+	err := ns.Put()
+	if err != nil {
+		log.Warn("Failed to put namespace: %v", err)
+	}
+
+	return org
+})
