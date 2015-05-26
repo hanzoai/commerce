cd8b87a22963e1098b637d7129a3f421b409565b
David Tai <david@verus.io>
2015-05-26 12:18:47 -0700

    coupon tests

--- models/fixtures/coupon.go
+++ models/fixtures/coupon.go
@@ -13,18 +13,33 @@ const Month = time.Hour * 24 * 30
 var Coupon = New("coupon", func(c *gin.Context) *coupon.Coupon {
 	db := getNamespaceDb(c)
 
-	coupon := coupon.New(db)
-	coupon.Code = "such-coupon"
-	coupon.GetOrCreate("Code=", coupon.Code)
-	coupon.Name = "Such Coupon"
-	coupon.Type = "flat"
 	now := time.Now()
-	coupon.StartDate = now
-	coupon.EndDate = now.Add(Month)
-	coupon.Once = true
-	coupon.Enabled = true
-	coupon.Amount = 5
 
-	coupon.MustPut()
-	return coupon
+	cpn := coupon.New(db)
+	cpn.Code = "sad-coupon"
+	cpn.GetOrCreate("Code=", cpn.Code)
+	cpn.Name = "Sad Coupon"
+	cpn.Type = "flat"
+	cpn.StartDate = now
+	cpn.EndDate = now.Add(Month)
+	cpn.Once = true
+	cpn.Enabled = true
+	cpn.Amount = 500
+	cpn.ProductId = Product(c).Id()
+
+	cpn.MustPut()
+
+	cpn = coupon.New(db)
+	cpn.Code = "such-coupon"
+	cpn.GetOrCreate("Code=", cpn.Code)
+	cpn.Name = "Such Coupon"
+	cpn.Type = "flat"
+	cpn.StartDate = now
+	cpn.EndDate = now.Add(Month)
+	cpn.Once = true
+	cpn.Enabled = true
+	cpn.Amount = 500
+
+	cpn.MustPut()
+	return cpn
 })
