0b1a69eb6335168bcb440f8c68692520c2d265c3
zeekay <zk@monoid.io>
2015-05-27 05:55:11 -0700

    Update referral model/referral program.

--- models/referrer/referrer.go
+++ models/referrer/referrer.go
@@ -1,11 +1,8 @@
 package referrer
 
 import (
-	aeds "appengine/datastore"
-
 	"crowdstart.com/datastore"
 	"crowdstart.com/models/mixin"
-	"crowdstart.com/models/referral"
 	"crowdstart.com/models/transaction"
 	"crowdstart.com/util/val"
 )
@@ -15,7 +12,7 @@ var IgnoreFieldMismatch = datastore.IgnoreFieldMismatch
 type Referrer struct {
 	mixin.Model
 
-	Referral         referral.Referral         `json:"referral"`
+	Program          Program                   `json:"program"`
 	OrderId          string                    `json:"orderId"`
 	UserId           string                    `json:"userId"`
 	ReferredOrderIds []string                  `json:"referredOrderIds"`
@@ -39,27 +36,13 @@ func (r Referrer) Kind() string {
 	return "referrer"
 }
 
-func (r *Referrer) Load(c <-chan aeds.Property) (err error) {
-	// Load supported properties
-	if err = IgnoreFieldMismatch(aeds.LoadStruct(r, c)); err != nil {
-		return err
-	}
-
-	return err
-}
-
-func (r *Referrer) Save(c chan<- aeds.Property) (err error) {
-	// Save properties
-	return IgnoreFieldMismatch(aeds.SaveStruct(r, c))
-}
-
 func (r *Referrer) Validator() *val.Validator {
 	return nil
 }
 
 func (r *Referrer) ApplyBonus() (*transaction.Transaction, error) {
 	trans := transaction.New(r.Db)
-	r.Referral.GetBonus(trans, len(r.ReferredOrderIds))
+	r.Program.GetBonus(trans, len(r.ReferredOrderIds))
 	trans.UserId = r.UserId
 	if err := trans.Put(); err != nil {
 		return nil, err
