478a8692309ea5cbec1a8544ce578b20ff6a6ab6
Zach Kelling <z@zeekay.io>
2015-05-27 05:55:11 -0700

    Update referral model/referral program.

--- models/fixtures/referral.go
+++ models/fixtures/referral.go
@@ -4,20 +4,20 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.com/models/referral"
-	"crowdstart.com/models/types/currency"
 )
 
 var Referral = New("referral", func(c *gin.Context) *referral.Referral {
 	// Get namespaced db
 	db := getNamespaceDb(c)
 
+	ord := Order(c)
+	u := User(c)
+
 	// Referral
 	ref := referral.New(db)
-	ref.Name = "Such Referral"
-	ref.GetOrCreate("Name=", ref.Name)
-	ref.Triggers = []int{0}
-	ref.Actions = []referral.Action{referral.Action{Type: referral.StoreCredit}}
-	ref.Actions[0].Credit = referral.Credit{Currency: currency.USD, Amount: currency.Cents(1000)}
+	ref.UserId = u.Id()
+	ref.OrderId = ord.Id()
+	ref.GetOrCreate("OrderId=", ref.OrderId)
 	ref.MustPut()
 
 	return ref
