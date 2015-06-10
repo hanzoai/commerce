b82b095b628bbaca64cac3269b3b14a25e8f5213
David Tai <antisocialmunky@gmail.com>
2015-06-10 12:19:59 -0700

    cors stuff

--- api/account/handlers.go
+++ api/account/handlers.go
@@ -1,6 +1,8 @@
 package account
 
 import (
+	"net/url"
+
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.com/middleware"
@@ -14,7 +16,14 @@ func Route(router router.Router, args ...gin.HandlerFunc) {
 	namespaced := middleware.Namespace()
 
 	api := router.Group("account")
-	api.Use(middleware.AccessControl("*"), publishedRequired)
+	api.Use(
+		func(c *gin.Context) {
+			domain, _ := url.Parse(c.Request.Referer())
+			origin := domain.Scheme + "://" + domain.Host
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		},
+		publishedRequired)
 
 	api.GET("", publishedRequired, accountRequired, namespaced, get)
 	api.PUT("", publishedRequired, accountRequired, namespaced, update)
