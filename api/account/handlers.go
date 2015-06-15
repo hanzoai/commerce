1daa1a5afbf7dc91d044f4aa69e0851f6324af6c
David Tai <antisocialmunky@gmail.com>
2015-06-14 20:02:17 -0700

    access origin header change uflly reverted

--- api/account/handlers.go
+++ api/account/handlers.go
@@ -1,8 +1,6 @@
 package account
 
 import (
-	"net/url"
-
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.com/middleware"
@@ -16,14 +14,7 @@ func Route(router router.Router, args ...gin.HandlerFunc) {
 	namespaced := middleware.Namespace()
 
 	api := router.Group("account")
-	api.Use(
-		func(c *gin.Context) {
-			domain, _ := url.Parse(c.Request.Referer())
-			origin := domain.Scheme + "://" + domain.Host
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		},
-		publishedRequired)
+	api.Use(publishedRequired)
 
 	api.GET("", publishedRequired, accountRequired, namespaced, get)
 	api.PUT("", publishedRequired, accountRequired, namespaced, update)
