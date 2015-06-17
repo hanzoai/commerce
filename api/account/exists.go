da7e8c00638ca2d888d0b46842e75ba03b9353a3
zeekay <zk@monoid.io>
2015-06-17 16:25:23 -0700

    Further split up account API stuff.

---
+++ api/account/exists.go
@@ -0,0 +1,28 @@
+package account
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/middleware"
+	"crowdstart.com/models/user"
+	"crowdstart.com/util/json/http"
+)
+
+func exists(c *gin.Context) {
+	org := middleware.GetOrganization(c)
+	db := datastore.New(org.Namespace(c))
+	usr := user.New(db)
+
+	query := c.Request.URL.Query()
+	email := query.Get("email")
+
+	if err := usr.GetByEmail(email); err == nil {
+		http.Fail(c, 400, "Email is in use", errors.New("Email is in use"))
+		return
+	}
+
+	http.Render(c, 200, gin.H{"status": "ok"})
+}
