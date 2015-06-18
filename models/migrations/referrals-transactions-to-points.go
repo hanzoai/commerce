c9a76bcfe86fdb13a31d2b8cdd6b6400fc3f662c
David Tai <david@verus.io>
2015-06-18 02:15:11 -0500

    migrations to add points and change usd to points

---
+++ models/migrations/referrals-transactions-to-points.go
@@ -0,0 +1,35 @@
+package migrations
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/referrer"
+	"crowdstart.com/models/transaction"
+	"crowdstart.com/models/types/currency"
+
+	ds "crowdstart.com/datastore"
+)
+
+var _ = New("referrals-transactions-to-points",
+	func(c *gin.Context) []interface{} {
+		c.Set("namespace", "bellabeat")
+
+		db := ds.New(c)
+		org := organization.New(db)
+		if _, err := org.Query().Filter("Name=", "bellabeat").First(); err != nil {
+			panic(err)
+		}
+		return NoArgs
+	},
+	func(db *ds.Datastore, ref *referrer.Referrer) {
+		for i, _ := range ref.Program.Actions {
+			ref.Program.Actions[i].Currency = currency.PNT
+		}
+		ref.Put()
+	},
+	func(db *ds.Datastore, trans *transaction.Transaction) {
+		trans.Currency = currency.PNT
+		trans.Put()
+	},
+)
