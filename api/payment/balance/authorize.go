7a7e5b040f684714248f48b28f702aaf26b500e2
David Tai <david@verus.io>
2015-06-18 02:59:10 -0500

    fixed store, fixed credit transaction liveness

--- api/payment/balance/authorize.go
+++ api/payment/balance/authorize.go
@@ -13,6 +13,7 @@ var InsufficientCredit = errors.New("Insufficient credit")
 
 func Authorize(org *organization.Organization, ord *order.Order, usr *user.User, pay *payment.Payment) error {
 	pay.Type = payment.Credit
+	pay.Live = true
 
 	if err := usr.CalculateBalances(); err != nil {
 		return err
