7b1884f237e9bf6932624d360cba7aba67126cb9
zeekay <zk@monoid.io>
2015-06-18 18:05:57 -0700

    Update migration.

--- models/migrations/collapse-users.go
+++ models/migrations/collapse-users.go
@@ -21,14 +21,14 @@ var _ = New("collapse-users",
 		// Look up user for this order
 		usr := user.New(db)
 		if err := usr.GetById(id); err != nil {
-			log.Warning("Failed to query for user: %v", id)
+			log.Warn("Failed to query for user: %v", id)
 			return
 		}
 
 		// Try to find newest instance of a user with this email
 		usr2 := user.New(db)
-		if err := usr2.Query().Order("-CreatedAt").Filter("Email=", usr.Email).First(); err != nil {
-			log.Warning("Failed to query for newest user: %v", usr)
+		if ok, err := usr2.Query().Order("-CreatedAt").Filter("Email=", usr.Email).First(); !ok || err != nil {
+			log.Warn("Failed to query for newest user: %v", usr)
 			return
 		}
 
