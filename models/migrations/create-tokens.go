6a2bcb18246a6c290cca37c2f20f480a065988a8
zeekay <zk@monoid.io>
2015-06-19 09:35:08 -0700

    Create tokens for Bellabeat users.

---
+++ models/migrations/create-tokens.go
@@ -0,0 +1,27 @@
+package migrations
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/models/token"
+	"crowdstart.com/models/user"
+
+	ds "crowdstart.com/datastore"
+)
+
+var _ = New("create-tokens",
+	func(c *gin.Context) []interface{} {
+		c.Set("namespace", "bellabeat")
+		return NoArgs
+	},
+	func(db *ds.Datastore, usr *user.User) {
+		ctx := db.Context
+
+		tok := token.New(usr.Db)
+		tok.Email = usr.Email
+		tok.UserId = usr.Id()
+		tok.Expires = time.Now().Add(time.Hour * 168)
+	},
+)
