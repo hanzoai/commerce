8ac4ba51e805effe51bacd3c33ebaddc8b3a23ba
David Tai <david@verus.io>
2015-06-19 21:06:40 -0500

    referral/referrer migrations

---
+++ models/migrations/set-userid-for-referrals.go
@@ -0,0 +1,39 @@
+package migrations
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/models/order"
+	"crowdstart.com/models/referral"
+	"crowdstart.com/util/log"
+
+	ds "crowdstart.com/datastore"
+)
+
+var _ = New("set-userid-for-referrals",
+	func(c *gin.Context) []interface{} {
+		c.Set("namespace", "bellabeat")
+		return NoArgs
+	},
+	func(db *ds.Datastore, refl *referral.Referral) {
+		ctx := db.Context
+		orderId := refl.OrderId
+
+		// Look up order referenced by referral
+		ord := order.New(db)
+		if err := ord.GetById(orderId); err != nil {
+			log.Error("Failed to query for order: %v\n%v", orderId, err, ctx)
+			return
+		}
+
+		// Same user, just return
+		if ord.UserId == refl.UserId {
+			log.Debug("Same user")
+			return
+		}
+
+		// Update the user id on the referrer
+		refl.UserId = ord.UserId
+		refl.Put()
+	},
+)
