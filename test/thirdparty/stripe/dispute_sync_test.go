a2402c443b8238a86d44f4a3c87cdd981c72978d
Marvel Mathew <marvel@verus.io>
2015-06-22 18:36:06 -0500

    Add tests for other cases of dispute.Status

--- test/thirdparty/stripe/dispute_sync_test.go
+++ test/thirdparty/stripe/dispute_sync_test.go
@@ -1,7 +1,7 @@
 package test
 
 import (
-	"github.com/stripe/stripe-go/dispute"
+	sdispute "github.com/stripe/stripe-go/dispute"
 
 	"crowdstart.com/models/payment"
 	"crowdstart.com/thirdparty/stripe"
@@ -9,17 +9,46 @@ import (
 	. "crowdstart.com/util/test/ginkgo"
 )
 
+// Ginkgo doesn't understand string enums. As a result, `pay.Status`
+// needs to be cast to string.
 var _ = Describe("thirdparty.stripe.UpdatePaymentFromDispute", func() {
 	var construct = func() (*payment.Payment, *stripe.Dispute) {
 		return new(payment.Payment), new(stripe.Dispute)
 	}
 
 	Context("When a dispute is won", func() {
-		pay, dis := construct()
-		dis.Status = dispute.Won
+		pay, dispute := construct()
+		dispute.Status = sdispute.Won
 		It("should mark the payment as Paid", func() {
-			tasks.UpdatePaymentFromDispute(pay, dis)
-			Expect(pay.Status == payment.Paid)
+			tasks.UpdatePaymentFromDispute(pay, dispute)
+			Expect(string(pay.Status)).To(Equal(payment.Paid))
+		})
+	})
+
+	Context("When the charge of a dispute is refunded", func() {
+		pay, dispute := construct()
+		dispute.Status = sdispute.ChargeRefunded
+		It("should mark the payment as Refunded", func() {
+			tasks.UpdatePaymentFromDispute(pay, dispute)
+			Expect(string(pay.Status)).To(Equal(payment.Refunded))
+		})
+	})
+
+	Context("When a dispute is lost (or anything else)", func() {
+		It("should mark the payment as Disputed", func() {
+			pay, dispute := construct()
+			dispute.Status = sdispute.Lost
+
+			tasks.UpdatePaymentFromDispute(pay, dispute)
+			Expect(string(pay.Status)).To(Equal(payment.Disputed))
+		})
+
+		It("should mark the payment as Disputed", func() {
+			pay, dispute := construct()
+			dispute.Status = sdispute.Review
+
+			tasks.UpdatePaymentFromDispute(pay, dispute)
+			Expect(string(pay.Status)).To(Equal(payment.Disputed))
 		})
 	})
 })
