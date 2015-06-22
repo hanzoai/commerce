72599f463682b645b205bcb01990723856cda1a1
Marvel Mathew <marvel@verus.io>
2015-06-21 23:00:29 -0500

    Split up tasks.

---
+++ thirdparty/stripe/tasks/synccharges.go
@@ -0,0 +1,59 @@
+package tasks
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	sg "github.com/stripe/stripe-go"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/thirdparty/stripe"
+	"crowdstart.com/util/log"
+	"crowdstart.com/util/task"
+)
+
+var SyncCharges = task.Func("stripe-sync-charges", func(c *gin.Context) {
+	db := datastore.New(c)
+	org := organization.New(db)
+	ctx := org.Context()
+
+	// Get organization off query
+	query := c.Request.URL.Query()
+	orgname := query.Get("organization")
+	test := query.Get("test")
+
+	// Lookup organization
+	if err := org.GetById(orgname); err != nil {
+		log.Error("Unable to find organization(%s). %#v", orgname, err, c)
+		return
+	}
+
+	// Create stripe client for this organization
+	client := stripe.New(ctx, org.Stripe.AccessToken)
+
+	// Get all stripe charges
+	params := &sg.ChargeListParams{}
+	if test == "1" || test == "true" {
+		params.Filters.AddFilter("include[]", "", "total_count")
+		params.Filters.AddFilter("limit", "", "10")
+		params.Single = true
+	}
+
+	// Get namespace to use for later queries
+	ns := org.Name
+
+	i := client.Charges.List(params)
+	for i.Next() {
+		// Get next charge
+		ch := stripe.Charge(*i.Charge())
+
+		// Update payment, using the namespaced context (i hope)
+		start := time.Now()
+		UpdatePayment.Call(ctx, ns, ch, start)
+	}
+
+	if err := i.Err(); err != nil {
+		log.Error("Error while iterating over charges. %#v", err, ctx)
+	}
+})
---
+++ thirdparty/stripe/tasks/updatepayment.go
@@ -0,0 +1,63 @@
+package tasks
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"appengine"
+	"appengine/delay"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/payment"
+	"crowdstart.com/thirdparty/stripe"
+	"crowdstart.com/util/log"
+)
+
+var UpdatePayment = delay.Func("stripe-update-payment", func(ctx appengine.Context, ns string, ch stripe.Charge, start time.Time) {
+	ctx = getNamespace(ctx, ns)
+
+	key, err := getAncestor(ctx, ch)
+	if err != nil {
+		log.Panic("Unable to find payment matching charge: %s, %v", ch.ID, err, ctx)
+	}
+
+	db := datastore.New(ctx)
+	pay := payment.New(db)
+
+	err = pay.RunInTransaction(func() error {
+		// Query by ancestor so we can use a transaction
+		if ok, err := pay.Query().Ancestor(key).Filter("Account.ChargeId=", ch.ID).First(); !ok {
+			return errors.New(fmt.Sprintf("Unable to retrieve payment for charge (%s), ancestor, (%v):", ch.ID, key, err))
+		}
+
+		// Bail out if someone has updated payment since us
+		if start.Before(pay.UpdatedAt) {
+			log.Info(`The Payment(%s) associated with Charge(%s) has already been updated.
+					  Stopping 'stripe-update-payment' task.`, pay.Id(), ch.ID, ctx)
+			return nil
+		}
+
+		// Update status
+		if ch.Captured {
+			pay.Status = payment.Paid
+		} else if ch.Refunded {
+			pay.Status = payment.Refunded
+		} else if ch.Paid {
+			pay.Status = payment.Paid
+		} else {
+			pay.Status = payment.Unpaid
+		}
+
+		// Save updated payment
+		return pay.Put()
+	})
+
+	// Panic so we restart if something failed
+	if err != nil {
+		log.Panic("Error updating payment (%s): %v", pay.Id(), err, ctx)
+	}
+
+	// Update order
+	updateOrder.Call(ctx, ns, pay.OrderId, start)
+})
