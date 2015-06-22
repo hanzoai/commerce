db397f73b55080374c67c73160d016bc32b98e7e
Marvel Mathew <marvel@verus.io>
2015-06-21 21:17:12 -0500

    That was rather involved.

--- thirdparty/stripe/tasks/tasks.go
+++ thirdparty/stripe/tasks/tasks.go
@@ -1,9 +1,12 @@
 package tasks
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"appengine"
+	aeds "appengine/datastore"
 	"appengine/delay"
 
 	"github.com/gin-gonic/gin"
@@ -14,6 +17,7 @@ import (
 	"crowdstart.com/models/organization"
 	"crowdstart.com/models/payment"
 	"crowdstart.com/thirdparty/stripe"
+	"crowdstart.com/util/hashid"
 	"crowdstart.com/util/log"
 	"crowdstart.com/util/task"
 )
@@ -38,21 +42,59 @@ var updateOrder = delay.Func("stripe-update-order", func(ctx appengine.Context, 
 	}
 })
 
-var UpdatePayment = delay.Func("stripe-update-payment", func(c appengine.Context, ch stripe.Charge, start time.Time) {
-	db := datastore.New(c)
+func getAncestor(ctx appengine.Context, ch stripe.Charge) (*aeds.Key, error) {
+	// Try to user order id if possible
+	if id, ok := ch.Meta["order"]; ok {
+		return hashid.DecodeKey(ctx, id)
+	}
+
+	// Try to lookup payment
+	db := datastore.New(ctx)
 	pay := payment.New(db)
 
-	err := pay.RunInTransaction(func() error {
-		if ok, err := pay.Query().Filter("Account.ChargeId=", ch.ID).First(); !ok {
-			log.Panic("Unable to find payment matching charge: %s", ch.ID, err, c)
+	id, ok := ch.Meta["payment"]
+
+	// Try to get by payment id
+	var err error
+	if ok {
+		err = pay.Get(id)
+	}
+
+	// Lookup by charge id
+	if !ok || err != nil {
+		_, err = pay.Query().Filter("Account.ChargeId=", ch.ID).First()
+	}
+
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Unable to lookup payment by id (%s) or charge id (%s): %v", id, ch.ID, err, ctx))
+	}
+
+	return hashid.DecodeKey(ctx, pay.OrderId)
+}
+
+var UpdatePayment = delay.Func("stripe-update-payment", func(ctx appengine.Context, ch stripe.Charge, start time.Time) {
+	key, err := getAncestor(ctx, ch)
+	if err != nil {
+		log.Panic("Unable to find payment matching charge: %s", ch.ID, err, ctx)
+	}
+
+	db := datastore.New(ctx)
+	pay := payment.New(db)
+
+	err = pay.RunInTransaction(func() error {
+		// Query by ancestor so we can use a transaction
+		if ok, err := pay.Query().Ancestor(key).Filter("Account.ChargeId=", ch.ID).First(); !ok {
+			return errors.New(fmt.Sprintf("Unable to retrieve payment for charge (%s), ancestor, (%v):", ch.ID, key, err))
 		}
 
+		// Bail out if someone has updated payment since us
 		if start.Before(pay.UpdatedAt) {
 			log.Info(`The Payment(%s) associated with Charge(%s) has already been updated.
-					  Stopping 'stripe-update-payment' task.`, pay.Id(), ch.ID, c)
+					  Stopping 'stripe-update-payment' task.`, pay.Id(), ch.ID, ctx)
 			return nil
 		}
 
+		// Update status
 		if ch.Captured {
 			pay.Status = payment.Paid
 		} else if ch.Refunded {
@@ -63,14 +105,17 @@ var UpdatePayment = delay.Func("stripe-update-payment", func(c appengine.Context
 			pay.Status = payment.Unpaid
 		}
 
+		// Save updated payment
 		return pay.Put()
 	})
 
+	// Panic so we restart if something failed
 	if err != nil {
-		log.Panic("Error updating Payment(%s) in 'stripe-update-payment'. %#v", pay.Id(), err, c)
+		log.Panic("Error updating payment (%s): %v", pay.Id(), err, ctx)
 	}
 
-	updateOrder.Call(c, pay.OrderId)
+	// Update order
+	updateOrder.Call(ctx, pay.OrderId)
 })
 
 var UpdateDisputedPayment = delay.Func("stripe-update-disputed-payment", func(ctx appengine.Context, dispute stripe.Dispute, start time.Time) {
