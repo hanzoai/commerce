08f44734b4484ebbbf1ccb4eaed936af8dffe55d
Marvel Mathew <marvel@verus.io>
2015-06-23 19:20:54 -0500

    Add API endpoint for last order time.

---
+++ api/order/lastorder.go
@@ -0,0 +1,123 @@
+package order
+
+import (
+	"errors"
+	"strconv"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/memcache"
+
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/middleware"
+	"crowdstart.com/models"
+	"crowdstart.com/util/json"
+	"crowdstart.com/util/log"
+)
+
+type Cached struct {
+	Cursor string
+	Order  *models.Order
+}
+
+// Get cached cursor/order
+func getCached(ctx appengine.Context) (datastore.Cursor, *models.Order, error) {
+	var cur datastore.Cursor
+	ord := new(models.Order)
+	cached := new(Cached)
+
+	// Fetch cached items
+	if item, err := memcache.Get(ctx, "cached_order"); err != nil {
+		return cur, ord, errors.New("Unable to get cursor")
+	} else {
+		err := json.DecodeBytes(item.Value, cached)
+		if err != nil {
+			return cur, ord, err
+		}
+	}
+
+	// Use cached order
+	ord = cached.Order
+
+	// Try to decode cursor
+	cur, err := datastore.DecodeCursor(cached.Cursor)
+	if err != nil {
+		return cur, ord, errors.New("Unable to decode cursor")
+	}
+
+	return cur, ord, nil
+}
+
+// Update cached cursor/order
+func setCached(ctx appengine.Context, cursor datastore.Cursor, ord *models.Order) error {
+	return memcache.Set(ctx, &memcache.Item{
+		Key:   "cached_order",
+		Value: json.EncodeBytes(&Cached{cursor.String(), ord}),
+	})
+}
+
+// Get most recent order from datastore
+func getLastOrder(ctx appengine.Context) (datastore.Cursor, *models.Order, error) {
+	var cur datastore.Cursor
+	ord := new(models.Order)
+	count, err := datastore.NewQuery("order").Count(ctx)
+	if err != nil {
+		return cur, ord, errors.New("Unable to get count for orders")
+	}
+	q := datastore.NewQuery("order").Order("CreatedAt").Offset(count - 1)
+	it := q.Run(ctx)
+	_, err = it.Next(ord)
+	if err != nil {
+		return cur, ord, errors.New("Unable to query next order")
+	}
+	cur, err = it.Cursor()
+	return cur, ord, err
+}
+
+func LastOrder(c *gin.Context) {
+	ctx := middleware.GetAppEngine(c)
+
+	// Try to get cached cursor, order
+	log.Debug("Get cached order")
+	cur, ord, err := getCached(ctx)
+
+	// First run
+	if err != nil {
+		log.Debug("Get last order the hard way")
+		cur, ord, err = getLastOrder(ctx)
+		if err != nil {
+			c.Fail(500, errors.New("Failed to retrieve last order"))
+		}
+		setCached(ctx, cur, ord)
+		c.String(200, strconv.FormatInt(ord.CreatedAt.Unix(), 10))
+		return
+	}
+
+	// Subsequent runs, re-use cursor
+	q := datastore.NewQuery("order").Start(cur)
+
+	// Create iterator and return latest results
+	neword := new(models.Order)
+	it := q.Run(ctx)
+
+	// Iterate over all orders until we hit the last
+	log.Debug("Iterate over orders")
+
+	for {
+		log.Debug("Got next order")
+		if _, err := it.Next(neword); err != nil {
+			break
+		}
+	}
+
+	// Update cache if we found a newer order
+	if neword.CreatedAt.After(ord.CreatedAt) {
+		log.Debug("Got newer order")
+		ord = neword
+		// Set cache
+		setCached(ctx, cur, ord)
+	}
+
+	c.String(200, strconv.FormatInt(ord.CreatedAt.Unix(), 10))
+}
--- util/json/json.go
+++ util/json/json.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+
+	"appengine"
 )
 
 func Encode(value interface{}) string {
@@ -22,6 +24,30 @@ func Decode(body io.ReadCloser, v interface{}) error {
 		return err
 	}
 	err = json.Unmarshal(content, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func EncodeBytes(value interface{}) []byte {
+	var b []byte
+	var err error
+
+	if appengine.IsDevAppServer() {
+		b, err = json.MarshalIndent(value, "", "  ")
+	} else {
+		b, err = json.Marshal(value)
+	}
+
+	if err != nil {
+		fmt.Println("%v", err)
+	}
+	return b
+}
+
+func DecodeBytes(data []byte, v interface{}) error {
+	err := json.Unmarshal(data, v)
 	if err != nil {
 		return err
 	}
