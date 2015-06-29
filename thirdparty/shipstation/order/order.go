9569204e89a1582bbb2bc180f1c76fb71f66a5e4
zeekay <zk@verus.io>
2015-06-29 15:25:56 -0700

    Add shipstation integration.

---
+++ thirdparty/shipstation/order/order.go
@@ -0,0 +1,121 @@
+package order
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+
+	stripe "crowdstart.com/thirdparty/stripe/models"
+	"crowdstart.com/util/log"
+)
+
+// <?xml version="1.0" encoding="utf-8"?>
+// <Orders>
+// 	<Order>
+// 		<OrderID><![CDATA[123456]]></OrderID>
+// 		<OrderNumber><![CDATA[ABC123]]></OrderNumber>
+// 		<OrderDate>12/8/2011 21:56 PM</OrderDate>
+// 		<OrderStatus><![CDATA[AwaitingShipment]]></OrderStatus>
+// 		<LastModified>12/8/2011 12:56 PM</LastModified>
+// 		<ShippingMethod><![CDATA[USPSPriorityMail]]></ShippingMethod>
+// 		<PaymentMethod><![CDATA[Credit Card]]></PaymentMethod>
+// 		<OrderTotal>123.45</OrderTotal>
+// 		<TaxAmount>0.00</TaxAmount>
+// 		<ShippingAmount>4.50</ShippingAmount>
+// 		<CustomerNotes><![CDATA[Please make sure it gets here by Dec. 22nd!]]></CustomerNotes>
+// 		<InternalNotes><![CDATA[Ship by December 18th via Priority Mail.]]></InternalNotes>
+// 		<Customer>
+// 			<CustomerCode><![CDATA[dev@hanzo.ai]]></CustomerCode>
+// 			<BillTo>
+// 				<Name><![CDATA[The President]]></Name>
+// 				<Company><![CDATA[US Govt]]></Company>
+// 				<Phone><![CDATA[512-555-5555]]></Phone>
+// 				<Email><![CDATA[dev@hanzo.ai]]></Email>
+// 			</BillTo>
+// 			<ShipTo>
+// 				<Name><![CDATA[The President]]></Name>
+// 				<Company><![CDATA[US Govt]]></Company>
+// 				<Address1><![CDATA[1600 Pennsylvania Ave]]></Address1>
+// 				<Address2></Address2>
+// 				<City><![CDATA[Washington]]></City>
+// 				<State><![CDATA[DC]]></State>
+// 				<PostalCode><![CDATA[20500]]></PostalCode>
+// 				<Country><![CDATA[US]]></Country>
+// 				<Phone><![CDATA[512-555-5555]]></Phone>
+// 			</ShipTo>
+// 		</Customer>
+// 		<Items>
+// 			<Item>
+// 				<SKU><![CDATA[FD88821]]></SKU>
+// 				<Name><![CDATA[My Product Name]]></Name>
+// 				<ImageUrl><![CDATA[http://www.mystore.com/products/12345.jpg]]></ImageUrl>
+// 				<Weight>8</Weight>
+// 				<WeightUnits>Ounces</WeightUnits>
+// 				<Quantity>2</Quantity>
+// 				<UnitPrice>13.99</UnitPrice>
+// 				<Location><![CDATA[A1-B2]]></Location>
+// 				<Options>
+// 					<Option>
+// 						<Name><![CDATA[Size]]></Name>
+// 						<Value><![CDATA[Large]]></Value>
+// 						<Weight>10</Weight>
+// 					</Option>
+// 					<Option>
+// 						<Name><![CDATA[Color]]></Name>
+// 						<Value><![CDATA[Green]]></Value>
+// 						<Weight>5</Weight>
+// 					</Option>
+// 				</Options>
+// 			</Item>
+// 		</Items>
+// 	</Order>
+// </Orders>
+
+type Order struct {
+	OrderID string
+	OrderNumber string
+	OrderDate string
+	OrderStatus string
+	LastModified string
+	ShippingMethod string
+	PaymentMethod string
+	OrderTotal string
+	TaxAmount string
+	ShippingAmount string
+	CustomerNotes string
+	InternalNotes string
+	Customer struct {
+		CustomerCode string
+		BillTo struct {
+			Name string
+			Company string
+			Phone string
+			Email string
+		}
+		ShipTo struct {
+			Name string
+			Company string
+			Address1 string
+			Address2 string
+			City string
+			State string
+			PostalCode string
+			Country string
+			Phone string
+		}
+	}
+	Items Item[]
+
+
+}
+
+type Response struct {
+	Orders []Order
+}
+
+func Get(c *gin.Context) {
+
+	query := c.Request.URL.Query()
+
+}
