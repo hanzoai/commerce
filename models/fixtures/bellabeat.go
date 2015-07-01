45a722e33c63cb2fee164a6bc8ccbc7d11cca5a2
zeekay <zk@verus.io>
2015-06-30 17:57:15 -0700

    Fixture to add API user for ShipStation.

--- models/fixtures/bellabeat.go
+++ models/fixtures/bellabeat.go
@@ -3,10 +3,10 @@ package fixtures
 import (
 	"github.com/gin-gonic/gin"
 
+	"crowdstart.com/auth/password"
 	"crowdstart.com/datastore"
-	"crowdstart.com/models/namespace"
 	"crowdstart.com/models/organization"
-	"crowdstart.com/util/log"
+	"crowdstart.com/models/user"
 )
 
 var Bellabeat = New("bellabeat", func(c *gin.Context) *organization.Organization {
@@ -16,14 +16,18 @@ var Bellabeat = New("bellabeat", func(c *gin.Context) *organization.Organization
 	org.Name = "bellabeat"
 	org.GetOrCreate("Name=", org.Name)
 
-	// u := user.New(db)
-	// u.Email = "sandro@bellabeat.com"
-	// u.GetOrCreate("Email=", u.Email)
-	// u.FirstName = "Sandro"
-	// u.LastName = "Mur"
-	// u.Organizations = []string{org.Id()}
-	// u.PasswordHash, _ = password.Hash("bellabeatpassword!")
-	// u.Put()
+	u := user.New(db)
+	u.Email = "bellabeat-shipstation@crowdstart.com"
+	u.GetOrCreate("Email=", u.Email)
+	u.FirstName = "Shipstation"
+	u.LastName = "API"
+	u.Organizations = []string{org.Id()}
+	u.PasswordHash, _ = password.Hash("xvMQrMv2c5dCFbVG")
+
+	u.Put()
+
+	org.Admins = append(org.Admins, u.Id())
+	org.Put()
 
 	// u2 := user.New(db)
 	// u2.Email = "marko@bellabeat.com"
@@ -62,37 +66,37 @@ var Bellabeat = New("bellabeat", func(c *gin.Context) *organization.Organization
 	// // Email configuration
 	// org.Mandrill.APIKey = ""
 
-	org.Email.Defaults.Enabled = true
-	org.Email.Defaults.FromName = "Bellabeat"
-	org.Email.Defaults.FromEmail = "hi@bellabeat.com"
+	// org.Email.Defaults.Enabled = true
+	// org.Email.Defaults.FromName = "Bellabeat"
+	// org.Email.Defaults.FromEmail = "hi@bellabeat.com"
 
-	org.Email.OrderConfirmation.Subject = "LEAF Order Confirmation"
-	org.Email.OrderConfirmation.Template = readEmailTemplate("/resources/bellabeat/emails/order-confirmation.html")
-	org.Email.OrderConfirmation.Enabled = true
+	// org.Email.OrderConfirmation.Subject = "LEAF Order Confirmation"
+	// org.Email.OrderConfirmation.Template = readEmailTemplate("/resources/bellabeat/emails/order-confirmation.html")
+	// org.Email.OrderConfirmation.Enabled = true
 
-	org.Email.User.PasswordReset.Template = readEmailTemplate("/resources/bellabeat/emails/user-password-reset.html")
-	org.Email.User.PasswordReset.Subject = "Reset your Bellabeat password"
-	org.Email.User.PasswordReset.Enabled = true
+	// org.Email.User.PasswordReset.Template = readEmailTemplate("/resources/bellabeat/emails/user-password-reset.html")
+	// org.Email.User.PasswordReset.Subject = "Reset your Bellabeat password"
+	// org.Email.User.PasswordReset.Enabled = true
 
-	org.Email.User.EmailConfirmation.Template = readEmailTemplate("/resources/bellabeat/emails/user-email-confirmation.html")
-	org.Email.User.EmailConfirmation.Subject = "Please confirm your email"
-	org.Email.User.EmailConfirmation.Enabled = true
+	// org.Email.User.EmailConfirmation.Template = readEmailTemplate("/resources/bellabeat/emails/user-email-confirmation.html")
+	// org.Email.User.EmailConfirmation.Subject = "Please confirm your email"
+	// org.Email.User.EmailConfirmation.Enabled = true
 
 	// org.Email.User.EmailConfirmed.Subject = "Thank you for confirming your email"
 	// org.Email.User.EmailConfirmed.Template = readEmailTemplate("/resources/bellabeat/emails/user-email-confirmed.html")
 	// org.Email.User.EmailConfirmed.Enabled = true
 
 	// // Save org into default namespace
-	org.Put()
+	// org.Put()
 
-	// Save namespace so we can decode keys for this organization later
-	ns := namespace.New(db)
-	ns.Name = org.Name
-	ns.IntId = org.Key().IntID()
-	err := ns.Put()
-	if err != nil {
-		log.Warn("Failed to put namespace: %v", err)
-	}
+	// 	// Save namespace so we can decode keys for this organization later
+	// 	ns := namespace.New(db)
+	// 	ns.Name = org.Name
+	// 	ns.IntId = org.Key().IntID()
+	// 	err := ns.Put()
+	// 	if err != nil {
+	// 		log.Warn("Failed to put namespace: %v", err)
+	// 	}
 
 	return org
 })
