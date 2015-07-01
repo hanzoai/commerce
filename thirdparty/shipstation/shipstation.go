0bf90ba3ee47c1132ac18bbadeab544a1ff2f675
zeekay <zk@verus.io>
2015-06-30 17:21:04 -0700

    Use Basic Auth with Shipstation integration.

--- thirdparty/shipstation/shipstation.go
+++ thirdparty/shipstation/shipstation.go
@@ -1,23 +1,43 @@
 package shipstation
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 
+	"crowdstart.com/auth"
+	"crowdstart.com/datastore"
 	"crowdstart.com/middleware"
+	"crowdstart.com/models/organization"
 	"crowdstart.com/thirdparty/shipstation/export"
 	"crowdstart.com/thirdparty/shipstation/shipnotify"
-	"crowdstart.com/util/permission"
 	"crowdstart.com/util/router"
 )
+
+func setOrg(c *gin.Context) {
+	db := datastore.New(c)
+	org := organization.New(db)
+	if err := org.GetById(c.Params.ByName("organization")); err != nil {
+		log.Panic("Unable to get current user", c)
+	}
+
+	userid, err := auth.GetCurrentUserId(c)
+	if err != nil {
+		log.Panic("Unable to get current user", c)
+	}
+
+	if !(org.IsAdminId(userid) || org.IsOwnerId(userid)) {
+		log.Panic("Not a valid admin/owner for this organization", c)
+	}
+
+	c.Set("organization", org)
+}
 
 func Route(router router.Router, args ...gin.HandlerFunc) {
 	api := router.Group("shipstation")
 
-	api.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	})
-	adminRequired := middleware.TokenRequired(permission.Admin)
+	basicAuth := middleware.BasicAuth()
 
-	api.GET("", adminRequired, export.Export)
-	api.POST("", adminRequired, shipnotify.ShipNotify)
+	api.GET("/:organization", basicAuth, setOrg, export.Export)
+	api.POST("/:organization", basicAuth, setOrg, shipnotify.ShipNotify)
 }
