6eb08ee404e773aa433955b2ddeb056ec6d6be41
zeekay <zk@verus.io>
2015-07-03 15:53:08 -0700

    Can only write 500 entities at once.

--- models/migrations/dedupe-payments-3.go
+++ models/migrations/dedupe-payments-3.go
@@ -12,7 +12,7 @@ var _ = New("dedupe-payments-3", func(c *gin.Context) []interface{} {
 	db := datastore.New(c)
 	db.SetNamespace("bellabeat")
 
-	keys, err := payment.Query(db).Filter("Deleted=", true).KeysOnly().Limit(10000).GetAll(nil)
+	keys, err := payment.Query(db).Filter("Deleted=", true).KeysOnly().Limit(500).GetAll(nil)
 	if err != nil {
 		log.Panic("Failed to get keys for deleted payments: %v", err, c)
 	}
