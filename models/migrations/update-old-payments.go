f788ee817cc354d46a8ff5f539e71334c963e148
zeekay <zk@verus.io>
2015-07-03 02:54:44 -0700

    Update old-payments migration.

--- models/migrations/update-old-payments.go
+++ models/migrations/update-old-payments.go
@@ -33,6 +33,8 @@ var _ = New("update-old-payments",
 	func(db *ds.Datastore, pay *payment.Payment) {
 		var payments []*payment.Payment
 
+		ctx := db.Context
+
 		keys, err := payment.Query(db).Filter("Account.ChargeId=", pay.Account.ChargeId).GetAll(&payments)
 		if err != nil {
 			log.Warn("Unable to query out payments: %v", err, db.Context)
@@ -41,6 +43,24 @@ var _ = New("update-old-payments",
 
 		// Singular payment! Hooray
 		if len(payments) == 1 {
+			// MAKE SURE IT'S GOOD
+			ord := order.New(db)
+			if err := ord.Get(pay.OrderId); err != nil {
+				log.Error("We got only one payment here and it's bad: %#v", pay, ctx)
+				return
+			}
+			ord.PaymentIds = []string{pay.Id()}
+
+			// err = updateChargeFromPayment(ctx, pay)
+			// if err != nil {
+			// 	log.Error("Failed to update charge using payment: %#v", pay, ctx)
+			// 	return
+			// }
+
+			err = ord.Put()
+			if err != nil {
+				log.Error("Failed to update order: %#v", ord, ctx)
+			}
 			return
 		}
 
@@ -48,12 +68,16 @@ var _ = New("update-old-payments",
 		newest := pay
 		oldest := pay
 		for i, p := range payments {
+			// make sure we have a payment we can work with
 			p.Mixin(db, p)
 			p.SetKey(keys[i])
+
+			// Find the oldest
 			if p.CreatedAt.Before(oldest.CreatedAt) {
 				oldest = p
 			}
 
+			// Find the youngest
 			if p.CreatedAt.After(newest.CreatedAt) {
 				newest = p
 			}
@@ -64,9 +88,11 @@ var _ = New("update-old-payments",
 		err = ord.Get(newest.OrderId)
 		if err == nil {
 			// The newest order is correct
+			log.Debug("newer order is correct", db.Context)
 			oldest.Buyer.UserId = newest.Buyer.UserId
 			oldest.OrderId = newest.OrderId
 		} else {
+			log.Debug("older order is correct", db.Context)
 			ord = order.New(db)
 			err := ord.Get(oldest.OrderId)
 			if err != nil {
@@ -76,16 +102,7 @@ var _ = New("update-old-payments",
 		}
 
 		// Update order
-		seen := false
-		for _, id := range ord.PaymentIds {
-			if id == oldest.Id() {
-				seen = true
-			}
-		}
-
-		if !seen {
-			ord.PaymentIds = append(ord.PaymentIds, oldest.Id())
-		}
+		ord.PaymentIds = []string{oldest.Id()}
 
 		if err := oldest.Put(); err != nil {
 			log.Debug("Unable to update oldest payment: %v", err, db.Context)
@@ -96,9 +113,9 @@ var _ = New("update-old-payments",
 		}
 
 		// Make stripe charge match this payment
-		err = updateChargeFromPayment(db.Context, oldest)
-		if err != nil {
-			log.Error("Unable to update charge from payent: %v", err, db.Context)
-		}
+		// err = updateChargeFromPayment(db.Context, oldest)
+		// if err != nil {
+		// 	log.Error("Unable to update charge from payent: %v", err, db.Context)
+		// }
 	},
 )
