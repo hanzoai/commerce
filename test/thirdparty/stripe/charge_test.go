5db53bc31384136b3306e0a65934ed32673cb68b
zeekay <zk@verus.io>
2015-07-03 08:47:07 -0700

    Update marvel tests.

--- test/thirdparty/stripe/charge_test.go
+++ test/thirdparty/stripe/charge_test.go
@@ -15,6 +15,7 @@ var _ = Describe("thirdparty.stripe.UpdatePaymentFromCharge", func() {
 	Context("When a charge is captured", func() {
 		pay, charge := construct()
 		charge.Captured = true
+		charge.Status = "success"
 		It("should mark the payment as Paid", func() {
 			tasks.UpdatePaymentFromCharge(pay, charge)
 			Expect(string(pay.Status)).To(Equal(payment.Paid))
@@ -31,6 +32,8 @@ var _ = Describe("thirdparty.stripe.UpdatePaymentFromCharge", func() {
 	Context("When a charge is paid", func() {
 		pay, charge := construct()
 		charge.Paid = true
+		charge.Captured = true
+		charge.Status = "success"
 		It("should mark the payment as Paid", func() {
 			tasks.UpdatePaymentFromCharge(pay, charge)
 			Expect(string(pay.Status)).To(Equal(payment.Paid))
