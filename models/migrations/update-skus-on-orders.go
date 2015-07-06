3b2e46cf6262734aee3a6bd261f6217881b35e5b
David Tai <antisocialmunky@gmail.com>
2015-07-06 13:46:12 -0500

    migraiton for updating skus

---
+++ models/migrations/update-skus-on-orders.go
@@ -0,0 +1,57 @@
+package migrations
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/models/order"
+	"crowdstart.com/models/product"
+	"crowdstart.com/util/log"
+
+	ds "crowdstart.com/datastore"
+)
+
+// Cache the products
+var products = make(map[string]string)
+
+func GetProduct(db *ds.Datastore, id string) string {
+	if slug, ok := products[id]; ok {
+		return slug
+	}
+
+	prod := product.New(db)
+	if err := prod.GetById(id); err != nil {
+		log.Error(err, db.Context)
+	}
+
+	products[prod.Id()] = prod.Slug
+
+	return prod.Slug
+}
+
+var _ = New("update-skus-on-orders",
+	func(c *gin.Context) []interface{} {
+		c.Set("namespace", "bellabeat")
+
+		return NoArgs
+	},
+	func(db *ds.Datastore, ord *order.Order) {
+		items := ord.Items
+
+		update := false
+
+		for i, item := range items {
+			slug := GetProduct(db, item.ProductId)
+
+			if item.ProductSlug != slug {
+				update = true
+				items[i].ProductSlug = slug
+			}
+		}
+
+		if update {
+			if err := ord.Put(); err != nil {
+				log.Error(err, db.Context)
+			}
+		}
+	},
+)
