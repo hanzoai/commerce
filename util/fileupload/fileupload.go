559113684cdd970304245222a59cee3eecbaa2ab
David Tai <antisocialmunky@gmail.com>
2015-07-08 18:39:35 -0700

    made ord.test check trigger before all the other lookups in shipstation export

--- util/fileupload/fileupload.go
+++ util/fileupload/fileupload.go
@@ -1,1 +1,24 @@
 package fileupload
+
+// import (
+// 	"golang.org/x/net/context"
+
+// 	"appengine"
+
+// 	"golang.org/x/oauth2/google"
+// 	storage "google.golang.org/api/storage/v1"
+
+// 	"crowdstart.com/models/organization"
+// 	"crowdstart.com/util/log"
+// )
+
+// func UploadFile(c *appengine.Context, org *organization.Organization) string {
+// 	client, err := google.DefaultClient(context.Background(), scope)
+// 	if err != nil {
+// 		log.Error("Unable to get default client: %v", err, c)
+// 	}
+
+// 	service, err := storage.New(client)
+
+// 	return ""
+// }
