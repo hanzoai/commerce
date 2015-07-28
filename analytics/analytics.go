62b6d68a05aa28fce641c50c5c571c64d827faee
David Tai <antisocialmunky@gmail.com>
2015-07-28 11:02:43 -0700

    changed analytics to use org id instead of token

--- analytics/analytics.go
+++ analytics/analytics.go
@@ -4,14 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.com/middleware"
-	"crowdstart.com/util/permission"
 	"crowdstart.com/util/router"
 )
 
 func init() {
-	publishedRequired := middleware.TokenRequired(permission.Admin, permission.Published)
-	namespaced := middleware.Namespace()
-
 	analytics := router.New("analytics")
 
 	// Use permissive CORS policy for all API routes.
@@ -20,7 +16,7 @@ func init() {
 		c.Next()
 	})
 
-	analytics.GET("/native/js", publishedRequired, namespaced, js)
-	analytics.POST("/", publishedRequired, namespaced, create)
+	analytics.GET("/:orgid/native/js", js)
+	analytics.POST("/:orgid/", create)
 	analytics.HEAD("/", router.Empty)
 }
