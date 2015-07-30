e542c122258225c5dcdb327edb57485c2c1727d1
David Tai <david@verus.io>
2015-07-30 11:58:02 -0700

    polish test for polish names

--- util/searchpartial/searchpartial.go
+++ util/searchpartial/searchpartial.go
@@ -2,7 +2,7 @@ package searchpartial
 
 func Partials(full string) string {
 	fullRunes := []rune(full)
-	runes := []rune("")
+	runes := []rune(full)
 	for size := 3; size < len(fullRunes); size++ {
 		for i := 0; i <= len(fullRunes)-size; i++ {
 			runes = append(append(runes, ' '), fullRunes[i:i+size]...)
