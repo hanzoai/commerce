05ed340ffa07779067ada9d7163ed4c2f15e01d3
Zach Kelling <z@zeekay.io>
2015-08-04 05:02:11 -0700

    Render analytics bundle.

--- cdn/analytics/analytics.go
+++ cdn/analytics/analytics.go
@@ -2,12 +2,30 @@ package analytics
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 
 	"crowdstart.com/datastore"
 	"crowdstart.com/models/organization"
+	"crowdstart.com/util/fs"
 )
+
+var jsTemplate = ""
+
+func Render(org *organization.Organization) string {
+	if jsTemplate == "" {
+		var cwd, _ = os.Getwd()
+		bundleJs := string(fs.ReadFile(cwd + "/resources/analytics/bundle.js"))
+		jsTemplate = string(fs.ReadFile(cwd + "/resources/analytics/analytics.js"))
+		jsTemplate = strings.Replace(jsTemplate, "require(\"./index\")", bundleJs, 1)
+		jsTemplate = strings.Replace(jsTemplate, "e(\"./index\")", bundleJs, 1)
+		jsTemplate = strings.Replace(jsTemplate, "analytics.initialize({})", "analytics.initialize(%s)", 1)
+	}
+
+	return fmt.Sprintf(jsTemplate, org.Analytics.JSON())
+}
 
 func Js(c *gin.Context) {
 	id := c.Params.ByName("organizationid")
@@ -20,5 +38,5 @@ func Js(c *gin.Context) {
 	}
 
 	c.Writer.Header().Add("Content-Type", "application/javascript")
-	c.String(200, org.AnalyticsJs())
+	c.String(200, Render(org))
 }
