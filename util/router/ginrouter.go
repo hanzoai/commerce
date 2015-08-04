ce8962d37fd3f642acbff3a47dd101c7d32e72ed
David Tai <antisocialmunky@gmail.com>
2015-08-04 12:07:40 -0700

    updated gin

--- util/router/ginrouter.go
+++ util/router/ginrouter.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Router interface {
-	DELETE(relativePath string, handlers ...gin.HandlerFunc)
-	GET(relativePath string, handlers ...gin.HandlerFunc)
-	HEAD(relativePath string, handlers ...gin.HandlerFunc)
-	OPTIONS(relativePath string, handlers ...gin.HandlerFunc)
-	PATCH(relativePath string, handlers ...gin.HandlerFunc)
-	POST(relativePath string, handlers ...gin.HandlerFunc)
-	PUT(relativePath string, handlers ...gin.HandlerFunc)
+	DELETE(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
+	GET(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
+	HEAD(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
+	OPTIONS(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
+	PATCH(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
+	POST(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
+	PUT(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
 	Group(relativePath string, handlers ...gin.HandlerFunc) *gin.RouterGroup
-	Handle(httpMethod, relativePath string, handlers []gin.HandlerFunc)
-	Static(relativePath, root string)
-	Use(middlewares ...gin.HandlerFunc)
+	Handle(httpMethod, relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
+	Static(relativePath, root string) gin.IRoutes
+	Use(middlewares ...gin.HandlerFunc) gin.IRoutes
 }
