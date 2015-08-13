56ff19f1c9ef03cee6395ce2fa0ebd5ca99ae963
zeekay <zk@monoid.io>
2015-08-12 20:55:18 -0700

    Add analytics integration api endpoint.

---
+++ api/organization/analytics/analytics.go
@@ -0,0 +1,83 @@
+package analytics
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/types/analytics"
+	"crowdstart.com/util/json"
+	"crowdstart.com/util/json/http"
+)
+
+func Get(c *gin.Context) {
+	id := c.Params.ByName("organizationid")
+	db := datastore.New(c)
+
+	// Get organization
+	org := organization.New(db)
+	if err := org.GetById(id); err != nil {
+		http.Fail(c, 404, fmt.Sprintf("Failed to retrieve organization '%v': %v", id, err), err)
+		return
+	}
+
+	http.Render(c, 200, org.Analytics)
+}
+
+func Set(c *gin.Context) {
+	id := c.Params.ByName("organizationid")
+	db := datastore.New(c)
+
+	// Get organization
+	org := organization.New(db)
+	if err := org.GetById(id); err != nil {
+		http.Fail(c, 404, fmt.Sprintf("Failed to retrieve organization '%v': %v", id, err), err)
+		return
+	}
+
+	integrations := analytics.Analytics{}
+
+	// Decode response body for listing
+	if err := json.Decode(c.Request.Body, &integrations); err != nil {
+		http.Fail(c, 400, "Failed decode request body", err)
+		return
+	}
+
+	// Update integrations
+	org.Analytics = integrations
+
+	// Save organization
+	if err := org.Put(); err != nil {
+		http.Fail(c, 500, "Failed to save analytics integrations", err)
+	} else {
+		c.Writer.Header().Add("Location", c.Request.URL.Path)
+		http.Render(c, 201, integrations)
+	}
+}
+
+func Update(c *gin.Context) {
+	id := c.Params.ByName("organizationid")
+	db := datastore.New(c)
+
+	// Get organization
+	org := organization.New(db)
+	if err := org.GetById(id); err != nil {
+		http.Fail(c, 404, fmt.Sprintf("Failed to retrieve organization '%v': %v", id, err), err)
+		return
+	}
+
+	// Decode response body for listing
+	if err := json.Decode(c.Request.Body, &org.Analytics); err != nil {
+		http.Fail(c, 400, "Failed decode request body", err)
+		return
+	}
+
+	if err := org.Put(); err != nil {
+		http.Fail(c, 500, "Failed to save organization integrations", err)
+	} else {
+		c.Writer.Header().Add("Location", c.Request.URL.Path)
+		http.Render(c, 201, org.Analytics)
+	}
+}
