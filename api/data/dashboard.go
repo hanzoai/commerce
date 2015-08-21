4d22e9e4ee0f17a011f7a9ba46dbc65492a2ff94
David Tai <antisocialmunky@gmail.com>
2015-08-20 21:16:27 -0500

    dashboard data api endpoint

---
+++ api/data/dashboard.go
@@ -0,0 +1,22 @@
+package data
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/middleware"
+	"crowdstart.com/thirdparty/redis"
+	"crowdstart.com/util/json/http"
+)
+
+func dashboard(c *gin.Context) {
+	org := middleware.GetOrganization(c)
+
+	data, err := redis.GetDashboardData(redis.Weekly, time.Now(), org)
+	if err != nil {
+		http.Fail(c, 500, "Failed to load data", err)
+	} else {
+		http.Render(c, 200, data)
+	}
+}
