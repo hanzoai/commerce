34ae6c150853ab1cafd09cace980bd3fb6b34021
David Tai <antisocialmunky@gmail.com>
2015-08-20 20:13:48 -0500

    databoard statistics from redis done

--- thirdparty/redis/redis.go
+++ thirdparty/redis/redis.go
@@ -5,19 +5,21 @@ import (
 	"time"
 
 	"crowdstart.com/config"
-	"crowdstart.com/models/order"
 	"crowdstart.com/models/organization"
+	"crowdstart.com/models/payment"
+	"crowdstart.com/models/types/currency"
 	"crowdstart.com/util/log"
 
 	redis "gopkg.in/redis.v3"
 )
 
 var (
-	sep       string
-	salesKey  string
-	ordersKey string
-	subsKey   string
-	client    *redis.Client
+	sep            string
+	salesKey       string
+	currencySetKey string
+	ordersKey      string
+	subsKey        string
+	client         *redis.Client
 )
 
 func init() {
@@ -27,6 +29,7 @@ func init() {
 	salesKey = "sales"
 	ordersKey = "orders"
 	subsKey = "subscribers"
+	currencySetKey = "currencies"
 
 	client, err = New(config.Redis.Url, config.Redis.Password)
 	if err != nil {
@@ -34,7 +37,7 @@ func init() {
 	}
 }
 
-type TimeFunc func(t time.Time) time.Time
+type TimeFunc func(t time.Time) string
 
 func New(addr string, pw string) (*redis.Client, error) {
 	db := int64(0) // unknown db assumed to be dev
@@ -63,8 +66,13 @@ func New(addr string, pw string) (*redis.Client, error) {
 	return client, nil
 }
 
-func Hourly(t time.Time) time.Time {
-	return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location())
+func AllTime(t time.Time) string {
+	return "all"
+}
+
+func Hourly(t time.Time) string {
+	t2 := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location())
+	return strconv.FormatInt(t2.Unix(), 10)
 }
 
 func addEnvironment(key string) string {
@@ -83,70 +91,160 @@ func addEnvironment(key string) string {
 	return key + sep + env
 }
 
-func addTimestamp(key string, tf TimeFunc) string {
-	return key + sep + strconv.FormatInt(tf(time.Now()).Unix(), 10)
+func setName(org *organization.Organization, name string) string {
+	name = org.Name + sep + name
+	name = addEnvironment(name)
+
+	return name
 }
 
-func totalKey(org *organization.Organization, key string, tf TimeFunc) string {
+func totalKey(org *organization.Organization, key string, timeStamp string) string {
 	key = org.Name + sep + key
 	key = addEnvironment(key)
-	key = addTimestamp(key, tf)
+	key = key + sep + timeStamp
 
 	return key
 }
 
-func storeKey(org *organization.Organization, storeId, key string, tf TimeFunc) string {
+func storeKey(org *organization.Organization, storeId, key string, timeStamp string) string {
 	key = org.Name + sep + storeId + sep + key
 	key = addEnvironment(key)
-	key = addTimestamp(key, tf)
+	key = key + sep + timeStamp
 
 	return key
 }
 
-func subKey(org *organization.Organization, key string, tf TimeFunc) string {
+func subKey(org *organization.Organization, key string, timeStamp string) string {
 	key = org.Name + sep + subsKey + sep + key
 	key = addEnvironment(key)
-	key = addTimestamp(key, tf)
+	key = key + sep + timeStamp
 
 	return key
 }
 
-func IncrTotalSales(tf TimeFunc, org *organization.Organization, ord *order.Order) error {
-	key := totalKey(org, salesKey+sep+string(ord.Currency), tf)
-
-	log.Debug("%v incremented by %v", key, int64(ord.Total), org.Db.Context)
-
-	return client.IncrBy(key, int64(ord.Total)).Err()
+func salesKeyId(cur currency.Type) string {
+	return salesKey + sep + string(cur)
 }
 
-func IncrStoreSales(tf TimeFunc, org *organization.Organization, storeId string, ord *order.Order) error {
-	key := storeKey(org, storeId, salesKey+sep+string(ord.Currency), tf)
+func IncrTotalSales(tf TimeFunc, org *organization.Organization, pays []*payment.Payment) error {
+	var total currency.Cents
+	var currency currency.Type
 
-	log.Debug("%v incremented by %v", key, int64(ord.Total), org.Db.Context)
+	for _, pay := range pays {
+		if pay.CurrencyTransferred != "" {
+			// This is first because we care about it more :p
+			total += pay.AmountTransferred
+			if currency == "" {
+				currency = pay.CurrencyTransferred
+			} else if currency != pay.CurrencyTransferred {
+				log.Error("Multiple currencies in a single payment set should not happen", org.Db.Context)
+			}
+		} else {
+			total += pay.Amount
+			if currency == "" {
+				currency = pay.Currency
+			} else if currency != pay.Currency {
+				log.Error("Multiple currencies in a single payment set should not happen", org.Db.Context)
+			}
+		}
+	}
 
-	return client.IncrBy(key, int64(ord.Total)).Err()
+	keyId := salesKeyId(currency)
+	key := totalKey(org, keyId, tf(time.Now()))
+	log.Debug("%v incremented by %v", key, int64(total), org.Db.Context)
+	err := client.IncrBy(key, int64(total)).Err()
+	if err != nil {
+		return err
+	}
+
+	currencySet := setName(org, currencySetKey)
+	err = client.SAdd(currencySet, string(currency)).Err()
+	if err != nil {
+		return err
+	}
+
+	key = totalKey(org, keyId, AllTime(time.Now()))
+	log.Debug("%v incremented by %v", key, int64(total), org.Db.Context)
+	return client.IncrBy(key, int64(total)).Err()
+}
+
+func IncrStoreSales(tf TimeFunc, org *organization.Organization, storeId string, pays []*payment.Payment) error {
+	var total currency.Cents
+	var currency currency.Type
+
+	for _, pay := range pays {
+		// This is first because we care about it more :p
+		if pay.Type == payment.Stripe && pay.CurrencyTransferred != "" {
+			total += pay.AmountTransferred
+			if currency == "" {
+				currency = pay.CurrencyTransferred
+			} else if currency != pay.CurrencyTransferred {
+				log.Error("Multiple currencies in a single payment set should not happen", org.Db.Context)
+			}
+		} else {
+			total += pay.Amount
+			if currency == "" {
+				currency = pay.Currency
+			} else if currency != pay.Currency {
+				log.Error("Multiple currencies in a single payment set should not happen", org.Db.Context)
+			}
+		}
+	}
+
+	keyId := salesKeyId(currency)
+	key := storeKey(org, storeId, keyId, tf(time.Now()))
+	log.Debug("%v incremented by %v", key, int64(total), org.Db.Context)
+	err := client.IncrBy(key, int64(total)).Err()
+	if err != nil {
+		return err
+	}
+
+	currencySet := setName(org, currencySetKey)
+	err = client.SAdd(currencySet, string(currency)).Err()
+	if err != nil {
+		return err
+	}
+
+	key = storeKey(org, storeId, keyId, AllTime(time.Now()))
+	log.Debug("%v incremented by %v", key, int64(total), org.Db.Context)
+	return client.IncrBy(key, int64(total)).Err()
 }
 
 func IncrTotalOrders(tf TimeFunc, org *organization.Organization) error {
-	key := totalKey(org, ordersKey, tf)
-
+	key := totalKey(org, ordersKey, tf(time.Now()))
 	log.Debug("%v incremented by %v", key, 1, org.Db.Context)
+	err := client.Incr(key).Err()
+	if err != nil {
+		return err
+	}
 
+	key = totalKey(org, ordersKey, AllTime(time.Now()))
+	log.Debug("%v incremented by %v", key, 1, org.Db.Context)
 	return client.Incr(key).Err()
 }
 
 func IncrStoreOrders(tf TimeFunc, org *organization.Organization, storeId string) error {
-	key := storeKey(org, storeId, ordersKey, tf)
-
+	key := storeKey(org, storeId, ordersKey, tf(time.Now()))
 	log.Debug("%v incremented by %v", key, 1, org.Db.Context)
+	err := client.Incr(key).Err()
+	if err != nil {
+		return err
+	}
 
+	key = storeKey(org, storeId, ordersKey, AllTime(time.Now()))
+	log.Debug("%v incremented by %v", key, 1, org.Db.Context)
 	return client.Incr(key).Err()
 }
 
 func IncrSubscribers(tf TimeFunc, org *organization.Organization, mailinglistId string) error {
-	key := subKey(org, mailinglistId, tf)
-
+	key := subKey(org, mailinglistId, tf(time.Now()))
 	log.Debug("%v incremented by %v", key, 1, org.Db.Context)
+	err := client.Incr(key).Err()
+	if err != nil {
+		return err
+	}
 
+	key = subKey(org, mailinglistId, AllTime(time.Now()))
+	log.Debug("%v incremented by %v", key, 1, org.Db.Context)
 	return client.Incr(key).Err()
 }
