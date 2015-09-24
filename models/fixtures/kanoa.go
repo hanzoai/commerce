a8b014bb9e3e3bce65b3607ed39eb8249ae6813e
David Tai <david@verus.io>
2015-09-24 16:32:49 -0500

    inline email

--- models/fixtures/kanoa.go
+++ models/fixtures/kanoa.go
@@ -3,13 +3,8 @@ package fixtures
 import (
 	"github.com/gin-gonic/gin"
 
-	"crowdstart.com/auth/password"
 	"crowdstart.com/datastore"
-	"crowdstart.com/models/namespace"
 	"crowdstart.com/models/organization"
-	"crowdstart.com/models/user"
-
-	"crowdstart.com/util/log"
 )
 
 var Kanoa = New("kanoa", func(c *gin.Context) *organization.Organization {
@@ -19,21 +14,21 @@ var Kanoa = New("kanoa", func(c *gin.Context) *organization.Organization {
 	org.Name = "kanoa"
 	org.GetOrCreate("Name=", org.Name)
 
-	u := user.New(db)
-	u.Email = "cival@getkanoa.com"
-	u.GetOrCreate("Email=", u.Email)
-	u.FirstName = "Cival"
-	u.LastName = ""
-	u.Organizations = []string{org.Id()}
-	u.PasswordHash, _ = password.Hash("kanoapassword!")
-	u.Put()
+	// u := user.New(db)
+	// u.Email = "cival@getkanoa.com"
+	// u.GetOrCreate("Email=", u.Email)
+	// u.FirstName = "Cival"
+	// u.LastName = ""
+	// u.Organizations = []string{org.Id()}
+	// u.PasswordHash, _ = password.Hash("kanoapassword!")
+	// u.Put()
 
-	org.FullName = "KANOA Inc"
-	org.Owners = []string{u.Id()}
-	org.Website = "http://www.getkanoa.com"
-	org.SecretKey = []byte("EZ2E011iX2Bp5lv149N2STd1d580cU58")
-	org.AddDefaultTokens()
-	org.Fee = 0.05
+	// org.FullName = "KANOA Inc"
+	// org.Owners = []string{u.Id()}
+	// org.Website = "http://www.getkanoa.com"
+	// org.SecretKey = []byte("EZ2E011iX2Bp5lv149N2STd1d580cU58")
+	// org.AddDefaultTokens()
+	// org.Fee = 0.05
 
 	// Email configuration
 	org.Mandrill.APIKey = ""
@@ -62,13 +57,13 @@ var Kanoa = New("kanoa", func(c *gin.Context) *organization.Organization {
 	org.Put()
 
 	// Save namespace so we can decode keys for this organization later
-	ns := namespace.New(db)
-	ns.Name = org.Name
-	ns.IntId = org.Key().IntID()
-	err := ns.Put()
-	if err != nil {
-		log.Warn("Failed to put namespace: %v", err)
-	}
+	// ns := namespace.New(db)
+	// ns.Name = org.Name
+	// ns.IntId = org.Key().IntID()
+	// err := ns.Put()
+	// if err != nil {
+	// 	log.Warn("Failed to put namespace: %v", err)
+	// }
 
 	return org
 })
