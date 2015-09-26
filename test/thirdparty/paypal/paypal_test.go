2c4ae1096a1088a25ebaf4460a7709325a7984e2
David Tai <antisocialmunky@gmail.com>
2015-09-26 17:57:23 -0500

    test boiler plat for paypal

--- test/thirdparty/paypal/paypal_test.go
+++ test/thirdparty/paypal/paypal_test.go
@@ -7,6 +7,7 @@ import (
 	"crowdstart.com/models/order"
 	"crowdstart.com/models/organization"
 	"crowdstart.com/models/payment"
+	"crowdstart.com/models/types/currency"
 	"crowdstart.com/models/user"
 
 	. "crowdstart.com/util/test/ginkgo"
@@ -28,9 +29,27 @@ var _ = BeforeSuite(func() {
 	db := datastore.New(ctx)
 
 	usr = user.New(db)
+	usr.PaypalEmail = ""
+
+	org = organization.New(db)
+	org.Paypal.Email = ""
+	org.Paypal.ConfirmUrl = ""
+	org.Paypal.CancelUrl = ""
+	org.Fee = 0.05
+
+	pay = payment.New(db)
+	pay.Amount = 100
+	pay.Currency = currency.USD
+	pay.Client.Ip = "1.1.1.1"
 })
 
 var _ = AfterSuite(func() {
 	err := ctx.Close()
 	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = Describe("paypal.GetPayKey", func() {
+	Context("Should Succeed", func() {
+		// Do things here
+	})
 })
