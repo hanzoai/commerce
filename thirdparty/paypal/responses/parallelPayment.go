cd46f9891cf1e085631f06384aff102865b8f726
David Tai <david@verus.io>
2015-09-29 16:34:45 -0500

    paypal test works

--- thirdparty/paypal/responses/parallelPayment.go
+++ thirdparty/paypal/responses/parallelPayment.go
@@ -1,6 +1,16 @@
 package responses
 
+type Error struct {
+	ErrorId   string
+	Domain    string
+	Subdomain string
+	Severity  string
+	Category  string
+	Message   string
+}
+
 type ParallelPaymentResponse struct {
 	PayKey            string
 	PaymentExecStatus string
+	Error             []Error
 }
