dd1daa70ffe6d50da29e5d13306225dbb1a397c3
Zach Kelling <z@zeekay.io>
2015-11-17 18:28:48 -0600

    Rename file.

---
+++ thirdparty/paypal/ipn/ipnmessage.go
@@ -0,0 +1,31 @@
+// Reference: https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNIntro/
+package ipn
+
+import (
+	"net/url"
+	"strings"
+
+	"crowdstart.com/models/types/currency"
+)
+
+type IpnMessage struct {
+	Status     string         // transaction[0].status
+	PayerEmail string         // sender_email
+	PayeeEmail string         // transaction[0].receiver
+	Amount     currency.Cents // extracted from transaction[0].amount
+	PayKey     string         // pay_key
+}
+
+func NewIpnMessage(form url.Values) *IpnMessage {
+	message := new(IpnMessage)
+	message.Status = form.Get("transaction[0].status")
+	message.PayerEmail = form.Get("sender_email")
+	message.PayeeEmail = form.Get("transaction[0].receiver")
+	message.PayKey = form.Get("pay_key")
+
+	amount := form.Get("transaction[0].amount")
+
+	message.Amount = currency.CentsFromString(strings.Split(amount, " ")[1])
+
+	return message
+}
