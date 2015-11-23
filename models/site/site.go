4ad1ff24870b56fd83c602f41ff8d72b75f9c484
Timothy Messer <tmesser@verus.io>
2015-11-23 17:37:03 -0600

    Update site API.

--- models/site/site.go
+++ models/site/site.go
@@ -7,6 +7,37 @@ import (
 
 type Site struct {
 	mixin.Model
+
+	Name               string             `json: "name"`
+	CustomDomain       string             `json: "custom_domain"`
+	Password           string             `json:"password"`
+	ProcessingSettings ProcessingSettings `json:"processing_settings"`
+}
+
+type ProcessingSettings struct {
+	Css    Css    `json: "css"`
+	Js     Js     `json: "js"`
+	Html   Html   `json: "html"`
+	Images Images `json: "images"`
+}
+
+type Css struct {
+	Bundle bool `json: "bundle"`
+	Minify bool `json: "minify"`
+}
+
+type Js struct {
+	Bundle bool `json: "bundle"`
+	Minify bool `json: "minify"`
+}
+
+type Html struct {
+	PrettyUrls    bool `json: "pretty_urls"`
+	CanonicalUrls bool `json: "canonical_urls"`
+}
+
+type Images struct {
+	Optimize bool `json: "optimize"`
 }
 
 func (s *Site) Init() {
--- thirdparty/netlify/netlify.go
+++ thirdparty/netlify/netlify.go
@@ -1,1 +1,37 @@
 package netlify
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/config"
+	"crowdstart.com/middleware"
+	"crowdstart.com/models/site"
+	"crowdstart.com/util/log"
+
+	"appengine/urlfetch"
+)
+
+func CreateSite(c *gin.Context, s site.Site) {
+	ctx := middleware.GetAppEngine(c)
+	jsonreq, _ := json.Marshal(s)
+	reqbuf := bytes.NewBuffer(jsonreq)
+	req, err := http.NewRequest("POST", config.Netlify.BaseUrl+"sites/", reqbuf)
+
+	if err != nil {
+		log.Error("Error upon creating new request %v", err, ctx)
+		return
+	}
+
+	client := urlfetch.Client(ctx)
+	res, err := client.Do(req)
+	defer res.Body.Close()
+
+	if err != nil {
+		log.Error("Request came back with error %v", err, ctx)
+		return
+	}
+}
