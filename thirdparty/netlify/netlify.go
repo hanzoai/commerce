100681eb1ee885334f906afd70b23850988cdb58
Timothy Messer <tmesser@verus.io>
2015-11-24 12:23:36 -0600

    More API updates - only get all sites remains

--- thirdparty/netlify/netlify.go
+++ thirdparty/netlify/netlify.go
@@ -3,6 +3,7 @@ package netlify
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,7 +16,7 @@ import (
 	"appengine/urlfetch"
 )
 
-func CreateSite(c *gin.Context, s site.Site) {
+func CreateSite(c *gin.Context, s *site.Site) {
 	ctx := middleware.GetAppEngine(c)
 	jsonreq, _ := json.Marshal(s)
 	reqbuf := bytes.NewBuffer(jsonreq)
@@ -34,4 +35,75 @@ func CreateSite(c *gin.Context, s site.Site) {
 		log.Error("Request came back with error %v", err, ctx)
 		return
 	}
+}
+
+func UpdateSite(c *gin.Context, s *site.Site) {
+	ctx := middleware.GetAppEngine(c)
+	jsonreq, _ := json.Marshal(s)
+	reqbuf := bytes.NewBuffer(jsonreq)
+	req, err := http.NewRequest("PUT", config.Netlify.BaseUrl+"sites/"+s.SiteId, reqbuf)
+
+	if err != nil {
+		log.Error("Error upon creating new request %v", err, ctx)
+		return
+	}
+
+	client := urlfetch.Client(ctx)
+	res, err := client.Do(req)
+	defer res.Body.Close()
+
+	if err != nil {
+		log.Error("Request came back with error %v", err, ctx)
+		return
+	}
+}
+
+func DeleteSite(c *gin.Context, s *site.Site) {
+	ctx := middleware.GetAppEngine(c)
+	req, err := http.NewRequest("DELETE", config.Netlify.BaseUrl+"sites/"+s.SiteId, nil)
+
+	if err != nil {
+		log.Error("Error upon creating new request %v", err, ctx)
+		return
+	}
+
+	client := urlfetch.Client(ctx)
+	res, err := client.Do(req)
+	defer res.Body.Close()
+
+	if err != nil {
+		log.Error("Request came back with error %v", err, ctx)
+		return
+	}
+}
+
+func GetSingleSite(c *gin.Context, s *site.Site) (*site.Site, error) {
+	ctx := middleware.GetAppEngine(c)
+	req, err := http.NewRequest("GET", config.Netlify.BaseUrl+"sites/"+s.SiteId, nil)
+
+	if err != nil {
+		log.Error("Error upon creating new request %v", err, ctx)
+		return nil, err
+	}
+
+	client := urlfetch.Client(ctx)
+	res, err := client.Do(req)
+	defer res.Body.Close()
+
+	if err != nil {
+		log.Error("Request came back with error %v", err, ctx)
+		return nil, err
+	}
+
+	responseBytes, err := ioutil.ReadAll(res.Body)
+
+	log.Debug("Response Bytes: %v", string(responseBytes), ctx)
+	err = json.Unmarshal(responseBytes, s)
+
+	if err != nil {
+		log.Error("Could not unmarshal response: %v", err, ctx)
+		return nil, err
+	}
+
+	return s, nil
 }
