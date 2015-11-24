b4baebb3db6b64485e9c0f787125e2c3cd243226
zeekay <zk@monoid.io>
2015-11-24 13:07:45 -0600

    Add byName sort for organizations.

---
+++ models/organization/sort.go
@@ -0,0 +1,7 @@
+package organization
+
+type ByName []Organization
+
+func (o ByName) Len() int           { return len(o) }
+func (o ByName) Swap(i, j int)      { o[i], o[j] = o[j], o[i] }
+func (o ByName) Less(i, j int) bool { return o[i].Name < o[j].Name }
