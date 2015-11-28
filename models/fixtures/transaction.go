fb9086d5ce6b70d66999237ec506b08eae4c08b5
Timothy Messer <tmesser@verus.io>
2015-11-28 16:54:27 -0600

    Transaction call documentation and fixture.

---
+++ models/fixtures/transaction.go
@@ -0,0 +1,25 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/models/transaction"
+	"crowdstart.com/models/types/currency"
+)
+
+var Transaction = New("transaction", func(c *gin.Context) *transaction.Transaction {
+	// Get namespaced db
+	db := getNamespaceDb(c)
+
+	u := User(c)
+
+	tran := transaction.New(db)
+	tran.UserId = u.Id()
+	tran.GetOrCreate("UserId=", tran.UserId)
+	tran.Type = "deposit"
+	tran.Currency = currency.USD
+	tran.Amount = 1000
+	tran.MustPut()
+
+	return tran
+})
