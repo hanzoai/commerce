159377214ae243f1565b6dd862cb4e91ed2915f9
zeekay <zk@monoid.io>
2015-12-05 16:34:25 -0600

    Use netlify client.

--- models/site/site.go
+++ models/site/site.go
@@ -3,6 +3,8 @@ package site
 import (
 	"crowdstart.com/datastore"
 	"crowdstart.com/models/mixin"
+
+	"github.com/netlify/netlify-go"
 )
 
 type Site struct {
@@ -12,49 +14,7 @@ type Site struct {
 	Name   string
 	Url    string
 
-	Netlify struct {
-		AdminUrl           string             `json:"admin_url"`
-		Claimed            bool               `json:"claimed"`
-		CreatedAt          string             `json:"created_at"`
-		CustomDomain       string             `json:"custom_domain"`
-		Name               string             `json:"name"`
-		NotificationEmail  string             `json:"notification_email"`
-		Password           string             `json:"password"`
-		Premium            bool               `json:"premium"`
-		ProcessingSettings ProcessingSettings `json:"processing_settings"`
-		Repo               string             `json:"repo"`
-		ScreenshotUrl      string             `json:"screenshot_url"`
-		Id                 string             `json:"id"`
-		State              string             `json:"state"`
-		UpdatedAt          string             `json:"updated_at"`
-		Url                string             `json:"url"`
-	} `json:"-"`
-}
-
-type ProcessingSettings struct {
-	Css    Css    `json: "css"`
-	Js     Js     `json: "js"`
-	Html   Html   `json: "html"`
-	Images Images `json: "images"`
-}
-
-type Css struct {
-	Bundle bool `json: "bundle"`
-	Minify bool `json: "minify"`
-}
-
-type Js struct {
-	Bundle bool `json: "bundle"`
-	Minify bool `json: "minify"`
-}
-
-type Html struct {
-	PrettyUrls    bool `json: "pretty_urls"`
-	CanonicalUrls bool `json: "canonical_urls"`
-}
-
-type Images struct {
-	Optimize bool `json: "optimize"`
+	Netlify netlify.Site `json:"-"`
 }
 
 func (s *Site) Init() {
