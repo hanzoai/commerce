c871a9e6967a0366ec2ab62720c6963277e7a0b6
zeekay <zk@monoid.io>
2015-12-05 15:55:37 -0600

    Add placeholders.

--- api/site/site.go
+++ api/site/site.go
@@ -1,22 +1,55 @@
 package site
 
 import (
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/site"
+	"crowdstart.com/thirdparty/netlify"
+	"crowdstart.com/util/json"
+	"crowdstart.com/util/json/http"
 	"github.com/gin-gonic/gin"
-
-	"crowdstart.com/middleware"
-	"crowdstart.com/util/permission"
-	"crowdstart.com/util/router"
 )
 
-func Route(router router.Router, args ...gin.HandlerFunc) {
-	adminRequired := middleware.TokenRequired(permission.Admin)
+func createSite(c *gin.Context) {
+	db := datastore.New(c)
+	s := site.New(db)
+	if err := json.Decode(c.Request.Body, s); err != nil {
+		http.Fail(c, 400, "Failed to decode request body", err)
+		return
+	}
 
-	api := router.Group("site")
+	netlify.CreateSite(c, s)
+}
 
-	api.GET("/", adminRequired, list)
-	api.GET("/:siteid", adminRequired, get)
-	api.POST("/", adminRequired, create)
-	api.PATCH("/:siteid", adminRequired, update)
-	api.PUT("/:siteid", adminRequired, update)
-	api.DELETE("/:siteid", adminRequired, delete)
+func updateSite(c *gin.Context) {
+	db := datastore.New(c)
+	s := site.New(db)
+	siteid := c.Param("siteid")
+	s.Netlify.Id = siteid
+	if err := json.Decode(c.Request.Body, s); err != nil {
+		http.Fail(c, 400, "Failed to decode request body", err)
+		return
+	}
+
+	netlify.UpdateSite(c, s)
+}
+
+func deleteSite(c *gin.Context) {
+	db := datastore.New(c)
+	s := site.New(db)
+	siteid := c.Param("siteid")
+	s.Netlify.Id = siteid
+	netlify.DeleteSite(c, s)
+}
+
+func listSites(c *gin.Context) {
+	netlify.ListSites(c)
+}
+
+func getSite(c *gin.Context) {
+	db := datastore.New(c)
+	s := site.New(db)
+	siteid := c.Param("siteid")
+	s.Netlify.Id = siteid
+
+	netlify.GetSite(c, s)
 }
