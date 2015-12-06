c9535a853a173cf5ce41a9528014ebe335deb94f
Zach Kelling <z@zeekay.io>
2015-12-05 21:42:09 -0600

    Add PUT redirection for file uploads to netlify.

--- api/site/file.go
+++ api/site/file.go
@@ -1,6 +1,9 @@
 package site
 
-import "github.com/gin-gonic/gin"
+import (
+	"crowdstart.com/config"
+	"github.com/gin-gonic/gin"
+)
 
 func listFiles(c *gin.Context) {
 }
@@ -9,4 +12,11 @@ func getFile(c *gin.Context) {
 }
 
 func putFile(c *gin.Context) {
+	siteid := c.Param("siteid")
+	deployid := c.Param("deployid")
+	filepath := c.Param("filepath")
+
+	url := "https://api.netlify.com/api/v1/sites/" + siteid + "/deploys/" + deployid + "/" + filepath
+	url += "?access_token=" + config.Netlify.AccessToken
+	c.Redirect(307, url)
 }
