1d09817953bdf02bd18c0cb36e2958c0826a8afb
zeekay <zk@monoid.io>
2015-12-09 17:06:30 -0600

    Add kanoa fixture.

---
+++ models/fixtures/kanoass.go
@@ -0,0 +1,32 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/auth/password"
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/user"
+)
+
+var KanoaSS = New("kanoa-ss", func(c *gin.Context) *organization.Organization {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.Name = "kanoa"
+	org.GetOrCreate("Name=", org.Name)
+
+	u := user.New(db)
+	u.Email = "shipstation@getkanoa.com"
+	u.GetOrCreate("Email=", u.Email)
+	u.FirstName = "KANOA"
+	u.LastName = "Shipstation"
+	u.Organizations = []string{org.Id()}
+	u.PasswordHash, _ = password.Hash("6bgX8LVwzwJaDwCd")
+	u.Put()
+
+	// Save org into default namespace
+	org.Put()
+
+	return org
+})
