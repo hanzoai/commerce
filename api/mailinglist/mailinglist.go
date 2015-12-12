5f45237cc87ebc8fa0e80e0fe37bfdb2b3f271b7
zeekay <zk@monoid.io>
2015-12-12 07:32:37 -0600

    Add ability to forward email on form submit.

--- api/mailinglist/mailinglist.go
+++ api/mailinglist/mailinglist.go
@@ -18,6 +18,7 @@ import (
 	"crowdstart.com/util/log"
 
 	mailchimp "crowdstart.com/thirdparty/mailchimp/tasks"
+	mandrill "crowdstart.com/thirdparty/mandrill/tasks"
 )
 
 var subscriberEndpoint = config.UrlFor("api", "/subscriber/")
@@ -35,6 +36,11 @@ func addSubscriber(c *gin.Context) {
 	db.Context = ml.Db.Context
 	ctx := db.Context
 
+	// Get organization for mailinglist
+	org := organization.New(db)
+	org.GetById(ml.Key().Namespace())
+
+	// Mailing list doesn't exist
 	if err := ml.Get(); err != nil {
 		http.Fail(c, 404, fmt.Sprintf("Failed to retrieve mailing list '%v': %v", id, err), err)
 		return
@@ -56,19 +62,34 @@ func addSubscriber(c *gin.Context) {
 	if err := ml.AddSubscriber(s); err != nil {
 		if err == mailinglist.SubscriberAlreadyExists {
 			http.Fail(c, 409, "Subscriber already exists", nil)
-		} else {
-			http.Fail(c, 500, "Failed to save subscriber to mailing list", err)
 		}
-	} else {
-		mailchimp.Subscriber.Call(db.Context, ml.JSON(), s.JSON())
-		c.Writer.Header().Add("Location", subscriberEndpoint+s.Id())
-		http.Render(c, 201, s)
+		http.Fail(c, 500, "Failed to save subscriber to mailing list", err)
+		return
 	}
 
-	org := organization.New(db)
-	org.GetById(ml.Key().Namespace())
-
+	// Increment subscribers
 	if err := counter.IncrSubscribers(ctx, org, ml.Id(), time.Now()); err != nil {
 		log.Warn("Redis Error %s", err, ctx)
 	}
+
+	// Add subscriber to Mailchimp
+	if ml.Mailchimp.Enabled {
+		mailchimp.Subscriber.Call(db.Context, ml.JSON(), s.JSON())
+	}
+
+	// Forward contact information
+	if ml.Forward.Enabled {
+		toEmail := ml.Forward.Email
+		toName := ml.Forward.Name
+		fromEmail := "noreply@crowdstart.com"
+		fromName := "Crowdstart"
+		subject := "New submission for form " + ml.Name
+		html := fmt.Sprintf("%v", s)
+		mandrill.Send.Call(ctx, org.Mandrill.APIKey, toEmail, toName, fromEmail, fromName, subject, html)
+	}
+
+	// Success!
+	c.Writer.Header().Add("Location", subscriberEndpoint+s.Id())
+	http.Render(c, 201, s)
+
 }
