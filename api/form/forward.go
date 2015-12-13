a93f11d9295f3aa4ec30acebf320cd649979bacd
zeekay <zk@monoid.io>
2015-12-12 23:18:02 -0600

    Nicer email forwarding template.

--- api/form/forward.go
+++ api/form/forward.go
@@ -8,6 +8,10 @@ import (
 	"crowdstart.com/config"
 	"crowdstart.com/models/mailinglist"
 	"crowdstart.com/models/organization"
+	"crowdstart.com/models/submission"
+	"crowdstart.com/models/subscriber"
+
+	. "crowdstart.com/models"
 
 	mandrill "crowdstart.com/thirdparty/mandrill/tasks"
 )
@@ -18,13 +22,30 @@ func forward(ctx appengine.Context, org *organization.Organization, ml *mailingl
 		return
 	}
 
+	replyTo := ""
+	metadata := make(Metadata)
+
+	switch v := s.(type) {
+	case *subscriber.Subscriber:
+		replyTo = v.Email
+		metadata = v.Metadata
+	case *submission.Submission:
+		replyTo = v.Email
+		metadata = v.Metadata
+	}
+
 	// Forward form submission
 	toEmail := ml.Forward.Email
 	toName := ml.Forward.Name
 	fromEmail := "noreply@crowdstart.com"
 	fromName := "Crowdstart"
 	subject := "New submission for form " + ml.Name
-	html := fmt.Sprintf("%v", s)
+
+	html := fmt.Sprintf("Form submission from: %v\n", replyTo)
+
+	for k, v := range metadata {
+		html += fmt.Sprintf("%s: %s\n", k, v)
+	}
 
 	mandrill.Send.Call(ctx, config.Mandrill.APIKey, toEmail, toName, fromEmail, fromName, subject, html)
 }
