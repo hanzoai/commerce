60c7cfc8c3421696d3d533e2e7b726bc6c805694
Zach Kelling <z@zeekay.io>
2015-12-13 01:16:21 -0600

    Add ItemId() and ValidFor() to coupon.

--- models/order/order.go
+++ models/order/order.go
@@ -346,16 +346,14 @@ func (o *Order) UpdateDiscount() {
 			// Coupons per product
 			for _, item := range o.Items {
 				log.Debug("Coupon.ProductId: %v, Item.ProductId: %v", c.ProductId, item.ProductId, ctx)
-				// log.Warn("%v, %v ==? %v", item.ProductName, item.ProductId, c.ProductId)
-				if item.ProductId == c.ProductId {
+				if item.Id() == c.ProductId || item.Id() == c.FreeProductId || item.Id() == c.FreeVariantId {
 					switch c.Type {
 					case coupon.Flat:
 						o.Discount += currency.Cents(item.Quantity * c.Amount)
 					case coupon.Percent:
-						o.Discount += currency.Cents(int(math.Floor(float64(item.TotalPrice()) * float64(c.Amount) * 0.01)))
+						o.Discount += currency.Cents(math.Floor(float64(item.TotalPrice()) * float64(c.Amount) * 0.01))
 					case coupon.FreeItem:
 						o.Discount += currency.Cents(item.TotalPrice())
-						break
 					}
 
 					// Break out unless required to apply to each product
