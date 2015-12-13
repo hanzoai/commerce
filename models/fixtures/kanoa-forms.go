2efd1501a3a5f51d2dbb140f91eaa6581f2c3ebe
zeekay <zk@monoid.io>
2015-12-12 19:42:55 -0600

    Add new KANOA fixture.

---
+++ models/fixtures/kanoa-forms.go
@@ -0,0 +1,20 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/mailinglist"
+)
+
+var _ = New("kanoa-forms", func(c *gin.Context) *mailinglist.MailingList {
+	db := datastore.New(c)
+
+	ml := mailinglist.New(db)
+	ml.MustGet("3XudPY2SQeXQ3")
+	ml.Forward.Name = "Cival"
+	ml.Forward.Email = "dev@hanzo.ai"
+	ml.Forward.Enabled = true
+
+	return ml
+})
