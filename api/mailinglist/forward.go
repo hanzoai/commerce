7feefbaabf5f7a181e6edfc24a8f32b034dd0225
zeekay <zk@monoid.io>
2015-12-12 21:15:09 -0600

    Support multiple types of forms w/diff behaviors.

---
+++ api/mailinglist/forward.go
@@ -0,0 +1,28 @@
+package mailinglist
+
+import (
+	"fmt"
+
+	"appengine"
+
+	"crowdstart.com/models/mailinglist"
+	"crowdstart.com/models/organization"
+
+	mandrill "crowdstart.com/thirdparty/mandrill/tasks"
+)
+
+// Add subscriber to mailing list
+func forward(ctx appengine.Context, org *organization.Organization, ml *mailinglist.MailingList, s interface{}) {
+	if !ml.Forward.Enabled {
+		return
+	}
+
+	// Forward form submission
+	toEmail := ml.Forward.Email
+	toName := ml.Forward.Name
+	fromEmail := "noreply@crowdstart.com"
+	fromName := "Crowdstart"
+	subject := "New submission for form " + ml.Name
+	html := fmt.Sprintf("%v", s)
+	mandrill.Send.Call(ctx, org.Mandrill.APIKey, toEmail, toName, fromEmail, fromName, subject, html)
+}
---
+++ api/mailinglist/submit.go
@@ -0,0 +1,35 @@
+package mailinglist
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/mailinglist"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/submission"
+	"crowdstart.com/models/types/client"
+	"crowdstart.com/util/json"
+	"crowdstart.com/util/json/http"
+)
+
+func submit(c *gin.Context, db *datastore.Datastore, org *organization.Organization, ml *mailinglist.MailingList) {
+	ctx := db.Context
+
+	// Make sure Subscriber is created with the right context
+	s := submission.New(db)
+
+	// Decode response body for subscriber
+	if err := json.Decode(c.Request.Body, s); err != nil {
+		http.Fail(c, 400, "Failed decode request body", err)
+		return
+	}
+
+	// Store metadata about client
+	s.Client = client.New(c)
+
+	// Forward submission (if enabled)
+	forward(ctx, org, ml, s)
+
+	// Success!
+	http.Render(c, 200, s)
+}
