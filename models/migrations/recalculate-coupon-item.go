778bf00b3a953746e667b2ceccbfbbaca50f1bdc
David Tai <antisocialmunky@gmail.com>
2015-12-12 19:03:47 -0600

    make free items show up with other items

---
+++ models/migrations/recalculate-coupon-item.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/models/order"
+	"crowdstart.com/models/store"
+	"crowdstart.com/util/log"
+
+	ds "crowdstart.com/datastore"
+)
+
+var _ = New("recalculate-coupon-items",
+	func(c *gin.Context) []interface{} {
+		return NoArgs
+	},
+	func(db *ds.Datastore, ord *order.Order) {
+		if len(ord.CouponCodes) > 0 {
+			stor := store.New(db)
+			if ord.StoreId != "" {
+				if err := stor.GetById(ord.StoreId); err != nil {
+					log.Error("Could not find store", err, db.Context)
+				}
+			}
+
+			ord.UpdateAndTally(stor)
+			ord.MustPut()
+		}
+	},
+)
