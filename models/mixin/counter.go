3b5ff63e24117e5f053cccc5da3e560c66e761c6
zeekay <zk@monoid.io>
2015-12-14 07:21:16 -0600

    Add counter mixin.

---
+++ models/mixin/counter.go
@@ -0,0 +1,34 @@
+package mixin
+
+import (
+	"time"
+
+	counter "crowdstart.com/util/counter2"
+)
+
+type Countable interface {
+	Increment()
+	IncrementDay()
+	IncrementHour()
+	IncrementMonth()
+}
+
+type Counter struct {
+	Entity Entity `json:"-" datastore:"-"`
+}
+
+func (c *Counter) Increment() {
+	counter.Increment(c.Entity.Context(), c.Entity.Kind())
+}
+
+func (c *Counter) IncrementHour() {
+	counter.IncrementHour(c.Entity.Context(), c.Entity.Kind(), time.Now())
+}
+
+func (c *Counter) IncrementDay() {
+	counter.IncrementDay(c.Entity.Context(), c.Entity.Kind(), time.Now())
+}
+
+func (c *Counter) IncrementMonth() {
+	counter.IncrementMonth(c.Entity.Context(), c.Entity.Kind(), time.Now())
+}
