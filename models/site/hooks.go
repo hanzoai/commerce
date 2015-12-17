79b62aa07a6d8037de1e948957ffae290c887509
Zach Kelling <z@zeekay.io>
2015-12-16 22:42:25 -0600

    Remove circular import.

---
+++ models/site/hooks.go
@@ -0,0 +1,51 @@
+package site
+
+import (
+	"crowdstart.com/thirdparty/netlify"
+	"crowdstart.com/util/webhook"
+)
+
+// Hooks
+func (s *Site) BeforeCreate() error {
+	nsite, err := netlify.CreateSite(s.Context(), s.Netlify())
+	if err != nil {
+		return err
+	}
+
+	s.SetNetlify(nsite)
+
+	return nil
+}
+
+func (s *Site) AfterCreate() error {
+	webhook.Emit(s.Context(), s.Namespace(), "site.created", s)
+	return nil
+}
+
+func (s *Site) BeforeUpdate(previous *Site) error {
+	nsite, err := netlify.UpdateSite(s.Context(), s.Netlify())
+	if err != nil {
+		return err
+	}
+
+	s.SetNetlify(nsite)
+
+	return nil
+}
+
+func (s *Site) AfterUpdate(previous *Site) error {
+	webhook.Emit(s.Context(), s.Namespace(), "site.spdated", s)
+	return nil
+}
+
+func (s *Site) BeforeDelete() error {
+	if err := netlify.DeleteSite(s.Context(), s.Netlify()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Site) AfterDelete() error {
+	webhook.Emit(s.Context(), s.Namespace(), "site.deleted", s)
+	return nil
+}
--- models/site/site.go
+++ models/site/site.go
@@ -4,7 +4,7 @@ import (
 	"crowdstart.com/datastore"
 	"crowdstart.com/models/mixin"
 
-	"github.com/netlify/netlify-go"
+	"crowdstart.com/thirdparty/netlify"
 )
 
 type Site struct {
@@ -14,7 +14,7 @@ type Site struct {
 	Name   string
 	Url    string
 
-	Netlify netlify.Site `json:"-"`
+	Netlify_ netlify.Site `json:"-"`
 }
 
 func (s *Site) Init() {
@@ -33,4 +33,15 @@ func (s Site) Kind() string {
 
 func (s Site) Document() mixin.Document {
 	return &Document{}
+}
+
+// Return netlify overriden with our local properties
+func (s Site) Netlify() netlify.Site {
+	s.Netlify_.Name = s.Name
+	s.Netlify_.CustomDomain = s.Domain
+	return s.Netlify_
+}
+
+func (s Site) SetNetlify(nsite *netlify.Site) {
+	s.Netlify_ = *nsite
 }
