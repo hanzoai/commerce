c056d6332d9bacb529925b715d8087e8e6da4cd3
Zach Kelling <z@zeekay.io>
2015-12-18 17:54:50 -0600

    Start adding deploy wrapper.

--- thirdparty/netlify/logger.go
+++ thirdparty/netlify/logger.go
@@ -10,17 +10,14 @@ import (
 	"crowdstart.com/util/log"
 )
 
-type netlifyLogger func(*netlify.Site, *netlify.Response, error) (*netlify.Site, *netlify.Response, error)
-
-func logger(ctx appengine.Context) netlifyLogger {
-	return func(site *netlify.Site, res *netlify.Response, err error) (*netlify.Site, *netlify.Response, error) {
+func logger(ctx appengine.Context) func(*netlify.Response, error) {
+	return func(res *netlify.Response, err error) {
 		if err != nil {
-			return site, res, err
+			return
 		}
 
 		defer res.Body.Close()
 		b, _ := ioutil.ReadAll(res.Body)
 		log.Debug("Netlify Response (%v): %v", res.StatusCode, string(b), ctx)
-		return site, res, err
 	}
 }
