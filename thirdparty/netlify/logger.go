f94ad55acc1a21239f6241e3589acd174771c536
zeekay <zk@monoid.io>
2015-12-18 17:22:46 -0600

    Add logging helper.

---
+++ thirdparty/netlify/logger.go
@@ -0,0 +1,27 @@
+package netlify
+
+import (
+	"io/ioutil"
+
+	"appengine"
+
+	"github.com/netlify/netlify-go"
+
+	"crowdstart.com/util/log"
+)
+
+type netlifyLogger func(*netlify.Site, *netlify.Response, error) (*netlify.Site, *netlify.Response, error)
+
+func logger(ctx appengine.Context) netlifyLogger {
+	return func(site *netlify.Site, res *netlify.Response, err error) (*netlify.Site, *netlify.Response, error) {
+		if err != nil {
+			log.Error("Failed to create site: %v", err, ctx)
+			return site, res, err
+		}
+
+		defer res.Body.Close()
+		b, _ := ioutil.ReadAll(res.Body)
+		log.Debug("Netlify Response (%v): %v", res.StatusCode, string(b), ctx)
+		return site, res, err
+	}
+}
