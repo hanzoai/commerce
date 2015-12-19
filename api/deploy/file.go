3cb1e795a81ba8dc61af9d520b1bf2bb14ca01bd
zeekay <zk@monoid.io>
2015-12-19 05:00:16 -0600

    Ensure netlify's site id is used in redirect.

--- api/deploy/file.go
+++ api/deploy/file.go
@@ -1,10 +1,17 @@
 package site
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 
+	"crowdstart.com/config"
+	"crowdstart.com/datastore"
 	"crowdstart.com/middleware"
+	"crowdstart.com/models/site"
 	"crowdstart.com/thirdparty/netlify"
+	"crowdstart.com/util/json/http"
+	"crowdstart.com/util/log"
 )
 
 func listFiles(c *gin.Context) {
@@ -14,15 +21,25 @@ func getFile(c *gin.Context) {
 }
 
 func putFile(c *gin.Context) {
-	siteid := c.Param("siteid")
+	siteid := c.Param("siteid") // oursiteid
 	deployid := c.Param("deployid")
 	filepath := c.Param("filepath")
+
+	db := datastore.New(c)
+	ste := site.New(db)
+	err := ste.GetById(siteid)
+	if err != nil {
+		msg := fmt.Sprintf("Site '%s' not found", siteid)
+		http.Fail(c, 404, msg, nil)
+		return
+	}
 
 	ctx := middleware.GetAppEngine(c)
 	org := middleware.GetOrganization(c)
 	accessToken := netlify.GetAccessToken(ctx, org.Name)
 
-	url := "https://api.netlify.com/api/v1/sites/" + siteid + "/deploys/" + deployid + "/" + filepath
+	url := config.Netlify.BaseUrl + "/sites/" + ste.Netlify().Id + "/deploys/" + deployid + "/" + filepath
 	url += "?access_token=" + accessToken
+	log.Debug("Returning redirect, upload file to: %s", url, c)
 	c.Redirect(307, url)
 }
