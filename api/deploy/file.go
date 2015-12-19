cd6c9be639eb37b13c6fd87a88882a9818de4ca2
zeekay <zk@monoid.io>
2015-12-19 05:10:03 -0600

    Return correct redirect for netlify.

--- api/deploy/file.go
+++ api/deploy/file.go
@@ -1,16 +1,13 @@
 package site
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 
+	// "crowdstart.com/datastore"
+	// "crowdstart.com/models/site"
 	"crowdstart.com/config"
-	"crowdstart.com/datastore"
 	"crowdstart.com/middleware"
-	"crowdstart.com/models/site"
 	"crowdstart.com/thirdparty/netlify"
-	"crowdstart.com/util/json/http"
 	"crowdstart.com/util/log"
 )
 
@@ -21,24 +18,24 @@ func getFile(c *gin.Context) {
 }
 
 func putFile(c *gin.Context) {
-	siteid := c.Param("siteid") // oursiteid
+	// siteid := c.Param("siteid") // oursiteid
 	deployid := c.Param("deployid")
 	filepath := c.Param("filepath")
 
-	db := datastore.New(c)
-	ste := site.New(db)
-	err := ste.GetById(siteid)
-	if err != nil {
-		msg := fmt.Sprintf("Site '%s' not found", siteid)
-		http.Fail(c, 404, msg, nil)
-		return
-	}
+	// db := datastore.New(c)
+	// ste := site.New(db)
+	// err := ste.GetById(siteid)
+	// if err != nil {
+	// 	msg := fmt.Sprintf("Site '%s' not found", siteid)
+	// 	http.Fail(c, 404, msg, nil)
+	// 	return
+	// }
 
 	ctx := middleware.GetAppEngine(c)
 	org := middleware.GetOrganization(c)
 	accessToken := netlify.GetAccessToken(ctx, org.Name)
 
-	url := config.Netlify.BaseUrl + "/sites/" + ste.Netlify().Id + "/deploys/" + deployid + "/" + filepath
+	url := config.Netlify.BaseUrl + "/deploys/" + deployid + "/files" + filepath
 	url += "?access_token=" + accessToken
 	log.Debug("Returning redirect, upload file to: %s", url, c)
 	c.Redirect(307, url)
