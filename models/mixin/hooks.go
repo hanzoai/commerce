54c872045d246fb360eaf9f2944dcb3f26e90cb6
zeekay <zk@monoid.io>
2015-12-18 22:00:02 -0600

    Get type safely.

--- models/mixin/hooks.go
+++ models/mixin/hooks.go
@@ -1,5 +1,7 @@
 package mixin
 
+import "reflect"
+
 type BeforeCreate interface {
 	BeforeCreate() error
 }
@@ -22,4 +24,24 @@ type AfterUpdate interface {
 
 type AfterDelete interface {
 	AfterDelete() error
+}
+
+// Try to get method off a model
+func getMethod(name string, model *Model) (reflect.Method, bool) {
+	typ := reflect.TypeOf(model.Entity)
+	return typ.MethodByName(name)
+}
+
+// Call method returned by getMethod
+func callMethod(method reflect.Method, previous Entity) error {
+	args := []reflect.Value{
+		reflect.ValueOf(previous),
+	}
+
+	ret := method.Func.Call(args)
+	err, ok := ret[0].Interface().(error)
+	if ok {
+		return err
+	}
+	return nil
 }
