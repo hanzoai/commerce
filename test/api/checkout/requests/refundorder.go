08ac917776d61385d13dbd2ae4b1007d6bf760ba
Marvel Mathew <marvel@verus.io>
2015-12-31 13:48:53 -0700

    Test for invalid refund requests

--- test/api/checkout/requests/refundorder.go
+++ test/api/checkout/requests/refundorder.go
@@ -1,6 +1,18 @@
 package requests
 
-var Refund = `
+var PartialRefund = `
 {
 	"amount": 123
 }`
+
+var NegativeRefund = `
+{
+	"amount": -1
+}
+`
+
+var LargeRefundAmount = `
+{
+	"amount": 9999999999
+}
+`
