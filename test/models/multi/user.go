0644d8809c26899728c475b253e3434185870058
Zach Kelling <z@zeekay.io>
2016-01-07 13:12:25 -0600

    Add basic tests around multi.

---
+++ test/models/multi/user.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/mixin"
+	"crowdstart.com/util/val"
+)
+
+type User struct {
+	mixin.Model
+	mixin.AccessToken
+
+	Name string
+}
+
+func (u *User) Kind() string {
+	return "user"
+}
+
+func (u *User) Document() mixin.Document {
+	return nil
+}
+
+func newUser(db *datastore.Datastore) *User {
+	u := new(User)
+	u.Model = mixin.Model{Db: db, Entity: u}
+	u.AccessToken = mixin.AccessToken{Entity: u}
+	return u
+}
+
+func (u *User) Validator() *val.Validator {
+	return val.New()
+}
