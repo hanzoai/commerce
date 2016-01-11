9abf24e0a13c99df808113ccca0d2f73e0fd2d13
David Tai <antisocialmunky@gmail.com>
2016-01-11 17:43:30 -0600

    test email fix

--- util/emails/emails.go
+++ util/emails/emails.go
@@ -1,25 +1,22 @@
 package emails
 
 import (
-	"github.com/gin-gonic/gin"
-
-	"crowdstart.com/middleware"
 	"crowdstart.com/models/order"
 	"crowdstart.com/models/organization"
 	"crowdstart.com/models/product"
 	"crowdstart.com/models/user"
 	"crowdstart.com/util/template"
 
+	"appengine"
+
 	mandrill "crowdstart.com/thirdparty/mandrill/tasks"
 )
 
-func SendOrderConfirmationEmail(c *gin.Context, org *organization.Organization, ord *order.Order, usr *user.User) {
+func SendOrderConfirmationEmail(ctx appengine.Context, org *organization.Organization, ord *order.Order, usr *user.User) {
 	conf := org.Email.OrderConfirmation.Config(org)
 	if !conf.Enabled || org.Mandrill.APIKey == "" {
 		return
 	}
-
-	ctx := middleware.GetAppEngine(c)
 
 	// From
 	fromName := conf.FromName
