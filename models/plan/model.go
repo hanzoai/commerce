e5c00953e790b8d6572ed7896fd344042bfc6ebb
zeekay <zk@monoid.io>
2016-01-12 01:51:36 -0600

    Update News.

--- models/plan/model.go
+++ models/plan/model.go
@@ -5,16 +5,18 @@ import (
 	"crowdstart.com/models/mixin"
 )
 
-func New(db *datastore.Datastore) *Plan {
-	return new(Plan).New(db).(*Plan)
-}
-
 func (p Plan) Kind() string {
 	return "plan"
 }
 
 func (p *Plan) Init(db *datastore.Datastore) {
 	p.Model = mixin.Model{Db: db, Entity: p}
+}
+
+func New(db *datastore.Datastore) *Plan {
+	p := new(Plan)
+	p.Init(db)
+	return p
 }
 
 func Query(db *datastore.Datastore) *mixin.Query {
