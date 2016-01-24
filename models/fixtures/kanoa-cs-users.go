1a27274ab0ce738fb41740631dbf80caf9eedd5c
David Tai <antisocialmunky@gmail.com>
2016-01-23 22:32:18 -0600

    added basic team support

---
+++ models/fixtures/kanoa-cs-users.go
@@ -0,0 +1,27 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/auth/password"
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/user"
+)
+
+var KanoaCSUsers = New("kanoa-cs-users", func(c *gin.Context) {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.Name = "kanoa"
+	org.GetOrCreate("Name=", org.Name)
+
+	u := user.New(db)
+	u.Email = "dev@hanzo.ai"
+	u.GetOrCreate("Email=", u.Email)
+	u.FirstName = "Lorenzo"
+	u.LastName = "Castillo"
+	u.Organizations = []string{org.Id()}
+	u.PasswordHash, _ = password.Hash("1Kanoa23")
+	u.Put()
+})
