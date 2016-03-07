cac90700f084f498d7474de65d1053e709280971
David Tai <david@verus.io>
2016-03-07 13:38:14 -0800

    uncommented out short circuit in kanoa bathc mgiration

--- models/migrations/add-batch-data-for-kanoa.go
+++ models/migrations/add-batch-data-for-kanoa.go
@@ -18,9 +18,9 @@ var _ = New("add-batch-data-for-kanoa",
 		loc, _ := time.LoadLocation("America/Los_Angeles")
 		batch1Time := time.Date(2016, 2, 28, 0, 0, 0, 0, loc)
 
-		// if _, ok := ord.Metadata["batch"]; ok {
-		// 	return
-		// }
+		if _, ok := ord.Metadata["batch"]; ok {
+			return
+		}
 
 		if ord.CreatedAt.Before(batch1Time) {
 			ord.Metadata["batch"] = "1"
