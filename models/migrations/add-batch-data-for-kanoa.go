04e823e09b0fb5fcc54cb4946280f9e1842c2d87
David Tai <david@verus.io>
2016-03-06 23:02:52 -0800

    add batch meta data support

---
+++ models/migrations/add-batch-data-for-kanoa.go
@@ -0,0 +1,32 @@
+package migrations
+
+import (
+	"time"
+
+	"crowdstart.com/models/order"
+	"github.com/gin-gonic/gin"
+
+	ds "crowdstart.com/datastore"
+)
+
+var _ = New("add-batch-data-for-kanoa",
+	func(c *gin.Context) []interface{} {
+		c.Set("namespace", "kanoa")
+		return NoArgs
+	},
+	func(db *ds.Datastore, ord *order.Order) {
+		loc, _ := time.LoadLocation("America/Los_Angeles")
+		batch1Time := time.Date(2016, 2, 28, 0, 0, 0, 0, loc)
+
+		// if _, ok := ord.Metadata["batch"]; ok {
+		// 	return
+		// }
+
+		if ord.CreatedAt.Before(batch1Time) {
+			ord.Metadata["batch"] = "1"
+		} else {
+			ord.Metadata["batch"] = "2"
+		}
+		ord.Put()
+	},
+)
