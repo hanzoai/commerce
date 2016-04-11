c1687c3424466138e6331f16c92107fe4327c742
Timothy Messer <tmesser@verus.io>
2016-04-10 20:12:11 -0500

    Adding Redemption and filling out Coupon with some of the new stuff it needs.

---
+++ models/redemption/model.go
@@ -0,0 +1,28 @@
+package coupon
+
+import (
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/mixin"
+)
+
+func (r Redemption) Kind() string {
+	return "redemption"
+}
+
+func (r *Redemption) Init(db *datastore.Datastore) {
+	r.Model.Init(db, r)
+}
+
+func (r *Redemption) Defaults() {
+}
+
+func New(db *datastore.Datastore) *Redemption {
+	r := new(Redemption)
+	r.Init(db)
+	r.Defaults()
+	return r
+}
+
+func Query(db *datastore.Datastore) *mixin.Query {
+	return New(db).Query()
+}
