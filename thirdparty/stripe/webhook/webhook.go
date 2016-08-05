dce0f1a806b37a135c19324bc2d8327f8a3ec891
zeekay <zk@monoid.io>
2016-08-05 11:52:05 -0500

    Improve logging during Stripe webhook handler.

--- thirdparty/stripe/webhook/webhook.go
+++ thirdparty/stripe/webhook/webhook.go
@@ -28,8 +28,12 @@ func Webhook(c *gin.Context) {
 	// Look up organization
 	db := datastore.New(c)
 	org := organization.New(db)
-	if ok, err := org.Query().Filter("Stripe.UserId=", event.UserID).First(); !ok || err != nil {
-		log.Error("Failed to look up organization by Stripe user id: %v", err, c)
+	if ok, err := org.Query().Filter("Stripe.UserId=", event.UserID).First(); !ok {
+		if err != nil {
+			log.Error("Failed to query organization using Stripe UserId '%s': %v", event.UserID, err, c)
+		} else {
+			log.Warn("No organization found with Stripe UserId '%s': %#v", event.UserID, event, c)
+		}
 		return
 	}
 
