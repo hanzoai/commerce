21bc801569fe1c1afd490e4d228bc4c2e1bd5879
Timothy Messer <tmesser@verus.io>
2016-08-24 14:10:55 -0500

    Adding affiliate model for use in new program stuff

---
+++ models/affiliate/model.go
@@ -0,0 +1,29 @@
+package affiliate
+
+import (
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/mixin"
+)
+
+func (a Affiliate) Kind() string {
+	return "affiliate"
+}
+
+func (a *Affiliate) Init(db *datastore.Datastore) {
+	a.Model.Init(db, a)
+	a.AccessToken.Init(a)
+}
+
+func (a *Affiliate) Defaults() {
+}
+
+func New(db *datastore.Datastore) *Affiliate {
+	r := new(Affiliate)
+	r.Init(db)
+	r.Defaults()
+	return r
+}
+
+func Query(db *datastore.Datastore) *mixin.Query {
+	return New(db).Query()
+}
