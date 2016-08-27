0a275617d0b39ed8157177908d747088c7410e93
Zach Kelling <z@zeekay.io>
2016-08-27 17:13:55 -0500

    Add KANOA fixture for mailchimp.

---
+++ models/fixtures/kanoa-mailchimp.go
@@ -0,0 +1,38 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/product"
+	"crowdstart.com/models/store"
+	"crowdstart.com/models/types/currency"
+	"crowdstart.com/thirdparty/mailchimp"
+)
+
+var _ = New("kanoa-mailchimp", func(c *gin.Context) *organization.Organization {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.Query().Filter("Name=", "kanoa").First()
+
+	// Create new store
+	stor := store.New(db)
+	stor.Name = "default"
+	stor.Prefix = "/"
+	stor.Currency = currency.USD
+	stor.Mailchimp.ListId = "23ad4e4ba4"
+	stor.Create()
+
+	// Fetch earphones
+	prod := product.New(db)
+	prod.GetById("Slug=", "earphone")
+
+	// Create corresponding Mailchimp entities
+	client := mailchimp.New(db.Context, org.Mailchimp.APIKey)
+	client.CreateStore(stor)
+	client.CreateProduct(stor.Id(), prod)
+
+	return org
+})
