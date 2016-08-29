84c36bb25a0db2031be15f3e40475b47a1ad69c2
zeekay <zk@monoid.io>
2016-08-29 17:30:27 -0500

    Refactor for clarity.

--- api/checkout/capture.go
+++ api/checkout/capture.go
@@ -100,39 +100,32 @@ func CompleteCapture(c *gin.Context, org *organization.Organization, ord *order.
 		}
 	}
 
-	// Used elsewhere
-	var client *mailchimp.API
-
-	// Create mailchimp subscriber, should be no-op on Mailchimp's end if subscriber already exists
-	if org.Mailchimp.APIKey != "" {
-		// Create new mailchimp client (used everywhere else)
-		client = mailchimp.New(ctx, org.Mailchimp.APIKey)
-		// Just get buyer off first payment
-		if err := client.SubscribeCustomer(org.Mailchimp.ListId, payments[0].Buyer); err != nil {
-			log.Warn("Failed to subscribe '%s' to Mailchimp list '%s': %v", payments[0].Buyer.Email, org.Mailchimp.ListId, err)
-		}
-	}
-
 	// Update cart
+	car := cart.New(db)
+
 	if ord.CartId != "" {
-		car := cart.New(db)
 		if err := car.GetById(ord.CartId); err != nil {
 			log.Warn("Unable to find cart: %v", err, ctx)
 		} else {
 			car.Status = cart.Ordered
 			if err := car.Update(); err != nil {
 				log.Warn("Unable to save cart: %v", err, ctx)
-			} else {
-				if org.Mailchimp.APIKey != "" {
-					client.DeleteCart(org.DefaultStore, car)
-				}
 			}
 		}
 	}
 
-	// Create mailchimp order
+	// Mailchimp shenanigans
 	if org.Mailchimp.APIKey != "" {
+		// Create new mailchimp client (used everywhere else)
+		client := mailchimp.New(ctx, org.Mailchimp.APIKey)
+
+		client.DeleteCart(org.DefaultStore, car)
 		client.CreateOrder(org.DefaultStore, ord)
+
+		// Just get buyer off first payment
+		if err := client.SubscribeCustomer(org.Mailchimp.ListId, payments[0].Buyer); err != nil {
+			log.Warn("Failed to subscribe '%s' to Mailchimp list '%s': %v", payments[0].Buyer.Email, org.Mailchimp.ListId, err)
+		}
 	}
 
 	log.Debug("Incrementing Counters? %v", ord.Test, c)
