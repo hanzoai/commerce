ee39667bb3bd19c99a48549d0863f05d5a47614d
zeekay <zk@monoid.io>
2016-08-30 17:13:38 -0500

    Fix tests.

--- test/api/checkout/stripe-integration/stripeverify.go
+++ test/api/checkout/stripe-integration/stripeverify.go
@@ -9,39 +9,42 @@ import (
 )
 
 func stripeVerifyCharge(pay *payment.Payment) {
-	c, err := sc.Charges.Get(pay.Account.ChargeId, nil)
-	Expect(c).ToNot(BeNil())
+	ch, err := sc.Charges.Get(pay.Account.ChargeId, nil)
+	Expect(ch).ToNot(BeNil())
 	Expect(err).ToNot(HaveOccurred())
 
-	Expect(c.Captured).To(BeTrue())
-	log.Debug("StripeVerifyCharge Results:\n%v\n%v", c, err)
+	Expect(ch.Captured).To(BeTrue())
+	log.Debug("StripeVerifyCharge Results:\n%v\n%v", ch, err)
 }
 
 func stripeVerifyAuth(pay *payment.Payment) {
-	c, err := sc.Charges.Get(pay.Account.ChargeId, nil)
-	Expect(c).ToNot(BeNil())
+	ch, err := sc.Charges.Get(pay.Account.ChargeId, nil)
+	Expect(ch).ToNot(BeNil())
 	Expect(err).ToNot(HaveOccurred())
 
-	Expect(c.Captured).To(BeFalse())
-	log.Debug("StripeVerifyAuth Results:\n%v\n%v", c, err)
+	Expect(ch.Captured).To(BeFalse())
+	log.Debug("StripeVerifyAuth Results:\n%v\n%v", ch, err)
 }
 
 func stripeVerifyUser(usr *user.User) {
-	c, err := sc.Customers.Get(usr.Accounts.Stripe.CustomerId, nil)
-	log.Debug("StripeVerifyUser Results:\n%v\n%v", c, err)
-	Expect(c).ToNot(BeNil())
+	cust, err := sc.Customers.Get(usr.Accounts.Stripe.CustomerId, nil)
+	log.Debug("StripeVerifyUser Results:\n%v\n%v", cust, err)
+	Expect(cust).ToNot(BeNil())
 	Expect(err).ToNot(HaveOccurred())
 }
 
 func stripeVerifyCards(usr *user.User, cardIds []string) {
-	c, err := sc.Customers.Get(usr.Accounts.Stripe.CustomerId, nil)
-	Expect(c).ToNot(BeNil())
+	cust, err := sc.Customers.Get(usr.Accounts.Stripe.CustomerId, nil)
+	Expect(cust).ToNot(BeNil())
 	Expect(err).ToNot(HaveOccurred())
 
-	Expect(len(c.Sources.Values)).To(Equal(len(cardIds)))
-	for i, source := range c.Sources.Values {
-		Expect(source.Card.ID).To(Equal(cardIds[i]))
+	sources := make([]string, 0)
+	for _, source := range cust.Sources.Values {
+		sources = append(sources, source.Card.ID)
 	}
 
-	log.Debug("StripeVerifyCard Results:\n%v\n%v", c, err)
+	log.Warn("StripeVerifyCard Expected: %v\nGot:%v", cardIds, sources)
+
+	Expect(len(cust.Sources.Values)).To(Equal(len(cardIds)))
+	Expect(sources).To(ConsistOf(cardIds))
 }
