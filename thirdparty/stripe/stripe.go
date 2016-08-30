23b2d0a0ac1227888a7daa7d3eeae73a58f62a93
zeekay <zk@monoid.io>
2016-08-30 17:13:38 -0500

    Fix tests.

--- thirdparty/stripe/stripe.go
+++ thirdparty/stripe/stripe.go
@@ -38,6 +38,13 @@ func New(ctx appengine.Context, accessToken string) *Client {
 	return &Client{sc, ctx}
 }
 
+// Enable debug logging in development
+func init() {
+	if appengine.IsDevAppServer() {
+		stripe.LogLevel = 2
+	}
+}
+
 // Covert a payment model into a card card we can use for authorization
 func PaymentToCard(pay *payment.Payment) *stripe.CardParams {
 	card := stripe.CardParams{}
@@ -66,7 +73,7 @@ func (c Client) Authorize(pay *payment.Payment) (*Token, error) {
 	}
 
 	// Cast back to our token
-	return (*Token)(t), err
+	return (*Token)(t), nil
 }
 
 // Attempts to refund payment and updates the payment in datastore
@@ -114,22 +121,20 @@ func (c Client) GetCard(cardId string, customerId string) (*Card, error) {
 		return nil, errors.New(err)
 	}
 
-	return (*Card)(card), err
+	return (*Card)(card), nil
 }
 
 // Get Stripe customer
-func (c Client) GetCustomer(token, user *user.User) (*Customer, error) {
+func (c Client) GetCustomer(token, usr *user.User) (*Customer, error) {
 	params := &stripe.CustomerParams{}
 	params.SetSource(token)
 
-	customerId := user.Accounts.Stripe.CustomerId
-
-	customer, err := c.API.Customers.Get(customerId, params)
+	cust, err := c.API.Customers.Get(usr.Accounts.Stripe.CustomerId, params)
 	if err != nil {
 		return nil, errors.New(err)
 	}
 
-	return (*Customer)(customer), err
+	return (*Customer)(cust), nil
 }
 
 // Update Stripe customer
@@ -152,12 +157,12 @@ func (c Client) UpdateCustomer(usr *user.User) (*Customer, error) {
 
 	customerId := usr.Accounts.Stripe.CustomerId
 
-	customer, err := c.API.Customers.Update(customerId, params)
+	cust, err := c.API.Customers.Update(customerId, params)
 	if err != nil {
 		return nil, errors.New(err)
 	}
 
-	return (*Customer)(customer), err
+	return (*Customer)(cust), nil
 }
 
 // Create new stripe customer
@@ -175,18 +180,18 @@ func (c Client) NewCustomer(token string, user *user.User) (*Customer, error) {
 
 	params.AddMeta("user", user.Id())
 
-	customer, err := c.API.Customers.New(params)
+	cust, err := c.API.Customers.New(params)
 	if err != nil {
 		return nil, errors.New(err)
 	}
 
-	return (*Customer)(customer), err
+	return (*Customer)(cust), nil
 }
 
 // Add new card to Stripe customer
-func (c Client) AddCard(token string, user *user.User) (*Card, error) {
+func (c Client) AddCard(token string, usr *user.User) (*Card, error) {
 	params := &stripe.CardParams{
-		Customer: user.Accounts.Stripe.CustomerId,
+		Customer: usr.Accounts.Stripe.CustomerId,
 		Token:    token,
 	}
 
@@ -195,14 +200,13 @@ func (c Client) AddCard(token string, user *user.User) (*Card, error) {
 		return nil, errors.New(err)
 	}
 
-	return (*Card)(card), err
+	return (*Card)(card), nil
 }
 
 // Update card associated with Stripe customer
-func (c Client) UpdateCard(token string, pay *payment.Payment, user *user.User) (*Card, error) {
-	acct := user.Accounts.Stripe
-	customerId := acct.CustomerId
-	cardId := acct.CardId
+func (c Client) UpdateCard(token string, pay *payment.Payment, usr *user.User) (*Card, error) {
+	customerId := usr.Accounts.Stripe.CustomerId
+	cardId := usr.Accounts.Stripe.CardId
 
 	params := &stripe.CardParams{
 		Customer: customerId,
@@ -214,35 +218,33 @@ func (c Client) UpdateCard(token string, pay *payment.Payment, user *user.User) 
 		return nil, errors.New(err)
 	}
 
-	return (*Card)(card), err
+	return (*Card)(card), nil
 }
 
 // Update card associated with Stripe customer
-func (c Client) DeleteCard(id string, usr *user.User) (*Card, error) {
-	acct := usr.Accounts.Stripe
-	customerId := acct.CustomerId
-
+func (c Client) DeleteCard(cardId string, usr *user.User) (*Card, error) {
 	params := &stripe.CardParams{
-		Customer: customerId,
+		Customer: usr.Accounts.Stripe.CustomerId,
 	}
 
-	card, err := c.API.Cards.Del(id, params)
+	card, err := c.API.Cards.Del(cardId, params)
 	if err != nil {
 		return nil, errors.New(err)
 	}
 
-	return (*Card)(card), err
+	return (*Card)(card), nil
 }
 
 func (c Client) GetCharge(chargeId string) (*Charge, error) {
 	params := &stripe.ChargeParams{}
 	params.Expand("balance_transaction")
+
 	charge, err := c.API.Charges.Get(chargeId, params)
 	if err != nil {
 		return nil, err
 	}
 
-	return (*Charge)(charge), err
+	return (*Charge)(charge), nil
 }
 
 // Update Stripe charge
@@ -272,7 +274,7 @@ func (c Client) UpdateCharge(pay *payment.Payment) (*Charge, error) {
 		return nil, errors.New(err)
 	}
 
-	return (*Charge)(charge), err
+	return (*Charge)(charge), nil
 }
 
 // Create new charge
@@ -316,16 +318,17 @@ func (c Client) NewCharge(source interface{}, pay *payment.Payment) (*Charge, er
 	// Update charge Id on payment
 	pay.Account.ChargeId = ch.ID
 
-	return (*Charge)(ch), err
+	return (*Charge)(ch), nil
 }
 
 // Capture charge
-func (c Client) Capture(id string) (*Charge, error) {
-	log.Debug("Capture %v", id)
-	ch, err := c.API.Charges.Capture(id, nil)
+func (c Client) Capture(chargeId string) (*Charge, error) {
+	log.Debug("Capture charge '%s'", chargeId)
+
+	ch, err := c.API.Charges.Capture(chargeId, nil)
 	if err != nil {
 		return nil, errors.New(err)
 	}
 
-	return (*Charge)(ch), err
+	return (*Charge)(ch), nil
 }
