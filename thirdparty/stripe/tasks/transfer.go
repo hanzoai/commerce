76d4c3f36aa345ec0dae59d8a7a7ae715172b5ce
Zach Kelling <z@zeekay.io>
2016-08-30 22:37:51 -0500

    Add Transfer API to integration. Refactor slightly.

--- thirdparty/stripe/tasks/transfer.go
+++ thirdparty/stripe/tasks/transfer.go
@@ -6,47 +6,9 @@ import (
 	"appengine"
 	"appengine/delay"
 
-	"crowdstart.com/models/transfer"
-	"crowdstart.com/models/types/currency"
 	"crowdstart.com/thirdparty/stripe"
 	"crowdstart.com/util/log"
 )
-
-// Update payment from transfer
-func UpdateTransfer(tr *transfer.Transfer, str *stripe.Transfer) {
-	tr.Amount = currency.Cents(str.Amount)
-	tr.AmountReversed = currency.Cents(str.AmountReversed)
-	tr.Currency = currency.Type(str.Currency)
-	tr.Live = str.Live
-
-	tr.Account.ApplicationFee = str.Tx.Fee
-	tr.Account.BalanceTransaction = str.Tx.Amount
-	tr.Account.Date = time.Unix(str.Date, 0)
-	tr.Account.Created = time.Unix(str.Date, 0)
-	tr.Account.Description = str.Desc
-	tr.Account.Destination = str.Dest.ID
-	tr.Account.DestinationType = string(str.Dest.Type)
-	tr.Account.FailureCode = string(str.FailCode)
-	tr.Account.FailureMessage = str.FailMsg
-	tr.Account.Reversed = str.Reversed
-	tr.Account.SourceTransaction = str.SourceTx.ID
-	tr.Account.SourceType = string(str.SourceType)
-	tr.Account.StatementDescriptor = str.Statement
-	tr.Account.Type = string(str.Type)
-
-	switch str.Status {
-	case "paid":
-		tr.Status = transfer.Paid
-	case "pending":
-		tr.Status = transfer.Pending
-	case "in_transit":
-		tr.Status = transfer.InTransit
-	case "cancelled":
-		tr.Status = transfer.Canceled
-	case "failed":
-		tr.Status = transfer.Failed
-	}
-}
 
 // Synchronize payment using transfer
 var TransferSync = delay.Func("stripe-transfer-sync", func(ctx appengine.Context, ns string, token string, str stripe.Transfer, start time.Time) {
@@ -72,7 +34,7 @@ var TransferSync = delay.Func("stripe-transfer-sync", func(ctx appengine.Context
 	// Update transfer
 	err = tr.RunInTransaction(func() error {
 		log.Debug("Transfer before: %+v", tr, ctx)
-		UpdateTransfer(tr, &str)
+		stripe.UpdateTransferFromStripe(tr, &str)
 		log.Debug("Transfer after: %+v", tr, ctx)
 
 		return tr.Put()
