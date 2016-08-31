57c2584f6e002e10f48b90e4f9d001b9b3a0edb6
zeekay <zk@monoid.io>
2016-08-30 19:28:06 -0500

    Add new model/key mappings.

--- util/hashid/kind.go
+++ util/hashid/kind.go
@@ -29,7 +29,10 @@ var kinds = map[string]int{
 	"site":         22,
 	"deploy":       23,
 	"submission":   24,
-	"cart":         31,
+	"affiliate":    32,
+	"fee":          33,
+	"transfer":     34,
+	"reversal":     35,
 }
 
 var kindsReversed = make(map[int]string)
