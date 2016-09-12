1e9b9c1c7abd0234448c11416ebe5b79e56bb62f
Timothy Messer <tmesser@verus.io>
2016-09-12 18:13:04 -0500

    Adding rolling x deterimination function to affiliate handler

--- cron/affiliate/handlers.go
+++ cron/affiliate/handlers.go
@@ -1,7 +1,19 @@
 package affiliate
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"crowdstart.com/models/affiliate"
+	"github.com/gin-gonic/gin"
+)
 
 func Payout(c *gin.Context) {
 
+}
+
+func isEligibleForPayout(aff *affiliate.Affiliate) bool {
+	year, month, day := time.Now().UTC().Date()
+	dateToday := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+	dateToPay := aff.LastPaid.AddDate(0, 0, aff.Period)
+	return (dateToday.After(dateToPay) || dateToday == dateToPay)
 }
