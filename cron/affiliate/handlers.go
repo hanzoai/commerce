a03dab6c15b7367d340e68b1d23827597e816d64
Timothy Messer <tmesser@verus.io>
2016-09-12 18:15:39 -0500

    adding in stripe sending and marking fees paid stuff

--- cron/affiliate/handlers.go
+++ cron/affiliate/handlers.go
@@ -4,6 +4,10 @@ import (
 	"time"
 
 	"crowdstart.com/models/affiliate"
+	"crowdstart.com/models/fee"
+	"crowdstart.com/models/multi"
+	"crowdstart.com/models/transfer"
+	"crowdstart.com/thirdparty/stripe"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,4 +20,24 @@ func isEligibleForPayout(aff *affiliate.Affiliate) bool {
 	dateToday := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 	dateToPay := aff.LastPaid.AddDate(0, 0, aff.Period)
 	return (dateToday.After(dateToPay) || dateToday == dateToPay)
+}
+
+func sendTransferToStripe(st *stripe.Client, tr *transfer.Transfer) error {
+	_, err := st.Transfer(tr)
+	if err != nil {
+		return err
+	}
+	tr.MustPut()
+	return nil
+}
+
+func markFeesPaid(fees []*fee.Fee) error {
+	for _, fe := range fees {
+		fe.Status = fee.Paid
+	}
+	err := multi.Update(fees)
+	if err != nil {
+		return err
+	}
+	return nil
 }
