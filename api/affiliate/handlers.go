40d717323d6f23dcf09176a48feb88701028a597
Zach Kelling <z@zeekay.io>
2016-09-13 14:53:57 -0500

    Fix a few typos.

--- api/affiliate/handlers.go
+++ api/affiliate/handlers.go
@@ -5,7 +5,6 @@ import (
 
 	"crowdstart.com/middleware"
 	"crowdstart.com/models/affiliate"
-	"crowdstart.com/util/permission"
 	"crowdstart.com/util/rest"
 	"crowdstart.com/util/router"
 )
@@ -13,14 +12,12 @@ import (
 func Route(router router.Router, args ...gin.HandlerFunc) {
 	namespaced := middleware.Namespace()
 	tokenRequired := middleware.TokenRequired()
-	writeAffiliateRequired := middleware.TokenRequired(permission.Admin, permission.WriteUser)
+	// writeAffiliateRequired := middleware.TokenRequired(permission.Admin, permission.WriteUser)
 
 	api := rest.New(affiliate.Affiliate{})
 	api.Create = create(api)
 
 	api.GET("/:affiliateid/connect", tokenRequired, namespaced, connect)
-	api.GET("/:affiliateid/enable", writeAffiliateRequired, namespaced, stripeCallback)
-	api.GET("/:affiliateid/disable", writeAffiliateRequired, namespaced, stripeCallback)
 
 	api.Route(router, args...)
 }
