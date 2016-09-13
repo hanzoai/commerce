74e950096e2d7595bc324187899d097e4e556eae
zeekay <zk@monoid.io>
2016-09-13 00:19:08 -0500

    Update cronjobs.

---
+++ cron/platform/platform.go
@@ -0,0 +1,37 @@
+package platform
+
+import (
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/fee"
+	"crowdstart.com/models/multi"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/transfer"
+	"crowdstart.com/thirdparty/stripe"
+)
+
+func payout(db *datastore.Datastore) error {
+	orgs := make([]*organization.Organization, 0)
+	if _, err := organization.Query(db).GetAll(&orgs); err != nil {
+		return err
+	}
+	return nil
+}
+
+func fetchFees(db *datastore.Datastore) ([]*fee.Fee, error) {
+	fees := make([]*fee.Fee, 0)
+	if _, err := fee.Query(db).Filter("TransferId=", "").GetAll(&fees); err != nil {
+		return nil, err
+	}
+	return fees, nil
+}
+
+func transferFee(db *datastore.Datastore, sc *stripe.Client, fe *fee.Fee) error {
+	tr := transfer.New(db)
+	fe.TransferId = tr.Id()
+	fe.Status = fee.Paid
+	if _, err := sc.Transfer(tr); err != nil {
+		return err
+	}
+	models := []interface{}{tr, fe}
+	return multi.Update(models)
+}
