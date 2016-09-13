92ca1a6ba69f7a12f7b96bd42ec5827a07a3a01a
Zach Kelling <z@zeekay.io>
2016-09-13 00:19:08 -0500

    Update cronjobs.

--- cron/platform/handlers.go
+++ cron/platform/handlers.go
@@ -1,32 +1,17 @@
 package platform
 
 import (
-	"crowdstart.com/datastore"
-	"crowdstart.com/models/fee"
-	"crowdstart.com/models/multi"
-	"crowdstart.com/models/transfer"
-	"crowdstart.com/thirdparty/stripe"
 	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/util/task"
 )
 
+// Handler to trigger payout
 func Payout(c *gin.Context) {
+	db := datastore.New(c)
+	payout(db)
 }
 
-func fetchFees(db *datastore.Datastore) ([]*fee.Fee, error) {
-	fees := make([]*fee.Fee, 0)
-	if _, err := fee.Query(db).Filter("TransferId=", "").GetAll(&fees); err != nil {
-		return nil, err
-	}
-	return fees, nil
-}
-
-func transferFee(db *datastore.Datastore, sc *stripe.Client, fe *fee.Fee) error {
-	tr := transfer.New(db)
-	fe.TransferId = tr.Id()
-	fe.Status = fee.Paid
-	if _, err := sc.Transfer(tr); err != nil {
-		return err
-	}
-	models := []interface{}{tr, fe}
-	return multi.Update(models)
-}
+// Register task
+var _ = task.New("payout-platform", Payout)
