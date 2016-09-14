b4ee725e30f79a99e3c27233bfbe58fceda16e80
Zach Kelling <z@zeekay.io>
2016-09-14 12:32:30 -0500

    Add minimum to commission type.

--- models/types/commission/commission.go
+++ models/types/commission/commission.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Commission struct {
+	Minimum currency.Cents `json:"minimum,omitempty"`
 	Percent float64        `json:"percent,omitempty"`
 	Flat    currency.Cents `json:"flat,omitempty"`
 }
