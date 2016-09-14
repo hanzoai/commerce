d49e714767f774b6b1229e3112b2338632f13842
Zach Kelling <z@zeekay.io>
2016-09-14 00:17:31 -0500

    Better error handling.

--- models/transfer/transfer.go
+++ models/transfer/transfer.go
@@ -18,29 +18,39 @@ const (
 type Status string
 
 const (
+	// Stripe status
 	Canceled  Status = "canceled"
 	Failed           = "failed"
 	InTransit        = "in-transit"
 	Paid             = "paid"
 	Pending          = "pending"
+
+	// Failed to submit to stripe
+	Error = "error"
 )
 
 type StripeAccount struct {
-	Id                  string    `json:"transferId,omimtempty"`
-	ApplicationFee      int64     `json:"applicationFee,omitempty"` // FIXME: Apparently not returned by stripe-go?
-	BalanceTransaction  int64     `json:"balanceTransaction,omitempty"`
-	Created             time.Time `json:"created,omitempty"`
-	Date                time.Time `json:"date,omitempty"`
-	Description         string    `json:"description,omitempty"`
-	Destination         string    `json:"destination,omitempty"`
-	DestinationType     string    `json:"destinationType,omitempty"`
-	FailureCode         string    `json:"failureCode,omitempty"`
-	FailureMessage      string    `json:"failureMessage,omitempty"`
-	Reversed            bool      `json:"reversed,omitempty"`
-	SourceTransaction   string    `json:"sourceTransaction,omitempty"`
-	SourceType          string    `json:"sourceType,omitempty"`
-	StatementDescriptor string    `json:"statementDescriptor,omitempty"`
-	Type                string    `json:"type,omitempty"`
+	Id   string `json:"transferId,omimtempty"`
+	Type string `json:"type,omitempty"`
+
+	ApplicationFee int64 `json:"applicationFee,omitempty"` // FIXME: Apparently not returned by stripe-go?
+
+	BalanceTransaction int64     `json:"balanceTransaction,omitempty"`
+	Created            time.Time `json:"created,omitempty"`
+	Date               time.Time `json:"date,omitempty"`
+	Description        string    `json:"description,omitempty"`
+	Destination        string    `json:"destination,omitempty"`
+	DestinationType    string    `json:"destinationType,omitempty"`
+
+	FailureCode    string `json:"failureCode,omitempty"`
+	FailureMessage string `json:"failureMessage,omitempty"`
+
+	Reversed bool `json:"reversed,omitempty"`
+
+	SourceTransaction string `json:"sourceTransaction,omitempty"`
+	SourceType        string `json:"sourceType,omitempty"`
+
+	StatementDescriptor string `json:"statementDescriptor,omitempty"`
 }
 
 type Account struct {
