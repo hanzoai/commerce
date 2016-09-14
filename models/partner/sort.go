37d116cea166db82e42c0ad35a08694b56dd6b15
Zach Kelling <z@zeekay.io>
2016-09-13 20:24:29 -0500

    Refactor, clean-up.

--- models/partner/sort.go
+++ models/partner/sort.go
@@ -4,4 +4,4 @@ type ByName []*Partner
 
 func (a ByName) Len() int           { return len(a) }
 func (a ByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByName) Less(i, j int) bool { return a[i].LastName < a[j].LastName }
+func (a ByName) Less(i, j int) bool { return a[i].Name < a[j].Name }
