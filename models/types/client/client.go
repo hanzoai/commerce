028934b02247a8953e9d524698af44374b97ad95
Zach Kelling <z@zeekay.io>
2016-09-17 05:27:44 -0500

    Save geopoint.

--- models/types/client/client.go
+++ models/types/client/client.go
@@ -1,26 +1,42 @@
 package client
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+
+	"appengine"
+)
 
 type Client struct {
-	Ip        string `json:"ip,omitempty"`
-	UserAgent string `json:"userAgent,omitempty"`
-	Language  string `json:"language,omitempty"`
-	Referer   string `json:"referer,omitempty"`
-	Country   string `json:"country,omitempty"`
-	Region    string `json:"region,omitempty"`
-	City      string `json:"city,omitempty"`
+	Ip        string             `json:"ip,omitempty"`
+	UserAgent string             `json:"userAgent,omitempty"`
+	Referer   string             `json:"referer,omitempty"`
+	Language  string             `json:"language,omitempty"`
+	Country   string             `json:"country,omitempty"`
+	Region    string             `json:"region,omitempty"`
+	City      string             `json:"city,omitempty"`
+	GeoPoint  appengine.GeoPoint `json:"geoPoint,omitempty"`
 }
 
 func New(c *gin.Context) Client {
+	req := c.Request
+
+	// Parse geopoint
+	geo := c.Request.Header.Get("X-AppEngine-CitLatLong")
+	lat, _ := strconv.ParseFloat(geo, 64)
+	long, _ := strconv.ParseFloat(geo, 64)
+	geoPoint := appengine.GeoPoint{lat, long}
+
 	return Client{
-		Ip:        c.Request.RemoteAddr,
-		UserAgent: c.Request.UserAgent(),
-		Language:  c.Request.Header.Get("Accept-Language"),
-		Referer:   c.Request.Referer(),
-		Country:   c.Request.Header.Get("X-AppEngine-Country"),
-		Region:    c.Request.Header.Get("X-AppEngine-Region"),
-		City:      c.Request.Header.Get("X-AppEngine-City"),
+		Ip:        req.RemoteAddr,
+		UserAgent: req.UserAgent(),
+		Referer:   req.Referer(),
+		Language:  req.Header.Get("Accept-Language"),
+		Country:   req.Header.Get("X-AppEngine-Country"),
+		Region:    req.Header.Get("X-AppEngine-Region"),
+		City:      req.Header.Get("X-AppEngine-City"),
+		GeoPoint:  geoPoint,
 	}
 }
 
