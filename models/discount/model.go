7f60b96762f2cecb879c733aa72bd178d26cfb84
Zach Kelling <z@zeekay.io>
2016-09-19 17:30:39 -0500

    Initial commit of discount type.

---
+++ models/discount/model.go
@@ -0,0 +1,28 @@
+package discount
+
+import (
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/mixin"
+)
+
+func (d Discount) Kind() string {
+	return "discount"
+}
+
+func (d *Discount) Init(db *datastore.Datastore) {
+	d.Model.Init(db, d)
+}
+
+func (d *Discount) Defaults() {
+	d.Enabled = true
+}
+
+func New(db *datastore.Datastore) *Discount {
+	d := new(Discount)
+	d.Init(db)
+	return d
+}
+
+func Query(db *datastore.Datastore) *mixin.Query {
+	return New(db).Query()
+}
