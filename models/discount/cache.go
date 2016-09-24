9319c8fb722bfc0e82f7643b3a66942ce9523002
Zach Kelling <z@zeekay.io>
2016-09-23 22:06:40 -0500

    Keep cache logic in one place.

---
+++ models/discount/cache.go
@@ -0,0 +1,109 @@
+package discount
+
+import (
+	"fmt"
+
+	"appengine"
+	"appengine/memcache"
+
+	aeds "appengine/datastore"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/discount/scope"
+)
+
+// Computes memcache key, using format:
+//	discount-keys-organization
+//  discount-keys-store-storeId
+//  ..etc
+func keyForScope(scopeType scope.Type, id string) string {
+	key := "discount-keys-"
+	keyFmt := key + "%s-%s"
+	scopeName := string(scopeType)
+
+	switch scopeType {
+	case scope.Organization:
+		key = key + scopeName
+	case scope.Store:
+		key = fmt.Sprintf(keyFmt, scopeName, id)
+	case scope.Collection:
+		key = fmt.Sprintf(keyFmt, scopeName, id)
+	case scope.Product:
+		key = fmt.Sprintf(keyFmt, scopeName, id)
+	case scope.Variant:
+		key = fmt.Sprintf(keyFmt, scopeName, id)
+	}
+
+	return key
+}
+
+// Invalidate cache for all keys in matching scope
+func (d *Discount) invalidateCache() error {
+	key := keyForScope(d.Scope.Type, d.ScopeId())
+	err := memcache.Delete(d.Context(), key)
+	if err == memcache.ErrCacheMiss {
+		err = nil
+	}
+	return err
+}
+
+// Cache discount keys
+func cacheDiscounts(ctx appengine.Context, key string, keys []*aeds.Key) error {
+	return memcache.Gob.Set(ctx,
+		&memcache.Item{
+			Key:    key,
+			Object: keys,
+		})
+}
+
+// Get cached discount keys
+func getCachedDiscounts(ctx appengine.Context, key string) ([]*aeds.Key, error) {
+	keys := make([]*aeds.Key, 0)
+	_, err := memcache.Gob.Get(ctx, key, keys)
+	if err != nil {
+		return keys, err
+	}
+
+	return keys, nil
+}
+
+func GetScopedDiscounts(ctx appengine.Context, sc scope.Type, id string, keyc chan []*aeds.Key, errc chan error) {
+	// Check memcache for keys
+	key := keyForScope(sc, id)
+	keys, err := getCachedDiscounts(ctx, key)
+
+	// Fetch keys from datastore if that fails
+	if err != nil {
+		var filter string
+		switch sc {
+		case scope.Store:
+			filter = "StoreId="
+		case scope.Collection:
+			filter = "CollectionId="
+		case scope.Product:
+			filter = "ProductId="
+		case scope.Variant:
+			filter = "VariantId="
+		}
+
+		query := Query(datastore.New(ctx)).Filter("Scope=", string(sc))
+
+		if filter != "" {
+			query = query.Filter(filter, id)
+		}
+
+		keys, err = query.
+			Filter("Enabled=", true).
+			KeysOnly().
+			GetAll(nil)
+
+		// Cache keys for later
+		if err != nil {
+			err = cacheDiscounts(ctx, key, keys)
+		}
+	}
+
+	// Return with keys
+	errc <- err
+	keyc <- keys
+}
--- models/discount/hooks.go
+++ models/discount/hooks.go
@@ -1,48 +1,5 @@
 package discount
 
-import (
-	"fmt"
-
-	"appengine/memcache"
-
-	"crowdstart.com/models/discount/scope"
-)
-
-// Computes memcache key, using format:
-//	discount-keys-organization
-//  discount-keys-store-storeId
-//  ..etc
-func KeyForScope(scopeType scope.Type, id string) string {
-	key := "discount-keys-"
-	keyFmt := key + "%s-%s"
-	scopeName := string(scopeType)
-
-	switch scopeType {
-	case scope.Organization:
-		key = key + scopeName
-	case scope.Store:
-		key = fmt.Sprintf(keyFmt, scopeName, id)
-	case scope.Collection:
-		key = fmt.Sprintf(keyFmt, scopeName, id)
-	case scope.Product:
-		key = fmt.Sprintf(keyFmt, scopeName, id)
-	case scope.Variant:
-		key = fmt.Sprintf(keyFmt, scopeName, id)
-	}
-
-	return key
-}
-
-// Invalidate cache for all keys in matching scope
-func (d *Discount) invalidateCache() error {
-	key := KeyForScope(d.Scope.Type, d.ScopeId())
-	err := memcache.Delete(d.Context(), key)
-	if err == memcache.ErrCacheMiss {
-		err = nil
-	}
-	return err
-}
-
 // Invalidate cache based on scope
 func (d *Discount) AfterCreate() error {
 	return d.invalidateCache()
