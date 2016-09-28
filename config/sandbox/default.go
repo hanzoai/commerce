ccccacda12d11bc3f9079f8d111cef0c51170177
zeekay <zk@monoid.io>
2016-09-28 15:57:07 -0500

    Always set releasemode for gin.

--- config/sandbox/default.go
+++ config/sandbox/default.go
@@ -9,6 +9,7 @@ import (
 func init() {
 	_default.Init()
 
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 	router.GET("/", func(c *gin.Context) {
 		c.Redirect(301, "http://www.crowdstart.com/docs")
