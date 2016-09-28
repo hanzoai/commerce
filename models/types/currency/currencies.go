f4a56ad29b643f13b68b11a18f3c0147f1902598
zeekay <zk@monoid.io>
2016-09-28 11:21:43 -0500

    Add fake helpers.

---
+++ models/types/currency/currencies.go
@@ -0,0 +1,287 @@
+package currency
+
+const (
+	USD Type = "usd"
+	AUD      = "aud"
+	CAD      = "cad"
+	EUR      = "eur"
+	GBP      = "gbp"
+	HKD      = "hkd"
+	JPY      = "jpy"
+	NZD      = "nzd"
+	SGD      = "sgd"
+	AED      = "aed" // United Arab Emirates Dirham
+	AFN      = "afn" // Afghan Afghani*
+	ALL      = "all" // Albanian Lek
+	AMD      = "amd" // Armenian Dram
+	ANG      = "ang" // Netherlands Antillean Gulden
+	AOA      = "aoa" // Angolan Kwanza*
+	ARS      = "ars" // Argentine Peso*
+	AWG      = "awg" // Aruban Florin
+	AZN      = "azn" // Azerbaijani Manat
+	BAM      = "bam" // Bosnia & Herzegovina Convertible Mark
+	BBD      = "bbd" // Barbadian Dollar
+	BDT      = "bdt" // Bangladeshi Taka
+	BGN      = "bgn" // Bulgarian Lev
+	BIF      = "bif" // Burundian Franc
+	BMD      = "bmd" // Bermudian Dollar
+	BND      = "bnd" // Brunei Dollar
+	BOB      = "bob" // Bolivian Boliviano*
+	BRL      = "brl" // Brazilian Real*
+	BSD      = "bsd" // Bahamian Dollar
+	BWP      = "bwp" // Botswana Pula
+	BZD      = "bzd" // Belize Dollar
+	CDF      = "cdf" // Congolese Franc
+	CHF      = "chf" // Swiss Franc
+	CLP      = "clp" // Chilean Peso*
+	CNY      = "cny" // Chinese Renminbi Yuan
+	COP      = "cop" // Colombian Peso*
+	CRC      = "crc" // Costa Rican Colón*
+	CVE      = "cve" // Cape Verdean Escudo*
+	CZK      = "czk" // Czech Koruna*
+	DJF      = "djf" // Djiboutian Franc*
+	DKK      = "dkk" // Danish Krone
+	DOP      = "dop" // Dominican Peso
+	DZD      = "dzd" // Algerian Dinar
+	EEK      = "eek" // Estonian Kroon*
+	EGP      = "egp" // Egyptian Pound
+	ETB      = "etb" // Ethiopian Birr
+	FJD      = "fjd" // Fijian Dollar
+	FKP      = "fkp" // Falkland Islands Pound*
+	GEL      = "gel" // Georgian Lari
+	GIP      = "gip" // Gibraltar Pound
+	GMD      = "gmd" // Gambian Dalasi
+	GNF      = "gnf" // Guinean Franc*
+	GTQ      = "gtq" // Guatemalan Quetzal*
+	GYD      = "gyd" // Guyanese Dollar
+	HNL      = "hnl" // Honduran Lempira*
+	HRK      = "hrk" // Croatian Kuna
+	HTG      = "htg" // Haitian Gourde
+	HUF      = "huf" // Hungarian Forint*
+	IDR      = "idr" // Indonesian Rupiah
+	ILS      = "ils" // Israeli New Sheqel
+	INR      = "inr" // Indian Rupee*
+	ISK      = "isk" // Icelandic Króna
+	JMD      = "jmd" // Jamaican Dollar
+	KES      = "kes" // Kenyan Shilling
+	KGS      = "kgs" // Kyrgyzstani Som
+	KHR      = "khr" // Cambodian Riel
+	KMF      = "kmf" // Comorian Franc
+	KRW      = "krw" // South Korean Won
+	KYD      = "kyd" // Cayman Islands Dollar
+	KZT      = "kzt" // Kazakhstani Tenge
+	LAK      = "lak" // Lao Kip*
+	LBP      = "lbp" // Lebanese Pound
+	LKR      = "lkr" // Sri Lankan Rupee
+	LRD      = "lrd" // Liberian Dollar
+	LSL      = "lsl" // Lesotho Loti
+	LTL      = "ltl" // Lithuanian Litas
+	LVL      = "lvl" // Latvian Lats
+	MAD      = "mad" // Moroccan Dirham
+	MDL      = "mdl" // Moldovan Leu
+	MGA      = "mga" // Malagasy Ariary
+	MKD      = "mkd" // Macedonian Denar
+	MNT      = "mnt" // Mongolian Tögrög
+	MOP      = "mop" // Macanese Pataca
+	MRO      = "mro" // Mauritanian Ouguiya
+	MUR      = "mur" // Mauritian Rupee*
+	MVR      = "mvr" // Maldivian Rufiyaa
+	MWK      = "mwk" // Malawian Kwacha
+	MXN      = "mxn" // Mexican Peso*
+	MYR      = "myr" // Malaysian Ringgit
+	MZN      = "mzn" // Mozambican Metical
+	NAD      = "nad" // Namibian Dollar
+	NGN      = "ngn" // Nigerian Naira
+	NIO      = "nio" // Nicaraguan Córdoba*
+	NOK      = "nok" // Norwegian Krone
+	NPR      = "npr" // Nepalese Rupee
+	PAB      = "pab" // Panamanian Balboa*
+	PEN      = "pen" // Peruvian Nuevo Sol*
+	PGK      = "pgk" // Papua New Guinean Kina
+	PHP      = "php" // Philippine Peso
+	PKR      = "pkr" // Pakistani Rupee
+	PLN      = "pln" // Polish Złoty
+	PYG      = "pyg" // Paraguayan Guaraní*
+	QAR      = "qar" // Qatari Riyal
+	RON      = "ron" // Romanian Leu
+	RSD      = "rsd" // Serbian Dinar
+	RUB      = "rub" // Russian Ruble
+	RWF      = "rwf" // Rwandan Franc
+	SAR      = "sar" // Saudi Riyal
+	SBD      = "sbd" // Solomon Islands Dollar
+	SCR      = "scr" // Seychellois Rupee
+	SEK      = "sek" // Swedish Krona
+	SHP      = "shp" // Saint Helenian Pound*
+	SLL      = "sll" // Sierra Leonean Leone
+	SOS      = "sos" // Somali Shilling
+	SRD      = "srd" // Surinamese Dollar*
+	STD      = "std" // São Tomé and Príncipe Dobra
+	SVC      = "svc" // Salvadoran Colón*
+	SZL      = "szl" // Swazi Lilangeni
+	THB      = "thb" // Thai Baht
+	TJS      = "tjs" // Tajikistani Somoni
+	TOP      = "top" // Tongan Paʻanga
+	TRY      = "try" // Turkish Lira
+	TTD      = "ttd" // Trinidad and Tobago Dollar
+	TWD      = "twd" // New Taiwan Dollar
+	TZS      = "tzs" // Tanzanian Shilling
+	UAH      = "uah" // Ukrainian Hryvnia
+	UGX      = "ugx" // Ugandan Shilling
+	UYU      = "uyu" // Uruguayan Peso*
+	UZS      = "uzs" // Uzbekistani Som
+	VND      = "vnd" // Vietnamese Đồng
+	VUV      = "vuv" // Vanuatu Vat
+	UWT      = "uwt" // Samoan Tala
+	XAF      = "xaf" // Central African Cfa Franc
+	XCD      = "xcd" // East Caribbean Dollar
+	XOF      = "xof" // West African Cfa Franc*
+	XPF      = "xpf" // Cfp Franc*
+	YER      = "yer" // Yemeni Rial
+	ZAR      = "zar" // South African Rand
+	ZMW      = "zmw" // Zambian Kwacha
+
+	PNT = "points" // points
+)
+
+var Types = []Type{
+	USD,
+	AUD,
+	CAD,
+	EUR,
+	GBP,
+	HKD,
+	JPY,
+	NZD,
+	SGD,
+	AED,
+	AFN,
+	ALL,
+	AMD,
+	ANG,
+	AOA,
+	ARS,
+	AWG,
+	AZN,
+	BAM,
+	BBD,
+	BDT,
+	BGN,
+	BIF,
+	BMD,
+	BND,
+	BOB,
+	BRL,
+	BSD,
+	BWP,
+	BZD,
+	CDF,
+	CHF,
+	CLP,
+	CNY,
+	COP,
+	CRC,
+	CVE,
+	CZK,
+	DJF,
+	DKK,
+	DOP,
+	DZD,
+	EEK,
+	EGP,
+	ETB,
+	FJD,
+	FKP,
+	GEL,
+	GIP,
+	GMD,
+	GNF,
+	GTQ,
+	GYD,
+	HNL,
+	HRK,
+	HTG,
+	HUF,
+	IDR,
+	ILS,
+	INR,
+	ISK,
+	JMD,
+	KES,
+	KGS,
+	KHR,
+	KMF,
+	KRW,
+	KYD,
+	KZT,
+	LAK,
+	LBP,
+	LKR,
+	LRD,
+	LSL,
+	LTL,
+	LVL,
+	MAD,
+	MDL,
+	MGA,
+	MKD,
+	MNT,
+	MOP,
+	MRO,
+	MUR,
+	MVR,
+	MWK,
+	MXN,
+	MYR,
+	MZN,
+	NAD,
+	NGN,
+	NIO,
+	NOK,
+	NPR,
+	PAB,
+	PEN,
+	PGK,
+	PHP,
+	PKR,
+	PLN,
+	PYG,
+	QAR,
+	RON,
+	RSD,
+	RUB,
+	RWF,
+	SAR,
+	SBD,
+	SCR,
+	SEK,
+	SHP,
+	SLL,
+	SOS,
+	SRD,
+	STD,
+	SVC,
+	SZL,
+	THB,
+	TJS,
+	TOP,
+	TRY,
+	TTD,
+	TWD,
+	TZS,
+	UAH,
+	UGX,
+	UYU,
+	UZS,
+	VND,
+	VUV,
+	UWT,
+	XAF,
+	XCD,
+	XOF,
+	XPF,
+	YER,
+	ZAR,
+	ZMW,
+
+	PNT,
+}
