8f12ad57382f5df205f4fd3e3e65f6de9748013b
zeekay <zk@monoid.io>
2016-09-28 21:24:39 -0500

    Update product fake.

--- models/product/fake.go
+++ models/product/fake.go
@@ -2,6 +2,7 @@ package product
 
 import (
 	"crowdstart.com/datastore"
+	"crowdstart.com/models/types/currency"
 	"crowdstart.com/util/fake"
 	"crowdstart.com/util/slug"
 )
@@ -9,6 +10,12 @@ import (
 func Fake(db *datastore.Datastore) *Product {
 	prod := New(db)
 	prod.Name = fake.ProductName()
+	prod.Headline = fake.Sentence()
+	prod.Description = prod.Headline + " " + fake.Sentences(3)
 	prod.Slug = slug.Slugify(prod.Name)
+	prod.Currency = currency.USD
+	prod.Price = currency.Cents(0).Fake()
+	prod.Shipping = currency.Cents(0).FakeN(990)
+	prod.ListPrice = prod.Price * 2
 	return prod
 }
