156a4ca7eb7796f2c74fb90fbb5d587832ccac44
Zach Kelling <z@zeekay.io>
2016-10-02 16:54:20 -0500

    Add product tests.

---
+++ test/api/product_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"crowdstart.com/models/product"
+	"crowdstart.com/util/log"
+
+	. "crowdstart.com/util/test/ginkgo"
+)
+
+var _ = Describe("product", func() {
+	Context("New product", func() {
+		var req *product.Product
+		var res *product.Product
+
+		Before(func() {
+			req = product.Fake(db)
+			res = product.New(db)
+
+			// Create new product
+			cl.Post("/product", req, res)
+		})
+
+		It("Should create new products", func() {
+			Expect(res.Name).To(Equal(req.Name))
+			log.JSON("request:", req)
+			log.JSON("response:", res)
+		})
+	})
+})
