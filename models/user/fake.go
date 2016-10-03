790864a4b8aad2336b67fdbcca66b3a76b23153f
Timothy Messer <tmesser@verus.io>
2016-10-02 20:52:18 -0500

    Finishing affiliate test

---
+++ models/user/fake.go
@@ -0,0 +1,19 @@
+package user
+
+import (
+	"crowdstart.com/datastore"
+	. "crowdstart.com/models"
+	"crowdstart.com/util/fake"
+)
+
+func Fake(db *datastore.Datastore) *User {
+	usr := New(db)
+	usr.Username = fake.Username()
+	usr.FirstName = fake.FirstName()
+	usr.LastName = fake.LastName()
+	usr.BillingAddress = Address{Line1: fake.Street(), City: fake.City(), State: fake.State(), PostalCode: fake.Zip(), Country: "US"}
+	usr.ShippingAddress = Address{Line1: fake.Street(), City: fake.City(), State: fake.State(), PostalCode: fake.Zip(), Country: "US"}
+	usr.Email = fake.EmailAddress()
+	usr.Enabled = true
+	return usr
+}
