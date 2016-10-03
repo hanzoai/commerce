92f081119652ace651f5fa04b1690a081cd8daaf
Timothy Messer <tmesser@verus.io>
2016-10-03 11:36:47 -0500

    Order test/fake, un-screwing the fake.Bool() usage in a bunch of places

--- models/coupon/fake.go
+++ models/coupon/fake.go
@@ -13,12 +13,10 @@ func Fake(db *datastore.Datastore) *Coupon {
 	c.Name = fake.Word()
 	c.Type = Flat
 	c.Code_ = fake.Word()
-	c.Dynamic = fake.Bool
 	c.StartDate = time.Date(rand.Intn(25)+2000, time.Month(rand.Intn(12)+1), rand.Intn(25)+1, 0, 0, 0, 0, time.UTC)
 	c.EndDate = c.StartDate.AddDate(0, 0, rand.Intn(30))
-	c.Once = fake.Bool
 	c.Limit = rand.Intn(500) + 1
-	c.Enabled = fake.Bool
+	c.Enabled = true
 	c.Amount = rand.Intn(5000)
 	c.Used = c.Limit - rand.Intn(c.Limit)
 
