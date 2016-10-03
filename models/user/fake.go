314d9b987ca3340a28ee65adcdba1891ad4f4911
Timothy Messer <tmesser@verus.io>
2016-10-03 16:12:44 -0500

    adding user test and fixing user fake

--- models/user/fake.go
+++ models/user/fake.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"strings"
+
 	"crowdstart.com/datastore"
 	. "crowdstart.com/models"
 	"crowdstart.com/util/fake"
@@ -13,7 +15,7 @@ func Fake(db *datastore.Datastore) *User {
 	usr.LastName = fake.LastName()
 	usr.BillingAddress = Address{Line1: fake.Street(), City: fake.City(), State: fake.State(), PostalCode: fake.Zip(), Country: "US"}
 	usr.ShippingAddress = Address{Line1: fake.Street(), City: fake.City(), State: fake.State(), PostalCode: fake.Zip(), Country: "US"}
-	usr.Email = fake.EmailAddress()
+	usr.Email = strings.ToLower(fake.EmailAddress())
 	usr.Enabled = true
 	return usr
 }
