a2562924b9f8938159b9a6eba9f1b67e6e4b8c89
Timothy Messer <tmesser@verus.io>
2016-10-03 15:30:01 -0500

    Disabling site test until we have netlify reseller access sorted out

--- test/api/site_test.go
+++ test/api/site_test.go
@@ -18,11 +18,13 @@ var _ = Describe("site", func() {
 
 			// Create new site
 			log.JSON(req)
-			cl.Post("/site", req, res)
+			// TODO: Our netlify reseller access is outdated right now, so this test won't work
+			// cl.Post("/site", req, res)
 			log.JSON(res)
 		})
 
 		It("Should create new sites", func() {
+			Skip("netlify reseller access required/outdated right now")
 			Expect(res.Domain).To(Equal(req.Domain))
 			Expect(res.Name).To(Equal(req.Name))
 			Expect(res.Url).To(Equal(req.Url))
