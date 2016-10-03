2efe3fbfae0274400da75a25f7a74c14663dd666
Timothy Messer <tmesser@verus.io>
2016-10-03 13:29:06 -0500

    Finished un-screwing the order test, still looks like there's a bug

--- models/order/fake.go
+++ models/order/fake.go
@@ -11,21 +11,20 @@ import (
 )
 
 func Fake(db *datastore.Datastore, lis ...lineitem.LineItem) *Order {
+	linetotal := 0
+	for _, v := range lis {
+		linetotal += int(v.Price) * v.Quantity
+	}
 	o := New(db)
 	o.Email = fake.EmailAddress()
 	o.Items = lis
 	o.Status = Open
 	o.PaymentStatus = payment.Unpaid
 	o.Currency = currency.USD
-	o.LineTotal = currency.Cents(0).Fake()
-	o.Discount = currency.Cents(0).FakeN(990)
-	o.Subtotal = o.LineTotal - o.Discount
-	o.Shipping = currency.Cents(0).FakeN(990)
-	o.Tax = currency.Cents(0).FakeN(990)
+	o.LineTotal = currency.Cents(linetotal)
+	o.Subtotal = o.LineTotal
 	o.Total = o.Subtotal + o.Shipping + o.Tax
 	o.Balance = o.Total
-	o.Paid = currency.Cents(0)
-	o.Refunded = currency.Cents(0)
 	o.Company = fake.Company()
 	o.BillingAddress = Address{Line1: fake.Street(), City: fake.City(), State: fake.State(), PostalCode: fake.Zip(), Country: "US"}
 	o.ShippingAddress = Address{Line1: fake.Street(), City: fake.City(), State: fake.State(), PostalCode: fake.Zip(), Country: "US"}
