e183f0bc073603e73e5068cf681fc277dcad58d1
Timothy Messer <tmesser@verus.io>
2016-10-03 15:22:48 -0500

    Site fake, plus pushing a busted site test

---
+++ test/api/site_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"crowdstart.com/models/site"
+	"crowdstart.com/util/log"
+
+	. "crowdstart.com/util/test/ginkgo"
+)
+
+var _ = Describe("site", func() {
+	Context("New site", func() {
+		var req *site.Site
+		var res *site.Site
+
+		Before(func() {
+			req = site.Fake(db)
+			res = site.New(db)
+
+			// Create new site
+			log.JSON(req)
+			cl.Post("/site", req, res)
+			log.JSON(res)
+		})
+
+		It("Should create new sites", func() {
+			Expect(res.Domain).To(Equal(req.Domain))
+			Expect(res.Name).To(Equal(req.Name))
+			Expect(res.Url).To(Equal(req.Url))
+		})
+	})
+})
