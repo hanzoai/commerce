67506ee408f133ff142c8972647dae8ca593c8ba
Timothy Messer <tmesser@verus.io>
2016-10-02 22:02:22 -0500

    Fleshing out store fake

--- models/store/fake.go
+++ models/store/fake.go
@@ -3,9 +3,20 @@ package store
 import (
 	"crowdstart.com/datastore"
 	. "crowdstart.com/models"
+	"crowdstart.com/models/types/currency"
+	"crowdstart.com/util/fake"
+	"crowdstart.com/util/slug"
 )
 
 func Fake(db *datastore.Datastore) *Store {
 	s := New(db)
+	s.Name = fake.Company()
+	s.Slug = slug.Slugify(s.Name)
+	s.Domain = s.Name + ".com"
+	s.Prefix = fake.Word()
+	s.Currency = currency.USD
+	s.Address = Address{Line1: fake.Street(), City: fake.City(), State: fake.State(), PostalCode: fake.Zip(), Country: "US"}
+	s.Email = fake.EmailAddress()
+
 	return s
 }
