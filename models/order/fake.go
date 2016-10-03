321ce602572e6e49718b482e54f4c88087ab2711
Timothy Messer <tmesser@verus.io>
2016-10-03 01:03:20 -0500

    Fake for Order

---
+++ models/order/fake.go
@@ -0,0 +1,41 @@
+package order
+
+import (
+	"crowdstart.com/datastore"
+	. "crowdstart.com/models"
+	"crowdstart.com/models/payment"
+	"crowdstart.com/models/types/currency"
+	"crowdstart.com/util/fake"
+)
+
+func Fake(db *datastore.Datastore, storeId string, campaignId string, userId string, cartId string, referrerId string) *Order {
+	o := New(db)
+	o.StoreId = storeId
+	o.CampaignId = campaignId
+	o.UserId = userId
+	o.Email = fake.EmailAddress()
+	o.CartId = cartId
+	o.ReferrerId = referrerId
+	o.Status = Open
+	o.PaymentStatus = payment.Unpaid
+	o.Preorder = fake.Bool
+	o.Unconfirmed = fake.Bool
+	o.Currency = currency.USD
+	o.LineTotal = currency.Cents(0).Fake()
+	o.Discount = currency.Cents(0).FakeN(990)
+	o.Subtotal = o.LineTotal - o.Discount
+	o.Shipping = currency.Cents(0).FakeN(990)
+	o.Tax = currency.Cents(0).FakeN(990)
+	o.Total = o.Subtotal + o.Shipping + o.Tax
+	o.Balance = o.Total
+	o.Paid = currency.Cents(0)
+	o.Refunded = currency.Cents(0)
+	o.Company = fake.Company()
+	o.BillingAddress = Address{Line1: fake.Street(), City: fake.City(), State: fake.State(), PostalCode: fake.Zip(), Country: "US"}
+	o.ShippingAddress = Address{Line1: fake.Street(), City: fake.City(), State: fake.State(), PostalCode: fake.Zip(), Country: "US"}
+	o.Gift = fake.Bool
+	o.GiftMessage = fake.Sentence()
+	o.GiftEmail = fake.EmailAddress()
+
+	return o
+}
