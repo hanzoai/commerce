abdcde429760f55503b37f6a332c109817ba2e49
Timothy Messer <tmesser@verus.io>
2016-10-05 16:04:05 -0500

    Get/Delete for cart

--- test/api/cart_test.go
+++ test/api/cart_test.go
@@ -9,8 +9,8 @@ import (
 
 var _ = Describe("cart", func() {
 	Context("New cart", func() {
-		var req *cart.Cart
-		var res *cart.Cart
+		req := new(cart.Cart)
+		res := new(cart.Cart)
 
 		Before(func() {
 			usr := user.Fake(db)
@@ -40,6 +40,67 @@ var _ = Describe("cart", func() {
 			Expect(res.Gift).To(Equal(req.Gift))
 			Expect(res.GiftMessage).To(Equal(req.GiftMessage))
 			Expect(res.GiftEmail).To(Equal(req.GiftEmail))
+		})
+	})
+	Context("Get cart", func() {
+		req := new(cart.Cart)
+		res := new(cart.Cart)
+
+		Before(func() {
+			// Create user and cart
+			usr := user.Fake(db)
+			usr.MustCreate()
+
+			req = cart.Fake(db, usr.Id())
+			req.MustCreate()
+
+			// Verify it exists
+			res = cart.New(db)
+
+			// Get cart
+			cl.Get("/cart/"+req.Id(), res)
+		})
+
+		It("Should create new carts", func() {
+			Expect(res.UserId).To(Equal(req.UserId))
+			Expect(res.Email).To(Equal(req.Email))
+			Expect(res.ReferrerId).To(Equal(req.ReferrerId))
+			Expect(res.Status).To(Equal(req.Status))
+			Expect(res.Currency).To(Equal(req.Currency))
+			Expect(res.LineTotal).To(Equal(req.LineTotal))
+			Expect(res.Discount).To(Equal(req.Discount))
+			Expect(res.Subtotal).To(Equal(req.Subtotal))
+			Expect(res.Shipping).To(Equal(req.Shipping))
+			Expect(res.Tax).To(Equal(req.Tax))
+			Expect(res.Total).To(Equal(req.Total))
+			Expect(res.Company).To(Equal(req.Company))
+			Expect(res.BillingAddress).To(Equal(req.BillingAddress))
+			Expect(res.ShippingAddress).To(Equal(req.ShippingAddress))
+			Expect(res.Gift).To(Equal(req.Gift))
+			Expect(res.GiftMessage).To(Equal(req.GiftMessage))
+			Expect(res.GiftEmail).To(Equal(req.GiftEmail))
+		})
+	})
+	Context("Delete cart", func() {
+		res := ""
+
+		Before(func() {
+			usr := user.Fake(db)
+			usr.MustCreate()
+
+			req := cart.Fake(db, usr.Id())
+			req.MustCreate()
+
+			// Create new cart
+			cl.Delete("/cart/" + req.Id())
+
+			res = req.Id()
+		})
+
+		It("Should create new carts", func() {
+			cart := cart.New(db)
+			err := cart.GetById(res)
+			Expect(err).ToNot(BeNil())
 		})
 	})
 })
