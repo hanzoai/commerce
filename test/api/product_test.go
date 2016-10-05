ed43b53d39d7b61d4554f0f4626fafe88e357f00
Timothy Messer <tmesser@verus.io>
2016-10-05 17:37:53 -0500

    Product tests

--- test/api/product_test.go
+++ test/api/product_test.go
@@ -8,8 +8,8 @@ import (
 
 var _ = Describe("product", func() {
 	Context("New product", func() {
-		var req *product.Product
-		var res *product.Product
+		req := new(product.Product)
+		res := new(product.Product)
 
 		Before(func() {
 			req = product.Fake(db)
@@ -28,6 +28,46 @@ var _ = Describe("product", func() {
 			Expect(res.Price).To(Equal(req.Price))
 			Expect(res.Shipping).To(Equal(req.Shipping))
 			Expect(res.ListPrice).To(Equal(req.ListPrice))
+		})
+	})
+	Context("Get product", func() {
+		req := new(product.Product)
+		res := new(product.Product)
+
+		Before(func() {
+			req = product.Fake(db)
+			req.MustCreate()
+			res = product.New(db)
+
+			cl.Get("/product/"+req.Id(), res)
+		})
+
+		It("Should get products", func() {
+			Expect(res.Name).To(Equal(req.Name))
+			Expect(res.Headline).To(Equal(req.Headline))
+			Expect(res.Description).To(Equal(req.Description))
+			Expect(res.Slug).To(Equal(req.Slug))
+			Expect(res.Currency).To(Equal(req.Currency))
+			Expect(res.Price).To(Equal(req.Price))
+			Expect(res.Shipping).To(Equal(req.Shipping))
+			Expect(res.ListPrice).To(Equal(req.ListPrice))
+		})
+	})
+	Context("Delete product", func() {
+		res := ""
+
+		Before(func() {
+			req := product.Fake(db)
+			req.MustCreate()
+
+			cl.Delete("/product/" + req.Id())
+			res = req.Id()
+		})
+
+		It("Should get products", func() {
+			prod := product.New(db)
+			err := prod.GetById(res)
+			Expect(err).ToNot(BeNil())
 		})
 	})
 })
