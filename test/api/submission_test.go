fd4a8b6e7216e5a9a5753af0ca3c348902d51e1d
Timothy Messer <tmesser@verus.io>
2016-10-05 18:13:26 -0500

    Submission tests

--- test/api/submission_test.go
+++ test/api/submission_test.go
@@ -9,8 +9,8 @@ import (
 
 var _ = Describe("submission", func() {
 	Context("New submission", func() {
-		var req *submission.Submission
-		var res *submission.Submission
+		req := new(submission.Submission)
+		res := new(submission.Submission)
 
 		Before(func() {
 			usr := user.Fake(db)
@@ -25,6 +25,46 @@ var _ = Describe("submission", func() {
 		It("Should create new submissions", func() {
 			Expect(res.Email).To(Equal(req.Email))
 			Expect(res.UserId).To(Equal(req.UserId))
+		})
+	})
+	Context("Get submission", func() {
+		req := new(submission.Submission)
+		res := new(submission.Submission)
+
+		Before(func() {
+			usr := user.Fake(db)
+			usr.MustCreate()
+			req = submission.Fake(db, usr.Id())
+			req.MustCreate()
+
+			res = submission.New(db)
+
+			cl.Get("/submission/"+req.Id(), res)
+		})
+
+		It("Should get submissions", func() {
+			Expect(res.Email).To(Equal(req.Email))
+			Expect(res.UserId).To(Equal(req.UserId))
+		})
+	})
+	Context("Delete submission", func() {
+		res := ""
+
+		Before(func() {
+			usr := user.Fake(db)
+			usr.MustCreate()
+			req := submission.Fake(db, usr.Id())
+			req.MustCreate()
+
+			cl.Delete("/submission/" + req.Id())
+			res = req.Id()
+
+		})
+
+		It("Should delete submissions", func() {
+			sub := submission.New(db)
+			err := sub.GetById(res)
+			Expect(err).ToNot(BeNil())
 		})
 	})
 })
