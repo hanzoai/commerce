4c75b55f62f43949c789708323f033e19a6f4af5
Timothy Messer <tmesser@verus.io>
2016-10-05 18:05:03 -0500

    Store tests

--- test/api/store_test.go
+++ test/api/store_test.go
@@ -2,24 +2,20 @@ package test
 
 import (
 	"crowdstart.com/models/store"
-	"crowdstart.com/util/log"
 
 	. "crowdstart.com/util/test/ginkgo"
 )
 
 var _ = Describe("store", func() {
 	Context("New store", func() {
-		var req *store.Store
-		var res *store.Store
+		req := new(store.Store)
+		res := new(store.Store)
 
 		Before(func() {
 			req = store.Fake(db)
 			res = store.New(db)
 
-			// Create new store
-			log.JSON(req)
 			cl.Post("/store", req, res)
-			log.JSON(res)
 		})
 
 		It("Should create new stores", func() {
@@ -30,6 +26,46 @@ var _ = Describe("store", func() {
 			Expect(res.Currency).To(Equal(req.Currency))
 			Expect(res.Address).To(Equal(req.Address))
 			Expect(res.Email).To(Equal(req.Email))
+		})
+	})
+	Context("Get store", func() {
+		req := new(store.Store)
+		res := new(store.Store)
+
+		Before(func() {
+			req = store.Fake(db)
+			req.MustCreate()
+
+			res = store.New(db)
+
+			cl.Get("/store/"+req.Id(), res)
+		})
+
+		It("Should get stores", func() {
+			Expect(res.Name).To(Equal(req.Name))
+			Expect(res.Slug).To(Equal(req.Slug))
+			Expect(res.Domain).To(Equal(req.Domain))
+			Expect(res.Prefix).To(Equal(req.Prefix))
+			Expect(res.Currency).To(Equal(req.Currency))
+			Expect(res.Address).To(Equal(req.Address))
+			Expect(res.Email).To(Equal(req.Email))
+		})
+	})
+	Context("Delete store", func() {
+		res := ""
+
+		Before(func() {
+			req := store.Fake(db)
+			req.MustCreate()
+
+			cl.Delete("/store/" + req.Id())
+			res = req.Id()
+		})
+
+		It("Should delete stores", func() {
+			stor := store.New(db)
+			err := stor.GetById(res)
+			Expect(err).ToNot(BeNil())
 		})
 	})
 })
