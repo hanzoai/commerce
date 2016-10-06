23d8e6e5cab695a32bf1ebce907938e0eb1870a0
Zach Kelling <z@zeekay.io>
2016-10-06 13:03:56 -0500

    Update old integration tests to use new client.

---
+++ test/api/checkout/integration/requests/returninguserorder.go
@@ -0,0 +1,25 @@
+package requests
+
+var ReturningUserOrder = `
+{
+  "payment": {
+    "type": "stripe",
+    "account": {
+      "number": "4242424242424242",
+      "month": "12",
+      "year": "2016",
+      "cvc": "123"
+    }
+  },
+  "user": {
+	"id": "%s"
+  },
+  "order": {
+    "currency": "usd",
+    "items": [ {
+      "productSlug": "doge-shirt",
+      "price": 1000,
+      "quantity": 3
+    } ]
+  }
+}`
