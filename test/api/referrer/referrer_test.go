ed0b09ce1503abdb48df5dbb785182b8b356fcd7
Timothy Messer <tmesser@verus.io>
2016-10-06 01:13:18 -0500

    Referrer suite broken off and completed

--- test/api/referrer/referrer_test.go
+++ test/api/referrer/referrer_test.go
@@ -1,12 +1,16 @@
 package test
 
 import (
+	"math/rand"
+	"time"
+
 	"crowdstart.com/models/lineitem"
 	"crowdstart.com/models/order"
 	"crowdstart.com/models/product"
 	"crowdstart.com/models/referrer"
 	"crowdstart.com/models/user"
 	"crowdstart.com/models/variant"
+	"github.com/icrowley/fake"
 
 	. "crowdstart.com/util/test/ginkgo"
 )
@@ -69,6 +73,78 @@ var _ = Describe("referrer", func() {
 			Expect(res.FirstReferredAt.UTC()).To(Equal(req.FirstReferredAt.UTC()))
 		})
 	})
+
+	Context("Patch referrer", func() {
+		re := new(referrer.Referrer)
+		res := new(referrer.Referrer)
+
+		req := struct {
+			Code            string    `json:"Code"`
+			FirstReferredAt time.Time `json:"firstReferredAt"`
+		}{
+			fake.Word(),
+			time.Date(rand.Intn(15)+2000, time.Month(rand.Intn(12)+1), rand.Intn(25)+1, 0, 0, 0, 0, time.UTC),
+		}
+
+		Before(func() {
+			prod := product.Fake(db)
+			prod.MustCreate()
+			vari := variant.Fake(db, prod.Id())
+			vari.MustCreate()
+			li := lineitem.Fake(vari.Id(), vari.Name, vari.SKU)
+			ord := order.Fake(db, li)
+			ord.MustCreate()
+			usr := user.Fake(db)
+			usr.MustCreate()
+			re = referrer.Fake(db, usr.Id(), ord.Id())
+			re.MustCreate()
+
+			// Patch referrer
+			cl.Patch("/referrer/"+re.Id(), req, res)
+		})
+
+		It("Should patch referrer", func() {
+			Expect(res.Id_).To(Equal(re.Id()))
+			Expect(res.Code).To(Equal(req.Code))
+			Expect(res.OrderId).To(Equal(re.OrderId))
+			Expect(res.UserId).To(Equal(re.UserId))
+			Expect(res.FirstReferredAt.UTC()).To(Equal(req.FirstReferredAt.UTC()))
+		})
+	})
+
+	Context("Put referrer", func() {
+		re := new(referrer.Referrer)
+		res := new(referrer.Referrer)
+		req := new(referrer.Referrer)
+
+		Before(func() {
+			prod := product.Fake(db)
+			prod.MustCreate()
+			vari := variant.Fake(db, prod.Id())
+			vari.MustCreate()
+			li := lineitem.Fake(vari.Id(), vari.Name, vari.SKU)
+			ord := order.Fake(db, li)
+			ord.MustCreate()
+			usr := user.Fake(db)
+			usr.MustCreate()
+			re = referrer.Fake(db, usr.Id(), ord.Id())
+			re.MustCreate()
+
+			req = referrer.Fake(db, usr.Id(), ord.Id())
+
+			// Update referrer
+			cl.Put("/referrer/"+re.Id(), req, res)
+		})
+
+		It("Should put referrer", func() {
+			Expect(res.Id_).To(Equal(re.Id()))
+			Expect(res.Code).To(Equal(req.Code))
+			Expect(res.OrderId).To(Equal(req.OrderId))
+			Expect(res.UserId).To(Equal(req.UserId))
+			Expect(res.FirstReferredAt.UTC()).To(Equal(req.FirstReferredAt.UTC()))
+		})
+	})
+
 	Context("Delete referrer", func() {
 		res := ""
 
