fe9112c5d59a0f23df8c82fd5ab6ed599c3dccb8
Timothy Messer <tmesser@verus.io>
2016-10-08 10:53:16 -0500

    Add an idedicated fixture so we can support Michael's mailing list

---
+++ models/fixtures/idedicated.go
@@ -0,0 +1,94 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/auth/password"
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/namespace"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/product"
+	"crowdstart.com/models/store"
+	"crowdstart.com/models/types/currency"
+	"crowdstart.com/models/user"
+	"crowdstart.com/util/log"
+)
+
+var Idedicated = New("idedicated", func(c *gin.Context) *organization.Organization {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.Name = "idedicated"
+	org.GetOrCreate("Name=", org.Name)
+
+	u := user.New(db)
+	u.Email = "dev@hanzo.ai"
+	u.GetOrCreate("Email=", u.Email)
+	u.FirstName = "michael"
+	u.LastName = "walker"
+	u.Organizations = []string{org.Id()}
+	u.PasswordHash, _ = password.Hash("1dedicated23")
+	u.Update()
+
+	org.FullName = "IDedicated Inc"
+	org.Owners = []string{u.Id()}
+	org.Website = "http://www.idedicated.net"
+	org.SecretKey = []byte("EZ2E91RKX2BpRlv149N3STd1g580cp58")
+	org.AddDefaultTokens()
+
+	org.Fees.Card.Flat = 50
+	org.Fees.Card.Percent = 0.05
+	org.Fees.Affiliate.Flat = 30
+	org.Fees.Affiliate.Percent = 0.30
+
+	org.Mailchimp.APIKey = "37181ee9311a3eb5999cf457a2216aca-us9"
+	org.Mailchimp.ListId = "27eb8e23aab"
+
+	// Email configuration
+	org.Mandrill.APIKey = ""
+
+	org.Paypal.ConfirmUrl = "https://www.getkanoa.com"
+	org.Paypal.CancelUrl = "https://www.getkanoa.com"
+
+	org.Email.Defaults.Enabled = true
+	org.Email.Defaults.FromName = "IDedicated"
+	org.Email.Defaults.FromEmail = "hi@idedicated.net"
+
+	// Save org into default namespace
+	org.Put()
+
+	// Save namespace so we can decode keys for this organization later
+	ns := namespace.New(db)
+	ns.Name = org.Name
+	ns.IntId = org.Key().IntID()
+	err := ns.Put()
+	if err != nil {
+		log.Warn("Failed to put namespace: %v", err)
+	}
+
+	nsdb := datastore.New(org.Namespaced(db.Context))
+
+	// Create default store
+	stor := store.New(nsdb)
+	stor.Name = "default"
+	stor.GetOrCreate("Name=", stor.Name)
+	stor.Prefix = "/"
+	stor.Currency = currency.USD
+	stor.Mailchimp.APIKey = "37181ee9311a3eb5999cf457a2216aca-us9"
+	stor.Mailchimp.ListId = "27eb8e23aab"
+	stor.Update()
+
+	// Create earphone product
+	prod := product.New(nsdb)
+	prod.Slug = "earphone"
+	prod.GetOrCreate("Slug=", prod.Slug)
+	prod.Name = "KANOA Earphone"
+	prod.Description = "2 Ear Buds, 1 Charging Case, 3 Ergonomic Ear Tips, 1 Micro USB Cable"
+	prod.Price = currency.Cents(19999)
+	prod.Inventory = 9000
+	prod.Preorder = true
+	prod.Hidden = false
+	prod.Update()
+
+	return org
+})
