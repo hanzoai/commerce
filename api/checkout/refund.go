b679b1b5e46e6b196d43ab0004997476c1a1dbfe
zeekay <zk@monoid.io>
2016-10-10 10:59:57 -0500

    Update comments.

--- api/checkout/refund.go
+++ api/checkout/refund.go
@@ -6,19 +6,24 @@ import (
 	"crowdstart.com/api/checkout/stripe"
 	"crowdstart.com/models/order"
 	"crowdstart.com/models/organization"
+	"crowdstart.com/models/types/currency"
 	"crowdstart.com/util/json"
 	"crowdstart.com/util/log"
 )
 
 func refund(c *gin.Context, org *organization.Organization, ord *order.Order) error {
+	type Refund struct {
+		Amount currency.Cents `json:"amount"`
+	}
+
 	// Try decode request body
-	refundReq := new(RefundRequest)
-	if err := json.Decode(c.Request.Body, &refundReq); err != nil {
+	req := new(Refund)
+	if err := json.Decode(c.Request.Body, req); err != nil {
 		log.Error("Failed to decode request body: %v\n%v", c.Request.Body, err, c)
 		return FailedToDecodeRequestBody
 	}
 
-	log.JSON(refundReq)
+	log.JSON(req)
 
-	return stripe.Refund(org, ord, refundReq.Amount)
+	return stripe.Refund(org, ord, req.Amount)
 }
