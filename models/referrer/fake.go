b62d38ec39f48f0ad60a7431f763189f57f58747
zeekay <zk@monoid.io>
2016-10-10 17:28:37 -0500

    Update referrer fake, add more checkout tests.

--- models/referrer/fake.go
+++ models/referrer/fake.go
@@ -8,12 +8,10 @@ import (
 	"crowdstart.com/util/fake"
 )
 
-func Fake(db *datastore.Datastore, userId, orderId string) *Referrer {
-	r := New(db)
-	r.Code = fake.Word()
-	r.OrderId = orderId
-	r.UserId = userId
-	r.FirstReferredAt = time.Date(rand.Intn(15)+2000, time.Month(rand.Intn(12)+1), rand.Intn(25)+1, 0, 0, 0, 0, time.UTC)
-
-	return r
+func Fake(db *datastore.Datastore, userId string) *Referrer {
+	ref := New(db)
+	ref.Code = fake.Word()
+	ref.UserId = userId
+	ref.FirstReferredAt = time.Date(rand.Intn(15)+2000, time.Month(rand.Intn(12)+1), rand.Intn(25)+1, 0, 0, 0, 0, time.UTC)
+	return ref
 }
