b12c0f49763f5ef3f9f40e2ea06d6d56c9a4ff32
zeekay <zk@monoid.io>
2016-10-10 16:14:02 -0500

    Remove unnecessary hook reflection.

--- models/mixin/modelhooks.go
+++ models/mixin/modelhooks.go
@@ -1,7 +1,5 @@
 package mixin
 
-import "reflect"
-
 type BeforeCreate interface {
 	BeforeCreate() error
 }
@@ -24,25 +22,4 @@ type AfterUpdate interface {
 
 type AfterDelete interface {
 	AfterDelete() error
-}
-
-// Try to get method off a model
-func getHook(name string, model *Model) (reflect.Method, bool) {
-	typ := reflect.TypeOf(model.Entity)
-	return typ.MethodByName(name)
-}
-
-// Call method returned by getMethod
-func callHook(entity Kind, method reflect.Method, previous interface{}) error {
-	args := []reflect.Value{
-		reflect.ValueOf(entity),
-		reflect.ValueOf(previous),
-	}
-
-	ret := method.Func.Call(args)
-	err, ok := ret[0].Interface().(error)
-	if ok {
-		return err
-	}
-	return nil
 }
