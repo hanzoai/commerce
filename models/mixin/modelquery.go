3093634712aeeb3ec20c9656e3f2046c88e7017b
Zach Kelling <z@zeekay.io>
2016-10-10 09:47:09 -0500

    Disambiguate Query types.

--- models/mixin/modelquery.go
+++ models/mixin/modelquery.go
@@ -7,42 +7,42 @@ import (
 
 // This is a simple Query helper for individual models. Allows you to query for
 // a single entity or key only as a convenience on an individual model.
-type Query struct {
+type ModelQuery struct {
 	entity Entity
 	dsq    datastore.Query
 }
 
-func (q *Query) All() datastore.Query {
+func (q *ModelQuery) All() datastore.Query {
 	return q.dsq
 }
 
-func (q *Query) Ancestor(key datastore.Key) *Query {
+func (q *ModelQuery) Ancestor(key datastore.Key) *ModelQuery {
 	q.dsq = q.dsq.Ancestor(key)
 	return q
 }
 
-func (q *Query) Limit(limit int) *Query {
+func (q *ModelQuery) Limit(limit int) *ModelQuery {
 	q.dsq = q.dsq.Limit(limit)
 	return q
 }
 
-func (q *Query) Offset(offset int) *Query {
+func (q *ModelQuery) Offset(offset int) *ModelQuery {
 	q.dsq = q.dsq.Offset(offset)
 	return q
 }
 
-func (q *Query) Order(order string) *Query {
+func (q *ModelQuery) Order(order string) *ModelQuery {
 	q.dsq = q.dsq.Order(order)
 	return q
 }
 
-func (q *Query) Filter(filterStr string, value interface{}) *Query {
+func (q *ModelQuery) Filter(filterStr string, value interface{}) *ModelQuery {
 	q.dsq = q.dsq.Filter(filterStr, value)
 	return q
 }
 
 // Get entity
-func (q *Query) Get() (bool, error) {
+func (q *ModelQuery) Get() (bool, error) {
 	key, ok, err := q.dsq.First(q.entity)
 	if ok {
 		q.entity.SetKey(key)
@@ -57,23 +57,23 @@ func (q *Query) Get() (bool, error) {
 }
 
 // Get just key
-func (q *Query) GetKey() (bool, error) {
+func (q *ModelQuery) GetKey() (bool, error) {
 	q.dsq = q.dsq.KeysOnly()
 	return q.Get()
 }
 
 // Check if id exists
-func (q *Query) IdExists(id string) (datastore.Key, bool, error) {
+func (q *ModelQuery) IdExists(id string) (datastore.Key, bool, error) {
 	return q.dsq.IdExists(id)
 }
 
 // Check if key exists
-func (q *Query) KeyExists(key datastore.Key) (bool, error) {
+func (q *ModelQuery) KeyExists(key datastore.Key) (bool, error) {
 	return q.dsq.KeyExists(key)
 }
 
 // Helper to set key if it's query returns one
-func (q *Query) setAndForget(key datastore.Key, ok bool, err error) (bool, error) {
+func (q *ModelQuery) setAndForget(key datastore.Key, ok bool, err error) (bool, error) {
 	if ok {
 		q.entity.SetKey(key)
 	}
@@ -81,18 +81,18 @@ func (q *Query) setAndForget(key datastore.Key, ok bool, err error) (bool, error
 }
 
 // Filter by key
-func (q *Query) ByKey(key interface{}) (bool, error) {
+func (q *ModelQuery) ByKey(key interface{}) (bool, error) {
 	return q.setAndForget(q.dsq.ByKey(q.entity.Key(), q.entity))
 }
 
 // Filter By Id
-func (q *Query) ById(id string) (bool, error) {
+func (q *ModelQuery) ById(id string) (bool, error) {
 	return q.setAndForget(q.dsq.ById(id, q.entity))
 }
 
 // Return a query for this entity kind
-func (m *Model) Query() *Query {
-	q := new(Query)
+func (m *Model) Query() *ModelQuery {
+	q := new(ModelQuery)
 	q.entity = m.Entity.(Entity)
 	q.dsq = query.New(m.Context(), m.Kind())
 	return q
