a1ead9abc2e0406954e1b9760612fd1a81196baf
Zach Kelling <z@zeekay.io>
2016-10-15 14:37:59 -0500

    Reorganize operations.

--- datastore/key.go
+++ datastore/key.go
@@ -1,6 +1,10 @@
 package datastore
 
 import (
+	"fmt"
+
+	aeds "appengine/datastore"
+
 	"crowdstart.com/datastore/iface"
 	"crowdstart.com/datastore/key"
 )
@@ -9,3 +13,75 @@ type Key iface.Key
 
 var EncodeKey = key.Encode
 var DecodeKey = key.Decode
+
+func convertKey(key Key) *aeds.Key {
+	return key.(*aeds.Key)
+}
+
+func convertKeys(keys interface{}) []*aeds.Key {
+	var aekeys []*aeds.Key
+
+	switch v := keys.(type) {
+	case []Key:
+		n := len(v)
+		aekeys = make([]*aeds.Key, n)
+
+		for i := 0; i < n; i++ {
+			aekeys[i] = v[i].(*aeds.Key)
+		}
+
+		return aekeys
+	case []*aeds.Key:
+		return v
+	default:
+		panic(fmt.Errorf("Invalid slice of keys: %v", keys))
+	}
+}
+
+// Encode/decode hashid keys
+func (d *Datastore) DecodeKey(encoded string) (*aeds.Key, error) {
+	return DecodeKey(d.Context, encoded)
+}
+
+func (d *Datastore) EncodeKey(key Key) string {
+	return EncodeKey(d.Context, key)
+}
+
+// Wrap appengine key funcs
+func (d *Datastore) NewIncompleteKey(kind string, parent Key) *aeds.Key {
+	if p, ok := parent.(*aeds.Key); ok {
+		return aeds.NewIncompleteKey(d.Context, kind, p)
+	} else {
+		return aeds.NewIncompleteKey(d.Context, kind, nil)
+	}
+}
+
+func (d *Datastore) NewKey(kind, stringID string, intID int64, parent Key) *aeds.Key {
+	if p, ok := parent.(*aeds.Key); ok {
+		return aeds.NewKey(d.Context, kind, stringID, intID, p)
+	} else {
+		return aeds.NewKey(d.Context, kind, stringID, intID, nil)
+	}
+}
+
+func (d *Datastore) AllocateID(kind string, parent Key) int64 {
+	id, _ := d.AllocateIDs(kind, parent, 1)
+	return id
+}
+
+func (d *Datastore) AllocateIDs(kind string, parent Key, n int) (int64, int64) {
+	low, high, err := aeds.AllocateIDs(d.Context, kind, convertKey(parent), n)
+	if err != nil {
+		panic(fmt.Errorf("Unable to Allocate IDs: %v", err))
+	}
+	return low, high
+}
+
+func (d *Datastore) AllocateKey(kind string, parent Key) *aeds.Key {
+	id := d.AllocateID(kind, parent)
+	return d.NewKey(kind, "", id, parent)
+}
+
+func (d *Datastore) KeyFromInt(kind string, id interface{}) *aeds.Key {
+	return key.FromInt(d.Context, kind, id)
+}
