0c2bb0b3e653332303fc44b9e9f7dc5eff98341c
zeekay <zk@monoid.io>
2016-10-17 16:13:30 -0500

    Test json util.

---
+++ test/util/json/json_test.go
@@ -0,0 +1,49 @@
+package test
+
+import (
+	"bytes"
+	"testing"
+
+	"crowdstart.com/util/json"
+
+	. "crowdstart.com/util/test/ginkgo"
+)
+
+func Test(t *testing.T) {
+	Setup("util/json", t)
+}
+
+type Cart struct {
+	Codes []string
+}
+
+type ReadCloser struct {
+	*bytes.Buffer
+}
+
+func (cb *ReadCloser) Close() error {
+	return nil
+}
+
+var _ = Describe("json.EncodeBytes", func() {
+	It("Should be able to decode JSON", func() {
+		car := Cart{[]string{"A", "B", "C"}}
+		b := json.EncodeBytes(car)
+		json.DecodeBytes(b, &car)
+		json.DecodeBytes(b, &car)
+		json.DecodeBytes(b, &car)
+		Expect(len(car.Codes)).To(Equal(3))
+	})
+})
+
+var _ = Describe("json.Encode", func() {
+	It("Should be able to decode JSON", func() {
+		car := Cart{[]string{"A", "B", "C"}}
+		s := json.Encode(car)
+		cb := &ReadCloser{bytes.NewBufferString(s)}
+		json.Decode(cb, &car)
+		json.Decode(cb, &car)
+		json.Decode(cb, &car)
+		Expect(len(car.Codes)).To(Equal(3))
+	})
+})
