4eb25400cd77889ac3fc967bbddfd087f86bf16a
zeekay <zk@monoid.io>
2016-10-17 21:22:44 -0500

    Fix logging.

--- util/log/format.go
+++ util/log/format.go
@@ -414,8 +414,7 @@ func relativePath(file string) string {
 }
 
 func init() {
-	_, filename, _, _ := runtime.Caller(1)
-	sdkPath = strings.Replace(filename, "util/log/stack.go", "", 1)
-	sdkPath = strings.Replace(filename, "util/log/log.go", "", 1)
+	_, filename, _, _ := runtime.Caller(0)
+	sdkPath = strings.Replace(filename, "util/log/format.go", "", 1)
 	projectPath = strings.Replace(sdkPath, "/.sdk/gopath/src/crowdstart.com", "", 1)
 }
