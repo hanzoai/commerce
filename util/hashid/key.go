69ea70943ff1117624f52b8aa3515bed1ae04a1f
zeekay <zk@monoid.io>
2016-10-18 00:25:02 -0500

    Remove stack logging.

--- util/hashid/key.go
+++ util/hashid/key.go
@@ -185,7 +185,6 @@ func EncodeKey(ctx appengine.Context, key datastore.Key) string {
 	encoded := Encode(ids...)
 
 	log.Debug("%s%v encoded to '%s'", fmtNs(key.Namespace()), key, encoded)
-	log.Stack()
 
 	return encoded
 }
