a1ccc945fe9c2001bc5d4ab0629e2ad37a7a7b84
zeekay <zk@monoid.io>
2016-10-18 00:22:06 -0500

    Skip gin, net/http cruft in stack.

--- util/log/stack.go
+++ util/log/stack.go
@@ -19,9 +19,15 @@ func stack(offset int) string {
 	stack := strings.Split(string(debug.Stack()), "\n")
 	lines := []string{""}
 	for i := offset; i < len(stack); i++ {
+		// Skip after ginkgo, gin
 		if strings.Contains(stack[i], "github.com/onsi/ginkgo") {
 			break
 		}
+
+		if strings.Contains(stack[i], "github.com/gin-gonic/gin") {
+			break
+		}
+
 		lines = append(lines, stack[i])
 	}
 	return strings.Join(lines, "\n")
