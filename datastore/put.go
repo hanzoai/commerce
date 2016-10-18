3ea0c726a5f06557d7a3a17e0a319843b2f9dff0
Zach Kelling <z@zeekay.io>
2016-10-18 01:26:30 -0500

    Update db.Put to retain old behavior.

--- datastore/put.go
+++ datastore/put.go
@@ -1,14 +1,29 @@
 package datastore
 
 import (
+	"fmt"
+
 	aeds "appengine/datastore"
 
 	"github.com/qedus/nds"
 )
 
-// Puts entity, returning encoded key
-func (d *Datastore) Put(key Key, val interface{}) (*aeds.Key, error) {
-	if key, err := nds.Put(d.Context, convertKey(key), val); err != nil {
+// Helper to create new incomplete key from string kind
+func convertKeyOrKind(ds *Datastore, keyOrKind interface{}) *aeds.Key {
+	switch v := keyOrKind.(type) {
+	case string:
+		return ds.NewIncompleteKey(v, nil)
+	case Key:
+		return convertKey(v)
+	default:
+		panic(fmt.Errorf("Invalid key or kind: %v", keyOrKind))
+	}
+}
+
+// Puts entity using provided key or kind
+func (d *Datastore) Put(keyOrKind interface{}, val interface{}) (*aeds.Key, error) {
+	key := convertKeyOrKind(d, keyOrKind)
+	if key, err := nds.Put(d.Context, key, val); err != nil {
 		d.warn("Unable to put (%v, %#v): %v", key, val, err, d.Context)
 		return nil, err
 	} else {
@@ -22,8 +37,8 @@ func (d *Datastore) PutMulti(keys interface{}, vals interface{}) ([]*aeds.Key, e
 }
 
 // Gets an entity using datastore.Key or encoded Key
-func (d *Datastore) MustPut(key Key, value interface{}) *aeds.Key {
-	if key, err := d.Put(key, value); err != nil {
+func (d *Datastore) MustPut(keyOrKind interface{}, value interface{}) *aeds.Key {
+	if key, err := d.Put(keyOrKind, value); err != nil {
 		panic(err)
 	} else {
 		return key
