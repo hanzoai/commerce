3661da74ec965efeb27e8fd1f8c5ca4636b737a7
zeekay <zk@monoid.io>
2016-10-18 03:33:21 -0500

    Safer failure for bad integer keys.

--- datastore/query/byid.go
+++ datastore/query/byid.go
@@ -11,52 +11,62 @@ import (
 	"crowdstart.com/datastore/key"
 )
 
+var newKeyFromInt = key.NewFromInt
+
 // Get coupon from id
 func (q *Query) couponFromId(id string, dst interface{}) (*aeds.Key, bool, error) {
 	code := strings.ToUpper(id)
 
-	if k, ok, _ := q.Filter("Code=", code).First(dst); ok {
-		return k, true, nil
-	} else {
-		// Get ids from coupon id
-		ids := hashid.Decode(id)
-
-		if len(ids) == 0 {
-			return nil, false, ErrInvalidKey
-		}
-
-		// Recreate coupon key
-		k := key.NewFromInt(q.ctx, "coupon", ids[0], nil)
-
-		// Fetch coupon using key
-		_, ok, err := q.ByKey(k, dst)
-
-		if err != nil {
-			return nil, false, err
-		}
-
-		if !ok {
-			return nil, false, nil
-		}
-
-		// Set RawCode on fetched entity in case this was not parsed from JSON
-		v := reflect.ValueOf(dst).Elem().FieldByName("RawCode")
-		ptr := v.Addr().Interface().(*string)
-		*ptr = id
-
-		return k, true, nil
+	key, ok, _ := q.Filter("Code=", code).First(dst)
+	if ok {
+		return key, true, nil
 	}
+
+	// Get ids from coupon id
+	ids := hashid.Decode(id)
+	if len(ids) == 0 {
+		return nil, false, ErrInvalidKey
+	}
+
+	// Recreate coupon key
+	key, err := newKeyFromInt(q.ctx, "coupon", ids[0], nil)
+	if err != nil {
+		return nil, false, err
+	}
+
+	// Fetch coupon using key
+	_, ok, err = q.ByKey(key, dst)
+	if err != nil {
+		return nil, false, err
+	}
+
+	if !ok {
+		return nil, false, nil
+	}
+
+	// Set RawCode on fetched entity in case this was not parsed from JSON
+	v := reflect.ValueOf(dst).Elem().FieldByName("RawCode")
+	ptr := v.Addr().Interface().(*string)
+	*ptr = id
+
+	return key, true, nil
 }
 
 // Get order from id
 func (q *Query) orderFromId(id string, dst interface{}) (*aeds.Key, bool, error) {
-	k := key.NewFromInt(q.ctx, "order", id, nil)
-	k, ok, err := q.ByKey(k, dst)
+	key, err := newKeyFromInt(q.ctx, "order", id, nil)
 	if err != nil {
 		return nil, false, err
 	}
+
+	key, ok, err := q.ByKey(key, dst)
+	if err != nil {
+		return nil, false, err
+	}
+
 	if !ok {
 		return nil, false, nil
 	}
-	return k, ok, err
+
+	return key, ok, err
 }
