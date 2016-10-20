1a776975486c8c5b4fe1d180a6f5612f000e68c8
David Tai <antisocialmunky@gmail.com>
2016-10-20 11:52:41 -0500

    tested last change

--- models/affiliate/fake.go
+++ models/affiliate/fake.go
@@ -15,5 +15,6 @@ func Fake(db *datastore.Datastore, userId string) *Affiliate {
 	aff.Country = country.Fake()
 	aff.TaxId = fake.RandSeq(9, []rune("0123456789"))
 	aff.Commission = commission.Fake()
+	aff.CouponId = "TEST-COUPON"
 	return aff
 }
