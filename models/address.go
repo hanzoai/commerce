36eb5647791ada82461697a667bf8e6456231385
zeekay <zk@monoid.io>
2016-10-19 20:17:14 -0500

    Remove old cruft.

--- models/address.go
+++ models/address.go
@@ -1,11 +1,5 @@
 package models
 
-import (
-	"net/http"
-
-	"github.com/mholt/binding"
-)
-
 type Address struct {
 	Name       string `json:"name,omitempty"`
 	Line1      string `json:"line1,omitempty"`
@@ -26,40 +20,4 @@ func (a Address) Empty() bool {
 	}
 
 	return false
-}
-
-func (a Address) Validate(req *http.Request, errs binding.Errors) binding.Errors {
-
-	if a.Line() == "" {
-		errs = append(errs, binding.Error{
-			FieldNames:     []string{"Street"},
-			Classification: "InputError",
-			Message:        "Address Street is required.",
-		})
-	}
-
-	if a.City == "" {
-		errs = append(errs, binding.Error{
-			FieldNames:     []string{"City"},
-			Classification: "InputError",
-			Message:        "Address City is required.",
-		})
-	}
-
-	if a.State == "" {
-		errs = append(errs, binding.Error{
-			FieldNames:     []string{"State"},
-			Classification: "InputError",
-			Message:        "Address State is required.",
-		})
-	}
-
-	if a.Country == "" {
-		errs = append(errs, binding.Error{
-			FieldNames:     []string{"Country"},
-			Classification: "InputError",
-			Message:        "Address Country is required.",
-		})
-	}
-	return errs
 }
