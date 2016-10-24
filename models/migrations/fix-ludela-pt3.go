02d751cf22b6877afdb5f6352f0ef33f8ea6ccdf
zeekay <zk@monoid.io>
2016-10-24 14:09:52 -0500

    Fix buyer on payment (if corrupted).

---
+++ models/migrations/fix-ludela-pt3.go
@@ -0,0 +1,35 @@
+package migrations
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/models/payment"
+
+	ds "crowdstart.com/datastore"
+)
+
+var _ = New("fix-ludela-pt3",
+	func(c *gin.Context) []interface{} {
+		c.Set("namespace", "ludela")
+		return NoArgs
+	},
+	func(db *ds.Datastore, pay *payment.Payment) {
+		if pay.Buyer.FirstName == "\u263A" {
+			pay.Buyer.FirstName = ""
+		}
+
+		if pay.Buyer.LastName == "\u263A" {
+			pay.Buyer.LastName = ""
+		}
+
+		if pay.Buyer.FirstName == "☺" {
+			pay.Buyer.FirstName = ""
+		}
+
+		if pay.Buyer.LastName == "☺" {
+			pay.Buyer.LastName = ""
+		}
+
+		pay.MustPut()
+	},
+)
