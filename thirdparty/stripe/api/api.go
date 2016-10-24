489773883babc869e8ce7b66088872786886b42b
zeekay <zk@monoid.io>
2016-10-24 13:08:32 -0500

    Use GET for Stripe callback.

--- thirdparty/stripe/api/api.go
+++ thirdparty/stripe/api/api.go
@@ -8,6 +8,6 @@ import (
 // Wire up stripe endpoint
 func Route(router router.Router, args ...gin.HandlerFunc) {
 	api := router.Group("stripe")
+	api.GET("/callback", Callback)
 	api.POST("/webhook", Webhook)
-	api.POST("/callback", Callback)
 }
