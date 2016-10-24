262f95f83601dbc386cb1a9e916d1b4762904449
David Tai <david@verus.io>
2016-10-24 17:11:02 -0500

    Update mixin and user tests.

--- test/models/user/user_test.go
+++ test/models/user/user_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"crowdstart.com/datastore"
+	"crowdstart.com/models/user"
 	"crowdstart.com/util/test/ae"
 
 	. "crowdstart.com/util/test/ginkgo"
@@ -30,74 +31,15 @@ var _ = AfterSuite(func() {
 })
 
 var _ = Describe("User", func() {
-	// Context("Insert", func() {
-	// 	It("Should insert a new user", func() {
-	// 		// Insert User
-	// 		user := &models.User{Email: "u1@verus.io"}
-	// 		user.Insert(db)
+	It("should retrieve entity from datastore by email", func() {
+		// Manually create a new user and store in datastore
+		usr := user.Fake(db)
+		usr.MustCreate()
 
-	// 		// Get User and Compare Email
-	// 		var _user models.User
-	// 		db.Get(user.Id, &_user)
-	// 		Expect(_user.Email).To(Equal(user.Email))
-	// 	})
-	// })
-
-	// Context("Upsert", func() {
-	// 	It("Should upsert a user and overwrite what is in the datastore", func() {
-	// 		// Insert Via Upsert User
-	// 		user := &models.User{Email: "u2@verus.io"}
-	// 		user.Upsert(db)
-
-	// 		// Get User and Compare Email
-	// 		var _user models.User
-	// 		db.Get(user.Id, &_user)
-	// 		Expect(_user.Email).To(Equal(user.Email))
-
-	// 		// Change Email on User and Upsert User
-	// 		user.Email = "u3@verus.io"
-	// 		user.Upsert(db)
-
-	// 		// Get User and Compare Changed Email
-	// 		var __user models.User
-	// 		db.Get(user.Id, &__user)
-	// 		Expect(__user.Email).To(Equal(user.Email))
-	// 	})
-
-	// 	It("Should upsert a user and overwrite what is in the datastore based on email if id is missing", func() {
-	// 		// Insert Via Upsert User
-	// 		user := &models.User{Email: "u2@verus.io", FirstName: "u2"}
-	// 		user.Upsert(db)
-
-	// 		// Get User and Compare Email and FirstName
-	// 		var _user models.User
-	// 		db.Get(user.Id, &_user)
-	// 		Expect(_user.Email).To(Equal(user.Email))
-	// 		Expect(_user.FirstName).To(Equal(user.FirstName))
-
-	// 		// Change Email on User and Upsert User
-	// 		user2 := models.User{Email: "u2@verus.io", FirstName: "u3"}
-	// 		user2.Upsert(db)
-
-	// 		// Get User and Compare Changed FirstName, Email, and Id
-	// 		var __user models.User
-	// 		db.Get(user2.Id, &__user)
-	// 		Expect(__user.Id).To(Equal(_user.Id))
-	// 		Expect(__user.Email).To(Equal(user.Email))
-	// 		Expect(__user.FirstName).To(Equal(user2.FirstName))
-	// 	})
-
-	// 	Context("GetByEmail", func() {
-	// 		It("Should be able to GetUserByEmail", func() {
-	// 			// Insert User
-	// 			user := &models.User{Email: "u1@verus.io"}
-	// 			user.Insert(db)
-
-	// 			// Get User by Email and Check Email
-	// 			_user := &models.User{}
-	// 			_ = _user.GetByEmail(db, "u1@verus.io")
-	// 			Expect(_user.Email).To(Equal(user.Email))
-	// 		})
-	// 	})
-	// })
+		// Retrieve usr from datastore using Model mixin
+		usr2 := user.New(db)
+		usr2.MustGetById(usr.Email)
+		Expect(usr2.Email).To(Equal(usr2.Email))
+		Expect(usr2.Name()).To(Equal(usr2.Name()))
+	})
 })
