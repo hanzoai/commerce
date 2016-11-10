b13c758b7ae7edcc9be0c474b5a0e3abe54ea0a4
David Tai <antisocialmunky@gmail.com>
2016-11-10 14:49:21 -0600

    fixed missing files and FIt in tests

---
+++ models/migrations/fee-status-update.go
@@ -0,0 +1,21 @@
+package migrations
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/models/payment"
+	"crowdstart.com/thirdparty/stripe/tasks"
+
+	ds "crowdstart.com/datastore"
+)
+
+var _ = New("fee-status-update",
+	func(c *gin.Context) []interface{} {
+		return NoArgs
+	},
+	func(db *ds.Datastore, pay *payment.Payment) {
+		if fees, err := pay.GetFees(); err == nil {
+			tasks.UpdateFeesFromPayment(fees, pay)
+		}
+	},
+)
