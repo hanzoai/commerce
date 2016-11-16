26d89451b07277316d4e340681836d96a9b9110e
zeekay <zk@monoid.io>
2016-11-16 10:03:41 -0600

    Use mailchimp.Error consistently.

--- thirdparty/mailchimp/errors.go
+++ thirdparty/mailchimp/errors.go
@@ -3,12 +3,32 @@ package mailchimp
 import "github.com/zeekay/gochimp3"
 
 type Error struct {
-	*gochimp3.APIError
+	Unknown   error
+	Mailchimp *gochimp3.APIError
+	Status    int
 }
 
-func newError(err error) *Error {
-	if aerr, ok := err.(*gochimp3.APIError); ok {
-		return &Error{aerr}
+func (e *Error) Error() string {
+	if e.Mailchimp != nil {
+		return e.Mailchimp.Error()
 	}
-	return &Error{nil}
+	return e.Unknown.Error()
+}
+
+func wrapError(fn func() error) *Error {
+	// Do work
+	err := fn()
+
+	// Not an error
+	if err == nil {
+		return nil
+	}
+
+	// Handle Mailchimp API Errors
+	if merr, ok := err.(*gochimp3.APIError); ok {
+		return &Error{Mailchimp: merr, Status: merr.Status}
+	}
+
+	// Handle any other errors
+	return &Error{Unknown: err, Status: 500}
 }
