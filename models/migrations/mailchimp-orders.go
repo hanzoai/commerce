10501f99780f1e17b60edcba66751a9e6461722f
David Tai <antisocialmunky@gmail.com>
2016-11-22 10:29:16 -0600

    better mailchimp integration

--- models/migrations/mailchimp-orders.go
+++ models/migrations/mailchimp-orders.go
@@ -3,9 +3,9 @@ package migrations
 import (
 	"github.com/gin-gonic/gin"
 
+	"crowdstart.com/models/cart"
 	"crowdstart.com/models/order"
 	"crowdstart.com/models/organization"
-	"crowdstart.com/models/payment"
 	"crowdstart.com/thirdparty/mailchimp"
 	"crowdstart.com/util/log"
 
@@ -42,16 +42,30 @@ var _ = New("mailchimp-orders",
 		client := mailchimp.New(db.Context, apiKey)
 		client.CreateOrder(defaultStore, ord)
 
-		pay := payment.New(db)
+		// Update cart
+		car := cart.New(ord.Db)
 
-		if _, err := pay.Query().Filter("OrderId=", ord.Id()).Get(); err != nil {
-			log.Warn("No Payment Found for %v: %v", ord.Id(), err, db.Context)
-			return
+		if ord.CartId != "" {
+			if err := car.GetById(ord.CartId); err != nil {
+				log.Warn("Unable to find cart: %v", err, db.Context)
+			} else {
+				// Delete cart in mailchimp
+				if err := client.DeleteCart(defaultStore, car); err != nil {
+					log.Warn("Failed to create Mailchimp cart: %v", err, db.Context)
+				}
+			}
 		}
+
+		// pay := payment.New(db)
+
+		// if _, err := pay.Query().Filter("OrderId=", ord.Id()).Get(); err != nil {
+		// 	log.Warn("No Payment Found for %v: %v", ord.Id(), err, db.Context)
+		// 	return
+		// }
 
 		// Just get buyer off first payment
-		if err := client.SubscribeCustomer(listId, pay.Buyer); err != nil {
-			log.Warn("Failed to subscribe '%s' to Mailchimp list '%s': %v", pay.Buyer.Email, listId, err, db.Context)
-		}
+		// if err := client.SubscribeCustomer(listId, pay.Buyer); err != nil {
+		// 	log.Warn("Failed to subscribe '%s' to Mailchimp list '%s': %v", pay.Buyer.Email, listId, err, db.Context)
+		// }
 	},
 )
