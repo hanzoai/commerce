62304d9c62c5ed4342d37afb3ee6e63a0fbeee49
zeekay <zk@monoid.io>
2016-12-20 13:36:06 -0600

    Add shipstation fixture.

---
+++ models/fixtures/sa-shipstation.go
@@ -0,0 +1,30 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/auth/password"
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/user"
+)
+
+var _ = New("stoned-shipstation", func(c *gin.Context) *organization.Organization {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.Name = "stoned"
+	org.GetOrCreate("Name=", org.Name)
+
+	u := user.New(db)
+	u.Email = "shipstation@stoned.audio"
+	u.GetOrCreate("Email=", u.Email)
+	u.Delete()
+	u.FirstName = "Shipstation"
+	u.LastName = ""
+	u.Organizations = []string{org.Id()}
+	u.PasswordHash, _ = password.Hash("ZGvb49Pik8Ms!")
+	u.Put()
+
+	return org
+})
