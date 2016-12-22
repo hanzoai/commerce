d66cd73469feeeaf2c7cf34b57811094e98b08ab
David Tai <david@verus.io>
2016-12-22 14:35:41 -0600

    make transaction

--- models/fixtures/moveordertonewuser.go
+++ models/fixtures/moveordertonewuser.go
@@ -34,53 +34,56 @@ var MoveOrderToNewUser = New("move-order-to-new-user", func(c *gin.Context) {
 		log.Panic(err, ctx)
 	}
 
-	ords := make([]*order.Order, 0)
-	if _, err := order.Query(nsDb).
-		Filter("UserId=", oldUsr.Id()).
-		GetAll(&ords); err != nil {
-		log.Panic(err, ctx)
-	}
-	log.Warn("Moving %v orders", len(ords), ctx)
-
-	for _, o := range ords {
-		ord := order.New(nsDb)
-		ord.GetById(o.Id_)
-		oldOK := ord.Key()
-		newOK := nsDb.NewKey(ord.Kind(), oldOK.StringID(), oldOK.IntID(), newUsr.Key())
-
-		ord.SetKey(newOK)
-		if err := ord.Put(); err != nil {
+	nsDb.RunInTransaction(func(nsDb *datastore.Datastore) error {
+		ords := make([]*order.Order, 0)
+		if _, err := order.Query(nsDb).
+			Filter("UserId=", oldUsr.Id()).
+			GetAll(&ords); err != nil {
 			log.Panic(err, ctx)
 		}
+		log.Warn("Moving %v orders", len(ords), ctx)
 
-		ord.SetKey(oldOK)
-		if err := ord.Delete(); err != nil {
-			log.Panic(err, ctx)
-		}
+		for _, o := range ords {
+			ord := order.New(nsDb)
+			ord.GetById(o.Id_)
+			oldOK := ord.Key()
+			newOK := nsDb.NewKey(ord.Kind(), oldOK.StringID(), oldOK.IntID(), newUsr.Key())
 
-		pays := make([]*payment.Payment, 0)
-		if _, err := payment.Query(nsDb).
-			Filter("OrderId", ord.Id()).
-			GetAll(&pays); err != nil {
-			log.Panic(err, ctx)
-		}
-		log.Warn("Moving %v payments", len(ords), ctx)
-
-		for _, p := range pays {
-			pay := payment.New(nsDb)
-			pay.GetById(p.Id_)
-			oldPK := pay.Key()
-			newPK := nsDb.NewKey(pay.Kind(), oldPK.StringID(), oldPK.IntID(), newOK)
-
-			pay.SetKey(newPK)
-			if err := pay.Put(); err != nil {
+			ord.SetKey(newOK)
+			if err := ord.Put(); err != nil {
 				log.Panic(err, ctx)
 			}
 
-			pay.SetKey(oldPK)
-			if err := pay.Delete(); err != nil {
+			pays := make([]*payment.Payment, 0)
+			if _, err := payment.Query(nsDb).
+				Filter("OrderId=", ord.Id()).
+				GetAll(&pays); err != nil {
+				log.Panic(err, ctx)
+			}
+			log.Warn("Moving %v payments", len(ords), ctx)
+
+			for _, p := range pays {
+				pay := payment.New(nsDb)
+				pay.GetById(p.Id_)
+				oldPK := pay.Key()
+				newPK := nsDb.NewKey(pay.Kind(), oldPK.StringID(), oldPK.IntID(), newOK)
+
+				pay.SetKey(newPK)
+				if err := pay.Put(); err != nil {
+					log.Panic(err, ctx)
+				}
+
+				pay.SetKey(oldPK)
+				if err := pay.Delete(); err != nil {
+					log.Panic(err, ctx)
+				}
+			}
+
+			ord.SetKey(oldOK)
+			if err := ord.Delete(); err != nil {
 				log.Panic(err, ctx)
 			}
 		}
-	}
+		return nil
+	})
 })
