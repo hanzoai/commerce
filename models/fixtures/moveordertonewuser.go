891ecae74b2c9076206840ac73b21ea82a0308a1
David Tai <antisocialmunky@gmail.com>
2016-12-22 14:19:50 -0600

    log panic

--- models/fixtures/moveordertonewuser.go
+++ models/fixtures/moveordertonewuser.go
@@ -26,56 +26,58 @@ var MoveOrderToNewUser = New("move-order-to-new-user", func(c *gin.Context) {
 	nsDb := datastore.New(org.Namespaced(ctx))
 	oldUsr := user.New(nsDb)
 	if err := oldUsr.GetById(oldEmail); err != nil {
-		log.Error(err, ctx)
+		log.Panic(err, ctx)
 	}
 
 	newUsr := user.New(nsDb)
 	if err := newUsr.GetById(newEmail); err != nil {
-		log.Error(err, ctx)
+		log.Panic(err, ctx)
 	}
 
 	ords := make([]*order.Order, 0)
 	if _, err := order.Query(nsDb).
 		Filter("UserId=", oldUsr.Id()).
 		GetAll(&ords); err != nil {
-		log.Error(err, ctx)
+		log.Panic(err, ctx)
 	}
 	log.Warn("Moving %v orders", len(ords), ctx)
 
 	for _, ord := range ords {
+		ord.GetById(ord.Id_)
 		oldOK := ord.Key()
 		newOK := nsDb.NewKey(ord.Kind(), oldOK.StringID(), oldOK.IntID(), newUsr.Key())
 
 		ord.SetKey(newOK)
 		if err := ord.Put(); err != nil {
-			log.Error(err, ctx)
+			log.Panic(err, ctx)
 		}
 
 		ord.SetKey(oldOK)
 		if err := ord.Delete(); err != nil {
-			log.Error(err, ctx)
+			log.Panic(err, ctx)
 		}
 
 		pays := make([]*payment.Payment, 0)
 		if _, err := payment.Query(nsDb).
 			Filter("OrderId", ord.Id()).
 			GetAll(&pays); err != nil {
-			log.Error(err, ctx)
+			log.Panic(err, ctx)
 		}
 		log.Warn("Moving %v payments", len(ords), ctx)
 
 		for _, pay := range pays {
+			pay.GetById(pay.Id_)
 			oldPK := pay.Key()
 			newPK := nsDb.NewKey(pay.Kind(), oldPK.StringID(), oldPK.IntID(), newOK)
 
 			pay.SetKey(newPK)
 			if err := pay.Put(); err != nil {
-				log.Error(err, ctx)
+				log.Panic(err, ctx)
 			}
 
 			pay.SetKey(oldPK)
 			if err := pay.Delete(); err != nil {
-				log.Error(err, ctx)
+				log.Panic(err, ctx)
 			}
 		}
 	}
