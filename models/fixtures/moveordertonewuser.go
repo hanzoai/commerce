daacf0e869785d1c7c23ee179ff9a08fe813793c
David Tai <antisocialmunky@gmail.com>
2016-12-21 20:06:58 -0600

    fixture

---
+++ models/fixtures/moveordertonewuser.go
@@ -0,0 +1,75 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"crowdstart.com/datastore"
+	"crowdstart.com/models/order"
+	"crowdstart.com/models/organization"
+	"crowdstart.com/models/payment"
+	"crowdstart.com/models/user"
+	"crowdstart.com/util/log"
+)
+
+var MoveOrderToNewUser = New("move-order-to-new-user", func(c *gin.Context) {
+	oldEmail := "marktwellsa@mac.com"
+	newEmail := "marktwells@mac.com"
+
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.MustGetById("stoned")
+
+	ctx := org.Context()
+
+	nsDb := datastore.New(org.Namespaced(ctx))
+	oldUsr := user.New(nsDb)
+	oldUsr.GetById(oldEmail)
+
+	newUsr := user.New(nsDb)
+	newUsr.GetById(newEmail)
+
+	ords := make([]*order.Order, 0)
+	if _, err := order.Query(db).
+		Filter("UserId=", oldUsr.Id()).
+		GetAll(&ords); err != nil {
+		log.Error(err, ctx)
+	}
+
+	for _, ord := range ords {
+		oldOK := ord.Key()
+		newOK := db.NewKey(ord.Kind(), oldOK.StringID(), oldOK.IntID(), newUsr.Key())
+
+		ord.SetKey(newOK)
+		if err := ord.Put(); err != nil {
+			log.Error(err, ctx)
+		}
+
+		ord.SetKey(oldOK)
+		if err := ord.Delete(); err != nil {
+			log.Error(err, ctx)
+		}
+
+		pays := make([]*payment.Payment, 0)
+		if _, err := payment.Query(db).
+			Filter("OrderId", ord.Id()).
+			GetAll(&pays); err != nil {
+			log.Error(err, ctx)
+		}
+
+		for _, pay := range pays {
+			oldPK := pay.Key()
+			newPK := db.NewKey(pay.Kind(), oldPK.StringID(), oldPK.IntID(), newOK)
+
+			pay.SetKey(newPK)
+			if err := pay.Put(); err != nil {
+				log.Error(err, ctx)
+			}
+
+			pay.SetKey(oldPK)
+			if err := pay.Delete(); err != nil {
+				log.Error(err, ctx)
+			}
+		}
+	}
+})
