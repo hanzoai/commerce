fe9de0b1195191bd603697fc988ef3caceee3f0c
David Tai <antisocialmunky@gmail.com>
2017-01-22 17:30:18 -0600

    shipping email

--- util/emails/emails.go
+++ util/emails/emails.go
@@ -506,3 +506,115 @@ func SendFullRefundEmail(ctx appengine.Context, org *organization.Organization, 
 
 	mandrill.SendTemplate(ctx, "order-refunded", org.Mandrill.APIKey, toEmail, toName, fromEmail, fromName, subject, vars)
 }
+
+func SendFulfillmentEmail(ctx appengine.Context, org *organization.Organization, ord *order.Order, usr *user.User, pay *payment.Payment) {
+	conf := org.Email.OrderConfirmation.Config(org)
+	if !MandrillEnabled(ctx, org, conf) {
+		return
+	}
+
+	// From
+	fromName := conf.FromName
+	fromEmail := conf.FromEmail
+
+	// To
+	toEmail := usr.Email
+	toName := usr.Name()
+
+	// Subject, HTML
+	subject := conf.Subject
+
+	currencyCode := strings.ToUpper(ord.Currency.Code())
+	countryName := country.ByISOCodeISO3166_2[ord.ShippingAddress.Country].ISO3166OneEnglishShortNameReadingOrder
+	stateName := ord.ShippingAddress.State
+	if len(stateName) <= 2 {
+		stateName = strings.ToUpper(stateName)
+	}
+	items := make([]map[string]interface{}, len(ord.Items))
+	vars := map[string]interface{}{
+		"order": map[string]interface{}{
+			"number":           ord.DisplayId(),
+			"displaysubtotal":  ord.DisplaySubtotal(),
+			"displaytax":       ord.DisplayTax(),
+			"displayshipping":  ord.DisplayShipping(),
+			"displaytotal":     ord.DisplayTotal(),
+			"displayrefunded":  ord.DisplayRefunded(),
+			"displayremaining": ord.DisplayRemaining(),
+			"currency":         currencyCode,
+			"items":            items,
+			"shippingaddress": map[string]interface{}{
+				"line1":      ord.ShippingAddress.Line1,
+				"line2":      ord.ShippingAddress.Line2,
+				"postalcode": ord.ShippingAddress.PostalCode,
+				"city":       ord.ShippingAddress.City,
+				"state":      stateName,
+				"country":    countryName,
+			},
+			"createdday":       ord.CreatedAt.Day(),
+			"createdmonthname": ord.CreatedAt.Month().String(),
+			"createdyear":      ord.CreatedAt.Year(),
+			"fulfillment": map[string]interface{}{
+				"trackingNumber": ord.Fulfillment.TrackingNumber,
+				"service":        ord.Fulfillment.Service,
+				"carrier":        ord.Fulfillment.Carrier,
+			},
+		},
+		"ORDER_NUMBER":                      ord.DisplayId(),
+		"ORDER_DISPLAY_SUBTOTAL":            ord.DisplaySubtotal(),
+		"ORDER_DISPLAY_TAX":                 ord.DisplayTax(),
+		"ORDER_DISPLAY_SHIPPING":            ord.DisplayShipping(),
+		"ORDER_DISPLAY_TOTAL":               ord.DisplayTotal(),
+		"ORDER_DISPLAY_REFUNDED":            ord.DisplayRefunded(),
+		"ORDER_DISPLAY_REMAINING":           ord.DisplayRemaining(),
+		"ORDER_CURRENCY":                    currencyCode,
+		"ORDER_SHIPPING_ADDRESS_LINE1":      ord.ShippingAddress.Line1,
+		"ORDER_SHIPPING_ADDRESS_LINE2":      ord.ShippingAddress.Line2,
+		"ORDER_SHIPPING_ADDRESS_POSTALCODE": ord.ShippingAddress.PostalCode,
+		"ORDER_SHIPPING_ADDRESS_CITY":       ord.ShippingAddress.City,
+		"ORDER_SHIPPING_ADDRESS_STATE":      stateName,
+		"ORDER_SHIPPING_ADDRESS_COUNTRY":    countryName,
+		"ORDER_CREATED_DAY":                 ord.CreatedAt.Day(),
+		"ORDER_CREATED_MONTH_NAME":          ord.CreatedAt.Month().String(),
+		"ORDER_CREATED_YEAR":                ord.CreatedAt.Year(),
+		"ORDER_FULFILLMENT_TRACKING_NUMBER": ord.Fulfillment.TrackingNumber,
+		"ORDER_FULFILLMENT_SERVICE":         ord.Fulfillment.Service,
+		"ORDER_FULFILLMENT_CARRIER":         ord.Fulfillment.Carrier,
+
+		"user": map[string]interface{}{
+			"firstname": usr.FirstName,
+			"lastname":  usr.LastName,
+		},
+
+		"USER_FIRSTNAME": usr.FirstName,
+		"USER_LASTNAME":  usr.LastName,
+
+		"payment": map[string]interface{}{
+			"lastfour": pay.Account.LastFour,
+		},
+
+		"PAYMENT_LASTFOUR": pay.Account.LastFour,
+	}
+
+	if ord.Discount != 0 {
+		ordVars := vars["order"].(map[string]interface{})
+		ordVars["displaydiscount"] = ord.DisplayDiscount()
+		vars["ORDER_DISPLAY_DISCOUNT"] = ord.DisplayDiscount()
+	}
+
+	for i, item := range ord.Items {
+		items[i] = map[string]interface{}{
+			"productname":  item.ProductName,
+			"quantity":     item.Quantity,
+			"displayprice": item.DisplayPrice(ord.Currency),
+			"currency":     currencyCode,
+		}
+
+		idx := strconv.Itoa(i)
+		vars["ORDER_ITEMS_"+idx+"_PRODUCT_NAME"] = item.ProductName
+		vars["ORDER_ITEMS_"+idx+"_QUANTITY"] = item.Quantity
+		vars["ORDER_ITEMS_"+idx+"_DISPLAY_PRICE"] = item.DisplayTotalPrice(ord.Currency)
+		vars["ORDER_ITEMS_"+idx+"_CURRENCY"] = currencyCode
+	}
+
+	mandrill.SendTemplate(ctx, "order-shipped", org.Mandrill.APIKey, toEmail, toName, fromEmail, fromName, subject, vars)
+}
