5c03ca6d516dc927c8ac7b23392014fef99e37a9
Zach Kelling <z@zeekay.io>
2017-02-02 16:57:38 -0600

    Update export destination.

--- scripts/shipwire_export.py
+++ scripts/shipwire_export.py
@@ -2,12 +2,16 @@
 from shipwire import *
 import json
 
-s = Shipwire(username='dev@hanzo.ai',
-             password='',
-             host='api.shipwire.com')
+def export_orders():
+    s = Shipwire(username='dev@hanzo.ai',
+                 password='',
+                 host='api.shipwire.com')
 
-r = s.orders.list()
+    r = s.orders.list()
 
-with open('shipwire.json', 'w') as f:
-    for order in r.all_serial():
-        f.write(json.dumps(order['resource']) + '\n')
+    with open('shipwire.json', 'w') as f:
+        for order in r.all_serial():
+            f.write(json.dumps(order['resource']) + '\n')
+
+if __name__ == '__main__':
+    export_orders()
