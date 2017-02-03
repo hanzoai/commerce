6282fb63766326574d4bb15b701e773558adba62
Zach Kelling <z@zeekay.io>
2017-02-02 22:35:40 -0600

    Sort imports.

--- scripts/shipwire_export.py
+++ scripts/shipwire_export.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python
-from shipwire import *
-import json
-
-
-def read_cached():
-    with open('shipwire.json') as f:
-        for line in f:
-            yield json.loads(line)
-
-
-def write_cached():
-    s = Shipwire(username='dev@hanzo.ai',
-                 password='',
-                 host='api.shipwire.com')
-
-    r = s.orders.list()
-
-    with open('shipwire.json', 'w') as f:
-        for order in r.all_serial():
-            f.write(json.dumps(order['resource']) + '\n')
-
+from shipwire import write_cache
 
 if __name__ == '__main__':
-    write_cached()
+    write_cache()
