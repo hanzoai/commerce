ec05b111dbd9c0addfa2c8596109bc8a38ff4cd4
Zach Kelling <z@zeekay.io>
2017-02-02 21:54:44 -0600

    Use modern Python.

--- scripts/filter_orders.py
+++ scripts/filter_orders.py
@@ -2,6 +2,7 @@
 import os
 from datetime import datetime
 from export import Export, json, latest_csv, to_csv
+from export.filters import *
 from shipwire import *
 from shipwire_export import read_cached, write_cached
 
@@ -101,7 +102,7 @@ def get_orders(filter):
     """Return orders matching some predicate(s)."""
 
     # Load shipwire orders
-    s_orders = dict((x['orderNo'], x) for x in read_cached())
+    s_orders = {x['orderNo']: x for x in read_cached()}
 
     # Load latest users, orders
     users  = User(latest_csv('user')).to_dict()
@@ -128,41 +129,6 @@ def get_orders(filter):
             print order
 
     return filtered_orders
-
-
-# Various predicates to use for filtering orders
-def open(order):
-    return order.status == 'open' and order.payment_status == 'paid'
-
-def cancelled(order):
-    return order.status == 'cancelled' or order.payment_status == 'refunded'
-
-def locked(order):
-    return order.status == 'locked'
-
-def disputed(order):
-    return order.payment_status == 'disputed'
-
-def invalid(order):
-    return not open(order) and not cancelled(order) and not disputed(order)
-
-def domestic(order):
-    return order.shipping_address_country == 'US'
-
-def international(order):
-    return not domestic(order)
-
-def batch1(order):
-    return order.batch == 1
-
-def f2k(order):
-    return order.batch == 'f2k'
-
-def processed(order):
-    return order.s_status and order.s_status != 'cancelled'
-
-def from2016(order):
-    return order.created_at.year == 2016
 
 
 if __name__ == '__main__':
