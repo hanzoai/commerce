8b1431f33750f689a387e6609ade0b136287029a
David Tai <david@verus.io>
2017-02-04 13:28:41 -0600

    shipwire webhook

--- thirdparty/shipwire/webhook/webhook.go
+++ thirdparty/shipwire/webhook/webhook.go
@@ -5,29 +5,91 @@ import (
 
 	"github.com/gin-gonic/gin"
 
+	"crowdstart.com/datastore"
+	"crowdstart.com/middleware"
+	"crowdstart.com/models/order"
 	"crowdstart.com/thirdparty/shipwire/response"
+	"crowdstart.com/util/json"
+	"crowdstart.com/util/json/http"
+
+	. "crowdstart.com/models"
 )
 
-// Webhook Response Body
-type MessageBody struct {
-	Body struct {
-		response.Ref
+// Webhook Response Bodies
+type TrackingMessageBody struct {
+	response.TrackingRef
 
-		Status  string `json:"status"`
-		Message string `json:"message"`
-	}
+	Status  string `json:"status"`
+	Message string `json:"message"`
 }
 
-// Webhook Response
+// Webhook Responses
 type Message struct {
-	Topic                 string      `json:"topic"`
-	Attempt               string      `json:"attempt"`
-	Timestamp             time.Time   `json:"timestamp"`
-	UniqueEventId         string      `json:"uniqueEventID"`
-	WebhookSubscriptionId int         `json:"webhookSubscriptionID"`
-	Body                  MessageBody `json:"body"`
+	Topic                 string    `json:"topic"`
+	Attempt               string    `json:"attempt"`
+	Timestamp             time.Time `json:"timestamp"`
+	UniqueEventId         string    `json:"uniqueEventID"`
+	WebhookSubscriptionId int       `json:"webhookSubscriptionID"`
+}
+
+type TrackingMessage struct {
+	Topic                 string              `json:"topic"`
+	Attempt               string              `json:"attempt"`
+	Timestamp             time.Time           `json:"timestamp"`
+	UniqueEventId         string              `json:"uniqueEventID"`
+	WebhookSubscriptionId int                 `json:"webhookSubscriptionID"`
+	Body                  TrackingMessageBody `json:"body"`
+}
+
+func Send200(c *gin.Context) {
+	c.String(200, "ok\n")
 }
 
 func Process(c *gin.Context) {
+	org := middleware.GetOrganization(c)
+	db := datastore.New(org.Namespaced(c))
+
+	var trackingMsg *TrackingMessage
+
+	if err := json.Decode(c.Request.Body, trackingMsg); err != nil {
+		http.Fail(c, 400, "Failed decode request body", err)
+		return
+	}
+
+	switch trackingMsg.Topic {
+	case "tracking.created", "tracking.updated", "tracking.delivered":
+		ord := order.New(db)
+		trackingRsrc := trackingMsg.Body.Resource
+		if err := ord.GetById(trackingRsrc.OrderExternalId); err != nil {
+			http.Fail(c, 400, "Failed decode request body", err)
+			return
+		}
+
+		ord.Fulfillment.TrackingNumber = trackingRsrc.Tracking
+		if !trackingRsrc.LabelCreatedDate.IsZero() {
+			ord.FulfillmentStatus = FulfillmentLabelled
+		} else if !trackingRsrc.TrackedDate.IsZero() {
+			ord.FulfillmentStatus = FulfillmentProcessing
+		} else if !trackingRsrc.FirstScanDate.IsZero() {
+			ord.FulfillmentStatus = FulfillmentShipped
+		} else if !trackingRsrc.DeliveredDate.IsZero() {
+			ord.FulfillmentStatus = FulfillmentDelivered
+		}
+		ord.Fulfillment.CreatedAt = trackingRsrc.LabelCreatedDate
+		ord.Fulfillment.ShippedAt = trackingRsrc.FirstScanDate
+		ord.Fulfillment.DeliveredAt = trackingRsrc.DeliveredDate
+		// ord.Fulfillment.Service = req.Service
+		ord.Fulfillment.Carrier = trackingRsrc.Carrier
+
+		// usr := user.New(db)
+		// usr.MustGetById(ord.UserId)
+
+		// pay := payment.New(db)
+		// pay.MustGetById(ord.PaymentIds[0])
+
+		// emails.SendFulfillmentEmail(db.Context, org, ord, usr, pay)
+		ord.MustPut()
+	}
+
 	c.String(200, "ok\n")
 }
