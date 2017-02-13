f073acfd8f735c88a48f23c47a0d14edd46d68ca
zeekay <zk@monoid.io>
2017-02-13 06:19:36 -0600

    Capture more information from return API.

--- models/types/fulfillment/fulfillment.go
+++ models/types/fulfillment/fulfillment.go
@@ -16,6 +16,7 @@ const (
 	Completed Status = "completed"
 	Delivered Status = "delivered"
 	Returned  Status = "returned"
+	Returning Status = "returning"
 	Submitted Status = "submitted"
 	Held      Status = "held"
 	Tracked   Status = "tracked"
@@ -27,16 +28,27 @@ const (
 	Manual      Type = "manual"
 )
 
+type Return struct {
+	Status       string    `json:"status"`
+	ExternalId   string    `json:"externalId,omitempty"`
+	CommerceName string    `json:"commerceName"`
+	ExpectedAt   time.Time `json:"expectedAt,omitempty"`
+	UpdatedAt    time.Time `json:"updatedAt,omitempty"`
+}
+
 type Fulfillment struct {
-	Type           Type           `json:"type"`
-	Status         Status         `json:"status"`
-	ExternalId     string         `json:"externalId,omitempty"`
-	Carrier        string         `json:"carrier,omitempty"`
-	Summary        string         `json:"summary,omitempty"`
-	Service        string         `json:"service,omitempty"`
-	TrackingNumber string         `json:"trackingNumber,omitempty"`
-	CreatedAt      time.Time      `json:"createdAt,omitempty"`
-	ShippedAt      time.Time      `json:"shippedAt,omitempty"`
-	DeliveredAt    time.Time      `json:"deliveredAt,omitempty"`
-	Cost           currency.Cents `json:"cost,omitempty"`
+	Type           Type   `json:"type"`
+	Status         Status `json:"status"`
+	ExternalId     string `json:"externalId,omitempty"`
+	Carrier        string `json:"carrier,omitempty"`
+	Summary        string `json:"summary,omitempty"`
+	Service        string `json:"service,omitempty"`
+	TrackingNumber string `json:"trackingNumber,omitempty"`
+
+	CreatedAt   time.Time `json:"createdAt,omitempty"`
+	ShippedAt   time.Time `json:"shippedAt,omitempty"`
+	DeliveredAt time.Time `json:"deliveredAt,omitempty"`
+
+	Cost   currency.Cents `json:"cost,omitempty"`
+	Return Return         `json:"return,omitempty"`
 }
