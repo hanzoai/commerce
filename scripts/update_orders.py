442d399682094333d86d2b5ef74b7bef35525638
zeekay <zk@monoid.io>
2017-02-13 15:50:17 -0600

    Update order update script.

--- scripts/update_orders.py
+++ scripts/update_orders.py
@@ -12,19 +12,21 @@ if __name__ == '__main__':
 
     s_orders = {x['orderNo']: x for x in shipwire.read_cache()}
 
-    for order in s_order:
-        if order['status'] != "cancelled":
-            req = {
-                "attempt": 1,
-                "body": {
-                    "status": 200,
-                    "message": "Successful",
-                    "resourceLocation": "",
-                    "resource": order,
-                },
-                "timestamp": "2017-02-13T13:31:29-08:00",
-                "topic": "order.updated",
-                "uniqueEventID": "424242424242",
-                "webhookSubscriptionID": 42,
-            }
-            requests.post("https://api.hanz.io/shipwire/stoned", json=req)
+    for order in s_orders:
+        if order['status'] == "cancelled":
+            continue
+
+        req = {
+            "attempt": 1,
+            "body": {
+                "status": 200,
+                "message": "Successful",
+                "resourceLocation": "",
+                "resource": order,
+            },
+            "timestamp": "2017-02-13T13:31:29-08:00",
+            "topic": "order.updated",
+            "uniqueEventID": "424242424242",
+            "webhookSubscriptionID": 42,
+        }
+        requests.post("https://api.hanz.io/shipwire/stoned", json=req)
