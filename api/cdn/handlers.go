deb8474150487afc24ffae1a696b8a571a00e6eb
Zach Kelling <z@zeekay.io>
2017-02-13 01:21:07 -0600

    Never got rid of old cruft.

--- api/cdn/handlers.go
+++ api/cdn/handlers.go
@@ -10,28 +10,6 @@ import (
 	"hanzo.io/util/router"
 )
 
-func init() {
-	cdn := router.New("cdn")
-
-	// Use permissive CORS policy for all API routes.
-	cdn.Use(middleware.AccessControl("*"))
-	cdn.OPTIONS("*wildcard", func(c *gin.Context) {
-		c.Next()
-	})
-
-	cdn.GET("/a/:organizationid", analytics.Js)
-	cdn.GET("/a/:organizationid/analytics.js", analytics.Js)
-	cdn.GET("/a/:organizationid/js", analytics.Js)
-
-	cdn.GET("/m/:mailinglistid/mailinglist.js", mailinglist.Js)
-	cdn.GET("/m/:mailinglistid/js", mailinglist.Js)
-
-	cdn.GET("/n/:organizationid/native.js", native.Js)
-
-	cdn.HEAD("/", router.Empty)
-	cdn.GET("/robots.txt", router.Robots)
-}
-
 func group(r router.Router, prefix string) *gin.RouterGroup {
 	g := r.Group(prefix)
 
