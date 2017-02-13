568a587cc253295f99433244504880b1dc43bac6
Zach Kelling <z@zeekay.io>
2017-02-13 14:56:15 -0600

    I don't think you understand how disappointed David is.

---
+++ scripts/update_orders.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from util.export import Export, json
+from util import shipwire
+
+
+class Order(Export):
+    fields = {
+        'id_':    str,
+        'items_': json,
+        'number': str,
+        'email':  str,
+
+        'shipping_address_name':        str,
+        'shipping_address_country':     str,
+        'shipping_address_state':       str,
+        'shipping_address_city':        str,
+        'shipping_address_postal_code': str,
+        'shipping_address_line1':       str,
+        'shipping_address_line2':       str,
+    }
+
+
+if __name__ == '__main__':
+    sw = shipwire.Shipwire()
+    for order in Order('orders.csv').read_csv():
+        sw.ship_order(order, level='2D')
