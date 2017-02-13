66a2fc01f0f98e84b51e100f6783e7b8f5cc8740
Zach Kelling <z@zeekay.io>
2017-02-13 04:41:09 -0600

    shipwire: better logging.

--- thirdparty/shipwire/client.go
+++ thirdparty/shipwire/client.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	"time"
 
+	"appengine"
 	"appengine/urlfetch"
 
 	"github.com/gin-gonic/gin"
 
 	"hanzo.io/middleware"
 	"hanzo.io/util/json"
+	"hanzo.io/util/log"
 
 	. "hanzo.io/thirdparty/shipwire/types"
 )
@@ -21,6 +23,7 @@ type Client struct {
 	Endpoint string
 
 	client *http.Client
+	ctx    appengine.Context
 }
 
 func New(c *gin.Context, username, password string) *Client {
@@ -37,6 +40,7 @@ func New(c *gin.Context, username, password string) *Client {
 		Password: password,
 		Endpoint: "https://api.shipwire.com/api/v3/",
 		client:   client,
+		ctx:      ctx,
 	}
 }
 
@@ -50,26 +54,37 @@ func (c *Client) Request(method, url string, body interface{}, dst interface{}) 
 
 	req, err := http.NewRequest(method, c.Endpoint+url, payload)
 	if err != nil {
+		log.Error("Failed to create Shipwire request: %v", err, c.ctx)
 		return nil, err
 	}
 
 	// req.SetBasicAuth(c.Username, c.Password)
 	req.Header.Add("Content-Type", "application/json")
 
+	log.Warn("Shipwire Req:\n%v\n%v", req, c.ctx)
+
 	// Do request
 	r, err := c.client.Do(req)
 	if err != nil {
+		log.Error("Shipwire request failed: %v", err, c.ctx)
 		return &res, err
 	}
 
-	// Automatically decode body
-	if dst != nil {
-		// TODO: Do we need to close this?
-		err = json.Decode(r.Body, &res)
-		if err != nil {
-			// Get first resource
-			err = json.Unmarshal(res.Resource.Items[0].Resource, &dst)
-		}
+	log.Warn("Shipwire response:\n%v", res, c.ctx)
+
+	if dst == nil {
+		return &res, nil
+	}
+
+	// Automatically decode response
+	err = json.Decode(r.Body, &res)
+	if err == nil {
+		// Try to decode inner resource as dst assuming list of one item
+		err = json.Unmarshal(res.Resource.Items[0].Resource, &dst)
+	}
+
+	if err != nil {
+		log.Warn("Failed to decode Shipwire response:\n%v", err, c.ctx)
 	}
 
 	return &res, err
