4c25e8ed62a3fc49a69a899547d14d948313b112
Zach Kelling <z@zeekay.io>
2017-02-14 02:04:06 -0600

    More robust shipwire tracking update.

---
+++ thirdparty/shipwire/webhook/handlers.go
@@ -0,0 +1,28 @@
+package webhook
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/datastore"
+	"hanzo.io/models/organization"
+	"hanzo.io/util/log"
+	"hanzo.io/util/router"
+)
+
+func setOrg(c *gin.Context) {
+	db := datastore.New(c)
+	org := organization.New(db)
+	if err := org.GetById(c.Params.ByName("organization")); err != nil {
+		log.Panic("Organization not specified", c)
+	}
+
+	c.Set("organization", org)
+}
+
+func Route(r router.Router, args ...gin.HandlerFunc) {
+	api := r.Group("shipwire")
+
+	api.HEAD("/:organization", setOrg, router.Ok)
+	api.GET("/:organization", setOrg, Process)
+	api.POST("/:organization", setOrg, Process)
+}
