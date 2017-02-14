7921a32a5f3ad33b7543ea6db42fcc150ba1daca
Zach Kelling <z@zeekay.io>
2017-02-14 05:50:19 -0600

    Add rates API.

---
+++ thirdparty/shipwire/rate.go
@@ -0,0 +1,47 @@
+package shipwire
+
+import (
+	"time"
+
+	"hanzo.io/models/order"
+	"hanzo.io/models/user"
+
+	. "hanzo.io/thirdparty/shipwire/types"
+)
+
+func (c *Client) Rate(ord *order.Order, usr *user.User, serviceLevelCode ServiceLevelCode) (*Rates, *Response, error) {
+	req := RateRequest{}
+
+	req.Options.Currency = ord.Currency.Code()
+	req.Options.CanSplit = 1
+	req.Options.WarehouseArea = "US"
+	// req.Options.ChannelName = "My Channel"
+	req.Options.ExpectedShipDate = Date{time.Now()}
+	req.Options.HighAccuracyEstimates = 1
+	req.Options.ReturnAllRates = 1
+
+	req.Order.ShipTo.Address1 = ord.ShippingAddress.Line1
+	req.Order.ShipTo.Address2 = ord.ShippingAddress.Line2
+	req.Order.ShipTo.City = ord.ShippingAddress.City
+	req.Order.ShipTo.State = ord.ShippingAddress.State
+	req.Order.ShipTo.Country = ord.ShippingAddress.Country
+	req.Order.ShipTo.PostalCode = ord.ShippingAddress.PostalCode
+	req.Order.Items = make([]Item, len(ord.Items))
+
+	for i, item := range ord.Items {
+		req.Order.Items[i] = Item{
+			SKU:      item.SKU(),
+			Quantity: item.Quantity,
+		}
+	}
+
+	rates := Rates{}
+
+	// Use /api/v3.1/rate
+	res, err := c.Request("POST", ".1/rate", req, &rates)
+	if err != nil {
+		return &rates, res, err
+	}
+
+	return &rates, res, ord.Update()
+}
