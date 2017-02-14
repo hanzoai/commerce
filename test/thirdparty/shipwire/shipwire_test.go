3fe02e2811c668645cc2b0708704b772bfc46a9a
zeekay <zk@monoid.io>
2017-02-14 04:34:34 -0600

    Fix date parsing and add tests.

--- test/thirdparty/shipwire/shipwire_test.go
+++ test/thirdparty/shipwire/shipwire_test.go
@@ -1,17 +1,28 @@
 package test
 
 import (
-	"hanzo.io/api/checkout"
-	"hanzo.io/models/lineitem"
-	"hanzo.io/models/order"
-	"hanzo.io/models/payment"
-	"hanzo.io/models/product"
-	"hanzo.io/models/user"
-	"hanzo.io/models/variant"
-	"hanzo.io/util/log"
+	"hanzo.io/util/fs"
+	"hanzo.io/util/json"
 
+	. "hanzo.io/thirdparty/shipwire/types"
 	. "hanzo.io/util/test/ginkgo"
 )
 
+func decodeRequest(data []byte, dst interface{}) error {
+	var req Request
+	if err := json.DecodeBytes(data, &req); err != nil {
+		return err
+	}
+	return json.Unmarshal(req.Body.Resource, dst)
+}
+
 var _ = Describe("shipwire", func() {
+	Context("Date", func() {
+		It("Should unmarshall Shipwire dates correctly", func() {
+			data := fs.ReadFile("fixtures/tracking.updated.json")
+			var t Tracking
+			err := decodeRequest(data, &t)
+			Expect(err).To(BeNil())
+		})
+	})
 })
