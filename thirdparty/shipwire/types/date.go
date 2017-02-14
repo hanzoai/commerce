f5a0e8ff40cc0ff62b02f3fbbf7e1ec525f9bd33
Zach Kelling <z@zeekay.io>
2017-02-13 18:16:18 -0600

    I wondered why no one had a shipwire Go client.

--- thirdparty/shipwire/types/date.go
+++ thirdparty/shipwire/types/date.go
@@ -13,5 +13,15 @@ func (d *Date) UnmarshalJSON(data []byte) error {
 		return nil
 	}
 
+	if t, err := time.Parse("2006-01-02T15:04:05-07:00", string(data)); err == nil {
+		d.Time = t
+		return nil
+	}
+
+	if t, err := time.Parse("2006-01-02 15:04:05", string(data)); err == nil {
+		d.Time = t
+		return nil
+	}
+
 	return d.Time.UnmarshalJSON(data)
 }
