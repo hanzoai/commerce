20937754b21261073823482e08713f9f1315eabd
David Tai <antisocialmunky@gmail.com>
2017-02-27 19:17:35 -0600

    adding log apis

---
+++ models/log/model.go
@@ -0,0 +1,28 @@
+package log
+
+import "hanzo.io/datastore"
+
+var kind = "log"
+
+func (l Log) Kind() string {
+	return kind
+}
+
+func (l *Log) Init(db *datastore.Datastore) {
+	l.Model.Init(db, l)
+}
+
+func (l *Log) Defaults() {
+	l.Enabled = true
+}
+
+func New(db *datastore.Datastore) *Log {
+	l := new(Log)
+	l.Init(db)
+	l.Defaults()
+	return l
+}
+
+func Query(db *datastore.Datastore) datastore.Query {
+	return db.Query(kind)
+}
