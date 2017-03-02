a89cd31e811a4185cff670f3267a9e368994a783
David Tai <antisocialmunky@gmail.com>
2017-03-02 15:36:27 -0600

    temp POST search note

--- api/search/handlers.go
+++ api/search/handlers.go
@@ -17,5 +17,5 @@ func Route(router router.Router, args ...gin.HandlerFunc) {
 
 	group.GET("/user", adminRequired, namespaced, searchUser)
 	group.GET("/order", adminRequired, namespaced, searchOrder)
-	group.GET("/note", adminRequired, namespaced, searchNote)
+	group.POST("/note", adminRequired, namespaced, searchNote)
 }
