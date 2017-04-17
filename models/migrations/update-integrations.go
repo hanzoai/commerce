00753e6611b11f5857d2d397c4764074b0564a23
David Tai <david@verus.io>
2017-04-17 14:24:28 -0500

    integration migration + more tests

--- models/migrations/update-integrations.go
+++ models/migrations/update-integrations.go
@@ -41,6 +41,73 @@ var _ = New("update-integrations",
 
 			org.Analytics.Integrations[i].IntegrationId = in.Id
 		}
+
+		if mailchimps := org.Integrations.FilterByType(integrations.MailchimpType); len(mailchimps) > 0 {
+			m := mailchimps[0]
+			m.Mailchimp = org.Mailchimp
+			org.Integrations.MustUpdate(&m)
+		} else {
+			m := integrations.Integration{
+				Mailchimp: org.Mailchimp,
+			}
+			org.Integrations.MustUpdate(&m)
+		}
+
+		if mandrills := org.Integrations.FilterByType(integrations.MandrillType); len(mandrills) > 0 {
+			m := mandrills[0]
+			m.Mandrill = org.Mandrill
+			org.Integrations.MustUpdate(&m)
+		} else {
+			m := integrations.Integration{
+				Mandrill: org.Mandrill,
+			}
+			org.Integrations.MustUpdate(&m)
+		}
+
+		if netlifies := org.Integrations.FilterByType(integrations.NetlifyType); len(netlifies) > 0 {
+			n := netlifies[0]
+			n.Netlify = org.Netlify
+			org.Integrations.MustUpdate(&n)
+		} else {
+			n := integrations.Integration{
+				Netlify: org.Netlify,
+			}
+			org.Integrations.MustUpdate(&n)
+		}
+
+		if reamazes := org.Integrations.FilterByType(integrations.ReamazeType); len(reamazes) > 0 {
+			r := reamazes[0]
+			r.Reamaze = org.Reamaze
+			org.Integrations.MustUpdate(&r)
+		} else {
+			r := integrations.Integration{
+				Reamaze: org.Reamaze,
+			}
+			org.Integrations.MustUpdate(&r)
+		}
+
+		if recaptchas := org.Integrations.FilterByType(integrations.RecaptchaType); len(recaptchas) > 0 {
+			r := recaptchas[0]
+			r.Recaptcha = org.Recaptcha
+			org.Integrations.MustUpdate(&r)
+		} else {
+			r := integrations.Integration{
+				Recaptcha: org.Recaptcha,
+			}
+			org.Integrations.MustUpdate(&r)
+		}
+
+		if stripes := org.Integrations.FilterByType(integrations.MailchimpType); len(stripes) > 0 {
+			s := stripes[0]
+			s.Stripe = org.Stripe
+			org.Integrations.MustUpdate(&s)
+		} else {
+			s := integrations.Integration{
+				Stripe: org.Stripe,
+			}
+			org.Integrations.MustUpdate(&s)
+		}
+
 		org.Update()
 	},
 )
