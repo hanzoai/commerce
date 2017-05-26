d73ca65905ba18404e4f226d2b94e7cdad00e80e
David Tai <david@verus.io>
2017-05-25 22:32:32 -0500

    product document

--- models/migrations/add-search-documents.go
+++ models/migrations/add-search-documents.go
@@ -5,7 +5,8 @@ import (
 
 	ds "hanzo.io/datastore"
 	"hanzo.io/models/order"
-	"hanzo.io/models/user"
+	"hanzo.io/models/product"
+	// "hanzo.io/models/user"
 )
 
 var _ = New("add-search-documents",
@@ -14,10 +15,13 @@ var _ = New("add-search-documents",
 
 		return NoArgs
 	},
-	func(db *ds.Datastore, u *user.User) {
-		u.PutDocument()
-	},
+	// func(db *ds.Datastore, u *user.User) {
+	// 	u.PutDocument()
+	// },
 	func(db *ds.Datastore, o *order.Order) {
 		o.PutDocument()
+	},
+	func(db *ds.Datastore, p *product.Product) {
+		p.PutDocument()
 	},
 )
