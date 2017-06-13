4868bbac6f8b6bcf8107bf6cb9ca8ee637003d94
David Tai <david@verus.io>
2017-06-13 14:48:15 -0500

    add mailchimp store and product initialization migrations, add cover-referralprogram fixture, add default subscribe action for form submit

---
+++ models/fixtures/cover-referralprogram.go
@@ -0,0 +1,154 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/datastore"
+	"hanzo.io/models/organization"
+	"hanzo.io/models/referral"
+	"hanzo.io/models/referralprogram"
+	"hanzo.io/models/types/currency"
+)
+
+var CoverReferralProgram = New("cover-referralprogram", func(c *gin.Context) *referralprogram.ReferralProgram {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.MustGetById("cover")
+
+	nsDb := datastore.New(org.Namespaced(org.Context()))
+
+	rp := referralprogram.New(nsDb)
+	rp.Name = "Cover Referral Program"
+	rp.GetOrCreate("Name=", rp.Name)
+
+	rp.Actions = []referralprogram.Action{
+		// referralprogram.Action{
+		// 	Type: referralprogram.StoreCredit,
+		// 	Name: "Sign Up Action",
+
+		// 	CreditAction: referralprogram.CreditAction{
+		// 		Currency: currency.PNT,
+		// 		Amount:   currency.Cents(10),
+		// 	},
+
+		// 	Trigger: referralprogram.Trigger{
+		// 		Event: referral.NewUser,
+		// 		Type:  referralprogram.Always,
+		// 	},
+		// },
+		referralprogram.Action{
+			Type: referralprogram.StoreCredit,
+			Name: "Sale Action",
+
+			CreditAction: referralprogram.CreditAction{
+				Currency: currency.PNT,
+				Amount:   currency.Cents(100),
+			},
+
+			Trigger: referralprogram.Trigger{
+				Event: referral.NewOrder,
+				Type:  referralprogram.Always,
+			},
+		},
+
+		// referralprogram.Action{
+		// 	Type: referralprogram.SendUserEmail,
+		// 	Name: "L1 Referrer",
+		// 	Once: true,
+
+		// 	SendTransactionalUserEmailAction: referralprogram.SendTransactionalUserEmailAction{
+		// 		EmailTemplate: "referral-level-1-reached",
+		// 	},
+
+		// 	Trigger: referralprogram.Trigger{
+		// 		Type: referralprogram.CreditGreaterThanOrEquals,
+
+		// 		CreditGreaterThanOrEqualsTrigger: referralprogram.CreditGreaterThanOrEqualsTrigger{
+		// 			Currency:                  currency.PNT,
+		// 			CreditGreaterThanOrEquals: currency.Cents(840),
+		// 		},
+		// 	},
+		// },
+
+		// referralprogram.Action{
+		// 	Type: referralprogram.SendUserEmail,
+		// 	Name: "L2 Referrer",
+		// 	Once: true,
+
+		// 	SendTransactionalUserEmailAction: referralprogram.SendTransactionalUserEmailAction{
+		// 		EmailTemplate: "referral-level-2-reached",
+		// 	},
+
+		// 	Trigger: referralprogram.Trigger{
+		// 		Type: referralprogram.CreditGreaterThanOrEquals,
+
+		// 		CreditGreaterThanOrEqualsTrigger: referralprogram.CreditGreaterThanOrEqualsTrigger{
+		// 			Currency:                  currency.PNT,
+		// 			CreditGreaterThanOrEquals: currency.Cents(1680),
+		// 		},
+		// 	},
+		// },
+
+		// referralprogram.Action{
+		// 	Type: referralprogram.SendUserEmail,
+		// 	Name: "L3 Referrer",
+		// 	Once: true,
+
+		// 	SendTransactionalUserEmailAction: referralprogram.SendTransactionalUserEmailAction{
+		// 		EmailTemplate: "referral-level-3-reached",
+		// 	},
+
+		// 	Trigger: referralprogram.Trigger{
+		// 		Type: referralprogram.CreditGreaterThanOrEquals,
+
+		// 		CreditGreaterThanOrEqualsTrigger: referralprogram.CreditGreaterThanOrEqualsTrigger{
+		// 			Currency:                  currency.PNT,
+		// 			CreditGreaterThanOrEquals: currency.Cents(2520),
+		// 		},
+		// 	},
+		// },
+
+		// referralprogram.Action{
+		// 	Type: referralprogram.SendUserEmail,
+		// 	Name: "L4 Referrer",
+		// 	Once: true,
+
+		// 	SendTransactionalUserEmailAction: referralprogram.SendTransactionalUserEmailAction{
+		// 		EmailTemplate: "referral-level-4-reached",
+		// 	},
+
+		// 	Trigger: referralprogram.Trigger{
+		// 		Type: referralprogram.CreditGreaterThanOrEquals,
+
+		// 		CreditGreaterThanOrEqualsTrigger: referralprogram.CreditGreaterThanOrEqualsTrigger{
+		// 			Currency:                  currency.PNT,
+		// 			CreditGreaterThanOrEquals: currency.Cents(3360),
+		// 		},
+		// 	},
+		// },
+
+		// referralprogram.Action{
+		// 	Type: referralprogram.SendUserEmail,
+		// 	Name: "L5 Referrer",
+		// 	Once: true,
+
+		// 	SendTransactionalUserEmailAction: referralprogram.SendTransactionalUserEmailAction{
+		// 		EmailTemplate: "referral-level-5-reached",
+		// 	},
+
+		// 	Trigger: referralprogram.Trigger{
+		// 		Type: referralprogram.CreditGreaterThanOrEquals,
+
+		// 		CreditGreaterThanOrEqualsTrigger: referralprogram.CreditGreaterThanOrEqualsTrigger{
+		// 			Currency:                  currency.PNT,
+		// 			CreditGreaterThanOrEquals: currency.Cents(4200),
+		// 		},
+		// 	},
+		// },
+	}
+
+	rp.MustPut()
+
+	return rp
+})
