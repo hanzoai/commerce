ad628b60ef958640aee5d5864c9db766d2f44ee6
Timothy Messer <tmesser@verus.io>
2017-06-17 22:35:16 -0500

    Adding unikrn fixture.

---
+++ models/fixtures/unikrn.go
@@ -0,0 +1,71 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/datastore"
+	"hanzo.io/models/namespace"
+	"hanzo.io/models/organization"
+	"hanzo.io/models/user"
+	"hanzo.io/util/log"
+)
+
+var Unikrn = New("unikrn", func(c *gin.Context) *organization.Organization {
+	db := datastore.New(c)
+
+	// Create organization
+	org := organization.New(db)
+	org.Name = "unikrn"
+	org.GetOrCreate("Name=", org.Name)
+
+	// Create admins
+	u := user.New(db)
+	u.Email = "rs@unikrn.com"
+	u.GetOrCreate("Email=", u.Email)
+	u.AddOrganization(org.Id())
+	u.Put()
+
+	// Configure org
+	org.FullName = "unikrn"
+	org.AddOwner(u.Id())
+	org.Website = "http://unikrn.com"
+	org.SecretKey = []byte("75k33uj1ElU4LJNFAE8gtfC9DczoP9z0")
+	org.AddDefaultTokens()
+
+	// Email configuration
+	// org.Mandrill.APIKey = config.Mandrill.APIKey
+
+	// org.Email.Defaults.Enabled = true
+	// org.Email.Defaults.FromName = "hanzo"
+	// org.Email.Defaults.FromEmail = "hi@hanzo.com"
+
+	// org.Email.OrderConfirmation.Subject = "LEAF Order Confirmation"
+	// org.Email.OrderConfirmation.Template = readEmailTemplate("/resources/hanzo/emails/order-confirmation.html")
+	// org.Email.OrderConfirmation.Enabled = true
+
+	// org.Email.User.PasswordReset.Template = readEmailTemplate("/resources/hanzo/emails/user-password-reset.html")
+	// org.Email.User.PasswordReset.Subject = "Reset your hanzo password"
+	// org.Email.User.PasswordReset.Enabled = true
+
+	// org.Email.User.EmailConfirmation.Template = readEmailTemplate("/resources/hanzo/emails/user-email-confirmation.html")
+	// org.Email.User.EmailConfirmation.Subject = "Please confirm your email"
+	// org.Email.User.EmailConfirmation.Enabled = true
+
+	// org.Email.User.EmailConfirmed.Subject = "Thank you for confirming your email"
+	// org.Email.User.EmailConfirmed.Template = readEmailTemplate("/resources/hanzo/emails/user-email-confirmed.html")
+	// org.Email.User.EmailConfirmed.Enabled = true
+
+	// Save org into default namespace
+	org.Put()
+
+	// Save namespace so we can decode keys for this organization later
+	ns := namespace.New(db)
+	ns.Name = org.Name
+	ns.IntId = org.Key().IntID()
+	err := ns.Put()
+	if err != nil {
+		log.Warn("Failed to put namespace: %v", err)
+	}
+
+	return org
+})
