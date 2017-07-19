f9a78d9dfb63336e552762f98a356066343830f7
Zach Kelling <z@zeekay.io>
2017-07-19 14:41:16 -0500

    Add enable welcome fixture

---
+++ models/fixtures/cover-enable-welcome.go
@@ -0,0 +1,17 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/datastore"
+	"hanzo.io/models/organization"
+)
+
+var CoverEnableWelcome = New("cover-enable-welcome", func(c *gin.Context) *organization.Organization {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.MustGetById("cover")
+	org.Email.Subscriber.Welcome.Enabled = true
+	return org
+})
