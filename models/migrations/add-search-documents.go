a029f3acf5c04c81804e627bf283c2f1b978ac4a
David Tai <antisocialmunky@gmail.com>
2017-08-02 14:26:38 -0500

    kanoa search document migraiton

--- models/migrations/add-search-documents.go
+++ models/migrations/add-search-documents.go
@@ -6,18 +6,18 @@ import (
 	ds "hanzo.io/datastore"
 	"hanzo.io/models/order"
 	"hanzo.io/models/product"
-	// "hanzo.io/models/user"
+	"hanzo.io/models/user"
 )
 
 var _ = New("add-search-documents",
 	func(c *gin.Context) []interface{} {
-		c.Set("namespace", "stoned")
+		c.Set("namespace", "kanoa")
 
 		return NoArgs
 	},
-	// func(db *ds.Datastore, u *user.User) {
-	// 	u.PutDocument()
-	// },
+	func(db *ds.Datastore, u *user.User) {
+		u.PutDocument()
+	},
 	func(db *ds.Datastore, o *order.Order) {
 		o.PutDocument()
 	},
