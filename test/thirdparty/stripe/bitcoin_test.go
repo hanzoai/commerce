1151aa0b46baf4fb371a1c845e2bcd93466a77e1
Timothy Messer <tmesser@verus.io>
2017-08-30 17:28:33 -0500

    Adding crappy test to throw at stripe API

---
+++ test/thirdparty/stripe/bitcoin_test.go
@@ -0,0 +1,17 @@
+package test
+
+import (
+	"hanzo.io/models/payment"
+	"hanzo.io/models/user"
+	. "hanzo.io/util/test/ginkgo"
+)
+
+var _ = Describe("thirdparty.stripe.client.CreateSource", func() {
+
+	Context("When CreateSource is called", func() {
+		pay := payment.Payment{Amount: 20420}
+		usr := user.User{Email: "dev@hanzo.ai"}
+
+		client.CreateSource(&pay, &usr)
+	})
+})
