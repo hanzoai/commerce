3ae7075cf1f13259981ae72a639fdf115915375b
David Tai <antisocialmunky@gmail.com>
2017-10-01 17:25:06 -0500

    wallet account type

--- models/wallet/error.go
+++ models/wallet/error.go
@@ -5,3 +5,4 @@ import "errors"
 var NoPrivateKeySetError = errors.New("No private key is set.")
 var NoSaltSetError = errors.New("No salt is set.")
 var NoEncryptedKeyFound = errors.New("No encrypted key found.")
+var InvalidTypeSpecified = errors.New("Invalid account type specified.")
