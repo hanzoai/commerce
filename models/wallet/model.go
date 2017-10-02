b36bb5f8efe0c123affbdd6f5c6ce75b39668b54
David Tai <antisocialmunky@gmail.com>
2017-10-02 14:57:59 -0500

    wallet tests

--- models/wallet/model.go
+++ models/wallet/model.go
@@ -1,1 +1,28 @@
 package wallet
+
+import "hanzo.io/datastore"
+
+var kind = "wallet"
+
+func (w Wallet) Kind() string {
+	return kind
+}
+
+func (w *Wallet) Init(db *datastore.Datastore) {
+	w.Model.Init(db, w)
+}
+
+func (w *Wallet) Defaults() {
+	w.Accounts = make([]Account, 0, 0)
+}
+
+func New(db *datastore.Datastore) *Wallet {
+	w := new(Wallet)
+	w.Init(db)
+	w.Defaults()
+	return w
+}
+
+func Query(db *datastore.Datastore) datastore.Query {
+	return db.Query(kind)
+}
