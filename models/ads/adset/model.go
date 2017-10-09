965f2d8c1786ba1c678addb45b35ff59fb186cd9
David Tai <antisocialmunky@gmail.com>
2017-10-09 13:53:48 -0500

    Add Uploaded Media Model Support

---
+++ models/ads/adset/model.go
@@ -0,0 +1,27 @@
+package adset
+
+import "hanzo.io/datastore"
+
+var kind = "adset"
+
+func (a AdSet) Kind() string {
+	return kind
+}
+
+func (a *AdSet) Init(db *datastore.Datastore) {
+	a.Model.Init(db, a)
+}
+
+func (a *AdSet) Defaults() {
+}
+
+func New(db *datastore.Datastore) *AdSet {
+	a := new(AdSet)
+	a.Init(db)
+	a.Defaults()
+	return a
+}
+
+func Query(db *datastore.Datastore) datastore.Query {
+	return db.Query(kind)
+}
---
+++ models/ads/util/util.go
@@ -0,0 +1,14 @@
+package util
+
+import (
+	"hanzo.io/models/ads/adcampaign"
+	"hanzo.io/models/ads/adset"
+)
+
+func GetCampaign(h HasCampaign) (*adcampaign.AdCampaign, error) {
+	return nil, nil
+}
+
+func GetAdset(h HasAdset) (*adset.AdSet, error) {
+	return nil, nil
+}
