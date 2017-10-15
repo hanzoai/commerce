a077fd4c46b0317fd69a53e8782b6d9417a26c32
David Tai <antisocialmunky@gmail.com>
2017-10-15 17:39:15 -0500

    add blockaddress fixture and test

--- models/blockchains/blockaddress/blockaddress.go
+++ models/blockchains/blockaddress/blockaddress.go
@@ -8,14 +8,11 @@ import (
 
 // This is a reference to an address the blockchain reader needs to watch for
 type WatchedAddress struct {
-	// This is the namespace where the walletbearer is stored
-	WalletBearerNamespace string `json:"walletBearerNamespace`
+	// This is the namespace where the wallet is stored
+	WalletNamespace string `json:"walletNamespace`
 
-	// This is the kind where the walletbearer is stored
-	WalletBearerKind string `json:"walletBearerKind"`
-
-	// This is the id of the walletbearer
-	WalletBearerId string `json:"walletBearerId"`
+	// This is the id of the wallet
+	WalletId string `json:"walletId"`
 }
 
 // BlockAddress denotes an address on the blockchain that we wnat ot keep track
@@ -23,6 +20,11 @@ type WatchedAddress struct {
 type BlockAddress struct {
 	mixin.Model
 
-	Address
 	WatchedAddress
+
+	// Address on the blockchain
+	Address string `json:"address"`
+
+	// Which blockchain contains the address
+	Type Type `json:"type"`
 }
