1a1e2d29dd4751be0059bb7693c54f83dcbf3f59
David Tai <antisocialmunky@gmail.com>
2017-10-16 01:35:32 -0500

    fix go dep and breaking stripe api change

--- thirdparty/stripe/export.go
+++ thirdparty/stripe/export.go
@@ -13,7 +13,17 @@ type ChargeListParams stripe.ChargeListParams
 type ChargeParams stripe.ChargeParams
 type Customer stripe.Customer
 type Dispute stripe.Dispute
-type Event stripe.Event
+type Event struct {
+	ID       string            `json:"id"`
+	Account  string            `json:"account"`
+	Live     bool              `json:"livemode"`
+	Created  int64             `json:"created"`
+	Data     *stripe.EventData `json:"data"`
+	Webhooks uint64            `json:"pending_webhooks"`
+	Type     string            `json:"type"`
+	Request  string            `json:"request"`
+}
+
 type Reversal stripe.Reversal
 type Token stripe.Token
 type Transfer stripe.Transfer
