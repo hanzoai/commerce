f7e80d5a492c17f3fd8ff012a4359224e3b4afbb
David Tai <antisocialmunky@gmail.com>
2017-10-16 22:27:49 -0500

    add named accounts in wallet to make things easier

---
+++ models/fixtures/test-wallet.go
@@ -0,0 +1,23 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/config"
+	"hanzo.io/datastore"
+	"hanzo.io/models/wallet"
+)
+
+var TestWallet = New("test-wallet", func(c *gin.Context) *wallet.Wallet {
+	db := datastore.New(c)
+
+	w := wallet.New(db)
+	w.Id_ = "test-wallet"
+	w.GetOrCreate("Id_=", "test-wallet")
+
+	if len(w.Accounts) == 0 {
+		w.CreateAccount("Ropsten Test Account", wallet.Ethereum, []byte(config.Ethereum.TestPrivateKey))
+	}
+
+	return w
+})
