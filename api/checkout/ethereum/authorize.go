b342c27ca503c2a833ddbd9de7b2f09628d75ad9
David Tai <david@verus.io>
2017-10-20 23:33:58 -0500

    working test ethereum send transation

--- api/checkout/ethereum/authorize.go
+++ api/checkout/ethereum/authorize.go
@@ -1,15 +1,23 @@
 package ethereum
 
 import (
+	"math/big"
+
+	"hanzo.io/config"
+	"hanzo.io/models/blockchains"
 	"hanzo.io/models/order"
 	"hanzo.io/models/organization"
 	"hanzo.io/models/user"
 	"hanzo.io/models/wallet"
+	"hanzo.io/thirdparty/ethereum"
+	"hanzo.io/util/log"
 	"hanzo.io/util/rand"
 )
 
 // This creates the wallet for
 func Authorize(org *organization.Organization, ord *order.Order, usr *user.User) error {
+	ctx := org.Db.Context
+
 	w, err := ord.GetOrCreateWallet(ord.Db)
 	if err != nil {
 		return err
@@ -17,12 +25,33 @@ func Authorize(org *organization.Organization, ord *order.Order, usr *user.User)
 
 	ord.WalletPassphrase = rand.SecretKey()
 
-	_, err = w.CreateAccount("Receiver Account", wallet.Ethereum, []byte(ord.WalletPassphrase))
+	_, err = w.CreateAccount("Receiver Account", blockchains.EthereumType, []byte(ord.WalletPassphrase))
 
-	// id := rand.Int64()
+	if ord.Test {
+		var account wallet.Account
+		pw := wallet.New(org.Db)
+		if _, err := w.Query().Filter("Id_=", "platform-wallet").Get(); err != nil {
+			return err
+		}
 
-	// if ord.Test {
-	// 	ord.
-	// }
+		// Find The Test Account
+		for _, a := range pw.Accounts {
+			if a.Name != "Ethereum Ropsten Test Account" {
+				continue
+			}
+			log.Info("Account Found", ctx)
+			if err := a.Decrypt([]byte(config.Ethereum.TestPassword)); err != nil {
+				panic(err)
+			}
+			account = a
+			break
+		}
+
+		client := ethereum.New(org.Db.Context, config.Ethereum.TestNetNodes[0])
+
+		if _, err := client.SendTransaction(ethereum.Ropsten, account.PrivateKey, account.Address, w.Accounts[0].Address, big.NewInt(1000000000000000), big.NewInt(0), big.NewInt(0), []byte{}); err != nil {
+			return err
+		}
+	}
 	return err
 }
--- models/wallet/fake.go
+++ models/wallet/fake.go
@@ -2,12 +2,13 @@ package wallet
 
 import (
 	"hanzo.io/datastore"
+	"hanzo.io/models/blockchains"
 	"hanzo.io/util/rand"
 )
 
 func Fake(db *datastore.Datastore) (*Wallet, Account, string) {
 	w := New(db)
 	password := rand.ShortPassword()
-	a, _ := w.CreateAccount("Fake Account", Ethereum, []byte(password))
+	a, _ := w.CreateAccount("Fake Account", blockchains.EthereumRopstenType, []byte(password))
 	return w, a, password
 }
