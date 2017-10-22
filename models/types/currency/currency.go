6c5d246e68f7dae325164b1124dd3a4ffa5ecc97
David Tai <antisocialmunky@gmail.com>
2017-10-21 21:23:39 -0500

    currency tests

--- models/types/currency/currency.go
+++ models/types/currency/currency.go
@@ -1,12 +1,15 @@
 package currency
 
 import (
+	"math/big"
 	"strconv"
 	"strings"
 )
 
 type Type string
 
+// Does the currency not have a decimal convention such as Japanese Yen (¥100) instead
+// of USD ($1.00)
 func (t Type) IsZeroDecimal() bool {
 	switch t {
 	case BIF, CLP, DJF, GNF, JPY, KMF, KRW, MGA, PYG, RWF, VND, VUV, XAF, XOF, XPF:
@@ -16,26 +19,21 @@ func (t Type) IsZeroDecimal() bool {
 	return false
 }
 
-func (t Type) IsCrypto() bool {
-	switch t {
-	case BTC, ETH, XBT:
-		return true
-	}
-
-	return false
-}
-
+// Stringifies currency with symbol
 func (t Type) ToString(c Cents) string {
-	if t.IsZeroDecimal() {
-		return t.Symbol() + strconv.Itoa(int(c))
+	// Handle positives
+	if c >= 0 {
+		return t.Symbol() + t.ToStringNoSymbol(c)
 	}
-	cents := strconv.Itoa(int(c) % 100)
-	if len(cents) < 2 {
-		cents += "0"
-	}
-	return t.Symbol() + strconv.Itoa(int(c)/100) + "." + cents
+
+	// Handle Negatives
+	seg1 := t.Symbol()
+	seg2 := t.ToStringNoSymbol(c)
+
+	return string(seg2[0]) + seg1 + seg2[1:]
 }
 
+// Stringifies currency with no
 func (t Type) ToStringNoSymbol(c Cents) string {
 	if t.IsZeroDecimal() {
 		return strconv.Itoa(int(c))
@@ -47,6 +45,7 @@ func (t Type) ToStringNoSymbol(c Cents) string {
 	return strconv.Itoa(int(c)/100) + "." + cents
 }
 
+// Convert to float representation based on decimal convention
 func (t Type) ToFloat(c Cents) float64 {
 	if t.IsZeroDecimal() {
 		return float64(c)
@@ -54,10 +53,51 @@ func (t Type) ToFloat(c Cents) float64 {
 	return float64(c) / 100.0
 }
 
+// Give the currency's Symbol + Code string
 func (t Type) Label() string {
 	return t.Symbol() + " " + t.Code()
 }
 
+// Give the currency's Code
 func (t Type) Code() string {
 	return strings.ToUpper(string(t))
+}
+
+// ------ More or Less Crypto Specific ------
+
+// Is this a supported cryptocurrency
+func (t Type) IsCrypto() bool {
+	switch t {
+	case BTC, ETH, XBT:
+		return true
+	}
+
+	return false
+}
+
+// Since pricing things in a crypto minimal denomination exceed int64 and the
+// minimal domination is worth so little, we generally use a larger
+// denomination of the currency by convention that can capture the minimal
+// relatable values.
+//
+// This returns the ratio of convention denomination to minimal denomination
+func (t Type) MinimalUnitFactor() *big.Int {
+	switch t {
+	case ETH:
+		//ETH is priced in Gwei or 1e-9 ETH or 0.000000001 ETH
+		//Gwei or 1e9 ETH or 1,000,000,000 Wei so convert to wei
+		return big.NewInt(1e9)
+	}
+
+	return big.NewInt(1)
+}
+
+func (t Type) ToMinimalUnits(c Cents) *big.Int {
+	b := big.NewInt(int64(c))
+	return b.Mul(b, t.MinimalUnitFactor())
+}
+
+func (t Type) FromMinimalUnits(b *big.Int) Cents {
+	c := b.Div(b, t.MinimalUnitFactor())
+	return Cents(c.Int64())
 }
