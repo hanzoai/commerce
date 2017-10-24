dc6e127048d753c271d6a62ea28f74121dc55492
David Tai <david@verus.io>
2017-10-24 17:52:39 -0500

    fee transfer added for ethereum so we can get paid

--- thirdparty/ethereum/tasks/pay.go
+++ thirdparty/ethereum/tasks/pay.go
@@ -23,6 +23,12 @@ var SimpleTransactionGasUsed = big.NewInt(21000)
 var UnsupportedChainType = errors.New("Chain type is unsupported.")
 var PlatformWalletNotFound = errors.New("Platform Wallet Not Found.")
 var PlatformAccountNotFound = errors.New("Platform Account Not Found.")
+var PlatformAccountDecryptionFailed = errors.New("Platform Account Decryption Failed.")
+var OrderWalletNotFound = errors.New("Order Wallet Not Found.")
+var OrderAccountNotFound = errors.New("Order Account Not Found.")
+var OrderAccountDecryptionFailed = errors.New("Order Account Decryption Failed.")
+var InsufficientFee = errors.New("Not Enough Fee Balance To Cover Transaction Fee")
+var InsufficientTransfer = errors.New("Not Enough Transfer Balance To Cover Transaction Fee")
 
 var EthereumProcessPayment = delay.Func(
 	"ethereum-process-payment",
@@ -104,7 +110,7 @@ func EthereumProcessPaymentImpl(
 
 		pay.Account.EthereumTransactionHash = txHash
 		pay.Account.EthereumChainType = chainType
-		pay.Account.WeiAmount = blockchains.BigNumber(amount.String())
+		pay.Account.EthereumAmount = blockchains.BigNumber(amount.String())
 
 		log.Warn(ord.Currency)
 
@@ -124,7 +130,7 @@ func EthereumProcessPaymentImpl(
 		}
 
 		// Update order status
-		if pay.Amount >= ord.Total && ord.PaymentStatus != ord.PaymentStatus {
+		if pay.Amount >= ord.Total && ord.PaymentStatus != payment.Paid {
 			ord.PaymentStatus = payment.Paid
 
 			// Fees
@@ -140,6 +146,7 @@ func EthereumProcessPaymentImpl(
 			// Create client for transfering fees
 			address := ""
 			password := ""
+			var chainId ethereum.ChainId
 			var account *wallet.Account
 
 			pw := wallet.New(org.Db)
@@ -150,10 +157,12 @@ func EthereumProcessPaymentImpl(
 				return PlatformWalletNotFound
 			}
 
+			// Get the right account and credentials
 			switch chainType {
 			case blockchains.EthereumType:
 				address = config.Ethereum.MainNetNodes[0]
 				password = config.Ethereum.DepositPassword
+				chainId = ethereum.MainNet
 				if a, ok := pw.GetAccountByName("Ethereum Deposit Account"); ok {
 					account = a
 				} else {
@@ -162,6 +171,7 @@ func EthereumProcessPaymentImpl(
 			case blockchains.EthereumRopstenType:
 				address = config.Ethereum.TestNetNodes[0]
 				password = config.Ethereum.TestPassword
+				chainId = ethereum.Ropsten
 				if a, ok := pw.GetAccountByName("Ethereum Ropsten Test Account"); ok {
 					account = a
 				} else {
@@ -171,7 +181,29 @@ func EthereumProcessPaymentImpl(
 				return UnsupportedChainType
 			}
 
+			// Retrieve account information
+			if err := account.Decrypt([]byte(password)); err != nil {
+				return err
+			}
+
+			if account.PrivateKey == "" {
+				return PlatformAccountDecryptionFailed
+			}
+
+			// Initialize client
 			client := ethereum.New(ctx, address)
+
+			// Get From Address
+			ow, err := ord.GetOrCreateWallet(ord.Db)
+			if err != nil {
+				log.Error("Order Wallet Not Found: %v", err, ctx)
+				return OrderWalletNotFound
+			}
+
+			fromAccount, ok := ow.GetAccountByName("Receiver Account")
+			if !ok {
+				return OrderAccountNotFound
+			}
 
 			// Get current gas price so we can calculate transfer costs
 			gasPrice, err := client.GasPrice()
@@ -182,11 +214,83 @@ func EthereumProcessPaymentImpl(
 
 			log.Warn(password, gasPrice, account)
 
+			transferAmount := ord.Currency.ToMinimalUnits(ord.Total)
+
 			// Link payments/fees
 			for _, fe := range fees {
 				fe.PaymentId = pay.Id()
 				pay.FeeIds = append(pay.FeeIds, fe.Id())
+
+				// Subtract fee value from the total transfer value
+				feeValue := fe.Currency.ToMinimalUnits(fe.Amount)
+				transferAmount = transferAmount.Sub(transferAmount, feeValue)
+
+				// Only supported fee at hte moment is the platform one
+				if fe.Name == "Platform fee" {
+					gasUsed := big.NewInt(0).Set(SimpleTransactionGasUsed)
+					gasUsed = gasUsed.Add(gasUsed, gasPrice)
+
+					platformFee := fe
+					platformAmount := platformFee.Currency.ToMinimalUnits(platformFee.Amount)
+					platformAmount = platformAmount.Sub(platformAmount, gasUsed)
+
+					if platformAmount.Cmp(big.NewInt(0)) > 0 {
+						// Only deal with sending the platform fee for now
+						log.Info("Transfering Platform Fee '%s' to '%s'", platformAmount.String(), address, ctx)
+						if txHash, err := client.SendTransaction(
+							chainId,
+							account.PrivateKey,
+							fromAccount.Address,
+							account.Address,
+							platformAmount,
+							big.NewInt(0),
+							gasPrice,
+							[]byte{},
+						); err != nil {
+							return err
+						} else {
+							fe.Ethereum.FinalTransactionHash = txHash
+						}
+					} else {
+						log.Warn("Insufficient Fee To Cover Platform Fee Transaction, After Transaction Value is '%s'", platformAmount.String(), ctx)
+					}
+					fe.Ethereum.FinalAddress = account.Address
+					fe.Ethereum.FinalGasUsed = blockchains.BigNumber(gasUsed.String())
+					fe.Ethereum.FinalAmount = blockchains.BigNumber(platformAmount.String())
+				}
+
+				fe.Create()
 			}
+
+			finalGasUsed := big.NewInt(0).Set(SimpleTransactionGasUsed)
+			finalGasUsed = finalGasUsed.Add(finalGasUsed, gasPrice)
+
+			transferAmount = transferAmount.Sub(transferAmount, finalGasUsed)
+
+			if transferAmount.Cmp(big.NewInt(0)) > 0 {
+				// Transfer rest of the ethereum
+				log.Info("Transfering '%s' to '%s'", transferAmount.String(), address, ctx)
+				if txHash, err := client.SendTransaction(
+					chainId,
+					account.PrivateKey,
+					fromAccount.Address,
+					org.Ethereum.Address,
+					transferAmount,
+					big.NewInt(0),
+					gasPrice,
+					[]byte{},
+				); err != nil {
+					return err
+				} else {
+					pay.Account.EthereumFinalTransactionHash = txHash
+				}
+			} else {
+				log.Error("Insufficient Transfer To Cover Transaction, After Transaction Value is '%s'", transferAmount.String(), ctx)
+				return InsufficientTransfer
+			}
+			pay.Account.EthereumFinalAddress = org.Ethereum.Address
+			pay.Account.EthereumFinalGasUsed = blockchains.BigNumber(finalGasUsed.String())
+			pay.Account.EthereumFinalAmount = blockchains.BigNumber(transferAmount.String())
 		}
 
 		ord.Paid += pay.Amount
