27df8c5ac74aa279d5ada5ddeea35375308fcc8e
David Tai <antisocialmunky@gmail.com>
2017-10-24 13:13:54 -0500

    wallet getAccountByName tests

--- thirdparty/ethereum/tasks/pay.go
+++ thirdparty/ethereum/tasks/pay.go
@@ -1,60 +1,77 @@
 package tasks
 
 import (
+	"errors"
 	"math/big"
 
 	"appengine"
 
+	"hanzo.io/config"
 	"hanzo.io/datastore"
 	"hanzo.io/models/blockchains"
 	"hanzo.io/models/order"
+	"hanzo.io/models/organization"
 	"hanzo.io/models/payment"
 	"hanzo.io/models/user"
 	"hanzo.io/models/wallet"
+	"hanzo.io/thirdparty/ethereum"
 	"hanzo.io/util/delay"
 	"hanzo.io/util/log"
 )
+
+var SimpleTransactionGasUsed = big.NewInt(21000)
+var UnsupportedChainType = errors.New("Chain type is unsupported.")
+var PlatformWalletNotFound = errors.New("Platform Wallet Not Found.")
+var PlatformAccountNotFound = errors.New("Platform Account Not Found.")
 
 var EthereumProcessPayment = delay.Func(
 	"ethereum-process-payment",
 	func(
 		ctx appengine.Context,
-		walletNs,
+		orgName,
 		walletId,
 		txHash,
 		chainType string,
 		amount *big.Int,
 	) {
-		if err := EthereumProcessPaymentImpl(ctx, walletNs, walletId, txHash, chainType, amount); err != nil {
+		if err := EthereumProcessPaymentImpl(ctx, orgName, walletId, txHash, chainType, amount); err != nil {
 			panic(err)
 		}
 	})
 
 func EthereumProcessPaymentImpl(
 	ctx appengine.Context,
-	walletNs,
+	orgName,
 	walletId,
 	txHash,
-	chainType string,
+	ct string,
 	amount *big.Int,
 ) error {
-	// Namespace the context
-	nsCtx, err := appengine.Namespace(ctx, walletNs)
-	if err != nil {
-		log.Warn("Could not change to Namespace '%s': %v", walletNs, err, ctx)
+	// convert chaintype
+	chainType := blockchains.Type(ct)
+
+	// Get Org
+	db := datastore.New(ctx)
+	org := organization.New(db)
+	if err := org.GetById(orgName); err != nil {
+		log.Warn("Could not find Org '%s': %v", orgName, err, ctx)
 		return err
 	}
 
+	// Namespace the context
+	nsCtx := org.Namespaced(ctx)
+
 	// Namespace the datastore
-	db := datastore.New(nsCtx)
-	w := wallet.New(db)
+	nsDb := datastore.New(nsCtx)
+
+	w := wallet.New(nsDb)
 	if err := w.GetById(walletId); err != nil {
 		log.Warn("Could not find Wallet '%s': %v", walletId, err, ctx)
 		return err
 	}
 
 	// Check if there's an order with this wallet
-	ord := order.New(db)
+	ord := order.New(nsDb)
 	if ok, err := ord.Query().Filter("WalletId=", w.Id()).Get(); !ok {
 		if err != nil {
 			log.Warn("No order found for Wallet '%s': %v", w.Id(), err, ctx)
@@ -66,14 +83,14 @@ func EthereumProcessPaymentImpl(
 	}
 
 	// Get user so we can get a buyer
-	usr := user.New(db)
+	usr := user.New(nsDb)
 	if err := usr.GetById(ord.UserId); err != nil {
 		log.Warn("User not found for Order '%s', Wallet '%s'", ord.Id(), w.Id(), ctx)
 		return err
 	}
 
 	// Create payment, update order
-	pay := payment.New(db)
+	pay := payment.New(nsDb)
 	if err := pay.RunInTransaction(func() error {
 		// Make sure payment with TransactionHash does not exist (transaction
 		// already processed)
@@ -86,7 +103,7 @@ func EthereumProcessPaymentImpl(
 		}
 
 		pay.Account.EthereumTransactionHash = txHash
-		pay.Account.EthereumChainType = blockchains.Type(chainType)
+		pay.Account.EthereumChainType = chainType
 		pay.Account.WeiAmount = blockchains.BigNumber(amount.String())
 
 		log.Warn(ord.Currency)
@@ -107,8 +124,69 @@ func EthereumProcessPaymentImpl(
 		}
 
 		// Update order status
-		if pay.Amount >= ord.Total {
+		if pay.Amount >= ord.Total && ord.PaymentStatus != ord.PaymentStatus {
 			ord.PaymentStatus = payment.Paid
+
+			// Fees
+			platformFees, partnerFees := org.Pricing()
+			fee, fes, err := ord.CalculateFees(platformFees, partnerFees)
+			if err != nil {
+				log.Error("Fee calculation error: %v", err, ctx)
+				return err
+			}
+			fees := fes
+			pay.Fee = fee
+
+			// Create client for transfering fees
+			address := ""
+			password := ""
+			var account *wallet.Account
+
+			pw := wallet.New(org.Db)
+			if ok, err := pw.Query().Filter("Id_=", "platform-wallet").Get(); !ok {
+				if err != nil {
+					return err
+				}
+				return PlatformWalletNotFound
+			}
+
+			switch chainType {
+			case blockchains.EthereumType:
+				address = config.Ethereum.MainNetNodes[0]
+				password = config.Ethereum.DepositPassword
+				if a, ok := pw.GetAccountByName("Ethereum Deposit Account"); ok {
+					account = a
+				} else {
+					return PlatformAccountNotFound
+				}
+			case blockchains.EthereumRopstenType:
+				address = config.Ethereum.TestNetNodes[0]
+				password = config.Ethereum.TestPassword
+				if a, ok := pw.GetAccountByName("Ethereum Ropsten Test Account"); ok {
+					account = a
+				} else {
+					return PlatformAccountNotFound
+				}
+			default:
+				return UnsupportedChainType
+			}
+
+			client := ethereum.New(ctx, address)
+
+			// Get current gas price so we can calculate transfer costs
+			gasPrice, err := client.GasPrice()
+			if err != nil {
+				log.Error("Could not get gas price: %v", err, ctx)
+				return err
+			}
+
+			log.Warn(password, gasPrice, account)
+
+			// Link payments/fees
+			for _, fe := range fees {
+				fe.PaymentId = pay.Id()
+				pay.FeeIds = append(pay.FeeIds, fe.Id())
+			}
 		}
 
 		ord.Paid += pay.Amount
