f1c969a4e709a4e050597cc23321355aebd6a804
Timothy Messer <tmesser@verus.io>
2017-11-14 13:49:39 -0600

    Modifications to client to make it conform to Hanzo styles and the skeleton of the test fixture I need.

--- thirdparty/bitcoin/client.go
+++ thirdparty/bitcoin/client.go
@@ -1,18 +1,30 @@
 package bitcoin
 
 import (
+	"appengine"
+	"appengine/urlfetch"
 	"github.com/btcsuite/btcd/rpcclient"
+	"net/http"
+	"time"
 )
 
 type BitcoinClient struct {
-	client *rpcclient.Client
+	ctx        appengine.Context
+	httpClient *http.Client
+	client     *rpcclient.Client
 }
 
 // New creates a new RPC client based on the provided connection configuration
 // details.  The notification handlers parameter may be nil if you are not
 // interested in receiving notifications and will be ignored if the
 // configuration is set to run in HTTP POST mode.
-func New(host, endpoint, username, password string, certs []byte) (*BitcoinClient, error) {
+func New(ctx appengine.Context, host, endpoint, username, password string, certs []byte) (*BitcoinClient, error) {
+	httpClient := urlfetch.Client(ctx)
+	httpClient.Transport = &urlfetch.Transport{
+		Context:                       ctx,
+		Deadline:                      time.Duration(55) * time.Second,
+		AllowInvalidServerCertificate: appengine.IsDevAppServer(),
+	}
 	connCfg := &rpcclient.ConnConfig{
 		Host:         host,
 		Endpoint:     endpoint,
@@ -25,5 +37,5 @@ func New(host, endpoint, username, password string, certs []byte) (*BitcoinClien
 	if err != nil {
 		return nil, err
 	}
-	return &BitcoinClient{client: c}, nil
+	return &BitcoinClient{ctx: ctx, client: c}, nil
 }
