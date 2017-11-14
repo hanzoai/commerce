334a6d45bdfdf5ac1407cabab3d91e7c94be8d71
Timothy Messer <tmesser@verus.io>
2017-11-14 13:19:27 -0600

    Updating wallet.go to support bitcoin wallets (at least testnet)

--- models/wallet/wallet.go
+++ models/wallet/wallet.go
@@ -7,6 +7,7 @@ import (
 	"hanzo.io/models/blockchains"
 	"hanzo.io/models/blockchains/blockaddress"
 	"hanzo.io/models/mixin"
+	"hanzo.io/thirdparty/bitcoin"
 	"hanzo.io/thirdparty/ethereum"
 	"hanzo.io/util/hashid"
 	"hanzo.io/util/log"
@@ -29,6 +30,58 @@ func (w *Wallet) CreateAccount(name string, typ blockchains.Type, withPassword [
 		}
 
 		add = strings.ToLower(add)
+
+		a := Account{
+			Name:       name,
+			PrivateKey: priv,
+			PublicKey:  pub,
+			Address:    add,
+			Type:       typ,
+			CreatedAt:  time.Now(),
+		}
+
+		if err := a.Encrypt(withPassword); err != nil {
+			return Account{}, err
+		}
+
+		w.Accounts = append(w.Accounts, a)
+
+		// Create a blockaddress so we track this in the ethereum reader
+		ba := blockaddress.New(w.Db)
+		ba.Address = add
+		ba.Type = typ
+		ba.WalletId = w.Id()
+
+		ns, err := hashid.GetNamespace(w.Db.Context, w.Id())
+		if err != nil {
+			log.Warn("Could not determine namespace, probably '': %v", err, w.Context())
+		}
+
+		ba.WalletNamespace = ns
+		err = ba.Create()
+		if err != nil {
+			log.Error("Could not create BlockAddress: %v", err, w.Context())
+		}
+
+		// Only save if the wallet is created
+		// Otherwise let the user manage that
+		if w.Created() {
+			if err := w.Update(); err != nil {
+				return Account{}, err
+			}
+		}
+
+		return a, err
+	case blockchains.BitcoinTestnetType:
+		priv, pub, err := bitcoin.GenerateKeyPair()
+		if err != nil {
+			return Account{}, err
+		}
+
+		add, _, err := bitcoin.PubKeyToAddress(pub)
+		if err != nil {
+			return Account{}, err
+		}
 
 		a := Account{
 			Name:       name,
