e53cd22aceb23fbb09db2e58cbc4a6c5007be8d2
Timothy Messer <tmesser@verus.io>
2017-11-20 16:04:21 -0600

    Changes to make the bitcoin library more user friendly.

--- thirdparty/bitcoin/types.go
+++ thirdparty/bitcoin/types.go
@@ -1,12 +1,18 @@
 package bitcoin
 
-type TXOutput struct {
-	Value        int
-	ScriptPubKey string
+type Destination struct {
+	Value   int64
+	Address string
 }
 
-type TXInput struct {
-	Txid      []byte
-	Vout      int
-	ScriptSig string
+type Input struct {
+	TxId        string
+	OutputIndex int
+}
+
+type Sender struct {
+	PrivateKey     string
+	PublicKey      string
+	Address        string
+	TestNetAddress string
 }
