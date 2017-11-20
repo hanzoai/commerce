bc7f16d65230af9f23e99b890ffaaaaac23a4c73
Timothy Messer <tmesser@verus.io>
2017-11-20 16:43:14 -0600

    Adding a get raw transaction call to the client (plus a test fixture)

---
+++ models/fixtures/test-bitcoin-gettransaction-rpc.go
@@ -0,0 +1,29 @@
+package fixtures
+
+import (
+	//"math/big"
+
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/config"
+	"hanzo.io/datastore"
+	"hanzo.io/thirdparty/bitcoin"
+	"hanzo.io/util/log"
+)
+
+var GetTestBitcoinTransaction = New("test-bitcoin-gettransaction", func(c *gin.Context) {
+	db := datastore.New(c)
+
+	client, err := bitcoin.New(db.Context, config.Bitcoin.TestNetNodes[0], config.Bitcoin.TestNetUsernames[0], config.Bitcoin.TestNetPasswords[0])
+	if err != nil {
+		panic(err)
+	}
+	log.Info("Created Bitcoin client.")
+
+	res, err := client.GetRawTransaction("5b60d0684a8201ddac20f713782a1f03682b508e90d99d0887b4114ad4ccfd2c")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info("Btcd Node Response: %v", "", res)
+})
