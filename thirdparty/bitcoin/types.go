adafc2fe7f84b7aa77345878567501fa6e5cc8f3
Timothy Messer <tmesser@verus.io>
2017-11-20 21:23:12 -0600

    Fee calculation now works. So does automatically adding the sender onto the list for change. Disabled the test for transaction generation because it requires a     node to inquire about values now. Moved that functionality to a fixture.

--- thirdparty/bitcoin/types.go
+++ thirdparty/bitcoin/types.go
@@ -16,3 +16,50 @@ type Sender struct {
 	Address        string
 	TestNetAddress string
 }
+
+type GetRawTransactionResponseResult struct {
+	Txid      string
+	Hash      string
+	Version   int
+	Size      int
+	Vsize     int
+	Locktime  int
+	Vin       []GetRawTransactionResponseResultInput
+	Vout      []GetRawTransactionResponseResultOutput
+	Hex       string
+	Blockhash string
+	Time      int64
+	Blocktime int64
+}
+
+type GetRawTransactionResponseResultInput struct {
+	Txid        string
+	Vout        int
+	Scriptsig   GetRawTransactionResponseResultInputSig
+	Txinwitness []string
+	Sequence    int64
+}
+
+type GetRawTransactionResponseResultInputSig struct {
+	Asm string
+	Hex string
+}
+
+type GetRawTransactionResponseResultOutput struct {
+	N            int
+	Value        float64
+	Scriptpubkey GetRawTransactionResponseResultOutputScriptPubKey
+}
+
+type GetRawTransactionResponseResultOutputScriptPubKey struct {
+	Asm        string
+	Hex        string
+	ReqSigs    int
+	ScriptType string `json:"type"`
+	Addresses  []string
+}
+
+type GetRawTransactionResponseError struct {
+	Code    int
+	Message string
+}
