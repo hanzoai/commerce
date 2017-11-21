4c73440cbdccc30f2f61b6e4964d56b183667b89
David Tai <antisocialmunky@gmail.com>
2017-11-20 21:21:05 -0600

    add support for ethgasstation

--- test/thirdparty/ethereum/client_test.go
+++ test/thirdparty/ethereum/client_test.go
@@ -2,12 +2,14 @@ package test
 
 import (
 	. "hanzo.io/util/test/ginkgo"
+	"math/big"
 )
 
 var _ = Describe("client.GasPrice2", func() {
 	It("should work", func() {
 		gasPrice, res, err := client.GasPrice2()
 		Expect(gasPrice).ToNot(Equal(0))
+		Expect(gasPrice.Cmp(big.NewInt(int64(res.SafeLow + 1)))).To(Equal(0))
 		Expect(res.FastestWait <= res.FastWait).To(BeTrue())
 		Expect(res.FastWait <= res.AverageWait).To(BeTrue())
 		Expect(res.AverageWait <= res.SafeLowWait).To(BeTrue())
