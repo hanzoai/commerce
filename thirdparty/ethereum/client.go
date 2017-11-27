dac7556d05f6cde2600930cad221129d53be27a8
Timothy Messer <tmesser@verus.io>
2017-11-27 16:11:18 -0600

    Adding in get balance command to ethereum RPC client

--- thirdparty/ethereum/client.go
+++ thirdparty/ethereum/client.go
@@ -311,3 +311,30 @@ func (c Client) GasPrice2() (*big.Int, *EthGasStationResponse, error) {
 
 	return big.NewInt(int64(egr.SafeLow + 1)), egr, nil
 }
+
+func (c Client) GetBalance(address string) (balanceInGWei *big.Int, err error) {
+	log.Info("Getting ethereum balance for address: %v", address)
+	id := rand.Int64()
+	jsonRpcCommand := fmt.Sprintf(JsonRpcMessage, JsonRpcVersion, "eth_getBalance", paramsToString(address), id)
+	jrr, err := c.Post(jsonRpcCommand, id)
+	if err != nil {
+		return big.NewInt(DefaultGasPrice), err
+	}
+
+	priceHex := string(jrr.Result)
+	priceHex = priceHex[1 : len(priceHex)-1]
+
+	log.Info("Address balance is %v", priceHex)
+
+	a, err := hexutil.DecodeBig(priceHex)
+	if err != nil {
+		return big.NewInt(DefaultGasPrice), err
+	}
+
+	// 1 is the min price
+	if a.Cmp(big.NewInt(0)) == 0 {
+		return big.NewInt(1), nil
+	}
+
+	return a, nil
+}
