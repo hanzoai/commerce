3660db287613837bedaa444053e859751495fb32
Timothy Messer <tmesser@verus.io>
2017-11-28 13:08:57 -0600

    Fixing some silly errors

--- util/blockchain/ethereum.go
+++ util/blockchain/ethereum.go
@@ -1,9 +1,7 @@
 package ethereum
 
 import (
-	"crypto/ecdsa"
-	"crypto/rand"
-	"encoding/hex"
+	"appengine"
 	"errors"
 	"fmt"
 	"math/big"
@@ -12,23 +10,21 @@ import (
 	"hanzo.io/models/blockchains"
 	"hanzo.io/models/wallet"
 	"hanzo.io/thirdparty/ethereum"
-	"hanzo.io/thirdparty/ethereum/go-ethereum/crypto"
-	"hanzo.io/util/log"
 )
 
-func MakePayment(ctx appengine.Context, from wallet.Account, to wallet.Account, amount big.Int, typ blockchains.Type) error {
+func MakePayment(ctx appengine.Context, from wallet.Account, to wallet.Account, amount *big.Int, typ blockchains.Type) error {
 	// Create needed client.
 
 	client := ethereum.Client{}
 	switch typ {
 	case blockchains.EthereumType:
 		client = ethereum.New(ctx, config.Ethereum.MainNetNodes[0])
-		client.Chain = ethereum.MainNet.ChainId
+		client.Chain = ethereum.MainNet
 	case blockchains.EthereumRopstenType:
 		client = ethereum.New(ctx, config.Ethereum.TestNetNodes[0])
-		client.Chain = ethereum.Ropsten.ChainId
+		client.Chain = ethereum.Ropsten
 	default:
 		return errors.New(fmt.Sprintf("Unsupported blockchain type: %v", typ))
 	}
-	return ethereum.MakePayment(client, from.PrivateKey, from.Address, to.Address, client.Chain)
+	return ethereum.MakePayment(client, from.PrivateKey, from.Address, to.Address, amount, client.Chain)
 }
