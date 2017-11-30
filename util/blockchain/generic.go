8e2e7baebcfcad1ecd6edd540a765e4635f686ed
Timothy Messer <tmesser@verus.io>
2017-11-30 16:49:30 -0600

    Fixes per david's comments, all good stuff

--- util/blockchain/generic.go
+++ util/blockchain/generic.go
@@ -10,11 +10,11 @@ import (
 	"hanzo.io/models/wallet"
 )
 
-func MakePayment(ctx appengine.Context, from wallet.Account, to string, amount *big.Int, password []byte) error {
+func MakePayment(ctx appengine.Context, from wallet.Account, to string, amount *big.Int, password []byte) (string, error) {
 	switch from.Type {
 	case blockchains.EthereumType, blockchains.EthereumRopstenType:
 		return MakeEthereumPayment(ctx, from, to, amount, password)
 	default:
-		return errors.New(fmt.Sprintf("Unsupported blockchain type: %v", from.Type))
+		return "", errors.New(fmt.Sprintf("Unsupported blockchain type: %v", from.Type))
 	}
 }
