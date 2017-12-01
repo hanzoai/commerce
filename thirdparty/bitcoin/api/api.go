d5c026f8b068b65d7c219fbcdb0c08e6aa4a948f
David Tai <antisocialmunky@gmail.com>
2017-12-01 13:13:31 -0600

    bitcoin stubs

---
+++ thirdparty/bitcoin/api/api.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"hanzo.io/util/router"
+)
+
+// Wire up stripe endpoint
+func Route(router router.Router, args ...gin.HandlerFunc) {
+	api := router.Group("bitcoin")
+	api.POST("/webhook", Webhook)
+}
