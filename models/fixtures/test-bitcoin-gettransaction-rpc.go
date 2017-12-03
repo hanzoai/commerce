4bd91efd0ea98e3b5d6a78bd0c2884c65f0cf188
David Tai <antisocialmunky@gmail.com>
2017-12-03 17:24:27 -0600

    send test bitcoin fixture

--- models/fixtures/test-bitcoin-gettransaction-rpc.go
+++ models/fixtures/test-bitcoin-gettransaction-rpc.go
@@ -14,7 +14,7 @@ import (
 var GetTestBitcoinTransaction = New("test-bitcoin-gettransaction", func(c *gin.Context) {
 	db := datastore.New(c)
 
-	client := bitcoin.New(db.Context, config.Bitcoin.TestNetNodes[0], config.Bitcoin.TestNetUsernames[0], config.Bitcoin.TestNetPasswords[0], true)
+	client := bitcoin.New(db.Context, config.Bitcoin.TestNetNodes[0], config.Bitcoin.TestNetUsernames[0], config.Bitcoin.TestNetPasswords[0])
 	log.Info("Created Bitcoin client.")
 
 	res, err := client.GetRawTransaction("5b60d0684a8201ddac20f713782a1f03682b508e90d99d0887b4114ad4ccfd2c")
