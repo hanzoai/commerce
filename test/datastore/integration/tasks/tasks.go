ee95c58e3295ef0e181f07ca3ca0d599ee7af95d
Zach Kelling <z@zeekay.io>
2018-02-21 01:51:41 -0600

    Works on my machine

--- test/datastore/integration/tasks/tasks.go
+++ test/datastore/integration/tasks/tasks.go
@@ -3,23 +3,16 @@ package tasks
 import (
 	"hanzo.io/datastore"
 	"hanzo.io/datastore/parallel"
-	"hanzo.io/log"
 )
 
 // Define a new worker with parallel.Task
 var TaskPlus1 = parallel.New("test-worker", func(db *datastore.Datastore, model *Model) {
-	log.Debug("ADSFJKASJDFKASDJFLKASDJFLAKSDJFLASKDJFLSAKDJFALSKDJFLASKDJFLAKSDJFLAKSJDFLASKDJFALSKDFJASLDKJFALSKDFJ")
 	model.Count = model.Count + 1
-	if err := model.Put(); err != nil {
-		panic(err)
-	}
+	model.MustPut()
 })
 
 // Define a new worker with parallel.Task
 var TaskSetVal = parallel.New("test-worker2", func(db *datastore.Datastore, model *Model2, v int) {
-	log.Debug("ADSFJKASJDFKASDJFLKASDJFLAKSDJFLASKDJFLSAKDJFALSKDJFLASKDJFLAKSDJFLAKSJDFLASKDJFALSKDFJASLDKJFALSKDFJ")
 	model.Count = v
-	if err := model.Put(); err != nil {
-		panic(err)
-	}
+	model.MustPut()
 })
