42fddbe7db168596b81303aa38bb7ad114078dfd
Zach Kelling <z@zeekay.io>
2018-02-20 21:58:33 -0600

    A beautiful refactor courtesy of a lovely pair programming session.
    Thanks for all the lovely pears.

--- scripts/dev_appserver.py
+++ scripts/dev_appserver.py
@@ -3,30 +3,47 @@
 import os
 import sys
 
+
 PLATFORM_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 SDK_PATH      = os.path.join(PLATFORM_PATH, '.sdk')
 SERVER_PATH   = os.path.join(SDK_PATH, 'dev_appserver.py')
 
-# Rewrite default test ports to something we can predict
-for i, a in enumerate(sys.argv):
-    if a == '--admin_port=0':
-        sys.argv[i] = '--admin_port=8000'
-    if a == '--api_port=0':
-        sys.argv[i] = '--api_port=8001'
-    if a == '--port=0':
-        sys.argv[i] = '--port=8002'
 
-# Remove generated app.yaml from arg stack
-sys.argv.pop()
+PORTS = {
+    '--admin_port=0': int(os.environ['DEV_APP_SERVER_ADMIN_PORT']),
+    '--api_port=0':   int(os.environ['DEV_APP_SERVER_API_PORT']),
+    '--port=0':       int(os.environ['DEV_APP_SEVER_PORT']),
+}
 
-sys.argv.extend([
-    '--dev_appserver_log_level=info',
-    '--enable_task_running=true',
-    os.path.join(PLATFORM_PATH, 'config/test/app.yaml'),
-    os.path.join(PLATFORM_PATH, 'api/app.dev.yaml'),
-])
 
-argv = 'python2 {} {}'.format(SERVER_PATH, ' '.join(sys.argv[1:]))
-print(argv)
+# dev_appserver will increment port number as services launch from port value.
+# To avoid collisions we make sure it has the highest available port number.
+def reassign_ports():
+    # get highest pair
+    highest = max(PORTS.items(), key=lambda x: x[1])
+    current = PORTS['--port=0']
+    PORTS['--port=0'] = highest[1]
+    PORTS[highest[0]] = current
 
-sys.exit(os.system(argv))
+
+if __name__ == '__main__':
+    reassign_ports()
+
+    # Update dev_appserver ports with randomized port numbers
+    for i, argv in enumerate(sys.argv):
+        sys.argv[i] = PORTS[argv]
+
+    # Remove generated app.yaml from arg stack
+    sys.argv.pop()
+
+    sys.argv.extend([
+        '--dev_appserver_log_level=info',
+        '--enable_task_running=true',
+        os.path.join(PLATFORM_PATH, 'config/test/app.yaml'),
+        os.path.join(PLATFORM_PATH, 'api/app.dev.yaml'),
+    ])
+
+    argv = 'python2 {} {}'.format(SERVER_PATH, ' '.join(sys.argv[1:]))
+    print(argv)
+
+    sys.exit(os.system(argv))
