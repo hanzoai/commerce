4ff1fc1831d9a74fbdd7aecf422335449457b127
Zach Kelling <z@zeekay.io>
2018-02-20 18:54:46 -0600

    Respect ae.Option overrides

--- util/test/ae/options.go
+++ util/test/ae/options.go
@@ -3,21 +3,42 @@ package ae
 // Aliased for simplicity
 type Options struct {
 	AppID                       string
+	Debug                       bool
 	StronglyConsistentDatastore bool
 	Modules                     []string
-	Debug                       bool
 
 	// Deprecated
-	TaskQueues []string
 	Noisy      bool
+	TaskQueues []string
 }
 
 // Generate safe defaults
-func defaults() Options {
-	return Options{
+func defaults(args ...Options) Options {
+	opts := Options{
 		AppID: "None",
+		Debug: false,
 		StronglyConsistentDatastore: true,
 		Modules:                     []string{"default"},
-		Debug:                       false,
 	}
+
+	if len(args) == 1 {
+		o := (Options)(args[0])
+		if o.AppID != "" {
+			opts.AppID = o.AppID
+		}
+
+		if o.Debug != opts.Debug {
+			opts.Debug = o.Debug
+		}
+
+		if o.StronglyConsistentDatastore != opts.StronglyConsistentDatastore {
+			opts.StronglyConsistentDatastore = o.StronglyConsistentDatastore
+		}
+
+		if o.Modules != nil {
+			opts.Modules = o.Modules
+		}
+	}
+
+	return opts
 }
