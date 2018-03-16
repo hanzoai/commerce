223d3ffc1faf69c12f60339aa51cabcc356fc99a
David Tai <antisocialmunky@gmail.com>
2018-03-16 18:46:26 -0500

    fix partial pay bug

--- api/checkout/util/capture.go
+++ api/checkout/util/capture.go
@@ -213,7 +213,7 @@ func UpdateMailchimp(ctx context.Context, org *organization.Organization, ord *o
 
 func HandleDeposit(ord *order.Order) {
 	// Handle Deposit Logic
-	if ord.Mode == order.DepositMode {
+	if ord.Mode == order.DepositMode && ord.Paid >= ord.Total {
 		trans := transaction.New(ord.Db)
 		trans.DestinationId = ord.UserId
 		trans.DestinationKind = "user"
