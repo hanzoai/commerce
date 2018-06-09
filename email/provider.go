20702e8b8aba24bf3e37ff0b5eb47b9992b45ed0
Zach Kelling <z@zeekay.io>
2018-06-08 19:08:54 -0500

    Add new email interfaces

--- email/provider.go
+++ email/provider.go
@@ -5,11 +5,35 @@ import (
 )
 
 type Provider interface {
-	Send(context.Context, Email)
-	SendTemplate(context.Context)
+	Send(c context.Context, e Email)
+	SendCampaign(c context.Context, id string)
+	SendTemplate(d context.Context, id string)
+}
 
-	TemplateGet(context.Context, Email)
+type Campaigns interface {
+	CampaignGet(c context.Context, id string)
+	CampaignCreate(context.Context, Email)
+	CampaignUpdate(context.Context, Email)
+	CampaignDelete(context.Context, Email)
+}
+
+type Subscribers interface {
+	SubscriberGet(c context.Context, id string)
+	SubscriberCreate(context.Context, Email)
+	SubscriberUpdate(context.Context, Email)
+	SubscriberDelete(context.Context, Email)
+}
+
+type Templates interface {
+	TemplateGet(c context.Context, id string)
 	TemplateCreate(context.Context, Email)
 	TemplateUpdate(context.Context, Email)
 	TemplateDelete(context.Context, Email)
+}
+
+type Lists interface {
+	ListGet(c context.Context, id string)
+	ListCreate(context.Context, Email)
+	ListUpdate(context.Context, Email)
+	ListDelete(context.Context, Email)
 }
