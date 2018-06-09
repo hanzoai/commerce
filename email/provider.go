8144ed6ba9ace320090bb5ba3169199747de9d29
Zach Kelling <z@zeekay.io>
2018-06-08 19:53:42 -0500

    Do not pluralize interfaces

--- email/provider.go
+++ email/provider.go
@@ -10,28 +10,28 @@ type Provider interface {
 	SendTemplate(d context.Context, id string)
 }
 
-type Campaigns interface {
+type Campaign interface {
 	CampaignGet(c context.Context, id string)
 	CampaignCreate(context.Context, Email)
 	CampaignUpdate(context.Context, Email)
 	CampaignDelete(context.Context, Email)
 }
 
-type Subscribers interface {
+type Subscriber interface {
 	SubscriberGet(c context.Context, id string)
 	SubscriberCreate(context.Context, Email)
 	SubscriberUpdate(context.Context, Email)
 	SubscriberDelete(context.Context, Email)
 }
 
-type Templates interface {
+type Template interface {
 	TemplateGet(c context.Context, id string)
 	TemplateCreate(context.Context, Email)
 	TemplateUpdate(context.Context, Email)
 	TemplateDelete(context.Context, Email)
 }
 
-type Lists interface {
+type List interface {
 	ListGet(c context.Context, id string)
 	ListCreate(context.Context, Email)
 	ListUpdate(context.Context, Email)
