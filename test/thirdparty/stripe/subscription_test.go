f8fc7c1c6d4c90af5457051982c79fe89fae70e0
David Tai <antisocialmunky@gmail.com>
2018-06-26 14:14:11 -0500

    refactor payment account, make a subscription test

--- test/thirdparty/stripe/subscription_test.go
+++ test/thirdparty/stripe/subscription_test.go
@@ -1,20 +1,37 @@
 package test
 
 import (
+	"hanzo.io/models/plan"
 	"hanzo.io/models/subscription"
 	"hanzo.io/models/user"
 	"hanzo.io/log"
 	. "hanzo.io/util/test/ginkgo"
 )
 
-var _ = FDescribe("thirdparty.stripe.SubscriptionApiTest", func() {
+var _ = Describe("thirdparty.stripe.SubscriptionApiTest", func() {
 	Context("New Subscription", func() {
-		It("should create new subscription", func() {
+		It("should create new plan and subscription", func() {
+			pln := plan.Fake(db)
 			sub := subscription.Fake(db)
 			usr := user.Fake(db)
 
-			log.JSON("USER %v", usr.Id())
-			_, err := client.NewSubscription(token, usr, sub)
+			sub.Plan = *pln
+
+			tok, err := client.AuthorizeSubscription(sub)
+			Expect(err).To(BeNil())
+
+			cust, err := client.NewCustomer(tok.ID, usr)
+			log.JSON(usr)
+			Expect(err).To(BeNil())
+
+			spln, err := client.NewPlan(pln)
+			log.JSON(pln)
+			Expect(err).To(BeNil())
+			Expect(spln.ID).To(Equal(pln.Id()))
+			Expect(spln.ID).To(Equal(pln.Ref.Stripe.Id))
+
+			_, err = client.NewSubscription(cust, sub)
+			log.JSON(sub)
 			Expect(err).To(BeNil())
 		})
 	})
