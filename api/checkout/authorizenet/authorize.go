9ad0becf004308c61774b8ebd5309502de52e837
David Tai <david@verus.io>
2018-07-22 02:08:33 -0500

    make authorize/capture work using urlfetch, we will need to rewrite

--- api/checkout/authorizenet/authorize.go
+++ api/checkout/authorizenet/authorize.go
@@ -15,13 +15,16 @@ func Authorize(org *organization.Organization, ord *order.Order, usr *user.User,
 	// Create stripe client
 	con := org.AuthorizeNetTokens()
 
+	log.Warn("Connection: %v", con, ctx)
+	log.Warn("Test?: %v", !org.Live, ctx)
+
 	loginId := con.LoginId
 	transactionKey := con.TransactionKey
 	key := con.Key
 
 	pay.Amount = ord.Total
 
-	client := authorizenet.New(ctx, loginId, transactionKey, key, false)
+	client := authorizenet.New(ctx, loginId, transactionKey, key, !org.Live)
 
 	// Do authorization
 	_, err := client.Authorize(pay)
