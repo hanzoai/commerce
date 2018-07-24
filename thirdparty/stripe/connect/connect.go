65ba7ab4308e629c4f2a137cec924a881a47e42a
Zach Kelling <z@zeekay.io>
2018-07-24 15:01:55 -0700

    Update default email helpers, template rendering

--- thirdparty/stripe/connect/connect.go
+++ thirdparty/stripe/connect/connect.go
@@ -11,19 +11,11 @@ import (
 
 	"hanzo.io/config"
 	"hanzo.io/util/json"
+
+	"hanzo.io/thirdparty/stripe/connect/types"
 )
 
-type Token struct {
-	AccessToken      string `json:"access_token"`
-	RefreshToken     string `json:"refresh_token"`
-	PublishableKey   string `json:"stripe_publishable_key"`
-	UserId           string `json:"stripe_user_id"`
-	TokenType        string `json:"token_type"`
-	Scope            string `json:"scope"`
-	Livemode         bool   `json:"livemode"`
-	Error            string `json:"error"`
-	ErrorDescription string `json:"error_description"`
-}
+type Token = types.Token
 
 func GetToken(ctx context.Context, code string) (*Token, error) {
 	client := urlfetch.Client(ctx)
