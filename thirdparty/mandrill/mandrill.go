0592e01ffd29659de7b457fe7dfcaffdf171c5d5
Zach Kelling <z@zeekay.io>
2018-07-23 18:36:19 -0700

    Refactor mandrill client to match new email interface

--- thirdparty/mandrill/mandrill.go
+++ thirdparty/mandrill/mandrill.go
@@ -1,251 +1,106 @@
 package mandrill
 
 import (
-	"bytes"
 	"context"
-	"encoding/gob"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
 	"time"
 
+	"github.com/keighl/mandrill"
+	"google.golang.org/appengine"
 	"google.golang.org/appengine/urlfetch"
 
-	"hanzo.io/config"
 	iface "hanzo.io/iface/email"
 	"hanzo.io/log"
 	"hanzo.io/types/email"
 	"hanzo.io/types/integration"
-	"hanzo.io/util/json"
 )
 
-const root = "http://mandrillapp.com/api/1.0"
-
-func init() {
-	gob.Register(Var{})
+type Client struct {
+	client  *mandrill.Client
+	context context.Context
 }
 
-type Var struct {
-	Name    string      `json:"name"`
-	Content interface{} `json:"content"`
-}
+// Convert our message to mandrill message type
+func newMessage(message *email.Message) *mandrill.Message {
+	m := new(mandrill.Message)
 
-type RcptMergeVars struct {
-	Rcpt string `json:"rcpt"`
-	Vars []Var  `json:"vars"`
-}
+	m.FromEmail = message.From.Address
+	m.FromName = message.From.Name
+	m.Subject = message.Subject
 
-type Recipient struct {
-	Email string `json:"email"`
-	Name  string `json:"name"`
-	Type  string `json:"type"`
-}
-
-type SendReq struct {
-	Key     string `json:"key"`
-	Message struct {
-		Html string `json:"html"`
-		// Text      string      `json:"text"`
-		Subject   string      `json:"subject"`
-		FromEmail string      `json:"from_email"`
-		FromName  string      `json:"from_name"`
-		To        []Recipient `json:"to"`
-
-		Headers struct {
-			ReplyTo string `json:"Reply-To"`
-		} `json:"headers"`
-
-		// Important bool `json:"important"`
-		// TrackOpens         interface{} `json:"track_opens"`
-		// TrackClicks        interface{} `json:"track_clicks"`
-		// AutoText           interface{} `json:"auto_text"`
-		AutoHtml interface{} `json:"auto_html"`
-		// InlineCss          interface{} `json:"inline_css"`
-		// UrlStripQs         interface{} `json:"url_strip_qs"`
-		// PreserveRecipients interface{} `json:"preserve_recipients"`
-		// ViewContentLink    interface{} `json:"view_content_link"`
-		// BccAddress string `json:"bcc_address"`
-		// TrackingDomain     interface{} `json:"tracking_domain"`
-		// SigningDomain    interface{} `json:"signing_domain"`
-		// ReturnPathDomain interface{} `json:"return_path_domain"`
-
-		Merge         bool            `json:"merge"`
-		MergeLanguage string          `json:"merge_language,omitempty"`
-		MergeVars     []Var           `json:"global_merge_vars"`
-		RcptMergeVars []RcptMergeVars `json:"merge_vars"`
-
-		// Tags []string `json:"tags"`
-		// Subaccount      string            `json:"subaccount"`
-		// GoogleAnalyticsDomains  []string `json:"google_analytics_domains"`
-		// GoogleAnalyticsCampaign string   `json:"google_analytics_campaign"`
-
-		// Metadata struct {
-		//	Website string `json:"website"`
-		// } `json:"metadata"`
-
-		// RecipientMetadata []struct {
-		// 	Rcpt   string `json:"rcpt"`
-		// 	Values struct {
-		// 		UserId int `json:"user_id"`
-		// 	} `json:"values"`
-		// } `json:"recipient_metadata"`
-
-		// Attachments []struct {
-		// 	Type    string `json:"type"`
-		// 	Name    string `json:"name"`
-		// 	Content string `json:"content"`
-		// } `json:"attachments"`
-
-		// Images []struct {
-		// 	Type    string `json:"type"`
-		// 	Name    string `json:"name"`
-		// 	Content string `json:"content"`
-		// } `json:"images"`
-	} `json:"message"`
-	Async  bool   `json:"async"`
-	IpPool string `json:"ip_pool"`
-	// SendAt string `json:"send_at"`
-}
-
-func (r *SendReq) AddRecipient(email, name string) {
-	rcpt := Recipient{
-		Email: email,
-		Name:  name,
-		Type:  "to",
+	// Add recipients
+	for _, to := range message.To {
+		m.AddRecipient(to.Address, to.Name, "to")
+	}
+	for _, cc := range message.CC {
+		m.AddRecipient(cc.Address, cc.Name, "cc")
+	}
+	for _, bcc := range message.BCC {
+		m.AddRecipient(bcc.Address, bcc.Name, "bcc")
 	}
 
-	r.Message.To = append(r.Message.To, rcpt)
-}
+	// Add global mail merge variables
+	m.GlobalMergeVars = mandrill.MapToVars(message.Substitutions)
 
-func (r *SendReq) AddMergeVar(v Var) {
-	r.Message.MergeVars = append(r.Message.MergeVars, v)
-}
+	// Add recpient unique merge variables
+	mergeVars := make([]*mandrill.RcptMergeVars, 0)
 
-type SendTemplateReq struct {
-	SendReq
-	TemplateName    string `json:"template_name"`
-	TemplateContent []struct {
-		Name    string `json:"name"`
-		Content string `json:"content"`
-	} `json:"template_content"`
-}
-
-func NewSendReq() (req SendReq) {
-	req.Async = true
-	req.IpPool = "Main Pool"
-	req.Key = config.Mandrill.APIKey
-	req.Message.AutoHtml = true
-	req.Message.Merge = true
-	return req
-}
-
-func NewSendTemplateReq() (req SendTemplateReq) {
-	req.SendReq = NewSendReq()
-	return req
-}
-
-// Gets the content of a template
-func GetTemplate(filename string) string {
-	wd, _ := os.Getwd()
-	log.Info(wd)
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return ""
+	for k, v := range message.Personalizations {
+		vars := mandrill.MapToRecipientVars(k, v.Substitutions)
+		mergeVars = append(mergeVars, vars)
 	}
 
-	return string(bytes)
-}
+	m.MergeVars = mergeVars
 
-// PingMandrill checks if our credentials/url are okay
-// Returns true if Mandrill replies with  a 200 OK
-func Ping(ctx context.Context) bool {
-	url := root + "/users/ping.json"
-
-	str := fmt.Sprintf(`{"key": "%s"}`, config.Mandrill.APIKey)
-	body := []byte(str)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-	if err != nil {
-		return false
+	// Add content
+	if message.HTML != "" {
+		m.HTML = message.HTML
 	}
 
-	client := urlfetch.Client(ctx)
-	res, err := client.Do(req)
-	if err != nil {
-		return false
+	if message.Text != "" {
+		m.Text = message.Text
 	}
-	defer res.Body.Close()
-
-	return res.StatusCode == 200
+	return m
 }
 
-func SendTemplate(ctx context.Context, req *SendTemplateReq) error {
-	// Convert the map of vars to a byte buffer of a json string
-	url := root + "/messages/send-template.json"
+// Send email
+func (c *Client) Send(message *email.Message) error {
+	var (
+		res []*mandrill.Response
+		err error
+	)
 
-	j := json.Encode(req)
+	if message.TemplateID != "" {
+		res, err = c.client.MessagesSend(newMessage(message))
+	} else {
+		res, err = c.client.MessagesSend(newMessage(message))
+	}
 
-	hreq, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(j)))
 	if err != nil {
+		log.Error(err)
 		return err
 	}
 
-	// Set timeout
-	ctx, _ = context.WithTimeout(ctx, time.Second*55)
+	log.Info("%v", res)
 
-	client := urlfetch.Client(ctx)
-	client.Transport = &urlfetch.Transport{Context: ctx}
-
-	res, err := client.Do(hreq)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode == 200 {
-		return nil
-	}
-
-	// Failed to send
-	b, _ := ioutil.ReadAll(res.Body)
-	return errors.New(fmt.Sprintf("Invalid response from Mandrill: %s", b))
-}
-
-func Send(ctx context.Context, req *SendReq) error {
-	// Convert the map of vars to a byte buffer of a json string
-	url := root + "/messages/send.json"
-
-	j := json.Encode(req)
-
-	hreq, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(j)))
-	if err != nil {
-		return err
-	}
-
-	client := urlfetch.Client(ctx)
-	res, err := client.Do(hreq)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode == 200 {
-		return nil
-	}
-
-	// Failed to send
-	b, _ := ioutil.ReadAll(res.Body)
-	return errors.New(fmt.Sprintf("Invalid response from Mandrill: %s", b))
-}
-
-// TODO: Update Mandrill
-type placeholder struct{}
-
-func (p *placeholder) Send(message *email.Message) error {
-	return errors.New("Send is not implemented")
+	return nil
 }
 
 func New(c context.Context, in integration.Mandrill) iface.Provider {
-	return new(placeholder)
+	// Set deadline
+	c, _ = context.WithTimeout(c, time.Second*55)
+
+	// Set HTTP Client for App engine
+	httpClient := urlfetch.Client(c)
+
+	httpClient.Transport = &urlfetch.Transport{
+		Context: c,
+		AllowInvalidServerCertificate: appengine.IsDevAppServer(),
+	}
+
+	client := mandrill.ClientWithKey(in.APIKey)
+	client.HTTPClient = httpClient
+	return &Client{
+		client:  client,
+		context: c,
+	}
 }
