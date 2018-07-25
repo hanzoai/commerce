c4fa883c9c008f15c4f412c4413cb2da45a9c63b
David Tai <david@verus.io>
2018-07-25 17:38:16 -0500

    rebuild search docs migration

--- models/migrations/add-search-documents.go
+++ models/migrations/add-search-documents.go
@@ -12,7 +12,6 @@ import (
 var _ = New("add-search-documents",
 	func(c *gin.Context) []interface{} {
 		c.Set("namespace", "cryptounderground")
-
 		return NoArgs
 	},
 	func(db *ds.Datastore, u *user.User) {
