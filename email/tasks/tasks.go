32329180206adf9dbc0544f1a6af0c706b2e6c0f
David Tai <antisocialmunky@gmail.com>
2018-07-29 16:27:38 -0500

    plumb logging through all the email layers

--- email/tasks/tasks.go
+++ email/tasks/tasks.go
@@ -13,17 +13,22 @@ import (
 	"hanzo.io/thirdparty/smtprelay"
 	"hanzo.io/types/email"
 	"hanzo.io/types/integration"
+	"hanzo.io/util/json"
 )
 
 func getProvider(c context.Context, in integration.Integration) (iface.Provider, error) {
 	switch in.Type {
 	case integration.MandrillType:
+		log.Info("Using Mandrill: %v", json.Encode(in.Mandrill), c)
 		return mandrill.New(c, in.Mandrill), nil
 	case integration.SendGridType:
+		log.Info("Using SendGrid: %v", json.Encode(in.SendGrid), c)
 		return sendgrid.New(c, in.SendGrid), nil
 	case integration.SMTPRelayType:
+		log.Info("Using SMTPRelay: %v", json.Encode(in.SMTPRelay), c)
 		return smtprelay.New(c, in.SMTPRelay), nil
 	default:
+		log.Error("Invalid Email Provider", c)
 		return nil, errors.New("Invalid Email Provider")
 	}
 }
@@ -33,7 +38,12 @@ var Send = delay.Func("send-email", func(c context.Context, in integration.Integ
 	log.Debug("Sending email to %s, %v", message.To[0], c)
 	provider, err := getProvider(c, in)
 	if err != nil {
-		log.Error("Email provider integration not found")
+		log.Error("Email provider integration not found: %v", err, c)
+		panic(err)
 	}
-	provider.Send(&message)
+	err = provider.Send(&message)
+	if err != nil {
+		log.Error("Email provider error: %v", err, c)
+		panic(err)
+	}
 })
