1aca7c5a809603ba99f24ceaf704a6115abfd0ff
Zach Kelling <z@zeekay.io>
2018-08-14 13:36:45 -0700

    Finish wiring up subscribe task

--- email/subscribe.go
+++ email/subscribe.go
@@ -3,26 +3,31 @@ package email
 import (
 	"context"
 
+	"hanzo.io/email/tasks"
+	"hanzo.io/log"
 	"hanzo.io/models/form"
 	"hanzo.io/models/organization"
 	"hanzo.io/models/subscriber"
 )
 
 func Subscribe(c context.Context, f *form.Form, s *subscriber.Subscriber, org *organization.Organization) (err error) {
-	return nil
-	// // Default to built-in email provider
-	// in := &config.Email.Provider
+	in, err := org.Integrations.EmailMarketingProvider()
+	if err != nil {
+		log.Error("Could not get Email Provider from org %v: %v", org.Name, err, c)
+		return err
+	} else if in == nil {
+		return IntegrationShouldNotBeNilError
+	}
 
-	// // If org is provider use their email provider
-	// if org != nil {
-	// 	if in, err = org.Integrations.EmailMarketingProvider(); err != nil {
-	// 		log.Error("Could not get Email Marketing Provider from org %v: %v", org.Name, err, c)
-	// 		return err
-	// 	} else if in == nil {
-	// 		return IntegrationShouldNotBeNilError
-	// 	}
-	// }
+	list := List{
+		Id: f.EmailList.Id,
+	}
 
-	// // Fire off task to send email
-	// return tasks.Subscribe.Call(c, *in, message)
+	sub := Subscriber{
+		Email: Email{
+			Address: s.Email,
+		},
+	}
+
+	return tasks.Subscribe.Call(c, in, list, sub)
 }
