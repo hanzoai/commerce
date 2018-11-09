5a2d51b96fe1d9d3ae6bcfbe9bc3631531ceb946
Timothy Messer <tmesser@verus.io>
2018-11-09 13:22:17 -0800

    Add Disclosure searchable document and model infrastructure

---
+++ demo/disclosure/document.go
@@ -0,0 +1,51 @@
+package disclosure
+
+import (
+	"google.golang.org/appengine/search"
+
+	"hanzo.io/models/mixin"
+)
+
+type Document struct {
+	mixin.DocumentSaveLoad `datastore:"-" json:"-"`
+
+	// Special Kind Facet
+	Kind search.Atom `search:",facet"`
+
+	Id_ string
+
+	CreatedAt float64
+	UpdatedAt float64
+
+	Timestamp float64
+
+	Publication string
+	Type        string
+	Receiver    string
+}
+
+func (d Document) Id() string {
+	return string(d.Id_)
+}
+
+func (d *Document) Init() {
+	d.SetDocument(d)
+}
+
+func (d Disclosure) Document() mixin.Document {
+
+	doc := &Document{}
+	doc.Init()
+	doc.Kind = search.Atom(kind)
+	doc.Id_ = d.Id()
+
+	doc.CreatedAt = float64(d.CreatedAt.Unix())
+	doc.UpdatedAt = float64(d.UpdatedAt.Unix())
+	doc.Timestamp = float64(d.Timestamp.Unix())
+
+	doc.Publication = d.Publication
+	doc.Type = d.Type
+	doc.Receiver = d.Receiver
+
+	return doc
+}
---
+++ demo/disclosure/model.go
@@ -0,0 +1,29 @@
+package disclosure
+
+import (
+	"hanzo.io/datastore"
+)
+
+var kind = "disclosure"
+
+func (d Disclosure) Kind() string {
+	return kind
+}
+
+func (d *Disclosure) Init(db *datastore.Datastore) {
+	d.Model.Init(db, d)
+}
+
+func (d *Disclosure) Defaults() {
+}
+
+func New(db *datastore.Datastore) *Disclosure {
+	d := new(Disclosure)
+	d.Init(db)
+	d.Defaults()
+	return d
+}
+
+func Query(db *datastore.Datastore) datastore.Query {
+	return db.Query(kind)
+}
