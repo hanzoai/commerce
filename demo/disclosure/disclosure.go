0b9c3172c015a66d3443a828cbe407b3adc68c67
Timothy Messer <tmesser@verus.io>
2018-11-09 13:22:17 -0800

    Add Disclosure searchable document and model infrastructure

--- demo/disclosure/disclosure.go
+++ demo/disclosure/disclosure.go
@@ -6,11 +6,13 @@ import (
 )
 
 // Datastructure for Bitcoin Transaction
-type Transaction struct {
+type Disclosure struct {
 	mixin.Model
 
-	Document  string    `json:"document"`
-	Type      string    `json:"type"`
-	Receiver  string    `json:"receiver"`
-	Timestamp time.Time `json:"timestamp"`
+	// The searchable module we use is called 'document' so this has to be
+	// called something else.
+	Publication string    `json:"publication"`
+	Type        string    `json:"type"`
+	Receiver    string    `json:"receiver"`
+	Timestamp   time.Time `json:"timestamp"`
 }
