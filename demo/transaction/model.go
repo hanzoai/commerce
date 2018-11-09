ac556cce452559f04825a1d338fd0deead818611
Timothy Messer <tmesser@verus.io>
2018-11-09 13:05:08 -0800

    Make transaction searchable, give it model presence

---
+++ demo/transaction/model.go
@@ -0,0 +1,29 @@
+package transaction
+
+import (
+	"hanzo.io/datastore"
+)
+
+var kind = "transaction"
+
+func (t Transaction) Kind() string {
+	return kind
+}
+
+func (t *Transaction) Init(db *datastore.Datastore) {
+	t.Model.Init(db, t)
+}
+
+func (t *Transaction) Defaults() {
+}
+
+func New(db *datastore.Datastore) *Transaction {
+	t := new(Transaction)
+	t.Init(db)
+	t.Defaults()
+	return t
+}
+
+func Query(db *datastore.Datastore) datastore.Query {
+	return db.Query(kind)
+}
