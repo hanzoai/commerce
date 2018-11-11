1e0ad32c596d3fc10bfd6dc0b7272057319bcd8c
Timothy Messer <tmesser@verus.io>
2018-11-11 01:09:29 -0600

    Make token searchable

---
+++ models/token/document.go
@@ -0,0 +1,53 @@
+package token
+
+import (
+	"strings"
+
+	"google.golang.org/appengine/search"
+
+	"hanzo.io/models/mixin"
+	"hanzo.io/util/searchpartial"
+)
+
+type Document struct {
+	mixin.DocumentSaveLoad `datastore:"-" json:"-"`
+
+	// Special Kind Facet
+	Kind search.Atom `search:",facet"`
+
+	Id_           string
+	Email         search.Atom
+	EmailPartials string
+	UserId        string
+	Expires       float64
+
+	CreatedAt float64
+	UpdatedAt float64
+}
+
+func (d Document) Id() string {
+	return string(d.Id_)
+}
+
+func (d *Document) Init() {
+	d.SetDocument(d)
+}
+
+func (t Token) Document() mixin.Document {
+	emailUser := strings.Split(t.Email, "@")[0]
+
+	doc := &Document{}
+	doc.Init()
+	doc.Kind = search.Atom(kind)
+	doc.Id_ = t.Id()
+	doc.Email = search.Atom(t.Email)
+	doc.EmailPartials = searchpartial.Partials(emailUser) + " " + emailUser
+	doc.UserId = t.UserId
+
+	doc.CreatedAt = float64(t.CreatedAt.Unix())
+	doc.UpdatedAt = float64(t.UpdatedAt.Unix())
+
+	doc.Expires = float64(t.Expires.Unix())
+
+	return doc
+}
