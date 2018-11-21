bafa6e6d8255d3a1cdf62a82e396527a65b5a35f
David Tai <antisocialmunky@gmail.com>
2018-11-21 13:06:03 -0600

    migration

--- models/migrations/rebuild-search-documents.go
+++ models/migrations/rebuild-search-documents.go
@@ -3,13 +3,14 @@ package migrations
 import (
 	"github.com/gin-gonic/gin"
 
-	"google.golang.org/appengine/search"
+	// "google.golang.org/appengine/search"
 
-	"hanzo.io/log"
-	// "hanzo.io/models/order"
-	"hanzo.io/models/mixin"
+	// "hanzo.io/log"
+	"hanzo.io/models/order"
+	// "hanzo.io/models/mixin"
 	"hanzo.io/models/product"
-	// "hanzo.io/models/user"
+	"hanzo.io/models/user"
+	"hanzo.io/demo/tokentransaction"
 
 	ds "hanzo.io/datastore"
 )
@@ -18,44 +19,47 @@ var _ = New("rebuild-search-documents",
 	func(c *gin.Context) []interface{} {
 		db := ds.New(c)
 
-		c.Set("namespace", "halcyon")
-		db.SetNamespace("halcyon")
-		ctx := db.Context
+		c.Set("namespace", "sec-demo")
+		db.SetNamespace("sec-demo")
+		// ctx := db.Context
 
-		index, err := search.Open(mixin.DefaultIndex)
-		if err != nil {
-			log.Error("Failed to open search index for model", ctx)
-			return NoArgs
-		}
+		// index, err := search.Open(mixin.DefaultIndex)
+		// if err != nil {
+		// 	log.Error("Failed to open search index for model", ctx)
+		// 	return NoArgs
+		// }
 
-		opts := search.SearchOptions{}
-		opts.IDsOnly = true
-		opts.Refinements = []search.Facet{
-			search.Facet{
-				Name:  "Kind",
-				Value: search.Atom("product"),
-			},
-		}
+		// opts := search.SearchOptions{}
+		// opts.IDsOnly = true
+		// opts.Refinements = []search.Facet{
+		// 	search.Facet{
+		// 		Name:  "Kind",
+		// 		Value: search.Atom("product"),
+		// 	},
+		// }
 
-		iter := index.Search(ctx, "", &opts)
+		// iter := index.Search(ctx, "", &opts)
 
-		for {
-			id, err := iter.Next(nil)
-			if err != nil {
-				break
-			}
+		// for {
+		// 	id, err := iter.Next(nil)
+		// 	if err != nil {
+		// 		break
+		// 	}
 
-			index.Delete(ctx, id)
-		}
+		// 	index.Delete(ctx, id)
+		// }
 
 		return NoArgs
 	},
-	// func(db *ds.Datastore, u *user.User) {
-	// 	u.PutDocument()
-	// },
-	// func(db *ds.Datastore, o *order.Order) {
-	// 	o.PutDocument()
-	// },
+	func(db *ds.Datastore, u *user.User) {
+		u.PutDocument()
+	},
+	func(db *ds.Datastore, t *tokentransaction.Transaction) {
+		t.PutDocument()
+	},
+	func(db *ds.Datastore, o *order.Order) {
+		o.PutDocument()
+	},
 	func(db *ds.Datastore, p *product.Product) {
 		p.PutDocument()
 	},
