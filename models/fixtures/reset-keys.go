e8b156c73385e9435ee1e3636575c569f9c061c9
David Tai <antisocialmunky@gmail.com>
2018-12-12 12:41:55 -0600

    fixed a bunch of issues

---
+++ models/fixtures/reset-keys.go
@@ -0,0 +1,21 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/datastore"
+	"hanzo.io/models/organization"
+)
+
+var ResetKeys = New("sec-demo", func(c *gin.Context) *organization.Organization {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.Name = "sec-demo"
+	org.GetOrCreate("Name=", org.Name)
+
+	org.AddDefaultTokens()
+	org.MustUpdate()
+
+	return org
+})
