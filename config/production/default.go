c45432680661b0797ca316ceb4aa3e8e24c0057b
Zach Kelling <z@zeekay.io>
2018-12-15 12:07:31 -0600

    Fix default module

--- config/production/default.go
+++ config/production/default.go
@@ -1,7 +1,15 @@
 package main
 
-import "hanzo.io/util/default_"
+import (
+	"google.golang.org/appengine"
+
+	"hanzo.io/util/default_"
+)
+
+func init() {
+	default_.Init()
+}
 
 func main() {
-	default_.Init()
+	appengine.Main()
 }
