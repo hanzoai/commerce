46371fd71be54ff37688ef7b92d90cc200968864
Zach Kelling <z@zeekay.io>
2019-09-23 17:14:31 -0700

    Fix go get ./...

---
+++ models/plan/fake.go
@@ -0,0 +1,20 @@
+package plan
+
+import (
+	"hanzo.io/datastore"
+	"hanzo.io/models/types/currency"
+	"hanzo.io/util/fake"
+)
+
+func Fake(db *datastore.Datastore) *Plan {
+	p := New(db)
+	p.Slug = fake.Slug()
+	p.SKU = fake.SKU()
+	p.Name = fake.Word()
+	p.Id_ = fake.Id()
+	p.Description = fake.Word()
+	p.Price = currency.Cents(0).Fake()
+	p.Currency = currency.USD
+	p.Interval = Monthly
+	return p
+}
---
+++ models/plan/model.go
@@ -0,0 +1,27 @@
+package plan
+
+import "hanzo.io/datastore"
+
+var kind = "plan"
+
+func (p Plan) Kind() string {
+	return "plan"
+}
+
+func (p *Plan) Init(db *datastore.Datastore) {
+	p.Model.Init(db, p)
+}
+
+func (p *Plan) Defaults() {
+}
+
+func New(db *datastore.Datastore) *Plan {
+	p := new(Plan)
+	p.Init(db)
+	p.Defaults()
+	return p
+}
+
+func Query(db *datastore.Datastore) datastore.Query {
+	return db.Query(kind)
+}
