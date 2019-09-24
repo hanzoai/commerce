334d8e03cadd0b427b469b3454f2d42b420a8cd9
Zach Kelling <z@zeekay.io>
2019-09-23 17:14:31 -0700

    Fix go get ./...

--- models/plan/plan.go
+++ models/plan/plan.go
@@ -1,1 +1,94 @@
 package plan
+
+import (
+	aeds "google.golang.org/appengine/datastore"
+
+	"hanzo.io/datastore"
+	"hanzo.io/models/mixin"
+	"hanzo.io/models/types/currency"
+	"hanzo.io/models/types/refs"
+	"hanzo.io/util/json"
+	"hanzo.io/util/val"
+
+	. "hanzo.io/types"
+)
+
+type Interval string
+
+const (
+	Yearly  Interval = "year"
+	Monthly          = "month"
+)
+
+var IgnoreFieldMismatch = datastore.IgnoreFieldMismatch
+
+// Based On Stripe Plan
+// Stripe\Plan JSON: {
+//   "id": "gold21323",
+//   "object": "plan",
+//   "amount": 2000,
+//   "created": 1386247539,
+//   "currency": "usd",
+//   "interval": "month",
+//   "interval_count": 1,
+//   "livemode": false,
+//   "metadata": {
+//   },
+//   "name": "New plan name",
+//   "statement_descriptor": null,
+//   "trial_period_days": null
+// }
+
+type Plan struct {
+	mixin.Model
+
+	// Unique human readable id
+	Slug string `json:"slug"`
+	// Internal id
+	SKU string `json:"sku"`
+
+	// Human readable name
+	Name        string `json:"name"`
+	Description string `json:"description"`
+
+	Price           currency.Cents `json:"price"`
+	Currency        currency.Type  `json:"currency"`
+	Interval        Interval       `json:"interval"`
+	IntervalCount   int            `json:"intervalCount"`
+	TrialPeriodDays int            `json:"trialPeriodDays"`
+
+	Metadata  Map    `json:"metadata" datastore:"-"`
+	Metadata_ string `json:"-" datastore:"-"`
+
+	Ref refs.EcommerceRef `json:"ref,omitempty"`
+}
+
+func (p *Plan) Load(ps []aeds.Property) (err error) {
+	// Load supported properties
+	if err = datastore.LoadStruct(p, ps); err != nil {
+		return err
+	}
+
+	// Deserialize from datastore
+	if len(p.Metadata_) > 0 {
+		err = json.DecodeBytes([]byte(p.Metadata_), &p.Metadata)
+	}
+
+	return err
+}
+
+func (p *Plan) Save() (ps []aeds.Property, err error) {
+	// Serialize unsupported properties
+	p.Metadata_ = string(json.EncodeBytes(&p.Metadata))
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Save properties
+	return datastore.SaveStruct(p)
+}
+
+func (p *Plan) Validator() *val.Validator {
+	return val.New()
+}
