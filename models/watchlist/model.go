bad58fc23dd1c16e58193375eba3bfd08b1cba23
Timothy Messer <tmesser@verus.io>
2019-09-26 18:14:12 -0500

    New models for movie and watchlist, and basic APIs for both - should probably add some extra stuff for watch list.

---
+++ models/watchlist/model.go
@@ -0,0 +1,34 @@
+package watchlist
+
+import (
+	"hanzo.io/datastore"
+	"hanzo.io/models/movie"
+
+	. "hanzo.io/types"
+)
+
+var kind = "watchlist"
+
+func (w Watchlist) Kind() string {
+	return kind
+}
+
+func (w *Watchlist) Init(db *datastore.Datastore) {
+	w.Model.Init(db, w)
+}
+
+func (w *Watchlist) Defaults() {
+	w.Movies = make([]movie.Movie, 0)
+	w.Metadata = make(Map)
+}
+
+func New(db *datastore.Datastore) *Watchlist {
+	w := new(Watchlist)
+	w.Init(db)
+	w.Defaults()
+	return w
+}
+
+func Query(db *datastore.Datastore) datastore.Query {
+	return db.Query(kind)
+}
