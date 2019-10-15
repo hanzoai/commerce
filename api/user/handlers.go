3d63a6bdacaf7d3a577bbab627c4758d28b550b3
David Tai <antisocialmunky@gmail.com>
2019-10-14 21:57:20 -0500

    add payment methods

--- api/user/handlers.go
+++ api/user/handlers.go
@@ -25,6 +25,7 @@ func Route(router router.Router, args ...gin.HandlerFunc) {
 	api.GET("/:userid/referrers", readUserRequired, readReferrerRequired, getReferrers)
 	api.GET("/:userid/transactions", readUserRequired, getTransactions)
 	api.GET("/:userid/tokentransactions", readUserRequired, getTokenTransactions)
+	api.GET("/:userid/paymentmethods", readUserRequired, getPaymentMethods)
 	api.GET("/:userid/transfer", readUserRequired, getTransfers)
 	api.GET("/:userid/affiliate", readUserRequired, getAffiliate)
 
