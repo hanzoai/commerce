cf2fd499009fd4dd335cf254ee97726b5e29b8d4
David Tai <antisocialmunky@gmail.com>
2020-02-06 11:52:18 -0600

    refund-counters migration for damon

---
+++ models/migrations/damon-refund-counters.go
@@ -0,0 +1,33 @@
+package migrations
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/log"
+	"hanzo.io/models/order"
+	"hanzo.io/models/organization"
+	"hanzo.io/util/counter"
+
+	ds "hanzo.io/datastore"
+)
+
+var _ = New("reset-refund-counters",
+	func(c *gin.Context) []interface{} {
+		orgName := "damon"
+
+		c.Set("namespace", orgName)
+
+		db := ds.New(c)
+		org := organization.New(db)
+		if _, err := org.Query().Filter("Name=", orgName).Get(); err != nil {
+			panic(err)
+		}
+
+		return NoArgs
+	},
+	func(db *ds.Datastore, ord *order.Order) {
+		if err := counter.IncrOrderRefund(db.Context, ord, int(ord.Refunded), ord.UpdatedAt); err != nil {
+			log.Error("IncrOrderRefund Error %v", err, db.Context)
+		}
+	},
+)
