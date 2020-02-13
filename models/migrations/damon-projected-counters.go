99b2f622e023aef84fd0e799eebdef6e9a51f3a9
David Tai <david@verus.io>
2020-02-13 14:58:09 -0600

    fix projected value counter

---
+++ models/migrations/damon-projected-counters.go
@@ -0,0 +1,44 @@
+package migrations
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/log"
+	"hanzo.io/models/order"
+	"hanzo.io/models/product"
+	"hanzo.io/util/counter"
+
+	ds "hanzo.io/datastore"
+)
+
+var _ = New("damon-projected-counters",
+	func(c *gin.Context) []interface{} {
+		c.Set("namespace", "damon")
+		return NoArgs
+	},
+	func(db *ds.Datastore, ord *order.Order) {
+		ctx := db.Context
+
+		projectedPrice := 0
+		// Calculate Projected
+		ord.GetItemEntities()
+		for _, item := range ord.Items {
+			projectedPrice += item.Quantity * int(item.ProjectedPrice)
+		}
+
+		if err := counter.IncrementByAll(ctx, "order.projected.revenue", ord.StoreId, ord.ShippingAddress.Country, projectedPrice, ord.CreatedAt); err != nil {
+			log.Error("order.projected.revenue error %v", err, db.Context)
+		}
+		for _, item := range ord.Items {
+			prod := product.New(ord.Db)
+			if err := prod.GetById(item.ProductId); err != nil {
+				log.Error("no product found %v", err, ctx)
+			}
+			for i := 0; i < item.Quantity; i++ {
+				if err := counter.IncrementByAll(ctx, "product."+prod.Id()+".projected.revenue", ord.StoreId, ord.ShippingAddress.Country, int(prod.ProjectedPrice), ord.CreatedAt); err != nil {
+					log.Error("product."+prod.Id()+".projected.revenue error %v", err, db.Context)
+				}
+			}
+		}
+	},
+)
