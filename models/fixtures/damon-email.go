9a88319e1b9ba33053d8f9cd4583f98ee8fa2799
David Tai <antisocialmunky@gmail.com>
2020-02-18 10:23:32 -0600

    fix damon emails

--- models/fixtures/damon-email.go
+++ models/fixtures/damon-email.go
@@ -9,6 +9,7 @@ import (
 	"hanzo.io/models/organization"
 	"hanzo.io/types/email"
 	"hanzo.io/types/email/provider"
+	"hanzo.io/types/integration"
 )
 
 var _ = New("damon-email", func(c *gin.Context) *organization.Organization {
@@ -30,6 +31,15 @@ var _ = New("damon-email", func(c *gin.Context) *organization.Organization {
 	org.Email.Order.Confirmation = email.Setting{
 		Enabled:    true,
 		TemplateId: "order-confirmed",
+	}
+
+	if mandrills := org.Integrations.FilterByType(integration.MandrillType); len(mandrills) == 0 {
+		m := integration.Integration{
+			Type:     integration.MandrillType,
+			Enabled:  true,
+			Mandrill: org.Mandrill,
+		}
+		org.Integrations.MustAppend(&m)
 	}
 
 	// Save org into default namespace
