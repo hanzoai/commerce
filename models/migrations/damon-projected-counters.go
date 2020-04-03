165ee373f89fca6aa2fa7816cb017398bec1ba73
David Tai <antisocialmunky@gmail.com>
2020-04-02 22:08:39 -0500

    projected order refund counter

--- models/migrations/damon-projected-counters.go
+++ models/migrations/damon-projected-counters.go
@@ -11,7 +11,7 @@ import (
 	ds "hanzo.io/datastore"
 )
 
-var _ = New("damon-product-refund-counters",
+var _ = New("damon-projected-counters",
 	func(c *gin.Context) []interface{} {
 		c.Set("namespace", "damon")
 		return NoArgs
@@ -21,11 +21,25 @@ var _ = New("damon-product-refund-counters",
 			return
 		}
 
-		if ord.Total != ord.Refunded {
-			return
-		}
+		// if ord.Status == "cancelled" {
+		// 	return
+		// }
 
 		ctx := db.Context
+
+		projectedPrice := 0
+		// Calculate Projected
+		for _, item := range ord.Items {
+			log.Warn("item %v", item.ProjectedPrice, db.Context)
+			prod := product.New(ord.Db)
+			if err := prod.GetById(item.ProductId); err == nil {
+				projectedPrice += item.Quantity * int(prod.ProjectedPrice)
+			}
+		}
+
+		if err := counter.IncrementByAll(ctx, "order.projected.revenue", ord.StoreId, ord.ShippingAddress.Country, projectedPrice, ord.CreatedAt); err != nil {
+			log.Error("order.projected.revenue error %v", err, db.Context)
+		}
 
 		for _, item := range ord.Items {
 			prod := product.New(ord.Db)
@@ -33,8 +47,8 @@ var _ = New("damon-product-refund-counters",
 				log.Error("no product found %v", err, ctx)
 			}
 			for i := 0; i < item.Quantity; i++ {
-				if err := counter.IncrProductRefund(ctx, prod, ord); err != nil {
-					log.Error("product."+prod.Id()+".refunded error %v", err, db.Context)
+				if err := counter.IncrementByAll(ctx, "product."+prod.Id()+".projected.revenue", ord.StoreId, ord.ShippingAddress.Country, int(prod.ProjectedPrice), ord.CreatedAt); err != nil {
+					log.Error("product."+prod.Id()+".projected.revenue error %v", err, db.Context)
 				}
 			}
 		}
