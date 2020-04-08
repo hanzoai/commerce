b3ed2a19180f11cc384fec3c9895d4b4ee289e95
David Tai <antisocialmunky@gmail.com>
2020-04-08 12:10:56 -0500

    triller forms

--- models/fixtures/triller-forms.go
+++ models/fixtures/triller-forms.go
@@ -8,29 +8,29 @@ import (
 	"hanzo.io/models/organization"
 )
 
-var _ = New("damon-forms", func(c *gin.Context) *form.Form {
+var _ = New("triller-forms", func(c *gin.Context) *form.Form {
 	db := datastore.New(c)
 
 	org := organization.New(db)
-	org.Name = "damon"
+	org.Name = "triller"
 	org.GetOrCreate("Name=", org.Name)
 
 	nsdb := datastore.New(org.Namespaced(db.Context))
 
 	// Create mailinglist
-	f := form.New(nsdb)
-	f.Name = "Preorders"
-	f.GetOrCreate("Name=", f.Name)
-	f.Mailchimp.APIKey = ""
-	f.MustUpdate()
+	// f := form.New(nsdb)
+	// f.Name = "Preorders"
+	// f.GetOrCreate("Name=", f.Name)
+	// f.Mailchimp.APIKey = ""
+	// f.MustUpdate()
 
 	// Create mailinglist
 	f2 := form.New(nsdb)
 	f2.Name = "Newsletter"
 	f2.GetOrCreate("Name=", f2.Name)
-	f2.Mailchimp.APIKey = ""
-	f2.Mailchimp.ListId = "aacc13e678"
+	// f2.Mailchimp.APIKey = ""
+	// f2.Mailchimp.ListId = "aacc13e678"
 	f2.MustUpdate()
 
-	return f
+	return f2
 })
