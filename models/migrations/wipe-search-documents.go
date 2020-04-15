b747dbae276551a175e6f8435bfeefe44e8adbf2
David Tai <antisocialmunky@gmail.com>
2020-04-14 22:16:02 -0500

    wipe all documents in a namespace

--- models/migrations/wipe-search-documents.go
+++ models/migrations/wipe-search-documents.go
@@ -15,7 +15,7 @@ var _ = New("wipe-search-documents",
 		db.SetNamespace("damon")
 		ctx := db.Context
 
-		index, err := search.Open("user")
+		index, err := search.Open("everything")
 		if err != nil {
 			log.Error("Failed to open search index for model", ctx)
 			return NoArgs
@@ -32,22 +32,6 @@ var _ = New("wipe-search-documents",
 			index.Delete(ctx, id)
 		}
 
-		index, err = search.Open("order")
-		if err != nil {
-			log.Error("Failed to open search index for model", ctx)
-			return NoArgs
-		}
-
-		iter = index.List(ctx, &search.ListOptions{IDsOnly: true})
-
-		for {
-			id, err := iter.Next(nil)
-			if err != nil {
-				break
-			}
-
-			index.Delete(ctx, id)
-		}
 		return NoArgs
 	},
 )
