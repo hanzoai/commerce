401e4a4978760405565e046596bed18b6f664db5
David Tai <antisocialmunky@gmail.com>
2020-07-07 15:38:54 -0500

    test orders for david.lam and me

---
+++ models/fixtures/damon-test-orders.go
@@ -0,0 +1,85 @@
+package fixtures
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"hanzo.io/datastore"
+	"hanzo.io/models/lineitem"
+	"hanzo.io/models/order"
+	"hanzo.io/models/organization"
+	"hanzo.io/models/user"
+)
+
+var _ = New("damon-test-orders", func(c *gin.Context) {
+	db := datastore.New(c)
+
+	org := organization.New(db)
+	org.Name = "damon"
+	org.GetOrCreate("Name=", org.Name)
+
+	nsdb := datastore.New(org.Namespaced(db.Context))
+
+	usr1 := user.New(nsdb)
+	usr1.GetByEmail("david.lam@damon.com")
+
+	usr2 := user.New(nsdb)
+	usr2.GetByEmail("dtai@hanzo.ai")
+
+	premierSlugs := []string{
+		"HSP-BGL",
+		"HSP-BRS",
+		"HSP-GGP",
+		"HSP-GRS",
+		"HSP-GWP",
+		"HSP-RWS",
+		"HSP-WRW",
+		"HSP-BGP",
+		"HSP-BRW",
+		"HSP-GGRS",
+		"HSP-GRW",
+		"HSP-RWL",
+		"HSP-WGL",
+		"HSP-BGRS",
+		"HSP-GBRS",
+		"HSP-GGS",
+		"HSP-GRWL",
+		"HSP-RWP",
+		"HSP-WRRS",
+		"HSP-BGW",
+		"HSP-GBW",
+		"HSP-GRP",
+		"HSP-GWL",
+		"HSP-RWRS",
+		"HSP-WRS",
+		"HSP-MS",
+		"HSP-AS",
+		"HS",
+	}
+
+	for _, v := range premierSlugs {
+		ord1 := order.New(nsdb)
+		ord1.UserId = usr1.Id()
+		ord1.Currency = currency.USD
+		ord1.Items = []lineitem.LineItem{
+			lineitem.LineItem{
+				ProductSlug: v,
+				Quantity:    1,
+			},
+		}
+
+		ord1.UpdateAndTally(nil)
+		ord1.MustPut()
+
+		ord2 := order.New(nsdb)
+		ord2.UserId = usr2.Id()
+		ord2.Items = []lineitem.LineItem{
+			lineitem.LineItem{
+				ProductSlug: v,
+				Quantity:    1,
+			},
+		}
+
+		ord2.UpdateAndTally(nil)
+		ord2.MustPut()
+	}
+})
