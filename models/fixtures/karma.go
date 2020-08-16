71e8d44abc8fb8f781a39f5aeab9ed0cdce5cf2e
David Tai <antisocialmunky@gmail.com>
2020-08-16 15:32:38 -0500

    new karama products

--- models/fixtures/karma.go
+++ models/fixtures/karma.go
@@ -88,10 +88,49 @@ var _ = New("karma", func(c *gin.Context) *organization.Organization {
 
 	{
 		prod := product.New(nsdb)
-		prod.Slug = "masks"
+		prod.Slug = "postcard"
 		prod.GetOrCreate("Slug=", prod.Slug)
-		prod.Name = "2x Masks"
-		prod.Description = ""
+		prod.Name = "Postcard"
+		prod.Description = "We will select one of our best shots to make into a postcard with a hand written appreciation note from our designer."
+		prod.Price = currency.Cents(1000)
+		prod.ListPrice = currency.Cents(1000)
+		prod.Inventory = 9000
+		prod.Preorder = true
+		prod.MustUpdate()
+	}
+
+	{
+		prod := product.New(nsdb)
+		prod.Slug = "mug"
+		prod.GetOrCreate("Slug=", prod.Slug)
+		prod.Name = "Good Karma mug"
+		prod.Description = "A sweet reminder of our gratitude along with every morning cup o' joe."
+		prod.Price = currency.Cents(1000)
+		prod.ListPrice = currency.Cents(1000)
+		prod.Inventory = 9000
+		prod.Preorder = true
+		prod.MustUpdate()
+	}
+
+	{
+		prod := product.New(nsdb)
+		prod.Slug = "less-boring"
+		prod.GetOrCreate("Slug=", prod.Slug)
+		prod.Name = "Less Boring Summer Mask"
+		prod.Description = "Stay healthy and chic with our sustainable yet lightweight protective measures. Choose from our 2 prints (dragon blossom and trippy leopard) all made from recycled fish nets."
+		prod.Price = currency.Cents(3500)
+		prod.ListPrice = currency.Cents(3500)
+		prod.Inventory = 9000
+		prod.Preorder = true
+		prod.MustUpdate()
+	}
+
+	{
+		prod := product.New(nsdb)
+		prod.Slug = "karma-collab-t"
+		prod.GetOrCreate("Slug=", prod.Slug)
+		prod.Name = "Karma collaboration T"
+		prod.Description = "100% recycled cotton. We have curated an epic design that we are proud to call our first Karma graphic-t. Choose between Men/Women sizing."
 		prod.Price = currency.Cents(5000)
 		prod.ListPrice = currency.Cents(5000)
 		prod.Inventory = 9000
@@ -101,10 +140,49 @@ var _ = New("karma", func(c *gin.Context) *organization.Organization {
 
 	{
 		prod := product.New(nsdb)
-		prod.Slug = "karma-swim"
+		prod.Slug = "mystery-bikini"
 		prod.GetOrCreate("Slug=", prod.Slug)
-		prod.Name = "Karma Swim"
-		prod.Description = ""
+		prod.Name = "Mystery Bikini"
+		prod.Description = "We choose a sustainable suit in your size. Styles may vary from all the products on our website. (includes top size, product selection, includes bottom size product selection)"
+		prod.Price = currency.Cents(8000)
+		prod.ListPrice = currency.Cents(8000)
+		prod.Inventory = 9000
+		prod.Preorder = true
+		prod.MustUpdate()
+	}
+
+	{
+		prod := product.New(nsdb)
+		prod.Slug = "mens-trunks"
+		prod.GetOrCreate("Slug=", prod.Slug)
+		prod.Name = "Karma Men’s Swim Trunks"
+		prod.Description = "Eccentric enough to be cool. When you wear these you will stand out, but never stand alone. Functional and versatile with 2 pockets for convenient storage. Made from our ultra soft Italian Carvico fabric."
+		prod.Price = currency.Cents(8000)
+		prod.ListPrice = currency.Cents(8000)
+		prod.Inventory = 9000
+		prod.Preorder = true
+		prod.MustUpdate()
+	}
+
+	{
+		prod := product.New(nsdb)
+		prod.Slug = "dial-a-backer"
+		prod.GetOrCreate("Slug=", prod.Slug)
+		prod.Name = "Dial-a-backer"
+		prod.Description = "Schedule some time for a 30-minute phone call with our designer, ask for tips on making a project happen, fashion design, sustainability or even just about their day."
+		prod.Price = currency.Cents(12000)
+		prod.ListPrice = currency.Cents(12000)
+		prod.Inventory = 9000
+		prod.Preorder = true
+		prod.MustUpdate()
+	}
+
+	{
+		prod := product.New(nsdb)
+		prod.Slug = "dial-a-backer"
+		prod.GetOrCreate("Slug=", prod.Slug)
+		prod.Name = "Karma Bikini"
+		prod.Description = "Sustainable, chic, lightweight and made from recycled fish nets. All sales from every piece in our Less Boring Summer Collection directly contribute towards our mission to create a fully sustainable supply chain that empowers disadvantaged Women globally. Choose a suit from any piece in our Less Boring Summer Collection."
 		prod.Price = currency.Cents(20000)
 		prod.ListPrice = currency.Cents(20000)
 		prod.Inventory = 9000
@@ -114,12 +192,25 @@ var _ = New("karma", func(c *gin.Context) *organization.Organization {
 
 	{
 		prod := product.New(nsdb)
-		prod.Slug = "credit"
+		prod.Slug = "dial-a-backer"
 		prod.GetOrCreate("Slug=", prod.Slug)
-		prod.Name = "2x Credit"
-		prod.Description = ""
-		prod.Price = currency.Cents(50000)
-		prod.ListPrice = currency.Cents(50000)
+		prod.Name = "Trikini"
+		prod.Description = "Guess what it’s 2020 and the only way to look cute and safe at the beach is with your bikini and mask, a.k.a. the tri-kini matching set. Choose a bikini style from the Less Boring Summer Collection and any mask. Available in our Trippy Leopard print/Dragon Blossom print."
+		prod.Price = currency.Cents(23500)
+		prod.ListPrice = currency.Cents(23500)
+		prod.Inventory = 9000
+		prod.Preorder = true
+		prod.MustUpdate()
+	}
+
+	{
+		prod := product.New(nsdb)
+		prod.Slug = "save-30"
+		prod.GetOrCreate("Slug=", prod.Slug)
+		prod.Name = "save-30"
+		prod.Description = "Enjoy some good old fashioned retail therapy and support our mission, get $1000 worth of credit applicable to anything on our site for just $700. The code lasts forever and you can use the remaining amount to purchase items from our future collections."
+		prod.Price = currency.Cents(70000)
+		prod.ListPrice = currency.Cents(70000)
 		prod.Inventory = 9000
 		prod.Preorder = true
 		prod.MustUpdate()
@@ -129,8 +220,21 @@ var _ = New("karma", func(c *gin.Context) *organization.Organization {
 		prod := product.New(nsdb)
 		prod.Slug = "custom-designs"
 		prod.GetOrCreate("Slug=", prod.Slug)
-		prod.Name = "Custom Designs"
-		prod.Description = ""
+		prod.Name = "Custom bikini designed and named after you"
+		prod.Description = "Work alongside our designer and design the outfits of your dreams. We will work with you to create the best possible construction, fit and design."
+		prod.Price = currency.Cents(100000)
+		prod.ListPrice = currency.Cents(100000)
+		prod.Inventory = 9000
+		prod.Preorder = true
+		prod.MustUpdate()
+	}
+
+	{
+		prod := product.New(nsdb)
+		prod.Slug = "5-custom-designs"
+		prod.GetOrCreate("Slug=", prod.Slug)
+		prod.Name = "5 Custom designed bikinis (or shorts)"
+		prod.Description = "Work together with our designer to design the most fitted swim wear you will ever have while incorporating unique prints that will make every hot day pool party worthy."
 		prod.Price = currency.Cents(100000)
 		prod.ListPrice = currency.Cents(100000)
 		prod.Inventory = 9000
@@ -143,7 +247,7 @@ var _ = New("karma", func(c *gin.Context) *organization.Organization {
 		prod.Slug = "sponsor"
 		prod.GetOrCreate("Slug=", prod.Slug)
 		prod.Name = "Sponsor a Karma Shoot"
-		prod.Description = ""
+		prod.Description = "Let us have our next photoshoot at your place or another glamorous location. We will hire the best team of creatives and make beautiful art all sponsored by you! Get behind the scenes as you watch the magic happen. Each shoot sponsored by x) We will create a page on our site that will showcase all the sponsored shoots with a dedication to you!"
 		prod.Price = currency.Cents(500000)
 		prod.ListPrice = currency.Cents(500000)
 		prod.Inventory = 9000
@@ -156,7 +260,7 @@ var _ = New("karma", func(c *gin.Context) *organization.Organization {
 		prod.Slug = "capsule-collection"
 		prod.GetOrCreate("Slug=", prod.Slug)
 		prod.Name = "Capsule Collection"
-		prod.Description = ""
+		prod.Description = "Co-create a collection of sustainable swim and resort wear. Be the face of your own brand with Karma. Karma x You. A complete collection of 10 seperate pieces tailored and designed just for you. Work alongside our team and earn 20% of all future revenue from your Capsule Collection."
 		prod.Price = currency.Cents(1500000)
 		prod.ListPrice = currency.Cents(1500000)
 		prod.Inventory = 9000
@@ -166,10 +270,10 @@ var _ = New("karma", func(c *gin.Context) *organization.Organization {
 
 	{
 		prod := product.New(nsdb)
-		prod.Slug = "design-partner"
+		prod.Slug = "louge-wear"
 		prod.GetOrCreate("Slug=", prod.Slug)
-		prod.Name = "Design Partner"
-		prod.Description = ""
+		prod.Name = "Fund our Loungewear Collection"
+		prod.Description = "We are making a collection of Bamboo Lyocell which is almost 100% sustainable. The extended range will go beyond swimwear and carry products like dresses, tanks, skirts, pants and even some Men’s recycled linen suits. You will in turn earn 20% of all sales made from the first two years."
 		prod.Price = currency.Cents(4000000)
 		prod.ListPrice = currency.Cents(4000000)
 		prod.Inventory = 9000
@@ -181,8 +285,8 @@ var _ = New("karma", func(c *gin.Context) *organization.Organization {
 		prod := product.New(nsdb)
 		prod.Slug = "travel"
 		prod.GetOrCreate("Slug=", prod.Slug)
-		prod.Name = "Travel the World w/ Karma"
-		prod.Description = ""
+		prod.Name = "Fund a sustainable women run operation in Morocco"
+		prod.Description = "Help support our mission as we expand production to Morocco. We will employ women in our new production house, giving them access to work which otherwise is unavailable. We will use the most sustainable techniques, materials and sourcing which are at least 99% sustainable. Additionally we will be able to offer childcare to our workers and produce our extended range of lounge wear!"
 		prod.Price = currency.Cents(10000000)
 		prod.ListPrice = currency.Cents(10000000)
 		prod.Inventory = 9000
