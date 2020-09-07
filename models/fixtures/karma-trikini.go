5c37437baff3a4a12b49dda9145946e3461b0f21
David Tai <david@verus.io>
2020-09-07 17:26:46 -0500

    updated karma products

--- models/fixtures/karma-trikini.go
+++ models/fixtures/karma-trikini.go
@@ -44,107 +44,57 @@ var _ = New("karma-trikini", func(c *gin.Context) []*product.Product {
 		},
 	}
 
-	TOP_STYLES := []size{
+	BIKINI_STYLES := []size{
 		size{
 			name: "Railay Bikini in trippy leopard",
-			id:   "railay-top-tl",
+			id:   "railay-tl",
 		},
 		size{
 			name: "Railay Bikini in dragon blossom",
-			id:   "railay-top-db",
+			id:   "railay-db",
 		},
 		size{
 			name: "Ruby Bikini in trippy leopard",
-			id:   "ruby-top-tl",
+			id:   "ruby-tl",
 		},
 		size{
 			name: "Ruby Bikini in dragon blossom",
-			id:   "ruby-top-db",
+			id:   "ruby-db",
 		},
 		size{
 			name: "Lafayette Bikini in trippy leopard",
-			id:   "lafayette-top-tl",
+			id:   "lafayette-tl",
 		},
 		size{
 			name: "Lafayette Bikini in dragon blossom",
-			id:   "lafayette-top-db",
+			id:   "lafayette-db",
 		},
 		size{
 			name: "Bikini-n-Chill Bikini in trippy leopard",
-			id:   "bikini-n-chill-top-tl",
+			id:   "bikini-n-chill-tl",
 		},
 		size{
 			name: "Bikini-n-Chill Bikini in dragon blossom",
-			id:   "bikini-n-chill-top-db",
-		},
-	}
-
-	BOTTOM_STYLES := []size{
-		size{
-			name: "Railay Bikini in trippy leopard",
-			id:   "railay-btm-tl",
-		},
-		size{
-			name: "Railay Bikini in dragon blossom",
-			id:   "railay-btm-db",
-		},
-		size{
-			name: "Ruby Bikini in trippy leopard",
-			id:   "ruby-btm-tl",
-		},
-		size{
-			name: "Ruby Bikini in dragon blossom",
-			id:   "ruby-btm-db",
-		},
-		size{
-			name: "Lafayette Bikini in trippy leopard",
-			id:   "lafayette-btm-tl",
-		},
-		size{
-			name: "Lafayette Bikini in dragon blossom",
-			id:   "lafayette-btm-db",
-		},
-		size{
-			name: "Bikini-n-Chill Bikini in trippy leopard",
-			id:   "bikini-n-chill-btm-tl",
-		},
-		size{
-			name: "Bikini-n-Chill Bikini in dragon blossom",
-			id:   "bikini-n-chill-btm-db",
-		},
-	}
-
-	MASK_STYLES := []size{
-		size{
-			name: "Trippy Leopard Mask",
-			id:   "mask-trippy-leopard",
-		},
-		size{
-			name: "Less Boring Summer Mask",
-			id:   "mask-less-boring-summer",
+			id:   "bikini-n-chill-db",
 		},
 	}
 
 	prods := []*product.Product{}
 
-	for _, s1 := range TOP_STYLES {
+	for _, s1 := range BIKINI_STYLES {
 		for _, s2 := range SIZES {
-			for _, s3 := range BOTTOM_STYLES {
-				for _, s4 := range SIZES {
-					for _, s5 := range MASK_STYLES {
-						prod := product.New(nsdb)
-						prod.Slug = "trikini-" + s1.id + "-" + s2.id + "-" + s3.id + "-" + s4.id + "-" + s5.id
-						prod.GetOrCreate("Slug=", prod.Slug)
-						prod.Name = "Trikini " + s1.name + " " + s2.name + " Top " + s3.name + " " + s4.name + " Bottom " + s5.name + " Mask"
-						prod.Description = "Guess what it’s 2020 and the only way to look cute and safe at the beach is with your bikini and mask, a.k.a. the tri-kini matching set. Choose a bikini style from the Less Boring Summer Collection and any mask. Available in our Trippy Leopard print/Dragon Blossom print. "
-						prod.Currency = currency.USD
-						prod.ListPrice = currency.Cents(23500)
-						prod.Price = currency.Cents(23500)
-						prod.Update()
+			for _, s3 := range SIZES {
+				prod := product.New(nsdb)
+				prod.Slug = "trikini-" + s1.id + "-" + s2.id + "-" + s3.id
+				prod.GetOrCreate("Slug=", prod.Slug)
+				prod.Name = "Trikini " + s1.name + " " + s2.name + " Top " + s3.name + " Bottom"
+				prod.Description = "Guess what it’s 2020 and the only way to look cute and safe at the beach is with your bikini and mask, a.k.a. the tri-kini matching set. Choose a bikini style from the Less Boring Summer Collection and any mask. Available in our Trippy Leopard print/Dragon Blossom print. "
+				prod.Currency = currency.USD
+				prod.ListPrice = currency.Cents(23500)
+				prod.Price = currency.Cents(23500)
+				prod.Update()
 
-						prods = append(prods, prod)
-					}
-				}
+				prods = append(prods, prod)
 			}
 		}
 	}
