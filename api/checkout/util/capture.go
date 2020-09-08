5bc457bad81c20c17bc2bc2c17db6c0db0c6e26a
David Tai <antisocialmunky@gmail.com>
2020-09-08 17:42:56 -0500

    save referral does not refer itself

--- api/checkout/util/capture.go
+++ api/checkout/util/capture.go
@@ -104,11 +104,10 @@ func UpdateReferral(org *organization.Organization, ord *order.Order) {
 		ord.Id(),
 		ord.Kind(),
 		total,
-	}, !org.Live)
+	}, ord.UserId, !org.Live)
 
 	if err != nil {
 		log.Warn("Unable to save referral: %v", err, ctx)
-		return
 	}
 
 	if !ord.Test {
