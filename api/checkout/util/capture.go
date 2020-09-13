8c72d4f3211ab0b021e7d5ad0158f2c883995feb
David Tai <david@verus.io>
2020-09-13 15:56:06 -0500

    capture triggers woopra

--- api/checkout/util/capture.go
+++ api/checkout/util/capture.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"context"
+	"strconv"
 
 	"hanzo.io/log"
 	"hanzo.io/models/cart"
@@ -16,8 +17,10 @@ import (
 	"hanzo.io/models/types/currency"
 	"hanzo.io/models/user"
 	"hanzo.io/thirdparty/mailchimp"
+	"hanzo.io/types/integration"
 	"hanzo.io/util/counter"
 
+	"hanzo.io/thirdparty/woopra"
 	. "hanzo.io/types"
 )
 
@@ -258,5 +261,117 @@ func HandleDeposit(ord *order.Order) {
 		trans.Test = ord.Test
 		trans.Notes = "Deposit from Order '" + ord.Id() + "'"
 		trans.MustCreate()
+	}
+}
+
+func UpdateWoopraIntegration(ctx context.Context, org *organization.Organization, ord *order.Order) {
+	{
+		in := org.Integrations.FindByType(integration.WoopraType)
+		if in != nil {
+			usr := user.New(ord.Db)
+			if err := usr.GetById(ord.UserId); err != nil {
+				log.Error("no user found %v", err, ctx)
+				return
+			}
+
+			if err := usr.LoadOrders(); err != nil {
+				log.Error("loadorders error %v", err, ctx)
+				return
+			}
+
+			domain := in.Woopra.Domain
+			wt, _ := woopra.NewTracker(map[string]string{
+
+				// `host` is domain as registered in Woopra, it identifies which
+				// project environment to receive the tracking request
+				"host": domain,
+
+				// In milliseconds, defaults to 30000 (equivalent to 30 seconds)
+				// after which the event will expire and the visit will be marked
+				// as offline.
+				"timeout": "30000",
+			})
+
+			cancelledOrders := 0
+			creditOrders := 0
+			disputedOrders := 0
+			failedOrders := 0
+			fraudOrders := 0
+			paidOrders := 0
+			refundedOrders := 0
+			unpaidOrders := 0
+			for _, v := range usr.Orders {
+				switch ps := v.PaymentStatus; ps {
+				case payment.Cancelled:
+					cancelledOrders++
+				case payment.Credit:
+					creditOrders++
+				case payment.Disputed:
+					disputedOrders++
+				case payment.Failed:
+					failedOrders++
+				case payment.Fraudulent:
+					fraudOrders++
+				case payment.Paid:
+					paidOrders++
+				case payment.Refunded:
+					refundedOrders++
+				case payment.Unpaid:
+					unpaidOrders++
+				}
+			}
+
+			values := make(map[string]string)
+			values["firstName"] = usr.FirstName
+			values["lastName"] = usr.LastName
+			values["city"] = usr.ShippingAddress.City
+			values["country"] = usr.ShippingAddress.Country
+			values["referred_by"] = usr.ReferrerId
+			values["referrals"] = strconv.Itoa(len(usr.Referrals))
+			values["orders"] = strconv.Itoa(len(usr.Orders))
+			values["cancelledOrders"] = strconv.Itoa(cancelledOrders)
+			values["creditOrders"] = strconv.Itoa(creditOrders)
+			values["disputedOrders"] = strconv.Itoa(disputedOrders)
+			values["failedOrders"] = strconv.Itoa(failedOrders)
+			values["fraudOrders"] = strconv.Itoa(fraudOrders)
+			values["paidOrders"] = strconv.Itoa(paidOrders)
+			values["refundedOrders"] = strconv.Itoa(refundedOrders)
+			values["unpaidOrders"] = strconv.Itoa(unpaidOrders)
+
+			person := woopra.Person{
+				Id:     usr.Id(),
+				Name:   usr.Name(),
+				Email:  usr.Email,
+				Values: values,
+			}
+
+			// identifying current visitor in Woopra
+			ident := wt.Identify(
+				ctx,
+				person,
+				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7",
+			)
+
+			// Tracking custom event in Woopra. Each event can has additional data
+			ident.Track(
+				"orderCreate", // event name
+				map[string]string{ // custom data
+					"orderId":     ord.Id(),
+					"orderNumber": strconv.Itoa(ord.Number),
+					"name":        usr.Name(),
+					"email":       usr.Email,
+					"city":        usr.ShippingAddress.City,
+					"country":     usr.ShippingAddress.Country,
+					"referred_by": usr.ReferrerId,
+					"currency":    string(ord.Currency),
+					"revenue":     ord.Currency.ToStringNoSymbol(ord.Total),
+					"refunded":    ord.Currency.ToStringNoSymbol(ord.Refunded),
+					"cartId":      ord.CartId,
+				})
+
+			// it's possible to send only visitor's data to Woopra, without sending
+			// any custom event and/or data
+			ident.Push()
+		}
 	}
 }
