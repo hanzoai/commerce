d487e462d5183e05d0740fca4f2208f0b04a33c8
Hanzo Dev <dev@hanzo.ai>
2026-02-23 18:59:52 -0800

    Continue ORM EntityBridge migration for remaining models
    Migrate cart, collection, coupon, customergroup, discount, fulfillment,
    geozone, notification, price, pricelist, stocklocation, taxprovider,
    taxrate, and webhook models. Delete corresponding model.go files.

--- models/pricelist/pricelist.go
+++ models/pricelist/pricelist.go
@@ -6,16 +6,19 @@ import (
 	"github.com/hanzoai/commerce/datastore"
 	"github.com/hanzoai/commerce/models/mixin"
 	"github.com/hanzoai/commerce/util/json"
+	"github.com/hanzoai/orm"
 
 	. "github.com/hanzoai/commerce/types"
 )
 
+func init() { orm.Register[PriceList]("pricelist") }
+
 type PriceList struct {
-	mixin.Model
+	mixin.EntityBridge[PriceList]
 
 	Title       string     `json:"title"`
 	Description string     `json:"description"`
-	Status      string     `json:"status"`
+	Status      string     `json:"status" orm:"default:draft"`
 	Type        string     `json:"type"`
 	StartsAt    *time.Time `json:"startsAt,omitempty"`
 	EndsAt      *time.Time `json:"endsAt,omitempty"`
@@ -25,8 +28,6 @@ type PriceList struct {
 }
 
 func (p *PriceList) Load(ps []datastore.Property) (err error) {
-	p.Defaults()
-
 	if err = datastore.LoadStruct(p, ps); err != nil {
 		return err
 	}
@@ -42,4 +43,14 @@ func (p *PriceList) Save() ([]datastore.Property, error) {
 	p.Metadata_ = string(json.EncodeBytes(&p.Metadata))
 
 	return datastore.SaveStruct(p)
+}
+
+func New(db *datastore.Datastore) *PriceList {
+	t := new(PriceList)
+	t.Init(db)
+	return t
+}
+
+func Query(db *datastore.Datastore) datastore.Query {
+	return db.Query("pricelist")
 }
--- models/taxrate/taxrate.go
+++ models/taxrate/taxrate.go
@@ -4,12 +4,15 @@ import (
 	"github.com/hanzoai/commerce/datastore"
 	"github.com/hanzoai/commerce/models/mixin"
 	"github.com/hanzoai/commerce/util/json"
+	"github.com/hanzoai/orm"
 
 	. "github.com/hanzoai/commerce/types"
 )
 
+func init() { orm.Register[TaxRate]("taxrate") }
+
 type TaxRate struct {
-	mixin.Model
+	mixin.EntityBridge[TaxRate]
 
 	Rate         float64 `json:"rate"`
 	Code         string  `json:"code"`
@@ -19,13 +22,11 @@ type TaxRate struct {
 	TaxRegionId  string  `json:"taxRegionId"`
 
 	// Arbitrary key/value pairs associated with this tax rate
-	Metadata  Map    `json:"metadata,omitempty" datastore:"-"`
+	Metadata  Map    `json:"metadata,omitempty" datastore:"-" orm:"default:{}"`
 	Metadata_ string `json:"-" datastore:",noindex"`
 }
 
 func (t *TaxRate) Load(ps []datastore.Property) (err error) {
-	t.Defaults()
-
 	if err = datastore.LoadStruct(t, ps); err != nil {
 		return err
 	}
@@ -41,4 +42,14 @@ func (t *TaxRate) Save() ([]datastore.Property, error) {
 	t.Metadata_ = string(json.EncodeBytes(&t.Metadata))
 
 	return datastore.SaveStruct(t)
+}
+
+func New(db *datastore.Datastore) *TaxRate {
+	t := new(TaxRate)
+	t.Init(db)
+	return t
+}
+
+func Query(db *datastore.Datastore) datastore.Query {
+	return db.Query("taxrate")
 }
