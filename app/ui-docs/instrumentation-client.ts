27a5214e8d29de968309c19b9223bf62712c6c82
Zach Kelling <z@zeekay.io>
2026-03-13 17:12:28 -0700

    rebrand: purge remaining PostHog references

--- app/ui-docs/instrumentation-client.ts
+++ app/ui-docs/instrumentation-client.ts
@@ -1,11 +1,11 @@
-import posthog from "posthog-js"
+import posthog from "@hanzo/insights"
 
 posthog.init(
-  (process.env.NEXT_PUBLIC_INSIGHTS_KEY || process.env.NEXT_PUBLIC_POSTHOG_KEY)!,
+  (process.env.NEXT_PUBLIC_INSIGHTS_KEY || process.env.NEXT_PUBLIC_INSIGHTS_KEY)!,
   {
     api_host:
       process.env.NEXT_PUBLIC_INSIGHTS_HOST ||
-      process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      process.env.NEXT_PUBLIC_INSIGHTS_HOST,
     person_profiles: "always",
     defaults: "2025-05-24",
   }
