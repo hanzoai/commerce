2570379d2ebceb3b5ab82220dbc6751962128e81
Zach Kelling <z@zeekay.io>
2026-06-21 00:48:53 -0700

    chore: remove copied go-ethereum LGPL source from thirdparty/ethereum (clean-author denomination constants)

--- thirdparty/ethereum/denomination.go
+++ thirdparty/ethereum/denomination.go
@@ -1,34 +1,18 @@
-// Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package ethereum
 
+// Ether denomination multipliers, expressed in wei.
+//
+// Wei is the base, indivisible unit of ether. The names below are the
+// standard community labels for common powers-of-ten of wei; the values are
+// plain decimal unit-conversion facts.
+//
+//	1 Shannon (Gwei) = 1e9 wei
+//
+// Use these as scalar multipliers, e.g.
+//
+//	gasPrice := new(big.Int).Mul(big.NewInt(gwei), big.NewInt(Shannon))
 const (
-	// These are the multipliers for ether denominations.
-	// Example: To get the wei value of an amount in 'douglas', use
-	//
-	//    new(big.Int).Mul(value, big.NewInt(params.Douglas))
-	//
-	Wei      = 1
-	Ada      = 1e3
-	Babbage  = 1e6
-	Shannon  = 1e9
-	Szabo    = 1e12
-	Finney   = 1e15
-	Ether    = 1e18
-	Einstein = 1e21
-	Douglas  = 1e42
+	Wei     = 1
+	Shannon = 1e9 // a.k.a. Gwei, the customary unit for gas prices
+	Ether   = 1e18
 )
