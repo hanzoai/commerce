077ac9589f439e5400baca3c1582f1fd6c389b5f
Zach Kelling <z@zeekay.io>
2026-07-22 03:02:04 -0700

    release: v1.49.10

--- commerce.go
+++ commerce.go
@@ -69,7 +69,7 @@ import (
 // the immutable image tag (-X github.com/hanzoai/commerce.Version=<tag>) so
 // the running binary's /healthz version always equals its deployed tag.
 var (
-	Version   = "1.49.7"
+	Version   = "1.49.10"
 	GitCommit = "dev"
 	BuildTime = "unknown"
 )
